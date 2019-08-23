105388e525b7264febeccc5dbc5f2a707c889ea6
Simon kwok <seaman.kwok@gmail.com>
2019-08-23 15:33:46 +0800

    #12 format code.

--- wecube-core/src/main/java/com/webank/wecube/core/utils/StringUtils.java
+++ wecube-core/src/main/java/com/webank/wecube/core/utils/StringUtils.java
@@ -2,8 +2,8 @@ package com.webank.wecube.core.utils;
 
 public class StringUtils {
 
-	public static boolean containsOnlyAlphanumericOrHyphen(String urlPathSegment) {
-		if (urlPathSegment == null) return false;
-		return urlPathSegment.matches("[a-zA-Z0-9\\-]+");
-	}
+    public static boolean containsOnlyAlphanumericOrHyphen(String urlPathSegment) {
+        if (urlPathSegment == null) return false;
+        return urlPathSegment.matches("[a-zA-Z0-9\\-]+");
+    }
 }
