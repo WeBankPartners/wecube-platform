6a373248e7217f267db0862e1922f8087900c9c1
Simon kwok <seaman.kwok@gmail.com>
2019-09-03 14:52:36 +0800

    #131 build system variables function

---
+++ wecube-core/src/main/java/com/webank/wecube/core/service/SystemVariableService.java
@@ -0,0 +1,77 @@
+package com.webank.wecube.core.service;
+
+import static com.webank.wecube.core.domain.SystemVariable.SCOPE_TYPE_GLOBAL;
+import static com.webank.wecube.core.domain.SystemVariable.SCOPE_TYPE_PLUGIN_PACKAGE;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.collect.Lists;
+import com.webank.wecube.core.commons.WecubeCoreException;
+import com.webank.wecube.core.domain.SystemVariable;
+import com.webank.wecube.core.jpa.SystemVariableRepository;
+
+@Service
+@Transactional
+public class SystemVariableService {
+
+    @Autowired
+    SystemVariableRepository systemVariableRepository;
+    
+    public List<String> getSupportedScopeTypes() {
+        return Lists.newArrayList(SCOPE_TYPE_GLOBAL, SCOPE_TYPE_PLUGIN_PACKAGE);
+    }
+    
+    public List<SystemVariable> getAllSystemVariables(String status) {
+        if (status == null) {
+            return Lists.newArrayList(systemVariableRepository.findAll());
+        } else {
+            return systemVariableRepository.findAllByStatus(status);
+        }
+    }
+    
+    public List<SystemVariable> getGlobalSystemVariables(String status) {
+        if (status == null) {
+            return systemVariableRepository.findAllByScopeType(SCOPE_TYPE_GLOBAL);
+        } else {
+            return systemVariableRepository.findAllByScopeTypeAndStatus(SCOPE_TYPE_GLOBAL, status);
+        }
+    }
+    
+    public List<SystemVariable> getSystemVariables(String scopeType, String scopeValue, String status) {
+        if (status == null) {
+            return systemVariableRepository.findAllByScopeTypeAndScopeValue(scopeType, scopeValue);
+        } else {
+            return systemVariableRepository.findAllByScopeTypeAndScopeValueAndStatus(scopeType, scopeValue, status);
+        }
+    }
+    
+    public SystemVariable getSystemVariableById(int varId) {
+        Optional<SystemVariable> systemVariable = systemVariableRepository.findById(varId);
+        if (systemVariable.isPresent()) {
+            return systemVariable.get();
+        } else {
+            throw new WecubeCoreException("System Variable not found for id: " + varId);
+        }
+    }
+
+    public List<SystemVariable> saveSystemVariables(List<SystemVariable> variables) {
+        if (isNotEmpty(variables)) {
+            return variables.stream().map(p -> systemVariableRepository.save(p)).collect(Collectors.toList());
+        }
+        return variables;
+    }
+
+    public void deleteSystemVariables(List<Integer> variableIds) {
+        if (isNotEmpty(variableIds)) {
+            variableIds.forEach(systemVariableRepository::deleteById);
+        }
+    }
+
+}
