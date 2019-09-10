ccfb4932a7722b69a7beb660e063f21636ca4d1c
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-10 15:19:15 +0800

    #132 Update plugin moddel attribute for DTO and DAO separation.

--- wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelAttribute.java
+++ wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelAttribute.java
@@ -16,9 +16,15 @@ public class PluginModelAttribute {
 
     @Column(name = "entity_id")
     @NotBlank
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "plugin_model_entity_id", insertable = false, updatable = false)
     private PluginModelEntity pluginModelEntity;
+
+    @Column(name = "entity_id")
+    @NotBlank
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "plugin_model_entity_id", insertable = false, updatable = false)
+    private Integer pluginModelEntityId;
 
     @Column(name = "description")
     private String description;
@@ -32,7 +38,12 @@ public class PluginModelAttribute {
     private String inputType;
 
     @Column(name = "reference_id")
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "plugin_model_attr_id", insertable = false, updatable = false)
     private PluginModelAttribute pluginModelAttribute;
+
+    @Column(name = "reference_id")
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "plugin_model_attr_id", insertable = false, updatable = false)
+    private Integer pluginModelAttrId;
 }
