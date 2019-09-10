34b0ab24a9f53132910b687799f7807ea429d25c
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-10 10:04:57 +0800

    #132 Plugin model attribute design and implementation

---
+++ wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelAttributeRepository.java
@@ -0,0 +1,7 @@
+package com.webank.wecube.core.jpa;
+
+import com.webank.wecube.core.domain.plugin.PluginModelAttribute;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PluginModelAttributeRepository extends CrudRepository<PluginModelAttribute, Integer> {
+}
