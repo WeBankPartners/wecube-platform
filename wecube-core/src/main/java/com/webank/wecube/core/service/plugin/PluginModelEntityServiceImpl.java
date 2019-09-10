5dd1e733bcb0e8eaa38e68bb700c64bb02ab3f73
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-10 10:05:16 +0800

    #132 Plugin model entity design and implementation

---
+++ wecube-core/src/main/java/com/webank/wecube/core/service/plugin/PluginModelEntityServiceImpl.java
@@ -0,0 +1,89 @@
+package com.webank.wecube.core.service.plugin;
+
+import com.webank.wecube.core.commons.ApplicationProperties.PluginProperties;
+import com.webank.wecube.core.domain.plugin.PluginModelEntity;
+import com.webank.wecube.core.dto.PluginModelEntityDto;
+import com.webank.wecube.core.dto.PluginModelEntityDto;
+import com.webank.wecube.core.jpa.PluginModelEntityRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation of plugin model entity service
+ */
+@Service
+@Slf4j
+@Transactional
+public class PluginModelEntityServiceImpl implements PluginModelEntityService {
+
+    @Autowired
+    PluginModelEntityRepository pluginModelEntityRepository;
+
+    @Autowired
+    private PluginProperties pluginProperties;
+
+    /**
+     * Register plugin model entity to wecube
+     *
+     * @param pluginModelEntityDtos plugin model entity dtos as input
+     * @return registered model entities dtos
+     */
+    @Override
+    public List<PluginModelEntityDto> registerModelEntity(List<PluginModelEntityDto> pluginModelEntityDtos) {
+        Iterable<PluginModelEntity> savedPluginModelEntities = pluginModelEntityRepository.saveAll(convertPluginModelEntityDtoToDomain(pluginModelEntityDtos));
+        return convertPluginModelEntityDomainToDto(savedPluginModelEntities);
+    }
+
+    /**
+     * Update plugin model entity to wecube
+     *
+     * @param pluginModelEntityDtos plugin model entity dtos as input
+     * @return updated model entity dtos
+     */
+    @Override
+    public List<PluginModelEntityDto> updateModelEntity(List<PluginModelEntityDto> pluginModelEntityDtos) {
+        Iterable<PluginModelEntity> savedPluginModelEntities = pluginModelEntityRepository.saveAll(convertPluginModelEntityDtoToDomain(pluginModelEntityDtos));
+        return convertPluginModelEntityDomainToDto(savedPluginModelEntities);
+    }
+
+    /**
+     * Convert the plugin model entities from domains to dtos
+     *
+     * @param savedPluginModelEntity an Iterable pluginModelEntity
+     * @return converted dtos
+     */
+    private List<PluginModelEntityDto> convertPluginModelEntityDomainToDto(Iterable<PluginModelEntity> savedPluginModelEntity) {
+        List<PluginModelEntityDto> pluginModelEntityDtos = new ArrayList<>();
+        savedPluginModelEntity.forEach(domain -> {
+            pluginModelEntityDtos.add(PluginModelEntityDto.fromDomain(domain));
+        });
+        return pluginModelEntityDtos;
+    }
+
+    /**
+     * Convert the plugin model entities from dtos to domains
+     *
+     * @param pluginModelEntityDtos the list of pluginModelEntityDto
+     * @return the list of converted domains
+     */
+    private List<PluginModelEntity> convertPluginModelEntityDtoToDomain(List<PluginModelEntityDto> pluginModelEntityDtos) {
+        List<PluginModelEntity> pluginModelEntities = new ArrayList<>();
+        pluginModelEntityDtos.forEach(dto -> {
+            PluginModelEntity existedPluginModelEntity = null;
+            if (dto.getId() != null) {
+                Optional<PluginModelEntity> tmpPluginModelEntity = pluginModelEntityRepository.findById(dto.getId());
+                if (tmpPluginModelEntity.isPresent()) {
+                    existedPluginModelEntity = tmpPluginModelEntity.get();
+                }
+            }
+            pluginModelEntities.add(PluginModelEntityDto.toDomain(dto, existedPluginModelEntity));
+        });
+        return pluginModelEntities;
+    }
+}
