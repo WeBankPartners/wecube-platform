8cdc0f824b0610f8fe91212310765e95c6c91322
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-11 19:52:55 +0800

    #132 Remove validator, bind plugin model attribute list

--- wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelAttribute.java
+++ wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelAttribute.java
@@ -4,24 +4,22 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
 @Table(name = "plugin_model_attr")
 public class PluginModelAttribute {
     @Id
-    @NotBlank
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "entity_id")
-    @NotBlank
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "plugin_model_entity_id", insertable = false, updatable = false)
     private PluginModelEntity pluginModelEntity;
 
     @Column(name = "entity_id")
-    @NotBlank
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "plugin_model_entity_id", insertable = false, updatable = false)
     private Integer pluginModelEntityId;
@@ -30,11 +28,9 @@ public class PluginModelAttribute {
     private String description;
 
     @Column(name = "name")
-    @NotBlank
     private String name;
 
     @Column(name = "input_type", length = 32)
-    @NotBlank
     private String inputType;
 
     @Column(name = "reference_id")
