3080e7dda80366fbaee66c5f4959b5b4aac47762
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-24 11:31:15 +0800

    #132 update PluginModelService design and implementations

--- wecube-core/src/main/java/com/webank/wecube/core/service/plugin/PluginModelServiceImpl.java
+++ wecube-core/src/main/java/com/webank/wecube/core/service/plugin/PluginModelServiceImpl.java
@@ -1,67 +1,126 @@
 package com.webank.wecube.core.service.plugin;
 
+import com.webank.wecube.core.commons.WecubeCoreException;
 import com.webank.wecube.core.domain.plugin.PluginModelAttribute;
 import com.webank.wecube.core.domain.plugin.PluginModelEntity;
 import com.webank.wecube.core.dto.PluginModelAttributeDto;
 import com.webank.wecube.core.dto.PluginModelEntityDto;
 import com.webank.wecube.core.jpa.PluginModelAttributeRepository;
 import com.webank.wecube.core.jpa.PluginModelEntityRepository;
+import com.webank.wecube.core.jpa.PluginPackageRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @Slf4j
+@Transactional
 public class PluginModelServiceImpl implements PluginModelService {
 
     @Autowired
     PluginModelEntityRepository pluginModelEntityRepository;
     @Autowired
     PluginModelAttributeRepository pluginModelAttributeRepository;
+    @Autowired
+    PluginPackageRepository pluginPackageRepository;
 
     /**
      * Plugin model registration
      *
-     * @param inputEntityDtos list of plugin model entity dtos
+     * @param inputEntityDtoList list of plugin model entity dtos with embedded attribute dto list
      * @return an list of entity dtos which contain both entities and attributes
      */
     @Override
-    public List<PluginModelEntityDto> register(List<PluginModelEntityDto> inputEntityDtos) {
-        List<List<PluginModelAttributeDto>> attrDtoList = extractAttributes(inputEntityDtos);
+    public List<PluginModelEntityDto> register(List<PluginModelEntityDto> inputEntityDtoList) {
+        // map "{entity}.{attribute}" the reference, another "{entity}.{attribute}"
+        Map<String, String> referenceNameMap = new HashMap<>();
+        // map "{entity}.{attribute}" to attribute domain object
+        Map<String, PluginModelAttribute> nameToAttributeMap = new HashMap<>();
 
-        List<Integer> registeredEntityIdList = new ArrayList<>();
-        for (PluginModelEntityDto tmpEntityDto : registerEntity(inputEntityDtos)) {
-            registeredEntityIdList.add(tmpEntityDto.getId());
+        List<PluginModelEntity> pluginModelEntityList = new ArrayList<>();
+
+        // initialize the entity domain object and its nested attribute domain object
+        // also this for-loop build up the maps
+        for (PluginModelEntityDto entityDto : inputEntityDtoList) {
+
+            // build referenceName map
+            for (PluginModelAttributeDto attrDto : entityDto.getAttributeDtoList()) {
+                if (attrDto.getPackageName() != null && attrDto.getEntityName() != null && attrDto.getName() != null) {
+                    if (pluginPackageRepository.findLatestVersionByName(entityDto.getPackageName(), entityDto.getPackageVersion()).isPresent()) {
+                        String packageName = pluginPackageRepository.findLatestVersionByName(entityDto.getPackageName(), entityDto.getPackageVersion()).get().getName();
+                        referenceNameMap.put(
+                                packageName + "." + entityDto.getName() + "." + attrDto.getName(),
+                                attrDto.getPackageName() + "." + attrDto.getEntityName() + "." + attrDto.getName());
+                    } else {
+                        throw new WecubeCoreException(String.format("Cannot find entity by package name: [%s] with version: [%s]", entityDto.getPackageName(), entityDto.getPackageVersion()));
+                    }
+                }
+            }
+
+            // dto -> domain
+            PluginModelEntity tmp = PluginModelEntityDto.toDomain(entityDto);
+
+            // build nameToAttributeMap
+            for (PluginModelAttribute attr : tmp.getPluginModelAttributeList()) {
+                nameToAttributeMap.put(
+                        tmp.getPackageName() + tmp.getName() + "." + attr.getName(),
+                        attr);
+                attr.setPluginModelEntity(tmp);
+            }
+
+            pluginModelEntityList.add(tmp);
         }
-        List<PluginModelAttributeDto> flatAttributeList = setAttributeWithEntityId(attrDtoList, registeredEntityIdList);
-        List<PluginModelAttributeDto> pluginModelAttributeDtos = registerAttribute(flatAttributeList);
 
-        return overview();
+        // find the reference in nameToAttributeMap from referenceNameMap
+        for (PluginModelEntity pluginModelEntity : pluginModelEntityList) {
+            for (PluginModelAttribute pluginModelAttribute : pluginModelEntity.getPluginModelAttributeList()) {
+                // this would set the attribute reference to the domain object pre-recorded in nameToAttributeMap
+                pluginModelAttribute.setPluginModelAttribute(
+                        nameToAttributeMap.get(
+                                referenceNameMap.get(
+                                        pluginModelEntity.getPackageName() + "." + pluginModelEntity.getName() + "." + pluginModelAttribute.getName()
+                                )
+                        )
+                );
+            }
+        }
+        return convertEntityDomainToDto(pluginModelEntityRepository.saveAll(pluginModelEntityList));
+
+
     }
 
     /**
-     * Plugin model update
-     *
-     * @param inputEntityDtos list of plugin model entity dtos
-     * @return an list of entity dtos which contain both entities and attributes
+     * @param inputEntityDtoList input dto list that need to be updated
+     * @return updated entity dto list
      */
     @Override
-    public List<PluginModelEntityDto> update(List<PluginModelEntityDto> inputEntityDtos) {
-        List<List<PluginModelAttributeDto>> attrDtoList = extractAttributes(inputEntityDtos);
+    public List<PluginModelEntityDto> update(List<PluginModelEntityDto> inputEntityDtoList) {
+        List<PluginModelEntity> pluginModelEntityList = new ArrayList<>();
+        for (PluginModelEntityDto tmpEntityDto :
+                inputEntityDtoList) {
+            pluginModelEntityRepository.findAllByPackageNameAndName(
+                    tmpEntityDto.getPackageName(),
+                    tmpEntityDto.getName()).ifPresent(foundEntity -> {
+                tmpEntityDto.setId(foundEntity.getId());
+            });
 
-        List<Integer> registeredEntityIdList = new ArrayList<>();
-        for (PluginModelEntityDto tmpEntityDto : updateEntity(inputEntityDtos)) {
-            registeredEntityIdList.add(tmpEntityDto.getId());
+            for (PluginModelAttributeDto tmpAttributeDto :
+                    tmpEntityDto.getAttributeDtoList()) {
+                pluginModelAttributeRepository.findAllByPackageNameAndEntityNameAndName(
+                        tmpAttributeDto.getPackageName(),
+                        tmpAttributeDto.getEntityName(),
+                        tmpAttributeDto.getName()).ifPresent(foundAttribute -> {
+                    tmpAttributeDto.setId(foundAttribute.getId());
+                });
+            }
+            pluginModelEntityList.add(PluginModelEntityDto.toDomain(tmpEntityDto));
         }
-        List<PluginModelAttributeDto> flatAttributeList = setAttributeWithEntityId(attrDtoList, registeredEntityIdList);
-        List<PluginModelAttributeDto> pluginModelAttributeDtos = updateAttribute(flatAttributeList);
-
-        return overview();
+        return convertEntityDomainToDto(pluginModelEntityRepository.saveAll(pluginModelEntityList));
     }
+
 
     /**
      * Plugin model overview
@@ -74,55 +133,42 @@ public class PluginModelServiceImpl implements PluginModelService {
     }
 
     /**
-     * @param inputEntityDtos extract attribute out from the input entity dto list
-     * @return an nested list of attribute dto list
+     * @param packageName the name of package
+     * @param version     set of versions
+     * @return list of entity dto
      */
-    private List<List<PluginModelAttributeDto>> extractAttributes(List<PluginModelEntityDto> inputEntityDtos) {
-        List<List<PluginModelAttributeDto>> dtoList = new ArrayList<>();
-        inputEntityDtos.forEach(tmpEntityDto -> dtoList.add(tmpEntityDto.getAttributeDtoList()));
-        return dtoList;
-    }
-
-    /**
-     * @param attrDtoList            an nested plugin model attribute dto list
-     * @param registeredEntityIdList an entity id list with all registered id in one transaction
-     * @return a flat list of all attribute dto list with different entity registered id
-     */
-    private List<PluginModelAttributeDto> setAttributeWithEntityId(List<List<PluginModelAttributeDto>> attrDtoList, List<Integer> registeredEntityIdList) {
-        List<PluginModelAttributeDto> pluginModelAttributeDtos = new ArrayList<>();
-        for (int i = 0; i < registeredEntityIdList.size(); i++) {
-            Integer registeredId = registeredEntityIdList.get(i);
-            for (PluginModelAttributeDto tmp : attrDtoList.get(i)) {
-                tmp.setPluginModelEntityId(registeredId);
-                pluginModelAttributeDtos.add(tmp);
-            }
+    @Override
+    public List<PluginModelEntityDto> overview(String packageName, String... version) {
+        List<PluginModelEntity> pluginModelEntityList = new ArrayList<>();
+        if (pluginPackageRepository.findLatestVersionByName(packageName, version).isPresent()) {
+            Integer packageId = pluginPackageRepository.findLatestVersionByName(packageName, version).get().getId();
+            pluginModelEntityList.addAll(pluginModelEntityRepository.findAllByPackageId(packageId));
+        } else {
+            throw new WecubeCoreException(String.format("Cannot find entity by package name: [%s] with version: [%s]", packageName, Arrays.toString(version)));
         }
-        return pluginModelAttributeDtos;
-    }
-
-
-    // Entity Operation
-
-    /**
-     * Register plugin model entity to wecube
-     *
-     * @param pluginModelEntityDtos plugin model entity dtos as input
-     * @return registered model entities dtos
-     */
-    private List<PluginModelEntityDto> registerEntity(List<PluginModelEntityDto> pluginModelEntityDtos) {
-        Iterable<PluginModelEntity> savedPluginModelEntities = pluginModelEntityRepository.saveAll(convertEntityDtoToDomain(pluginModelEntityDtos));
-        return convertEntityDomainToDto(savedPluginModelEntities);
+        return convertEntityDomainToDto(pluginModelEntityList);
     }
 
     /**
-     * Update plugin model entity to wecube
-     *
-     * @param pluginModelEntityDtos plugin model entity dtos as input
-     * @return updated model entity dtos
+     * @param packageName the name of package
+     * @param entityNames the name of entity
      */
-    private List<PluginModelEntityDto> updateEntity(List<PluginModelEntityDto> pluginModelEntityDtos) {
-        Iterable<PluginModelEntity> savedPluginModelEntities = pluginModelEntityRepository.saveAll(convertEntityDtoToDomain(pluginModelEntityDtos));
-        return convertEntityDomainToDto(savedPluginModelEntities);
+    @Override
+    public void deleteEntity(String packageName, String... entityNames) {
+        if (pluginPackageRepository.findLatestVersionByName(packageName).isPresent()) {
+            Integer packageId = pluginPackageRepository.findLatestVersionByName(packageName).get().getId();
+            if (entityNames != null) {
+                for (String entityName : entityNames) {
+                    // delete specific entity under the package
+                    pluginModelEntityRepository.delete(packageId, entityName);
+                }
+            } else {
+                // delete all entities under same package
+                pluginModelEntityRepository.delete(packageId);
+            }
+        } else {
+            throw new WecubeCoreException(String.format("Cannot find entity by package name: [%s]", packageName));
+        }
     }
 
 
@@ -136,85 +182,6 @@ public class PluginModelServiceImpl implements PluginModelService {
         List<PluginModelEntityDto> pluginModelEntityDtos = new ArrayList<>();
         savedPluginModelEntity.forEach(domain -> pluginModelEntityDtos.add(PluginModelEntityDto.fromDomain(domain)));
         return pluginModelEntityDtos;
-    }
-
-    /**
-     * Convert the plugin model entities from dtos to domains
-     *
-     * @param pluginModelEntityDtos the list of pluginModelEntityDto
-     * @return the list of converted domains
-     */
-    private List<PluginModelEntity> convertEntityDtoToDomain(List<PluginModelEntityDto> pluginModelEntityDtos) {
-        List<PluginModelEntity> pluginModelEntities = new ArrayList<>();
-        pluginModelEntityDtos.forEach(dto -> {
-            PluginModelEntity existedPluginModelEntity = null;
-            if (dto.getId() != null) {
-                Optional<PluginModelEntity> tmpPluginModelEntity = pluginModelEntityRepository.findById(dto.getId());
-                if (tmpPluginModelEntity.isPresent()) {
-                    existedPluginModelEntity = tmpPluginModelEntity.get();
-                }
-            }
-            pluginModelEntities.add(PluginModelEntityDto.toDomain(dto, existedPluginModelEntity));
-        });
-        return pluginModelEntities;
-    }
-
-    // Attribute Operation
-
-    /**
-     * Register plugin model attribute to the wecube
-     *
-     * @param pluginModelAttributeDtos plugin model attribute dtos input
-     * @return saved plugin model attribute dto
-     */
-    private List<PluginModelAttributeDto> registerAttribute(List<PluginModelAttributeDto> pluginModelAttributeDtos) {
-        Iterable<PluginModelAttribute> savedPluginModelAttribute = pluginModelAttributeRepository.saveAll(convertAttributeDtoToDomain(pluginModelAttributeDtos));
-        return convertAttributeDomainToDto(savedPluginModelAttribute);
-    }
-
-
-    /**
-     * Update plugin model attribute to the wecube
-     *
-     * @param pluginModelAttributeDtos plugin model attribute dtos input
-     * @return updated plugin model attribute
-     */
-    private List<PluginModelAttributeDto> updateAttribute(List<PluginModelAttributeDto> pluginModelAttributeDtos) {
-        Iterable<PluginModelAttribute> updatedPluginModelAttribute = pluginModelAttributeRepository.saveAll(convertAttributeDtoToDomain(pluginModelAttributeDtos));
-        return convertAttributeDomainToDto(updatedPluginModelAttribute);
-    }
-
-    /**
-     * Convert the plugin model attributes from domains to dtos
-     *
-     * @param savedPluginModelAttribute an Iterable pluginModelAttribute
-     * @return converted dtos
-     */
-    private List<PluginModelAttributeDto> convertAttributeDomainToDto(Iterable<PluginModelAttribute> savedPluginModelAttribute) {
-        List<PluginModelAttributeDto> pluginModelAttributeDtos = new ArrayList<>();
-        savedPluginModelAttribute.forEach(domain -> pluginModelAttributeDtos.add(PluginModelAttributeDto.fromDomain(domain)));
-        return pluginModelAttributeDtos;
-    }
-
-    /**
-     * Convert the plugin model attributes from dtos to domains
-     *
-     * @param pluginModelAttributeDtos the list of pluginModelAttributeDto
-     * @return the list of converted domains
-     */
-    private List<PluginModelAttribute> convertAttributeDtoToDomain(List<PluginModelAttributeDto> pluginModelAttributeDtos) {
-        List<PluginModelAttribute> pluginModelAttributes = new ArrayList<>();
-        pluginModelAttributeDtos.forEach(dto -> {
-            PluginModelAttribute existedPluginModelAttribute = null;
-            if (dto.getId() != null) {
-                Optional<PluginModelAttribute> tmpPluginModelAttribute = pluginModelAttributeRepository.findById(dto.getId());
-                if (tmpPluginModelAttribute.isPresent()) {
-                    existedPluginModelAttribute = tmpPluginModelAttribute.get();
-                }
-            }
-            pluginModelAttributes.add(PluginModelAttributeDto.toDomain(dto, existedPluginModelAttribute));
-        });
-        return pluginModelAttributes;
     }
 
 
