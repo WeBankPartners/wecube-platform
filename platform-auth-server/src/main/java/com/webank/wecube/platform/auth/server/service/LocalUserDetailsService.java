43c1e221bb8e00613a809abe6b54588661eec05a
gavin2lee <gavin2lee@163.com>
2019-09-24 22:43:08 +0800

    #320 add entities

---
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/service/LocalUserDetailsService.java
@@ -0,0 +1,7 @@
+package com.webank.wecube.platform.auth.server.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface LocalUserDetailsService extends UserDetailsService {
+
+}
