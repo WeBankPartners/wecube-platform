f81874c5f305368d303f716fbda673ee927c5855
gavin2lee <gavin2lee@163.com>
2019-09-24 22:43:08 +0800

    #320 add entities

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/TokenController.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/TokenController.java
@@ -1,5 +1,17 @@
 package com.webank.wecube.platform.auth.server.controller;
 
+import java.security.Principal;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +22,28 @@ import com.webank.wecube.platform.auth.server.dto.JwtTokenDto;
 @RestController
 @RequestMapping(ApplicationConstants.ApiInfo.PREFIX_DEFAULT)
 public class TokenController {
+    
+    private static final Logger log = LoggerFactory.getLogger(TokenController.class);
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @PostConstruct
+    public void afterPropertiesSet(){
+        Assert.notNull(userDetailsService, "user details service should provide");
+    }
 
     @GetMapping("/token/refresh")
-    public JwtTokenDto refreshToken(){
+    public JwtTokenDto refreshToken(HttpServletRequest request, HttpServletResponse response, Principal principal){
+        log.info("refresh token ===");
         JwtTokenDto token = new JwtTokenDto();
         token.setName("jwt token");
         token.setPassword("111111");
+        
+        
+        UserDetails userDetails = userDetailsService.loadUserByUsername(principal.getName());
+        
+        log.info("UserDetails:{}", userDetails);
         
         return token;
     }
