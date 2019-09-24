5c6fe84952225ac2971933e6757cf820db66341e
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-24 11:30:57 +0800

    #132 update PluginModelAttribute design and implementations

--- wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelAttributeRepository.java
+++ wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelAttributeRepository.java
@@ -3,5 +3,9 @@ package com.webank.wecube.core.jpa;
 import com.webank.wecube.core.domain.plugin.PluginModelAttribute;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PluginModelAttributeRepository extends CrudRepository<PluginModelAttribute, Integer> {
+
+    Optional<PluginModelAttribute> findAllByPackageNameAndEntityNameAndName(String packageName, String entityName, String name);
 }
