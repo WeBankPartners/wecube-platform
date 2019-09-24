28c3870c92db8b04c6331fc7d9bf6d09aefcc728
HoweChen <yuhao.howe.chen@gmail.com>
2019-09-24 11:30:43 +0800

    #132 update PluginModelEntity design and implementations

--- wecube-core/src/main/java/com/webank/wecube/core/dto/PluginModelEntityDto.java
+++ wecube-core/src/main/java/com/webank/wecube/core/dto/PluginModelEntityDto.java
@@ -1,10 +1,12 @@
 package com.webank.wecube.core.dto;
 
+import com.webank.wecube.core.domain.plugin.PluginModelAttribute;
 import com.webank.wecube.core.domain.plugin.PluginModelEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,44 +17,66 @@ public class PluginModelEntityDto {
     private Integer packageId;
     private String description;
     private String name;
+    private String state = "draft";
     // plugin model attribute list
+    private String packageName;
+    private String packageVersion;
     private List<PluginModelAttributeDto> attributeDtoList;
 
+    /**
+     * @param pluginModelEntity input entity domain object
+     * @return entity dto exposed to the server
+     */
     public static PluginModelEntityDto fromDomain(PluginModelEntity pluginModelEntity) {
         PluginModelEntityDto pluginModelEntityDto = new PluginModelEntityDto();
-        pluginModelEntityDto.setId(pluginModelEntity.getId());
-        pluginModelEntityDto.setPackageId(pluginModelEntity.getPluginPackage().getId());
-        pluginModelEntityDto.setDescription(pluginModelEntity.getDescription());
         pluginModelEntityDto.setName(pluginModelEntity.getName());
+        pluginModelEntityDto.setPackageName(pluginModelEntity.getPackageName());
+        pluginModelEntityDto.setDescription(pluginModelEntity.getDescription());
+        pluginModelEntityDto.setState(pluginModelEntity.getState());
         if (pluginModelEntity.getPluginModelAttributeList() != null) {
             pluginModelEntity.getPluginModelAttributeList()
                     .forEach(pluginModelAttribute -> pluginModelEntityDto.attributeDtoList
                             .add(PluginModelAttributeDto.fromDomain(pluginModelAttribute)));
         }
+//        pluginModelEntityDto.setPackageName(pluginModelEntity.getPackageId());
+
         return pluginModelEntityDto;
     }
 
-    public static PluginModelEntity toDomain(PluginModelEntityDto pluginModelEntityDto,
-                                             PluginModelEntity existedPluginModelEnitty) {
-        PluginModelEntity pluginModelEntity = existedPluginModelEnitty;
-        if (pluginModelEntity == null) {
-            pluginModelEntity = new PluginModelEntity();
-        }
+    /**
+     * @param pluginModelEntityDto input entity dto
+     * @return transformed entity domain object
+     */
+    public static PluginModelEntity toDomain(PluginModelEntityDto pluginModelEntityDto) {
+        PluginModelEntity pluginModelEntity = new PluginModelEntity();
 
         if (pluginModelEntityDto.getId() != null) {
             pluginModelEntity.setId(pluginModelEntityDto.getId());
         }
-
         if (pluginModelEntityDto.getPackageId() != null) {
             pluginModelEntity.setPackageId(pluginModelEntityDto.getPackageId());
         }
-
+        if (pluginModelEntityDto.getName() != null) {
+            pluginModelEntity.setName(pluginModelEntityDto.getName());
+        }
+        if (pluginModelEntityDto.getPackageName() != null) {
+            pluginModelEntity.setPackageName(pluginModelEntityDto.getPackageName());
+        }
+        if (pluginModelEntityDto.getPackageVersion() != null) {
+            pluginModelEntity.setPackageVersion(pluginModelEntityDto.getPackageVersion());
+        }
         if (pluginModelEntityDto.getDescription() != null) {
             pluginModelEntity.setDescription(pluginModelEntityDto.getDescription());
         }
-
-        if (pluginModelEntityDto.getName() != null) {
-            pluginModelEntity.setName(pluginModelEntityDto.getName());
+        if (pluginModelEntityDto.getState() != null) {
+            pluginModelEntity.setState(pluginModelEntityDto.getState().toUpperCase());
+        }
+        if (pluginModelEntityDto.getAttributeDtoList() != null) {
+            List<PluginModelAttribute> pluginModelAttributeList = new ArrayList<>();
+            for (PluginModelAttributeDto pluginModelAttributeDto : pluginModelEntityDto.getAttributeDtoList()) {
+                pluginModelAttributeList.add(PluginModelAttributeDto.toDomain(pluginModelAttributeDto, null));
+            }
+            pluginModelEntity.setPluginModelAttributeList(pluginModelAttributeList);
         }
         return pluginModelEntity;
     }
--- wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelEntityRepository.java
+++ wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelEntityRepository.java
@@ -1,11 +1,20 @@
 package com.webank.wecube.core.jpa;
 
 import com.webank.wecube.core.domain.plugin.PluginModelEntity;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PluginModelEntityRepository extends CrudRepository<PluginModelEntity, Integer> {
 
+    List<PluginModelEntity> findAllByPackageId(Integer packageId);
+
+    Optional<PluginModelEntity> findAllByPackageNameAndName(String packageName, String entityName);
+
+    Optional<PluginModelEntity> findAllByPackageName(String packageName);
+
+    void delete(Integer packageId, String entityName);
+
+    void delete(Integer packageId);
 }
