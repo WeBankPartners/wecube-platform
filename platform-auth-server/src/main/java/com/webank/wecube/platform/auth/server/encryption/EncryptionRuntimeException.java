fe89cfcf29567aba95eaf92fd3207ce11020e273
gavin2lee <gavin2lee@163.com>
2019-09-25 23:43:01 +0800

    #320 add encryption utils

---
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/encryption/EncryptionRuntimeException.java
@@ -0,0 +1,32 @@
+package com.webank.wecube.platform.auth.server.encryption;
+
+public class EncryptionRuntimeException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 115523361878991641L;
+
+    public EncryptionRuntimeException() {
+        super();
+    }
+
+    public EncryptionRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public EncryptionRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EncryptionRuntimeException(String message) {
+        super(message);
+    }
+
+    public EncryptionRuntimeException(Throwable cause) {
+        super(cause);
+    }
+    
+    
+
+}
