10cda242343d3bf4be7277e4eb59a4710afe0af8
gavin2lee <gavin2lee@163.com>
2019-09-27 00:56:20 +0800

    #320 update auth controller

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/AuthenticationController.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/AuthenticationController.java
@@ -5,25 +5,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webank.wecube.platform.auth.server.common.ApplicationConstants;
 import com.webank.wecube.platform.auth.server.dto.CommonResponseDto;
-import com.webank.wecube.platform.auth.server.dto.CredentialDto;
 
 @RestController
 @RequestMapping(ApplicationConstants.ApiInfo.PREFIX_DEFAULT)
 public class AuthenticationController {
     private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
 
-    @PostMapping("/logout")
-    public CommonResponseDto userCentralLogin(HttpServletRequest request, HttpServletResponse response,
-            @RequestBody CredentialDto credentialDto) {
+    @PutMapping("/logout")
+    public CommonResponseDto userCentralLogout(HttpServletRequest request, HttpServletResponse response) {
         
-        log.info("=== login ===");
+        log.info("=== logout ===");
         
         return CommonResponseDto.okay();
 
