000871538d2be15fdf24f307833e5ef8bef68ae1
gavin2lee <gavin2lee@163.com>
2019-09-26 11:38:30 +0800

    #320 add subsystem authentication

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/authentication/SubSystemAuthenticationProvider.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/authentication/SubSystemAuthenticationProvider.java
@@ -1,27 +1,69 @@
 package com.webank.wecube.platform.auth.server.authentication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import com.webank.wecube.platform.auth.server.common.ApplicationConstants;
+import com.webank.wecube.platform.auth.server.model.SysSubSystemInfo;
 import com.webank.wecube.platform.auth.server.service.SubSystemInfoDataService;
 
 @Component("subSystemAuthenticationProvider")
 public class SubSystemAuthenticationProvider implements AuthenticationProvider {
     private static final Logger log = LoggerFactory.getLogger(SubSystemAuthenticationProvider.class);
-    
+
     @Autowired
     private SubSystemInfoDataService subSystemDataService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if (!(authentication instanceof SubSystemAuthenticationToken)) {
+            log.error("only {} type supported", SubSystemAuthenticationToken.class.getSimpleName());
+            throw new IllegalArgumentException("such authentication type doesnt supported");
+        }
+
+        SubSystemAuthenticationToken subSystemAuthToken = (SubSystemAuthenticationToken) authentication;
+        // TODO must verify password
         
+        String systemCode = (String) subSystemAuthToken.getPrincipal();
         
-        return null;
+        SysSubSystemInfo subSystemInfo = retrieveSubSystemInfo(systemCode, subSystemAuthToken);
+
+        return createSuccessAuthentication(subSystemInfo, subSystemAuthToken);
+    }
+
+    protected Authentication createSuccessAuthentication(SysSubSystemInfo retrievedSubSystemInfo,
+            SubSystemAuthenticationToken authToken) {
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(ApplicationConstants.Authority.SUBSYSTEM));
+        SubSystemAuthenticationToken returnAuthToken = new SubSystemAuthenticationToken(authToken.getPrincipal(),
+                authToken.getCredentials(), authToken.getNonce(), grantedAuthorities);
+        
+        return returnAuthToken;
+
+    }
+
+    protected SysSubSystemInfo retrieveSubSystemInfo(String systemCode, SubSystemAuthenticationToken authToken) {
+        SysSubSystemInfo subSystemInfo = subSystemDataService.retrieveSysSubSystemInfoWithSystemCode(systemCode);
+
+        if (subSystemInfo == null) {
+            String errMsg = String.format("%s doesnt exist", systemCode);
+            log.error(errMsg);
+            throw new UsernameNotFoundException(errMsg);
+        }
+
+        return subSystemInfo;
     }
 
     @Override
