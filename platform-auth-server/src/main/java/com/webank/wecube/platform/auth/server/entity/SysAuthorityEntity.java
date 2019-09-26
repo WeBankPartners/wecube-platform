4863ff06660ccbbf56bcb9815a8a0017f7c51a6a
haixinhuang <huanghaixin@126.com>
2019-09-26 23:04:30 +0800

    #320 Add create/delete/retrieve API for 'user', 'role' and 'api'

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/entity/SysAuthorityEntity.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/entity/SysAuthorityEntity.java
@@ -17,85 +17,96 @@ import javax.persistence.Table;
 @Table(name = "AUTH_SYS_AUTHORIRY")
 public class SysAuthorityEntity extends AbstractTraceableEntity {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "ID")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
 
-    @Column(name = "CODE")
-    private String code;
+	@Column(name = "CODE")
+	private String code;
 
-    @Column(name = "DISPLAY_NAME")
-    private String displayName;
+	@Column(name = "DISPLAY_NAME")
+	private String displayName;
 
-    @Column(name = "SYSTEM_ID")
-    private Long systemId;
+	@Column(name = "SYSTEM_ID")
+	private Long systemId;
 
-    @Column(name = "SYSTEM_CODE")
-    private String systemCode;
+	@Column(name = "SYSTEM_CODE")
+	private String systemCode;
 
-    @Column(name = "SYSTEM_NAME")
-    private String systemName;
+	@Column(name = "SYSTEM_NAME")
+	private String systemName;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "AUTH_SYS_AUTHORITY_ROLE", joinColumns = {
-            @JoinColumn(name = "AUTHORITY_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
-    private List<SysRoleEntity> boundRoles = new ArrayList<SysRoleEntity>();
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "AUTH_SYS_AUTHORITY_ROLE", joinColumns = {
+			@JoinColumn(name = "AUTHORITY_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
+	private List<SysRoleEntity> boundRoles = new ArrayList<SysRoleEntity>();
 
-    public Long getId() {
-        return id;
-    }
+	public SysAuthorityEntity() {
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public SysAuthorityEntity(String code, String displayName, Long systemId, String systemCode, String systemName) {
+		this.setCode(code);
+		this.setDisplayName(displayName);
+		this.setSystemId(systemId);
+		this.setSystemCode(systemCode);
+		this.setSystemName(systemName);
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getDisplayName() {
-        return displayName;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public Long getSystemId() {
-        return systemId;
-    }
+	public String getDisplayName() {
+		return displayName;
+	}
 
-    public void setSystemId(Long systemId) {
-        this.systemId = systemId;
-    }
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
 
-    public String getSystemCode() {
-        return systemCode;
-    }
+	public Long getSystemId() {
+		return systemId;
+	}
 
-    public void setSystemCode(String systemCode) {
-        this.systemCode = systemCode;
-    }
+	public void setSystemId(Long systemId) {
+		this.systemId = systemId;
+	}
 
-    public String getSystemName() {
-        return systemName;
-    }
+	public String getSystemCode() {
+		return systemCode;
+	}
 
-    public void setSystemName(String systemName) {
-        this.systemName = systemName;
-    }
+	public void setSystemCode(String systemCode) {
+		this.systemCode = systemCode;
+	}
 
-    public List<SysRoleEntity> getBoundRoles() {
-        return boundRoles;
-    }
+	public String getSystemName() {
+		return systemName;
+	}
 
-    public void setBoundRoles(List<SysRoleEntity> boundRoles) {
-        this.boundRoles = boundRoles;
-    }
+	public void setSystemName(String systemName) {
+		this.systemName = systemName;
+	}
+
+	public List<SysRoleEntity> getBoundRoles() {
+		return boundRoles;
+	}
+
+	public void setBoundRoles(List<SysRoleEntity> boundRoles) {
+		this.boundRoles = boundRoles;
+	}
 
 }
