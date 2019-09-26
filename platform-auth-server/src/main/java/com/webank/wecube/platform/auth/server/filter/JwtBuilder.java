a2761721ce6f98dbf46a9c9458b9dbea68c5e89d
gavin2lee <gavin2lee@163.com>
2019-09-26 16:33:21 +0800

    #320 add refresh token impl

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/filter/JwtBuilder.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/filter/JwtBuilder.java
@@ -4,9 +4,13 @@ import org.springframework.security.core.Authentication;
 
 import com.webank.wecube.platform.auth.server.model.JwtToken;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+
 public interface JwtBuilder {
     
     JwtToken buildRefreshToken(Authentication authentication);
     JwtToken buildAccessToken(Authentication authentication);
 
+    Jws<Claims> parseJwt(String token);
 }
