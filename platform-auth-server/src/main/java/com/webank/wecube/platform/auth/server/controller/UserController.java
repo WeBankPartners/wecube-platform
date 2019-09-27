5b5f9834e22ae67418565029ce215446d5ce6ae3
haixinhuang <huanghaixin@126.com>
2019-09-27 17:21:02 +0800

    #320 Mark TODO items

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/UserController.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/controller/UserController.java
@@ -34,7 +34,8 @@ public class UserController {
 
 	@PostMapping("/users")
 	@ResponseBody
-//	@PreAuthorize(value = "hasRole('AUTH_ADMIN') or hasAuthority('SUB_SYSTEM')")
+	// TODO
+	// @PreAuthorize(value = "hasRole('AUTH_ADMIN') or hasAuthority('SUB_SYSTEM')")
 	public CommonResponseDto createUser(@RequestBody CreateUserDto createUserDto, HttpServletRequest request)
 			throws Exception {
 		return okayWithData(userService.create(createUserDto));
