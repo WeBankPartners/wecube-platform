44e49e407644645257f520dfd4ecfbefe59506ff
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-08 15:21:07 +0800

    #132 plugin model entity remove lombok, refactor domain object, add repository methods

--- wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelEntity.java
+++ wecube-core/src/main/java/com/webank/wecube/core/domain/plugin/PluginModelEntity.java
@@ -1,47 +1,110 @@
 package com.webank.wecube.core.domain.plugin;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import com.webank.wecube.core.utils.constant.DataModelDataType;
+import com.webank.wecube.core.utils.constant.DataModelState;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "plugin_model_entity", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"package_name", "name"})
+        @UniqueConstraint(columnNames = {"package_id", "name"})
 })
 public class PluginModelEntity {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
-    @Column(name = "package_id")
-    private Integer packageId;
-
-    @Column(name = "package_version")
-    private String packageVersion;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "package_id")
+    private PluginPackage pluginPackage;
 
     @Column(name = "name")
     private String name;
 
-    @Column(name = "package_name")
-    private String packageName;
+    @Column(name = "display_name")
+    private String displayName;
 
     @Column(name = "description")
     private String description;
 
     @Column(name = "state")
-    private String state;
+    private DataModelState state = DataModelState.Draft;
 
-    @OneToMany(mappedBy = "pluginModelEntity")
-    public List<PluginModelAttribute> pluginModelAttributeList;
+    @OneToMany(mappedBy = "pluginModelEntity", cascade = CascadeType.ALL)
+    private List<PluginModelAttribute> pluginModelAttributeList;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "package_id", insertable = false, updatable = false)
-    private PluginPackage pluginPackage;
+    public PluginModelEntity() {
+    }
+
+    public PluginModelEntity(PluginPackage pluginPackage,
+                             String name,
+                             String displayName,
+                             String description,
+                             String state) {
+        this.pluginPackage = pluginPackage;
+        this.name = name;
+        this.displayName = displayName;
+        this.description = description;
+        this.state = DataModelState.fromCode(state);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public PluginPackage getPluginPackage() {
+        return pluginPackage;
+    }
+
+    public void setPluginPackage(PluginPackage pluginPackage) {
+        this.pluginPackage = pluginPackage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state.getCode();
+    }
+
+    public void setState(String state) {
+        this.state = DataModelState.fromCode(state);
+    }
+
+    public List<PluginModelAttribute> getPluginModelAttributeList() {
+        return pluginModelAttributeList;
+    }
+
+    public void setPluginModelAttributeList(List<PluginModelAttribute> pluginModelAttributeList) {
+        this.pluginModelAttributeList = pluginModelAttributeList;
+    }
 }
