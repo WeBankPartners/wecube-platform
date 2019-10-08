4055018c61220949a078a5f855e7c1731b656eab
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-08 15:21:07 +0800

    #132 plugin model entity remove lombok, refactor domain object, add repository methods

--- wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelEntityRepository.java
+++ wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginModelEntityRepository.java
@@ -1,6 +1,8 @@
 package com.webank.wecube.core.jpa;
 
 import com.webank.wecube.core.domain.plugin.PluginModelEntity;
+import com.webank.wecube.core.domain.plugin.PluginPackage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,13 +10,13 @@ import java.util.Optional;
 
 public interface PluginModelEntityRepository extends CrudRepository<PluginModelEntity, Integer> {
 
-    List<PluginModelEntity> findAllByPackageId(Integer packageId);
+    List<PluginModelEntity> findAllByPluginPackage_Name(String packageName);
 
-    Optional<PluginModelEntity> findAllByPackageNameAndName(String packageName, String entityName);
+    List<PluginModelEntity> findAllByPluginPackage(PluginPackage pluginPackage);
 
-    Optional<PluginModelEntity> findAllByPackageName(String packageName);
+    void deleteByPluginPackage_NameAndName(String pluginPackageName, String entityName);
 
-    void delete(Integer packageId, String entityName);
+    void deleteByPluginPackage_Name(String packageName);
 
-    void delete(Integer packageId);
+    Optional<PluginModelEntity> findByPluginPackage_NameAndName(String packageName, String name);
 }
