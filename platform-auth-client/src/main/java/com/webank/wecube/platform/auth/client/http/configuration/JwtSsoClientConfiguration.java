65736f753b548a48afe2ef9cf410e0e34d9feefd
gavin2lee <gavin2lee@163.com>
2019-10-09 23:39:07 +0800

    #322 add platform gateway

--- platform-auth-client/src/main/java/com/webank/wecube/platform/auth/client/http/configuration/JwtSsoClientConfiguration.java
+++ platform-auth-client/src/main/java/com/webank/wecube/platform/auth/client/http/configuration/JwtSsoClientConfiguration.java
@@ -2,6 +2,7 @@ package com.webank.wecube.platform.auth.client.http.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,30 +12,31 @@ import com.webank.wecube.platform.auth.client.context.JwtSsoClientContext;
 import com.webank.wecube.platform.auth.client.http.JwtSsoRestTemplate;
 
 @Configuration
+@EnableConfigurationProperties({ JwtSsoClientProperties.class })
 public class JwtSsoClientConfiguration {
-    
+
     @Autowired
     private JwtSsoClientProperties jwtSsoClientProperties;
-    
+
     @Autowired
     private RestTemplate restTemplate;
-    
+
     @ConditionalOnMissingBean(JwtSsoRestTemplate.class)
     @Bean
-    public JwtSsoRestTemplate jwtSsoRestTemplate(JwtSsoClientContext ctx){
+    public JwtSsoRestTemplate jwtSsoRestTemplate(JwtSsoClientContext ctx) {
         return new JwtSsoRestTemplate(ctx);
     }
-    
+
     @ConditionalOnMissingBean(JwtSsoClientContext.class)
     @Bean
-    public JwtSsoClientContext jwtSsoClientContext(){
+    public JwtSsoClientContext jwtSsoClientContext() {
         JwtSsoClientContext ctx = new DefaultJwtSsoClientContext(jwtSsoClientProperties, restTemplate);
         return ctx;
     }
-    
+
     @ConditionalOnMissingBean(RestTemplate.class)
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
