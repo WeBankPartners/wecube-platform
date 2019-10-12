ab0f7f7d4ad7309c0903714fc24ff167b3fb87fa
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-12 11:18:39 +0800

    #132 create controller for get all data models and data model by specific package id

--- wecube-core/src/main/java/com/webank/wecube/core/controller/PluginModelController.java
+++ wecube-core/src/main/java/com/webank/wecube/core/controller/PluginModelController.java
@@ -3,35 +3,33 @@ package com.webank.wecube.core.controller;
 import com.webank.wecube.core.domain.JsonResponse;
 import com.webank.wecube.core.dto.PluginModelEntityDto;
 import com.webank.wecube.core.service.plugin.PluginModelService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.webank.wecube.core.domain.MenuItem.MENU_COLLABORATION_PLUGIN_MANAGEMENT;
 
 @RestController
-@Slf4j
-@RequestMapping("/plugin")
 @RolesAllowed({MENU_COLLABORATION_PLUGIN_MANAGEMENT})
 public class PluginModelController {
 
     @Autowired
     private PluginModelService pluginModelService;
 
-    @GetMapping("/packages/datamodels")
+    @GetMapping("/models")
     @ResponseBody
     public JsonResponse getAllDataModels() {
         List<PluginModelEntityDto> allPluginModelEntityList = pluginModelService.overview();
         return JsonResponse.okayWithData(allPluginModelEntityList);
     }
 
-    @GetMapping("/packages/datamodels/{package-id}")
+    @GetMapping("/packages/{id}/models")
     @ResponseBody
-    public JsonResponse getDataModelByPackageId(@PathVariable(value = "package-id") int packageId) {
+    public JsonResponse getDataModelByPackageId(@PathVariable(value = "id") int packageId) {
         List<PluginModelEntityDto> allPluginModelEntityList = pluginModelService.packageView(packageId);
         return JsonResponse.okayWithData(allPluginModelEntityList);
     }
