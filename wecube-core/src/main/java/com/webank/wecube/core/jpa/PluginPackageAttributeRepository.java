a75a1d582728bce5e7a0e0020011ea5ca7646890
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-14 16:40:48 +0800

    #132 refactor attribute repository with JPQL support.

--- wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginPackageAttributeRepository.java
+++ wecube-core/src/main/java/com/webank/wecube/core/jpa/PluginPackageAttributeRepository.java
@@ -1,7 +1,9 @@
 package com.webank.wecube.core.jpa;
 
 import com.webank.wecube.core.domain.plugin.PluginPackageAttribute;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,15 +11,46 @@ import java.util.Optional;
 public interface PluginPackageAttributeRepository extends CrudRepository<PluginPackageAttribute, Integer> {
 
     // find by package name, package version, entity name and attribute name
-    Optional<PluginPackageAttribute> findByPluginPackageEntity_PluginPackage_NameAndPluginPackageEntity_PluginPackage_VersionAndPluginPackageEntity_NameAndName(String packageName, String packageVersion, String entityName, String name);
+    @Query("SELECT attribute FROM PluginPackageAttribute attribute " +
+            "LEFT OUTER JOIN PluginPackageEntity entity ON attribute.pluginPackageEntity.id=entity.id " +
+            "LEFT OUTER JOIN PluginPackage package ON entity.pluginPackage.id=package.id " +
+            "WHERE package.name=:packageName AND package.version=:packageVersion AND entity.name=:entityName AND attribute.name=:attributeName")
+    Optional<PluginPackageAttribute> findSingleAttribute(
+            @Param("packageName") String packageName,
+            @Param("packageVersion") String packageVersion,
+            @Param("entityName") String entityName,
+            @Param("attributeName") String attributeName);
 
-    // find all by referenced package name, package version, entity name
-    Optional<List<PluginPackageAttribute>> findAllByPluginPackageAttribute_PluginPackageEntity_PluginPackage_NameAndPluginPackageAttribute_PluginPackageEntity_PluginPackage_VersionAndPluginPackageAttribute_PluginPackageEntity_Name(String packageName, String packageVersion, String entityName);
+    // find all "reference by info " by referenced package name, package version, entity name
+    @Query("SELECT attribute FROM PluginPackageAttribute attribute " +
+            "LEFT OUTER JOIN PluginPackageAttribute refAttribute ON attribute.pluginPackageAttribute.id=refAttribute.id " +
+            "LEFT OUTER JOIN PluginPackageEntity entity ON refAttribute.pluginPackageEntity.id=entity.id " +
+            "LEFT OUTER JOIN PluginPackage package ON entity.pluginPackage.id=package.id " +
+            "WHERE package.name=:packageName AND package.version = :packageVersion AND entity.name=:entityName")
+    Optional<List<PluginPackageAttribute>> findAllReferenceByAttribute(
+            @Param("packageName") String packageName,
+            @Param("packageVersion") String packageVersion,
+            @Param("entityName") String entityName);
 
-    // find all by package name, package version
-    Optional<List<PluginPackageAttribute>> findAllByPluginPackageEntity_PluginPackage_NameAndPluginPackageEntity_PluginPackage_Version(String packageName, String version);
+    // find all "reference by" info by package name, package version
+    @Query("SELECT attribute FROM PluginPackageAttribute attribute " +
+            "LEFT OUTER JOIN PluginPackageEntity entity ON attribute.pluginPackageEntity.id=entity.id " +
+            "LEFT OUTER JOIN PluginPackage package ON entity.pluginPackage.id=package.id " +
+            "WHERE package.name=:packageName AND package.version=:version")
+    Optional<List<PluginPackageAttribute>> findAllReferenceByAttribute(
+            @Param("packageName") String packageName,
+            @Param("version") String version);
 
-    // count all references by given package name, package nersion, entity name and attribute name
-    long countAllByPluginPackageAttribute_PluginPackageEntity_PluginPackage_NameAndPluginPackageAttribute_PluginPackageEntity_PluginPackage_VersionAndPluginPackageAttribute_PluginPackageEntity_NameAndName(String packageName, String packageVersion, String entityName, String attributeName);
+    // count all "references by" info by given package name, package version, entity name and attribute name
+    @Query("SELECT COUNT(attribute.id) FROM PluginPackageAttribute attribute " +
+            "LEFT OUTER JOIN PluginPackageAttribute refAttribute ON attribute.pluginPackageAttribute.id=refAttribute.id " +
+            "LEFT OUTER JOIN PluginPackageEntity entity ON refAttribute.pluginPackageEntity.id=entity.id " +
+            "LEFT OUTER JOIN PluginPackage package ON entity.pluginPackage.id=package.id " +
+            "WHERE package.name=:packageName AND package.version=:packageVersion AND entity.name=:entityName AND attribute.name=:attributeName")
+    long countAllReferenceByAttribute(
+            @Param("packageName") String packageName,
+            @Param("packageVersion") String packageVersion,
+            @Param("entityName") String entityName,
+            @Param("attributeName") String attributeName);
 
 }
