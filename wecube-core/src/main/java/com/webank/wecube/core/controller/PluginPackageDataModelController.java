968599c8cdf30c7995cc60f4379d3704baa36685
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-14 16:41:53 +0800

    #132 update data model controller and service implementation

--- wecube-core/src/main/java/com/webank/wecube/core/controller/PluginPackageDataModelController.java
+++ wecube-core/src/main/java/com/webank/wecube/core/controller/PluginPackageDataModelController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import static com.webank.wecube.core.domain.MenuItem.MENU_COLLABORATION_PLUGIN_MANAGEMENT;
 
 @RestController
+@RequestMapping("/")
 @RolesAllowed({MENU_COLLABORATION_PLUGIN_MANAGEMENT})
 public class PluginPackageDataModelController {
 
