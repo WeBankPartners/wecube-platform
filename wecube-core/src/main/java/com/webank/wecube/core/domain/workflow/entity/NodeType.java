386b29449ebf752b71a971e5301a01437f23a22e
gavin2lee <gavin2lee@163.com>
2019-10-15 21:49:23 +0800

    #395 refactor workflow project

--- wecube-core/src/main/java/com/webank/wecube/core/domain/workflow/entity/NodeType.java
+++
@@ -1,9 +0,0 @@
-package com.webank.wecube.core.domain.workflow.entity;
-
-public class NodeType {
-    
-    public static final String USER_TASK = "userTask";
-    public static final String SERVICE_TASK = "serviceTask";
-    public static final String SUB_PROCESS = "subProcess";
-
-}
