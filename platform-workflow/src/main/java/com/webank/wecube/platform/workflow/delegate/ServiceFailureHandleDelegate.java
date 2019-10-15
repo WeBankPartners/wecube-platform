9c31a948ec6cb38824c734bf7aca3a2125896342
gavin2lee <gavin2lee@163.com>
2019-10-15 22:13:41 +0800

    #395 add workflow project

---
+++ platform-workflow/src/main/java/com/webank/wecube/platform/workflow/delegate/ServiceFailureHandleDelegate.java
@@ -0,0 +1,27 @@
+package com.webank.wecube.platform.workflow.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.webank.wecube.platform.workflow.WorkflowConstants;
+import com.webank.wecube.platform.workflow.model.TraceStatus;
+
+@Service("srvFailBean")
+public class ServiceFailureHandleDelegate extends AbstractServiceExceptionHandleDelegate implements JavaDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceFailureHandleDelegate.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        
+        logServiceNodeException(execution, TraceStatus.Faulted, WorkflowConstants.PREFIX_SRV_BEAN_FAILURE);
+    }
+    
+    protected Logger getLogger() {
+        return log;
+    }
+
+}
