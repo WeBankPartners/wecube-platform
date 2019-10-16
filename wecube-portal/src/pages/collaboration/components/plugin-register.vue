3386bc4eb96b7dd03b6bdf4790ce0afe08ae26cf
homiexie <624128114@qq.com>
2019-10-16 10:25:05 +0800

    #292 init plugin register component

---
+++ wecube-portal/src/pages/collaboration/components/plugin-register.vue
@@ -0,0 +1,32 @@
+<template>
+  <Row>
+    <Col span="3">
+      <RadioGroup v-model="vertical" vertical>
+        <Radio label="apple">
+          <Icon type="social-apple"></Icon>
+          <span>Apple</span>
+        </Radio>
+        <Radio label="android">
+          <Icon type="social-android"></Icon>
+          <span>Android</span>
+        </Radio>
+        <Radio label="windows">
+          <Icon type="social-windows"></Icon>
+          <span>Windows</span>
+        </Radio>
+      </RadioGroup>
+    </Col>
+    <Col span="21"> </Col>
+  </Row>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {},
+  watch: {},
+  methods: {}
+};
+</script>
+<style lang="scss"></style>
