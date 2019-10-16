a3acaf5f835e0fba0626f5ce65956895b6ccc758
gavin2lee <gavin2lee@163.com>
2019-10-16 14:34:41 +0800

    #395 #386 refactor platform core

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginConfigInterfaceParameterRepository.java
@@ -0,0 +1,9 @@
+package com.webank.wecube.platform.core.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterfaceParameter;
+
+public interface PluginConfigInterfaceParameterRepository extends CrudRepository<PluginConfigInterfaceParameter, Integer> {
+
+}
