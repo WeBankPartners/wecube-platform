732d847e5055308ea38a6beb5f3f774b0d361aeb
gavin2lee <gavin2lee@163.com>
2019-10-17 01:23:04 +0800

    #395 refactor process definition

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeDefInfoRepository.java
@@ -0,0 +1,20 @@
+package com.webank.wecube.platform.core.jpa.workflow;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
+
+public interface TaskNodeDefInfoRepository
+		extends JpaRepository<TaskNodeDefInfoEntity, String> {
+	
+	List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersion(String procDefKey, Integer version);
+	
+	List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersionAndTaskNodeId(String procDefKey, Integer version, String taskNodeId);
+	
+	@Query("select t from ProcessDefinitionTaskServiceEntity t  where t.coreProcDefId = :coreProcDefId")
+	List<TaskNodeDefInfoEntity> findAllByProcDefId(@Param("coreProcDefId") String coreProcDefId);
+}
