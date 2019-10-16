408df996779b9994cfa440f3afcc172123b7fff2
gavin2lee <gavin2lee@163.com>
2019-10-16 11:42:03 +0800

    #395 #386 init platform-core project for v2.0

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/Pageable.java
@@ -0,0 +1,28 @@
+package com.webank.wecube.platform.core.dto;
+
+public class Pageable{
+	private int startIndex;
+	private int pageSize = 10000;
+	
+	public Pageable() {
+	}
+	
+	public Pageable(int startIndex,int pageSize) {
+		this.startIndex = startIndex;
+		this.pageSize = pageSize;
+	}
+	
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getStartIndex() {
+		return startIndex;
+	}
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+}
+
