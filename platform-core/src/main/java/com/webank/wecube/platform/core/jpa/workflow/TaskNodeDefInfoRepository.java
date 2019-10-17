f58391ed3be382bc368ac085d35ea922ca28b204
gavin2lee <gavin2lee@163.com>
2019-10-18 00:34:38 +0800

    #395 get process defnition

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeDefInfoRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeDefInfoRepository.java
@@ -1,7 +1,5 @@
 package com.webank.wecube.platform.core.jpa.workflow;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,14 +7,6 @@ import org.springframework.data.repository.query.Param;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
 
 public interface TaskNodeDefInfoRepository extends JpaRepository<TaskNodeDefInfoEntity, String> {
-
-//    List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersion(String procDefKey, Integer version);
-//
-//    List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersionAndTaskNodeId(String procDefKey, Integer version,
-//            String taskNodeId);
-
-//    @Query("select t from ProcessDefinitionTaskServiceEntity t  where t.coreProcDefId = :coreProcDefId")
-//    List<TaskNodeDefInfoEntity> findAllByProcDefId(@Param("coreProcDefId") String coreProcDefId);
 
     @Query("select t from TaskNodeDefInfoEntity t "
             + " where t.procDefId = :procDefId and t.nodeId = :nodeId and t.status = :status")
