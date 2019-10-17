d9e509af9f5b3a50e42283799e98071744df2950
Benjamin H Q Li <benjaminli@webank.com>
2019-10-17 20:36:30 +0800

    #341 parse plugin package definition xml

--- platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginPackageAttribute.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginPackageAttribute.java
@@ -1,17 +1,9 @@
 package com.webank.wecube.platform.core.domain.plugin;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import com.webank.wecube.platform.core.utils.constant.DataModelDataType;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "plugin_package_attributes", uniqueConstraints = {
@@ -101,5 +93,10 @@ public class PluginPackageAttribute {
 
     public void setDataType(String dataType) {
         this.dataType = DataModelDataType.fromCode(dataType).getCode();
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toStringExclude(this, new String[]{"pluginPackageEntity", "pluginPackageAttribute"});
     }
 }
