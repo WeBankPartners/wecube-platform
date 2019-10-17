acf0dd6954d3a2b0cdfe396421c2c4d5c2c41583
gavin2lee <gavin2lee@163.com>
2019-10-17 14:35:33 +0800

    #395 commented some security config

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/WorkflowProcessDefinitionController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/WorkflowProcessDefinitionController.java
@@ -1,7 +1,9 @@
 package com.webank.wecube.platform.core.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import com.webank.wecube.platform.core.dto.CommonResponseDto;
 import com.webank.wecube.platform.core.dto.workflow.ProcessDefinitionDeployRequestDto;
-import com.webank.wecube.platform.core.dto.workflow.TaskNodeDraftRequestDto;
+import com.webank.wecube.platform.core.dto.workflow.TaskNodeInfoDto;
+import com.webank.wecube.platform.core.service.workflow.WorkflowProcessDefinitionService;
 
 @RestController
 @RequestMapping("/v1/api")
 public class WorkflowProcessDefinitionController {
     private static final Logger log = LoggerFactory.getLogger(WorkflowProcessDefinitionController.class);
+
+    @Autowired
+    private WorkflowProcessDefinitionService procDefService;
 
     @PostMapping("/process/definitions")
     public CommonResponseDto deployProcessDefinition(@RequestBody ProcessDefinitionDeployRequestDto deployRequest) {
@@ -38,8 +45,29 @@ public class WorkflowProcessDefinitionController {
 
     @PutMapping("/process/definitions/{process-id}/task-nodes")
     public CommonResponseDto configureTaskNode(@PathVariable(name = "process-id") String processId,
-            @RequestBody TaskNodeDraftRequestDto draftRequest) {
-        return null;
+            @RequestBody TaskNodeInfoDto request) {
+        if (log.isDebugEnabled()) {
+            log.debug("configure task node with process id:{},task node info:{}", processId, request);
+        }
+        
+        if (StringUtils.isBlank(processId)) {
+            throw new WecubeCoreException("process id is blank.");
+        }
+
+        TaskNodeInfoDto result = procDefService.configureTaskNode(processId, request);
+
+        return CommonResponseDto.okayWithData(result);
+    }
+    
+    @GetMapping("/process/definitions/{process-id}/task-nodes/{node-id}")
+    public CommonResponseDto getTaskNodeInfo(@PathVariable(name = "process-id") String processId, @PathVariable(name = "node-id") String nodeId){
+        if(StringUtils.isBlank(processId) || StringUtils.isBlank(nodeId)){
+            throw new WecubeCoreException("process id or node id is blank.");
+        }
+        
+        TaskNodeInfoDto result = procDefService.getTaskNodeInfo(processId, nodeId);
+        
+        return CommonResponseDto.okayWithData(result);
     }
 
 }
--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeDefInfoRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeDefInfoRepository.java
@@ -8,13 +8,18 @@ import org.springframework.data.repository.query.Param;
 
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
 
-public interface TaskNodeDefInfoRepository
-		extends JpaRepository<TaskNodeDefInfoEntity, String> {
-	
-	List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersion(String procDefKey, Integer version);
-	
-	List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersionAndTaskNodeId(String procDefKey, Integer version, String taskNodeId);
-	
-	@Query("select t from ProcessDefinitionTaskServiceEntity t  where t.coreProcDefId = :coreProcDefId")
-	List<TaskNodeDefInfoEntity> findAllByProcDefId(@Param("coreProcDefId") String coreProcDefId);
+public interface TaskNodeDefInfoRepository extends JpaRepository<TaskNodeDefInfoEntity, String> {
+
+    List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersion(String procDefKey, Integer version);
+
+    List<TaskNodeDefInfoEntity> findTaskServicesByProcDefKeyAndVersionAndTaskNodeId(String procDefKey, Integer version,
+            String taskNodeId);
+
+    @Query("select t from ProcessDefinitionTaskServiceEntity t  where t.coreProcDefId = :coreProcDefId")
+    List<TaskNodeDefInfoEntity> findAllByProcDefId(@Param("coreProcDefId") String coreProcDefId);
+
+    @Query("select t from TaskNodeDefInfoEntity t "
+            + " where t.procDefId = :processId and t.taskNodeId = :nodeId and t.status = :status")
+    TaskNodeDefInfoEntity findOneWithProcessIdAndNodeIdAndStatus(@Param("processId") String processId,
+            @Param("nodeId") String nodeId, @Param("status") String status);
 }
