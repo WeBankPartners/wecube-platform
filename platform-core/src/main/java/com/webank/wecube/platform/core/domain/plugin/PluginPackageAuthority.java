79bf6c863492153eb4c24b67e459a97531058014
Benjamin H Q Li <benjaminli@webank.com>
2019-10-17 22:40:23 +0800

    #386 plugin package upload

--- platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginPackageAuthority.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginPackageAuthority.java
@@ -5,68 +5,68 @@ import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "plugin_authorities")
+@Table(name = "plugin_package_authorities")
 public class PluginPackageAuthority {
 
-	@Id
-	@GeneratedValue
-	private Integer id;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
-	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
-	@JoinColumn(name = "plugin_package_id")
-	private PluginPackage pluginPackage;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "plugin_package_id")
+    private PluginPackage pluginPackage;
 
-	@Column
-	private String roleName;
+    @Column
+    private String roleName;
 
-	@Column
-	private String menuCode;
+    @Column
+    private String menuCode;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public PluginPackage getPluginPackage() {
-		return pluginPackage;
-	}
+    public PluginPackage getPluginPackage() {
+        return pluginPackage;
+    }
 
-	public void setPluginPackage(PluginPackage pluginPackage) {
-		this.pluginPackage = pluginPackage;
-	}
+    public void setPluginPackage(PluginPackage pluginPackage) {
+        this.pluginPackage = pluginPackage;
+    }
 
-	public String getRoleName() {
-		return roleName;
-	}
+    public String getRoleName() {
+        return roleName;
+    }
 
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 
-	public String getMenuCode() {
-		return menuCode;
-	}
+    public String getMenuCode() {
+        return menuCode;
+    }
 
-	public void setMenuCode(String menuCode) {
-		this.menuCode = menuCode;
-	}
+    public void setMenuCode(String menuCode) {
+        this.menuCode = menuCode;
+    }
 
-	public PluginPackageAuthority() {
-		super();
-	}
+    public PluginPackageAuthority() {
+        super();
+    }
 
-	public PluginPackageAuthority(Integer id, PluginPackage pluginPackage, String roleName, String menuCode) {
-		this.id = id;
-		this.pluginPackage = pluginPackage;
-		this.roleName = roleName;
-		this.menuCode = menuCode;
-	}
+    public PluginPackageAuthority(Integer id, PluginPackage pluginPackage, String roleName, String menuCode) {
+        this.id = id;
+        this.pluginPackage = pluginPackage;
+        this.roleName = roleName;
+        this.menuCode = menuCode;
+    }
 
-	@Override
-	public String toString() {
-		return ReflectionToStringBuilder.toStringExclude(this, new String[] {"pluginPackage"});
-	}
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toStringExclude(this, new String[]{"pluginPackage"});
+    }
 }
