57eb0f331562617cc5c076d801707d6002dbdcd6
Benjamin H Q Li <benjaminli@webank.com>
2019-10-18 17:10:06 +0800

    #386 update controller test PluginPackageControllerTest

--- platform-core/src/test/java/com/webank/wecube/platform/core/controller/PluginPackageControllerTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/controller/PluginPackageControllerTest.java
@@ -1,32 +1,53 @@
 package com.webank.wecube.platform.core.controller;
 
 import com.webank.wecube.platform.core.domain.JsonResponse;
-import com.webank.wecube.platform.core.dto.PluginPackageDto;
-import com.webank.wecube.platform.core.service.plugin.PluginPackageService;
+import com.webank.wecube.platform.core.domain.plugin.PluginPackage;
+import com.webank.wecube.platform.core.domain.plugin.PluginPackageEntity;
+import com.webank.wecube.platform.core.jpa.PluginPackageEntityRepository;
+import com.webank.wecube.platform.core.jpa.PluginPackageRepository;
 import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.rules.TemporaryFolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
+
+import static com.google.common.collect.Sets.newLinkedHashSet;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.when;
 
 public class PluginPackageControllerTest extends AbstractControllerTest {
+    @ClassRule
+    public static TemporaryFolder folder= new TemporaryFolder();
+
     @Autowired
     private PluginPackageController pluginPackageController;
-    @Mock
-    private PluginPackageService pluginPackageService;
+
+    @Autowired
+    private PluginPackageRepository pluginPackageRepository;
+
+    @Autowired
+    private PluginPackageEntityRepository pluginPackageEntityRepository;
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
+
+    @BeforeClass
+    public static void setupStatic() {
+        try {
+            System.setProperty("java.io.tmpdir", folder.newFolder().getCanonicalPath());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
 
     @Test
     public void givenNullPluginPackageWhenUploadThenThrowException() throws Exception {
@@ -46,7 +67,6 @@ public class PluginPackageControllerTest extends AbstractControllerTest {
 
     @Test
     public void givenPluginPackageNormalWhenUploadThenReturnSuccess() {
-        PluginPackageDto pluginPackageV2 = Mockito.mock(PluginPackageDto.class);
         String testPackageName = "service-manage-v0.1.zip";
         File testPackage = new File("src/test/resources/testpackage/service-manage-v0.1.zip");
         MockMultipartFile mockPluginPackageFile = null;
@@ -56,20 +76,31 @@ public class PluginPackageControllerTest extends AbstractControllerTest {
             fail(e.getMessage());
         }
 
-        try {
-            when(pluginPackageService.uploadPackage(mockPluginPackageFile)).thenReturn(pluginPackageV2);
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-
         assertThat(testPackage.exists()).isTrue();
         try {
             JsonResponse responseForMockedFile = pluginPackageController.uploadPluginPackage(mockPluginPackageFile);
             assertThat(responseForMockedFile.getStatus()).isEqualTo(JsonResponse.STATUS_OK);
+            assertThat(responseForMockedFile.getMessage()).isEqualTo(JsonResponse.SUCCESS);
         } catch (Exception e) {
-            e.printStackTrace();
             fail(e.getMessage());
         }
+
+        Iterable<PluginPackage> pluginPackages = pluginPackageRepository.findAll();
+        PluginPackage pluginPackage = pluginPackages.iterator().next();
+        assertThat(pluginPackage.getName()).isEqualTo("service-management");
+        assertThat(pluginPackage.getVersion()).isEqualTo("v0.1");
+
+        assertThat(pluginPackage.getPluginPackageDependencies()).hasSize(2);
+        assertThat(pluginPackage.getPluginPackageMenus()).hasSize(2);
+        assertThat(pluginPackage.getSystemVariables()).hasSize(2);
+        assertThat(pluginPackage.getPluginPackageAuthorities()).hasSize(3);
+        assertThat(pluginPackage.getPluginPackageRuntimeResourcesDocker()).hasSize(1);
+        assertThat(pluginPackage.getPluginPackageRuntimeResourcesMysql()).hasSize(1);
+        assertThat(pluginPackage.getPluginPackageRuntimeResourcesS3()).hasSize(1);
+        assertThat(pluginPackage.getPluginConfigs()).hasSize(2);
+
+        Set<PluginPackageEntity> pluginPackageEntities = newLinkedHashSet(pluginPackageEntityRepository.findAll());
+        assertThat(pluginPackageEntities).hasSize(5);
     }
 
 }
