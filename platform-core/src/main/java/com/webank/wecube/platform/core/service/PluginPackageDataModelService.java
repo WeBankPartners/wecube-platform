25e8f7561639680dcac4c58b4b72bdb6c0bdeefd
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-21 10:03:43 +0800

    #399 integrate data model and sys variable controllers, services, and their tests.

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/PluginPackageDataModelService.java
@@ -0,0 +1,21 @@
+package com.webank.wecube.platform.core.service;
+
+import com.webank.wecube.platform.core.dto.PluginPackageAttributeDto;
+import com.webank.wecube.platform.core.dto.PluginPackageEntityDto;
+
+import java.util.EnumMap;
+import java.util.List;
+
+public interface PluginPackageDataModelService {
+
+    List<PluginPackageEntityDto> register(List<PluginPackageEntityDto> pluginModelEntityDtos);
+
+    List<PluginPackageEntityDto> overview();
+
+    List<PluginPackageEntityDto> packageView(String packageName, String version);
+
+    List<PluginPackageEntityDto> packageView(int packageId);
+
+    void deleteModel(String packageName, String version);
+
+}
