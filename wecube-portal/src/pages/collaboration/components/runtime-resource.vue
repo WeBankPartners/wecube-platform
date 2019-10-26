a60598212aa717051fbff48916a642a4046c0eeb
irvinezhao <irvinezhao@gmail.com>
2019-10-26 15:48:41 +0800

    :tada: finish plugin config

--- wecube-portal/src/pages/collaboration/components/runtime-resource.vue
+++ wecube-portal/src/pages/collaboration/components/runtime-resource.vue
@@ -2,19 +2,19 @@
   <div>
     <Collapse :value="[1, 2, 3]">
       <Panel name="1">
-        <span style="font-size: 12px">运行容器</span>
+        <span style="font-size: 12px">{{ $t("runtime_container") }}</span>
         <p slot="content" v-for="(item, index) in data.docker" :key="index">
           {{ index + 1 + ": " + JSON.stringify(item) }}
         </p>
       </Panel>
       <Panel name="2">
-        <span style="font-size: 12px">数据库</span>
+        <span style="font-size: 12px">{{ $t("database") }}</span>
         <p slot="content" v-for="(item, index) in data.mysql" :key="index">
           {{ index + 1 + ": " + JSON.stringify(item) }}
         </p>
       </Panel>
       <Panel name="3">
-        <span style="font-size: 12px">对象存储</span>
+        <span style="font-size: 12px">{{ $t("storage_service") }}</span>
         <p slot="content" v-for="(item, index) in data.s3" :key="index">
           {{ index + 1 + ": " + JSON.stringify(item) }}
         </p>
