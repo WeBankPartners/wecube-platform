c580b4970242d3a29b028d4cef4d4e4849eb3d89
haixinhuang <huanghaixin@126.com>
2019-10-28 19:34:28 +0800

    #402 Test create docker instance API and fix issues

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginMysqlInstanceRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginMysqlInstanceRepository.java
@@ -1,7 +1,7 @@
 package com.webank.wecube.platform.core.jpa;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.webank.wecube.platform.core.domain.plugin.PluginMysqlInstance;
 
-public interface PluginMysqlInstanceRepository extends CrudRepository<PluginMysqlInstance, Integer> {
+public interface PluginMysqlInstanceRepository extends JpaRepository<PluginMysqlInstance, Integer> {
 }
