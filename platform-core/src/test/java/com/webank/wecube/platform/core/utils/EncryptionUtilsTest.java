e28c818253c6265d3db6ccaba15a1beec557c2af
haixinhuang <huanghaixin@126.com>
2019-10-28 21:28:55 +0800

    #402 Test create docker instance API and fix issues

--- platform-core/src/test/java/com/webank/wecube/platform/core/utils/EncryptionUtilsTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/utils/EncryptionUtilsTest.java
@@ -27,5 +27,18 @@ public class EncryptionUtilsTest extends DatabaseBasedTest {
         assertThat(EncryptionUtils.decryptWithAes(encryptedPassword, resourceProperties.getPasswordEncryptionSeed(),
                 additionalSalt)).isEqualTo(password);
     }
+    
+
+    @Test
+    public void encryptPasswordTest2() {
+        ***REMOVED***;
+        String additionalSalt = "containerHost";
+        String encryptedPassword = EncryptionUtils.encryptWithAes(password,
+                resourceProperties.getPasswordEncryptionSeed(), additionalSalt);
+        log.info("encryptedPassword: " + encryptedPassword);
+
+        assertThat(EncryptionUtils.decryptWithAes(encryptedPassword, resourceProperties.getPasswordEncryptionSeed(),
+                additionalSalt)).isEqualTo(password);
+    }
 
 }
