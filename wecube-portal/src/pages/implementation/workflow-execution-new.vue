4ac94d2392eed139a38b70f284bb76029f535fe7
irvinezhao <irvinezhao@gmail.com>
2019-10-30 16:32:59 +0800

    :clown_face: mocking api data wip

--- wecube-portal/src/pages/implementation/workflow-execution-new.vue
+++ wecube-portal/src/pages/implementation/workflow-execution-new.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <Row>Select</Row>
-    <Row>
-      <Col span="6">
-        <div class="graph-container" id="flow"></div>
-      </Col>
-      <Col span="18">
-        <div class="graph-container" id="graph"></div>
-      </Col>
-    </Row>
+    <Card dis-hover>
+      <Row>
+        <Col span="2">
+          <span>选择编排</span>
+        </Col>
+        <Col span="20">
+          <Select label="" v-model="selectedFlow" style="width:200px">
+            <Option v-for="item in allFlows" :value="item.id" :key="item.id">{{
+              item.orchestration.orchestrationName +
+                " " +
+                item.target.targetName +
+                " " +
+                item.timestamp +
+                " " +
+                item.createBy
+            }}</Option>
+          </Select>
+        </Col>
+      </Row>
+      <hr style="margin: 15px 0" />
+      <Row>
+        <Col span="6">
+          <div class="graph-container" id="flow"></div>
+        </Col>
+        <Col span="18">
+          <div class="graph-container" id="graph"></div>
+        </Col>
+      </Row>
+    </Card>
   </div>
 </template>
 <script>
@@ -16,7 +36,7 @@ import {} from "@/api/server";
 import * as d3 from "d3-selection";
 import * as d3Graphviz from "d3-graphviz";
 import { addEvent, removeEvent } from "../util/event.js";
-import { modelData, flowData } from "./mockData";
+import { modelData, flowData, allFlows } from "./mockData";
 export default {
   data() {
     return {
@@ -24,6 +44,7 @@ export default {
       flowGraph: {},
       modelData,
       flowData,
+      allFlows,
       currentFlowNodeId: "",
       foundRefAry: []
     };
@@ -48,12 +69,12 @@ export default {
         const isRecord = _.refFlowNodeIds.length > 0;
         const shape = isRecord ? "Mrecord" : "ellipse";
         const label =
-          _.refFlowNodeIds.toString().replace(/,/g, "|") +
+          _.refFlowNodeIds.toString().replace(/,/g, "\\n") +
           (isRecord ? "|" : "") +
           _.id;
-        return `${_.id} [label="${label}" class="model" id="${
-          _.id
-        }" color="${color}" shape="${shape}" ]`;
+        console.log("label: " + index, label);
+
+        return `${_.id} [label="${label}" class="model" id="${_.id}" color="${color}" shape="${shape}" ]`;
       });
       let genEdge = () => {
         let pathAry = [];
@@ -72,28 +93,31 @@ export default {
           .replace(/,/g, ";");
       };
       let nodesString =
-        "digraph G {" +
+        "digraph G { " +
+        'bgcolor="transparent";' +
+        'Node [fontname=Arial, shape="ellipse", fixedsize="true", width="1.6", height=".8",fontsize=12];' +
+        'Edge [fontname=Arial, minlen="1", color="#7f8fa6", fontsize=10];' +
         nodes.toString().replace(/,/g, "; ") +
         ";" +
         genEdge() +
         "}";
-      this.graph.graphviz.renderDot(nodesString);
+      this.graph.graphviz.renderDot(nodesString).fit(true);
     },
     renderFlowGraph() {
       let nodes = this.flowData.map((_, index) => {
         if (index === 0 || index === this.flowData.length - 1) {
-          return `${_.id} [label="${(_.id > 1 && _.id < 12
-            ? _.id - 1 + "、"
+          return `${_.id} [label="${(_.id > 1 && _.id < this.flowData.length
+            ? _.id + "、"
             : "") +
             _.name}", fontsize="10", class="flow", shape="circle", id="${
             _.id
           }"]`;
         } else {
-          return `${_.id} [label="${(_.id > 1 && _.id < 12
-            ? _.id - 1 + "、"
+          return `${_.id} [label="${(_.id > 0 && _.id < this.flowData.length
+            ? _.id + "、"
             : "") + _.name}" fontsize="10" class="flow" color=${
             _.id === this.currentFlowNodeId * 1 ? "green" : "black"
-          } shape="record" id="${_.id}"]`;
+          } shape="record" id="${_.id}"] height=.2`;
         }
       });
       let genEdge = () => {
@@ -114,11 +138,14 @@ export default {
       };
       let nodesString =
         "digraph G {" +
+        'bgcolor="transparent";' +
+        'Node [fontname=Arial, height=".3", fontsize=12];' +
+        'Edge [fontname=Arial, color="#7f8fa6", fontsize=10];' +
         nodes.toString().replace(/,/g, ";") +
         ";" +
         genEdge() +
         "}";
-      this.flowGraph.graphviz.renderDot(nodesString);
+      this.flowGraph.graphviz.renderDot(nodesString).fit(true);
     },
     bindEvents() {
       addEvent(".flow", "mouseover", e => {
