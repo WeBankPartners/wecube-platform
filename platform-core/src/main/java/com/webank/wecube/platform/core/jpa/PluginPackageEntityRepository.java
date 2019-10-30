09cb0a955eae027235d15f01a12ce1f33c16d376
HoweChen <yuhao.howe.chen@gmail.com>
2019-10-30 15:58:39 +0800

    #467 upgrade data model service for dynamic demand

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginPackageEntityRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginPackageEntityRepository.java
@@ -3,19 +3,26 @@ package com.webank.wecube.platform.core.jpa;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.webank.wecube.platform.core.domain.plugin.PluginPackageEntity;
+import org.springframework.data.repository.query.Param;
 
 public interface PluginPackageEntityRepository extends CrudRepository<PluginPackageEntity, Integer> {
 
     Optional<List<PluginPackageEntity>> findAllByPluginPackage_Name(String packageName);
 
-    Optional<List<PluginPackageEntity>> findAllByPluginPackage_Id(Integer id);
+    @Query(value = "SELECT entity " +
+            "FROM PluginPackageEntity entity " +
+            "WHERE  entity.dataModelVersion = (SELECT max(entity.dataModelVersion ) from PluginPackageEntity entity WHERE entity.pluginPackage.name=:packageName GROUP BY entity.pluginPackage.name) AND entity.pluginPackage.name=:packageName")
+    Optional<List<PluginPackageEntity>> findAllLatestEntityByPluginPackage_name(@Param("packageName") String packageName);
 
-    void deleteByPluginPackage_NameAndPluginPackage_Version(String pluginPackageName, String entityName);
+    @Query(value = "SELECT entity " +
+            "FROM PluginPackageEntity entity " +
+            "WHERE  entity.dataModelVersion = (SELECT max(entity.dataModelVersion ) from PluginPackageEntity entity WHERE entity.pluginPackage.id=:packageId GROUP BY entity.pluginPackage.name) AND entity.pluginPackage.id=:packageId")
+    Optional<List<PluginPackageEntity>> findAllLatestByPluginPackage_Id(@Param("packageId") Integer id);
 
-    Optional<List<PluginPackageEntity>> findAllByPluginPackage_NameAndPluginPackage_Version(String packageName, String packageVersion);
+    Optional<PluginPackageEntity> findTop1ByPluginPackage_NameAndNameOrderByDataModelVersionDesc(String packageName, String entityName);
 
-    Optional<PluginPackageEntity> findByPluginPackage_NameAndPluginPackage_VersionAndName(String packageName, String packageVersion, String entityName);
 }
