ee0ebd7b390257e28d665e126eab242565b6791d
Jun Chen <junchensz@foxmail.com>
2019-10-31 15:10:41 +0800

    #401 implement db query

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginMysqlInstanceRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginMysqlInstanceRepository.java
@@ -1,7 +1,9 @@
 package com.webank.wecube.platform.core.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.webank.wecube.platform.core.domain.plugin.PluginMysqlInstance;
 
 public interface PluginMysqlInstanceRepository extends JpaRepository<PluginMysqlInstance, Integer> {
+    PluginMysqlInstance findByPluginPackageId(int packageId);
 }
