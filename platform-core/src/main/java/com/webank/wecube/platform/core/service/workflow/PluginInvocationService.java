6398a359152d0ee30189035f991033726f62227a
gavin2lee <gavin2lee@163.com>
2019-11-05 11:59:46 +0800

    #520 add dynamic gateway

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/PluginInvocationService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/PluginInvocationService.java
@@ -1,8 +1,10 @@
 package com.webank.wecube.platform.core.service.workflow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -11,14 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
+import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterface;
+import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterfaceParameter;
+import com.webank.wecube.platform.core.entity.workflow.ProcExecBindingEntity;
 import com.webank.wecube.platform.core.entity.workflow.ProcInstInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeInstInfoEntity;
+import com.webank.wecube.platform.core.jpa.workflow.ProcExecBindingRepository;
 import com.webank.wecube.platform.core.jpa.workflow.ProcInstInfoRepository;
 import com.webank.wecube.platform.core.jpa.workflow.TaskNodeDefInfoRepository;
 import com.webank.wecube.platform.core.jpa.workflow.TaskNodeInstInfoRepository;
 import com.webank.wecube.platform.core.model.workflow.PluginInvocationCommand;
 import com.webank.wecube.platform.core.model.workflow.PluginInvocationResult;
+import com.webank.wecube.platform.core.service.plugin.PluginConfigService;
 import com.webank.wecube.platform.core.service.workflow.PluginInvocationProcessor.PluginInterfaceInvocationResult;
 import com.webank.wecube.platform.core.service.workflow.PluginInvocationProcessor.PluginInvocationOperation;
 import com.webank.wecube.platform.core.support.plugin.PluginServiceStub;
@@ -45,6 +52,12 @@ public class PluginInvocationService {
     @Autowired
     private TaskNodeDefInfoRepository taskNodeDefInfoRepository;
 
+    @Autowired
+    private PluginConfigService pluginConfigService;
+
+    @Autowired
+    private ProcExecBindingRepository procExecBindingRepository;
+
     public void invokePluginInterface(PluginInvocationCommand cmd) {
         if (log.isInfoEnabled()) {
             log.info("invoke plugin interface with:{}", cmd);
@@ -63,25 +76,59 @@ public class PluginInvocationService {
         String nodeId = cmd.getNodeId();
         TaskNodeDefInfoEntity taskNodeDefEntity = taskNodeDefInfoRepository.findOneWithProcessIdAndNodeIdAndStatus(
                 procInstEntity.getProcDefId(), nodeId, TaskNodeDefInfoEntity.DEPLOYED_STATUS);
-        
-        if(taskNodeDefEntity == null){
-            log.error("Task node definition does not exist for {} {} {}", procInstEntity.getProcDefId(), nodeId, TaskNodeDefInfoEntity.DEPLOYED_STATUS);
+
+        if (taskNodeDefEntity == null) {
+            log.error("Task node definition does not exist for {} {} {}", procInstEntity.getProcDefId(), nodeId,
+                    TaskNodeDefInfoEntity.DEPLOYED_STATUS);
             throw new WecubeCoreException("Task node definition does not exist.");
         }
-        
-        TaskNodeInstInfoEntity taskNodeInstEntity = taskNodeInstInfoRepository.findOneByProcInstIdAndNodeId(procInstEntity.getId(), nodeId);
-        if(taskNodeInstEntity == null){
+
+        TaskNodeInstInfoEntity taskNodeInstEntity = taskNodeInstInfoRepository
+                .findOneByProcInstIdAndNodeId(procInstEntity.getId(), nodeId);
+        if (taskNodeInstEntity == null) {
             log.error("Task node instance does not exist for {} {}", procInstEntity.getId(), nodeId);
             throw new WecubeCoreException("Task node instance does not exist.");
         }
-        
+
         String serviceId = taskNodeDefEntity.getServiceId();
-        if(StringUtils.isBlank(serviceId)){
+        if (StringUtils.isBlank(serviceId)) {
             log.error("service ID is invalid for {} {}", procInstEntity.getProcDefId(), nodeId);
             throw new WecubeCoreException("Service ID is invalid.");
         }
 
+        List<ProcExecBindingEntity> nodeObjectBindings = procExecBindingRepository
+                .findAllTaskNodeBindings(taskNodeInstEntity.getProcInstId(), taskNodeInstEntity.getId());
+
+        if (nodeObjectBindings == null) {
+            log.warn("node object bindings is empty for {} {}", taskNodeInstEntity.getProcInstId(),
+                    taskNodeInstEntity.getId());
+            nodeObjectBindings = new ArrayList<>();
+        }
+
+        PluginConfigInterface pluginConfigInterface = pluginConfigService
+                .getPluginConfigInterfaceByServiceName(serviceId);
+
+        // TODO
+        Set<PluginConfigInterfaceParameter> configInterfaceParams = pluginConfigInterface.getInputParameters();
+
+        // TODO
+
         List<Map<String, Object>> pluginParameters = new ArrayList<>();
+        for (ProcExecBindingEntity nodeObjectBinding : nodeObjectBindings) {
+            // TODO
+            String objectId = nodeObjectBinding.getObjectId();
+            // TODO
+
+            Map<String, List<Object>> parsedParamValues = new HashMap<String, List<Object>>();
+            for (PluginConfigInterfaceParameter param : configInterfaceParams) {
+                // TODO get from dataroute
+                String paramName = param.getName();
+                List<Object> objectVals = new ArrayList<Object>();
+                parsedParamValues.put(paramName, objectVals);
+
+            }
+        }
+
         String interfacePath = "";
         String instanceHost = "";
 
