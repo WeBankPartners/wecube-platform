4e5756828263d8abc4b6350ec502d3695bf2422b
HoweChen <yuhao.howe.chen@gmail.com>
2019-11-06 23:25:48 +0800

    #457 update data model expression parser and service

--- platform-core/src/main/java/com/webank/wecube/platform/core/parser/datamodel/DataModelExpressionParser.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/parser/datamodel/DataModelExpressionParser.java
@@ -1,14 +1,13 @@
 package com.webank.wecube.platform.core.parser.datamodel;
 
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
+import com.webank.wecube.platform.core.dto.DataModelExpressionDto;
 import com.webank.wecube.platform.core.parser.datamodel.generated.DataModelLexer;
 import com.webank.wecube.platform.core.parser.datamodel.generated.DataModelParser;
-import com.webank.wecube.platform.core.support.parser.DataModelExpressionHelper;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import java.util.LinkedList;
 import java.util.Queue;
 
 public class DataModelExpressionParser {
@@ -16,7 +15,7 @@ public class DataModelExpressionParser {
     public DataModelExpressionParser() {
     }
 
-    public Queue<DataModelExpressionHelper> parse(String expression) {
+    public Queue<DataModelExpressionDto> parse(String expression) {
         CharStream inputStream = CharStreams.fromString(expression);
         DataModelLexer dataModelLexer = new DataModelLexer(inputStream);
         CommonTokenStream tokens = new CommonTokenStream(dataModelLexer);
@@ -31,21 +30,20 @@ public class DataModelExpressionParser {
         ParseTreeWalker walker = new ParseTreeWalker();
         DataModelExpressionListener evalByListener = new DataModelExpressionListener();
         walker.walk(evalByListener, tree);
+        // TODO: add attr of peek's fetch is equal to expression's last attr
         return evalByListener.getExpressionQueue();
     }
 
-    public static void main(String[] args){
-        String expression = "C:c.c1-A:a.a1-B:b.b2-C:c.c2-D:d.d2-E:e.e2~F:f.f2-G:g.g1";
+    public static void main(String[] args) {
+//        String expression = "C:c.c1-A:a.a1-B:b.b2-C:c.c2-D:d.d2-E:e.e2~F:f.f2-G:g.g1";
+        String expression = "F:f~(a2)A:a.a1-B:b.b2-D:d.d2";
         DataModelExpressionParser dmeParser = new DataModelExpressionParser();
-        Queue<DataModelExpressionHelper> resultQueue = null;
+        Queue<DataModelExpressionDto> resultQueue = null;
         try {
             resultQueue = dmeParser.parse(expression);
         } catch (WecubeCoreException ex) {
             System.out.println(ex.getMessage());
         }
         assert resultQueue != null;
-        while (!resultQueue.isEmpty()){
-            System.out.println(resultQueue.remove().toString());
-        }
     }
 }
