674a724e24ab51522d3ae53a016fbd5d1c30dee3
Benjamin H Q Li <benjaminli@webank.com>
2019-11-06 09:23:52 +0800

    494 dynamic model support - temp commit for laptop transit

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/PluginPackageDataModelService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/PluginPackageDataModelService.java
@@ -1,6 +1,8 @@
 package com.webank.wecube.platform.core.service;
 
+import com.webank.wecube.platform.core.domain.plugin.PluginPackageDataModel;
 import com.webank.wecube.platform.core.dto.PluginPackageAttributeDto;
+import com.webank.wecube.platform.core.dto.PluginPackageDataModelDto;
 import com.webank.wecube.platform.core.dto.PluginPackageEntityDto;
 
 import java.util.EnumMap;
@@ -10,11 +12,15 @@ public interface PluginPackageDataModelService {
 
     List<PluginPackageEntityDto> register(List<PluginPackageEntityDto> pluginModelEntityDtos);
 
+    PluginPackageDataModelDto register(PluginPackageDataModelDto pluginPackageDataModelDto);
+
     List<PluginPackageEntityDto> overview();
 
 //    List<PluginPackageEntityDto> packageView(String packageName, String version);
 
     List<PluginPackageEntityDto> packageView(int packageId);
+
+    PluginPackageDataModelDto pullDynamicDataModel(int pluginPackageId);
 
 //    void deleteModel(String packageName, String version);
 
