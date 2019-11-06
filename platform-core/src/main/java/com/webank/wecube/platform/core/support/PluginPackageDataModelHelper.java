e04f3d0e137f9f44e1f222de087e1b61c4a9ec91
Benjamin H Q Li <benjamin.h.q.li@outlook.com>
2019-11-07 00:55:59 +0800

    494 dynamic model support

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/PluginPackageDataModelHelper.java
@@ -0,0 +1,10 @@
+package com.webank.wecube.platform.core.support;
+
+import com.webank.wecube.platform.core.domain.plugin.PluginPackageDataModel;
+
+public class PluginPackageDataModelHelper {
+
+    public static boolean isDataModelSameAsAnother(PluginPackageDataModel dataModel1, PluginPackageDataModel dataModel2) {
+        return false;
+    }
+}
