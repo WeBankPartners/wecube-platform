24819b1edf6bfc21ab2a6c4f4716e5881fc3e27f
haixinhuang <huanghaixin@126.com>
2019-11-07 20:03:18 +0800

    #544 Sort main-menu by ID from menu_items table, and sort sub-menu by ID from plugin_package_menus table

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/UserManagementServiceTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/UserManagementServiceTest.java
@@ -1,11 +1,16 @@
 package com.webank.wecube.platform.core.service;
 
 import com.webank.wecube.platform.core.DatabaseBasedTest;
+import com.webank.wecube.platform.core.domain.plugin.PluginPackage;
+import com.webank.wecube.platform.core.domain.plugin.PluginPackageMenu;
 import com.webank.wecube.platform.core.dto.MenuItemDto;
+import com.webank.wecube.platform.core.jpa.PluginPackageRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +18,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class UserManagementServiceTest extends DatabaseBasedTest {
     @Autowired
     UserManagerService userManagerService;
+    @Autowired
+    PluginPackageRepository pluginPackageRepository;
 
     @Test
-    public void sortMenusByCodeShouldSuccess() {
+    public void sortMenusByIdShouldSuccess() {
         boolean sortByMenuCodeFailed = false;
         for (int i = 0; i < 10; i++) {
-            List<MenuItemDto> menusDtos = userManagerService.getAllMenus();
-            if (menusDtos.get(menusDtos.size() - 1).getCode().compareTo(menusDtos.get(0).getCode()) < 0) {
+            List<MenuItemDto> menusDtos = userManagerService.getAllSysMenus();
+            if (menusDtos.get(menusDtos.size() - 1).getId().compareTo(menusDtos.get(0).getId()) < 0) {
                 sortByMenuCodeFailed = true;
             }
         }
         assertThat(sortByMenuCodeFailed).isEqualTo(false);
+    }
+
+    @Test
+    public void sortSubMenusByIdShouldSuccess() {
+        Optional<PluginPackage> pluginPackage = pluginPackageRepository.findById(25);
+        Set<PluginPackageMenu> packageMenus = pluginPackage.get().getPluginPackageMenus();
+        List<PluginPackageMenu> menusDtos = userManagerService.sortPluginPackageMenusById(packageMenus);
+        if (menusDtos.size() > 0) {
+            assertThat(menusDtos.get(menusDtos.size() - 1).getId().compareTo(menusDtos.get(0).getId()))
+                    .isGreaterThanOrEqualTo(0);
+        }
     }
 }
