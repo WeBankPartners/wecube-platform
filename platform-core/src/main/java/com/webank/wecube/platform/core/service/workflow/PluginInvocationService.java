21bbeaa58446b4ad3627504d6153359fe67f94a1
gavin2lee <gavin2lee@163.com>
2019-11-07 20:40:23 +0800

    #397 add workflow edit overview

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/PluginInvocationService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/PluginInvocationService.java
@@ -13,8 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
+import com.webank.wecube.platform.core.domain.SystemVariable;
+import com.webank.wecube.platform.core.domain.plugin.PluginConfig;
 import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterface;
 import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterfaceParameter;
+import com.webank.wecube.platform.core.domain.plugin.PluginInstance;
 import com.webank.wecube.platform.core.entity.workflow.ProcExecBindingEntity;
 import com.webank.wecube.platform.core.entity.workflow.ProcInstInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
@@ -27,7 +30,10 @@ import com.webank.wecube.platform.core.jpa.workflow.TaskNodeExecParamRepository;
 import com.webank.wecube.platform.core.jpa.workflow.TaskNodeInstInfoRepository;
 import com.webank.wecube.platform.core.model.workflow.PluginInvocationCommand;
 import com.webank.wecube.platform.core.model.workflow.PluginInvocationResult;
+import com.webank.wecube.platform.core.service.PluginInstanceService;
+import com.webank.wecube.platform.core.service.SystemVariableService;
 import com.webank.wecube.platform.core.service.plugin.PluginConfigService;
+import com.webank.wecube.platform.core.service.workflow.PluginInvocationProcessor.PluginInterfaceInvocationContext;
 import com.webank.wecube.platform.core.service.workflow.PluginInvocationProcessor.PluginInterfaceInvocationResult;
 import com.webank.wecube.platform.core.service.workflow.PluginInvocationProcessor.PluginInvocationOperation;
 import com.webank.wecube.platform.core.support.plugin.PluginServiceStub;
@@ -35,6 +41,10 @@ import com.webank.wecube.platform.core.support.plugin.PluginServiceStub;
 @Service
 public class PluginInvocationService {
     private static final Logger log = LoggerFactory.getLogger(PluginInvocationService.class);
+
+    private static final String MAPPING_TYPE_CONTEXT = "context";
+    private static final String MAPPING_TYPE_ENTITY = "entity";
+    private static final String MAPPING_TYPE_SYSTEM_VARIABLE = "system_variable";
 
     @Autowired
     private PluginInvocationResultService pluginInvocationResultService;
@@ -59,9 +69,15 @@ public class PluginInvocationService {
 
     @Autowired
     private ProcExecBindingRepository procExecBindingRepository;
-    
+
     @Autowired
     private TaskNodeExecParamRepository taskNodeExecParamRepository;
+
+    @Autowired
+    private SystemVariableService systemVariableService;
+
+    @Autowired
+    private PluginInstanceService pluginInstanceService;
 
     public void invokePluginInterface(PluginInvocationCommand cmd) {
         if (log.isInfoEnabled()) {
@@ -117,42 +133,156 @@ public class PluginInvocationService {
         Set<PluginConfigInterfaceParameter> configInterfaceParams = pluginConfigInterface.getInputParameters();
 
         // TODO
-
         List<Map<String, Object>> pluginParameters = new ArrayList<>();
         for (ProcExecBindingEntity nodeObjectBinding : nodeObjectBindings) {
             // TODO
-            String objectId = nodeObjectBinding.getObjectId();
-            // TODO
-
+            String entityId = nodeObjectBinding.getEntityId();
+            
+            // TODO to call data route service to get entity
             Map<String, List<Object>> parsedParamValues = new HashMap<String, List<Object>>();
             for (PluginConfigInterfaceParameter param : configInterfaceParams) {
-                // TODO get from dataroute
+                // TODO get from data route
                 String paramName = param.getName();
+                String paramType = param.getDataType();
+
                 List<Object> objectVals = new ArrayList<Object>();
+                //
+                String mappingType = param.getMappingType();
+                // TODO get value from entity expression
+                if (MAPPING_TYPE_ENTITY.equals(mappingType)) {
+
+                }
+
+                // TODO get value from execution context
+                if (MAPPING_TYPE_CONTEXT.equals(mappingType)) {
+                    
+                }
+
+                // TODO get value from system variable
+                if (MAPPING_TYPE_SYSTEM_VARIABLE.equals(mappingType)) {
+                    Integer svId = param.getMappingSystemVariableId();
+                    SystemVariable sVariable = systemVariableService.getSystemVariableById(svId);
+
+                    if (sVariable == null && "Y".equals(param.getRequired())) {
+                        log.error("variable is null but is mandatory for {}", paramName);
+                        throw new WecubeCoreException("Variable is null but mandatory.");
+                    }
+
+                    String sVal = sVariable.getValue();
+                    if (StringUtils.isBlank(sVal)) {
+                        sVal = sVariable.getDefaultValue();
+                    }
+
+                    if (StringUtils.isBlank(sVal) && "Y".equals(param.getRequired())) {
+                        log.error("variable is blank but is mandatory for {}", paramName);
+                        throw new WecubeCoreException("Variable is blank but mandatory.");
+                    }
+                    
+                    Object finalInputParam = null;
+                    if("int".equals(paramType)){
+                        finalInputParam = Integer.parseInt(sVal);
+                    }else{
+                        finalInputParam = sVal;
+                    }
+                    
+                    objectVals.add(finalInputParam);
+                }
+
                 parsedParamValues.put(paramName, objectVals);
 
             }
         }
-        
-        
 
-        String interfacePath = "";
-        String instanceHost = "";
+        PluginInstance pluginInstance = getAvailablePluginInstance(pluginConfigInterface);
+        String interfacePath = pluginConfigInterface.getPath();
+        String instanceHost = String.format("%s://%s:%s", pluginConfigInterface.getHttpMethod(),
+                pluginInstance.getHost(), pluginInstance.getPort());
 
-        PluginInvocationOperation operation = new PluginInvocationOperation();
-        operation.withCallback(this::handlePluginInterfaceInvocationResult)
-                .withPluginServiceStub(this.pluginServiceStub).withPluginParameters(pluginParameters)
-                .withInstanceHost(instanceHost).withInterfacePath(interfacePath);
+        PluginInterfaceInvocationContext ctx = new PluginInterfaceInvocationContext();
+        ctx.setNodeObjectBindings(nodeObjectBindings);
+        ctx.setPluginConfigInterface(pluginConfigInterface);
+        ctx.setProcInstEntity(procInstEntity);
+        ctx.setTaskNodeInstEntity(taskNodeInstEntity);
+
+        PluginInvocationOperation operation = new PluginInvocationOperation() //
+                .withCallback(this::handlePluginInterfaceInvocationResult) //
+                .withPluginServiceStub(this.pluginServiceStub) //
+                .withPluginParameters(pluginParameters) //
+                .withInstanceHost(instanceHost) //
+                .withInterfacePath(interfacePath) //
+                .withPluginInterfaceInvocationContext(ctx);
 
         pluginInvocationProcessor.process(operation);
     }
-    
-    protected void preSavePluginInputParameters(List<Map<String, Object>> pluginParameters){
+
+    private PluginInstance getAvailablePluginInstance(PluginConfigInterface intf) {
+        PluginConfig config = intf.getPluginConfig();
+        String pluginName = config.getName();
+
+        List<PluginInstance> instances = pluginInstanceService.getRunningPluginInstances(pluginName);
+
+        return instances.get(0);
+
+    }
+
+    private PluginConfigInterfaceParameter findInterfaceParameterWithParamName(
+            Set<PluginConfigInterfaceParameter> configInterfaceParams, String paramName) {
+        PluginConfigInterfaceParameter result = null;
+
+        for (PluginConfigInterfaceParameter p : configInterfaceParams) {
+            if (paramName.equals(p.getName())) {
+                result = p;
+                break;
+            }
+        }
+
+        if (result == null) {
+            throw new RuntimeException();
+        }
+
+        return result;
+    }
+
+    protected String asString(Object val, String sType) {
+        if (val == null) {
+            return null;
+        }
+        if (val instanceof String && "str".equals(sType)) {
+            return (String) val;
+        }
+
+        if (val instanceof Integer && "int".equals(sType)) {
+            return String.valueOf(val);
+        }
+
+        // TODO
+        return val.toString();
+
+    }
+
+    protected Object fromString(String val, String sType) {
+        if ("str".equals(sType)) {
+            return val;
+        }
+
+        if (StringUtils.isBlank(val)) {
+            return null;
+        }
+
+        if ("int".equals(sType)) {
+            return Integer.parseInt(val);
+        }
+
+        // TODO
+        return val;
+    }
+
+    protected void preSavePluginInputParameters(List<Map<String, Object>> pluginParameters) {
         TaskNodeExecParamEntity entity = new TaskNodeExecParamEntity();
-        entity.setDataType("");
+        entity.setParamDataType("");
         entity.setParamName("");
-        
-        //TODO
+
+        // TODO
         taskNodeExecParamRepository.save(entity);
     }
 
@@ -161,7 +291,11 @@ public class PluginInvocationService {
             log.debug("handle plugin interface invocation result");
         }
 
+        PluginInterfaceInvocationContext ctx = pluginInvocationResult.getPluginInterfaceInvocationContext();
+        // TODO
+
         PluginInvocationResult result = new PluginInvocationResult();
+        // TODO
         pluginInvocationResultService.responsePluginInterfaceInvocation(result);
     }
 
