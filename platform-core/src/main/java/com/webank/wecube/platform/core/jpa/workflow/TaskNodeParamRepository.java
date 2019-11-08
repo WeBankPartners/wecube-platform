c3328b6045fd5bf8775a89ac42aa3d5acd8bce3c
gavin2lee <gavin2lee@163.com>
2019-11-08 13:46:41 +0800

    #397 add task node param repo

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeParamRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeParamRepository.java
@@ -3,11 +3,19 @@ package com.webank.wecube.platform.core.jpa.workflow;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeParamEntity;
 
 public interface TaskNodeParamRepository extends JpaRepository<TaskNodeParamEntity, String> {
 
     List<TaskNodeParamEntity> findAllByProcDefId(String procDefId);
+
     List<TaskNodeParamEntity> findAllByProcDefIdAndTaskNodeDefId(String procDefId, String taskNodeDefId);
+
+    @Query("select t from TaskNodeParamEntity t "
+            + " where t.taskNodeDefId = :taskNodeDefId and t.paramName = :paramName")
+    TaskNodeParamEntity findOneByTaskNodeDefIdAndParamName(@Param("taskNodeDefId") String taskNodeDefId,
+            @Param("paramName") String paramName);
 }
