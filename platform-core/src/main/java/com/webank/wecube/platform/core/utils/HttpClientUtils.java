bd699ab8e3cb431292fea281523e796c06e0d9b1
HoweChen <yuhao.howe.chen@gmail.com>
2019-11-08 17:29:26 +0800

    #457 update HttpClientUtils

--- platform-core/src/main/java/com/webank/wecube/platform/core/utils/HttpClientUtils.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/utils/HttpClientUtils.java
@@ -1,36 +1,29 @@
 package com.webank.wecube.platform.core.utils;
 
-import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import org.springframework.http.*;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 public class HttpClientUtils {
 
     /**
      * Send get request to url with params
      *
-     * @param url     target url
-     * @param params  parameters
-     * @param headers request headers
+     * @param requestUri target uri
+     * @param headers    request headers
      * @return String
      */
-    public static ResponseEntity<String> sendGetRequestWithParamMap(String url, MultiValueMap<String, String> params, HttpHeaders headers) {
+    public static ResponseEntity<String> sendGetRequestWithParamMap(String requestUri, HttpHeaders headers) {
         RestTemplate client = new RestTemplate();
+
 
         HttpMethod method = HttpMethod.GET;
         // set content type as form
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        // combine param map and headers and one request entity
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // setup http request entity
+        HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+
         // send request and exchange the response to target class
-        return client.exchange(url, method, requestEntity, String.class);
+        return client.exchange(requestUri, method, requestEntity, String.class);
     }
 
     /**
@@ -39,7 +32,7 @@ public class HttpClientUtils {
      * @param url target url
      * @return String
      */
-    public static ResponseEntity<String> sendGetRequestWithoutParam(String url) throws IOException {
+    public static ResponseEntity<String> sendGetRequestWithoutParam(String url) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(url, String.class);
     }
