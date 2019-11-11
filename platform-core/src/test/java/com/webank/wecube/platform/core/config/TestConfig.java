7a7bfca3575e687b79ca5b927f185d2251973f88
Jun Chen <junchensz@foxmail.com>
2019-11-11 10:41:19 +0800

    add test config for H2 db query in testing

---
+++ platform-core/src/test/java/com/webank/wecube/platform/core/config/TestConfig.java
@@ -0,0 +1,34 @@
+package com.webank.wecube.platform.core.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.embedded.ConnectionProperties;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseConfigurer;
+
+@Configuration
+public class TestConfig {
+    private static Logger logger = LoggerFactory.getLogger(TestConfig.class);
+
+    @Bean(name = "h2server", destroyMethod = "stop")
+    public Server getH2Server() {
+        Server h2Server;
+        try {
+            h2Server = Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082");
+            h2Server.start();
+            return h2Server;
+        } catch (SQLException e) {
+            logger.info("Fail to start H2 server.", e);
+        }
+        return null;
+    }
+
+}
