93248f19346b7df678b7e1566d325cf282b3ae4b
gavin2lee <gavin2lee@163.com>
2019-11-11 21:32:13 +0800

    #397 refactor plugin invocation

---
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/InputParametersParseTest.java
@@ -0,0 +1,12 @@
+package com.webank.wecube.platform.core.service;
+
+import org.junit.Test;
+
+public class InputParametersParseTest {
+
+    
+    @Test
+    public void testParametersParse(){
+        
+    }
+}
