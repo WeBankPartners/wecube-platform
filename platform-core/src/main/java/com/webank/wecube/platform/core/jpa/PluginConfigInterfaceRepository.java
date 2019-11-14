96a9bd71414a19a4c7c6850d2a78a2da3cea2867
Benjamin H Q Li <benjamin.h.q.li@outlook.com>
2019-11-14 18:30:41 +0800

    #593 plugins query

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginConfigInterfaceRepository.java
@@ -0,0 +1,15 @@
+package com.webank.wecube.platform.core.jpa;
+
+import com.webank.wecube.platform.core.domain.plugin.PluginConfig;
+import com.webank.wecube.platform.core.domain.plugin.PluginConfig.Status;
+import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterface;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PluginConfigInterfaceRepository extends CrudRepository<PluginConfigInterface, Integer> {
+
+    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_Status(Status status);
+
+}
