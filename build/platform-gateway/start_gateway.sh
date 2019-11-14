11a8be943e1c4645911f52e2f13544c26ee5d52d
Jun Chen <junchensz@foxmail.com>
2019-11-14 16:27:58 +0800

    #537 config env variable for gateway

--- build/platform-gateway/start_gateway.sh
+++ build/platform-gateway/start_gateway.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 mkdir -p /log
 java -jar /application/platform-gateway.jar  --server.address=0.0.0.0 --server.port=8080 \
-#--platform.gateway.route.route-config-server=${GATEWAY_ROUTE_CONFIG_SERVER} \
-#--platform.gateway.route.route-config-uri=${GATEWAY_ROUTE_CONFIG_URI} \
-#--platform.gateway.route.route-config-access-key=${GATEWAY_ROUTE_ACCESS_KEY} \
-#--spring.cloud.gateway.routes.id=platform-core \
-#--spring.cloud.gateway.routes.uri=${GATEWAY_ROUTES_PLATFORM_CORE_URI} \
+--platform.gateway.route.route-config-server=${GATEWAY_ROUTE_CONFIG_SERVER} \
+--platform.gateway.route.route-config-uri=${GATEWAY_ROUTE_CONFIG_URI} \
+--platform.gateway.route.route-config-access-key=${GATEWAY_ROUTE_ACCESS_KEY} \
+--spring.cloud.gateway.routes[0].id=platform-core \
+--spring.cloud.gateway.routes[0].uri=${GATEWAY_ROUTES_PLATFORM_CORE_URI} \
+--spring.cloud.gateway.routes[0].predicates[0].Path=${GATEWAY_ROUTES_PLATFORM_CORE_PREDICATE_PATH} \
+--spring.cloud.gateway.routes[0].filters[0].DynamicRoute=${GATEWAY_ROUTES_PLATFORM_CORE_FILTERS_DYNROUTE} \
 2>&1 >>/log/wecube-gateway.log 
