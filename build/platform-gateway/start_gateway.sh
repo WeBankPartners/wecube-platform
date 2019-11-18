22cbca5c6525c162bfe7936ee375ac4b0cbded4e
Jun Chen <junchensz@foxmail.com>
2019-11-18 10:44:19 +0800

    #537 just keep uri argument for gateway start script

--- build/platform-gateway/start_gateway.sh
+++ build/platform-gateway/start_gateway.sh
@@ -6,6 +6,4 @@ java -jar /application/platform-gateway.jar  --server.address=0.0.0.0 --server.p
 --platform.gateway.route.route-config-access-key=${GATEWAY_ROUTE_ACCESS_KEY} \
 --spring.cloud.gateway.routes[0].id=platform-core \
 --spring.cloud.gateway.routes[0].uri=${GATEWAY_ROUTES_PLATFORM_CORE_URI} \
---spring.cloud.gateway.routes[0].predicates[0].Path=${GATEWAY_ROUTES_PLATFORM_CORE_PREDICATE_PATH} \
---spring.cloud.gateway.routes[0].filters[0].DynamicRoute=${GATEWAY_ROUTES_PLATFORM_CORE_FILTERS_DYNROUTE} \
 2>&1 >>/log/wecube-gateway.log 
