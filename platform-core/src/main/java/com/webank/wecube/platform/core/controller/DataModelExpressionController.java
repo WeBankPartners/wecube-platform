0fe2768799e390da714d8d128a810855fefd8fe8
HoweChen <yuhao.howe.chen@gmail.com>
2019-11-20 18:05:56 +0800

    #648 add data model expresion (DME) controller with target entity query function.

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/DataModelExpressionController.java
@@ -0,0 +1,27 @@
+package com.webank.wecube.platform.core.controller;
+
+import com.webank.wecube.platform.core.domain.JsonResponse;
+import com.webank.wecube.platform.core.service.DataModelExpressionServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+public class DataModelExpressionController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private DataModelExpressionServiceImpl dataModelExpressionService;
+
+    @GetMapping("/dme/target-entity")
+    @ResponseBody
+    public JsonResponse getRefByIdInfoByPackageNameAndEntityName(
+            @RequestParam(value = "package") String packageName,
+            @RequestParam(value = "entity") String entityName
+    ) {
+        return JsonResponse.okayWithData(dataModelExpressionService.targetEntityQuery(packageName, entityName));
+    }
+
+}
