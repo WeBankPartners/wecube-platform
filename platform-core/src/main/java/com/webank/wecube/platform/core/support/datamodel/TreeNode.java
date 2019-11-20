b85f2b2cad1d313faaacf1f19e8ee27bb443978f
HoweChen <yuhao.howe.chen@gmail.com>
2019-11-21 00:18:30 +0800

    #649 add TreeNode class

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/datamodel/TreeNode.java
@@ -0,0 +1,54 @@
+package com.webank.wecube.platform.core.support.datamodel;
+
+import java.util.List;
+
+public class TreeNode {
+    private String packageName;
+    private String entityName;
+    private String rootId;
+    private TreeNode parent;
+    private List<TreeNode> children;
+
+    public TreeNode() {
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public String getRootId() {
+        return rootId;
+    }
+
+    public void setRootId(String rootId) {
+        this.rootId = rootId;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<TreeNode> children) {
+        this.children = children;
+    }
+}
