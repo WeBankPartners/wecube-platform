0624312ae7165e06dc89bbe95cc970b1ae347ea8
Benjamin H Q Li <benjamin.h.q.li@outlook.com>
2019-11-22 20:30:49 +0800

    #598 orgainze imports

--- platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginMysqlInstance.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/PluginMysqlInstance.java
@@ -1,9 +1,8 @@
 package com.webank.wecube.platform.core.domain.plugin;
 
-import javax.persistence.*;
-
 import com.webank.wecube.platform.core.domain.ResourceItem;
-import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 import static com.webank.wecube.platform.core.utils.Constants.KEY_COLUMN_DELIMITER;
 
