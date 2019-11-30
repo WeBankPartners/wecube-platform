a2a8aaf88498af8f482ccc35492922c76ad3eb8c
HoweChen <yuhao.howe.chen@gmail.com>
2019-11-30 14:58:31 +0800

    #822 use DataModelExpressionOpType to do the logical switch

--- platform-core/src/main/java/com/webank/wecube/platform/core/utils/constant/DataModelExpressionOpType.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/utils/constant/DataModelExpressionOpType.java
@@ -1,24 +1,5 @@
 package com.webank.wecube.platform.core.utils.constant;
 
 public enum DataModelExpressionOpType {
-    ReferenceBy("~"), ReferenceTo("-");
-
-    private String code;
-
-    DataModelExpressionOpType(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public static DataModelExpressionOpType fromCode(String code) throws IllegalArgumentException {
-        for (DataModelExpressionOpType dataModelDataType : values()) {
-            if (dataModelDataType.getCode().equals(code)) {
-                return dataModelDataType;
-            }
-        }
-        throw new IllegalArgumentException(java.lang.String.format("Cannot find the data model expression operation type from code %s", code));
-    }
+    LINK_FETCH, ENTITY_FETCH, REF_TO, REF_BY
 }
