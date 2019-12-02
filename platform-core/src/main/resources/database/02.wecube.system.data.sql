3f5f37b533cf9a903066e1fafe104fa5d56faf6f
haixinhuang <huanghaixin@126.com>
2019-12-02 18:46:56 +0800

    #746 Fix init data script

--- platform-core/src/main/resources/database/02.wecube.system.data.sql
+++ platform-core/src/main/resources/database/02.wecube.system.data.sql
@@ -15,8 +15,7 @@ insert into menu_items (id,parent_code,code,source,menu_order,description) value
 ,('ADMIN:ADMIN_RESOURCES_MANAGEMENT','ADMIN','ADMIN_RESOURCES_MANAGEMENT','SYSTEM', 13, '');
 
 
-INSERT INTO `system_variables` (`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES (NULL, 'CORE_ADDR', NULL, 'http://127.0.0.1:19090', 'global', NULL, 0, 'active');
-INSERT INTO `system_variables` (`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES (NULL, 'BASE_MOUNT_PATH', NULL, '/data', 'global', NULL, 0, 'active');
-INSERT INTO `system_variables` (`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES (NULL, 'CMDB_URL', NULL, 'http://127.0.0.1:19090', 'global', NULL, 0, 'active');
-INSERT INTO `system_variables` (`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES (NULL, 'CALLBACK_URL', NULL, 'http://127.0.0.1:19090/workflow/callback', 'global', NULL, 0, 'active');
-
+INSERT INTO `system_variables` (`id`,`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES ('global:CORE_ADDR', NULL, 'CORE_ADDR', NULL, 'http://127.0.0.1:19090', 'global', NULL, 0, 'active');
+INSERT INTO `system_variables` (`id`,`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES ('global:BASE_MOUNT_PATH', NULL, 'BASE_MOUNT_PATH', NULL, '/data', 'global', NULL, 0, 'active');
+INSERT INTO `system_variables` (`id`,`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES ('global:CMDB_URL', NULL, 'CMDB_URL', NULL, 'http://127.0.0.1:19090', 'global', NULL, 0, 'active');
+INSERT INTO `system_variables` (`id`,`plugin_package_id`, `name`, `value`, `default_value`, `scope_type`, `scope_value`, `seq_no`, `status`) VALUES ('global:CALLBACK_URL', NULL, 'CALLBACK_URL', NULL, 'http://127.0.0.1:19090/workflow/callback', 'global', NULL, 0, 'active');
