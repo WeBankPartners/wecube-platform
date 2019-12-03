ed1ac7ff6f53911da4377b46880197a986a7aead
gavin2lee <gavin2lee@163.com>
2019-12-03 10:55:29 +0800

    #775 async invocation of wf

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeExecParamRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/TaskNodeExecParamRepository.java
@@ -3,12 +3,20 @@ package com.webank.wecube.platform.core.jpa.workflow;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeExecParamEntity;
 
 public interface TaskNodeExecParamRepository extends JpaRepository<TaskNodeExecParamEntity, Integer> {
 
-    List<TaskNodeExecParamEntity> findAllByRequestIdAndParamNameAndParamType(String requestId, String paramName,
-            String paramType);
+    @Query("select t from TaskNodeExecParamEntity t "
+            + " where t.requestId = :requestId and t.paramName = :paramName and t.paramType = :paramType")
+    List<TaskNodeExecParamEntity> findAllByRequestIdAndParamNameAndParamType(@Param("requestId") String requestId,
+            @Param("paramName") String paramName, @Param("paramType") String paramType);
+
+    @Query("select t from TaskNodeExecParamEntity t where t.requestId = :requestId and t.paramType = :paramType ")
+    List<TaskNodeExecParamEntity> findAllByRequestIdAndParamType(@Param("requestId") String requestId,
+            @Param("paramType") String paramType);
 
 }
