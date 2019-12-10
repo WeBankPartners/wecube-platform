967b366c538879a941c26b2757e9076d38d1684f
HoweChen <yuhao.howe.chen@gmail.com>
2019-12-10 15:58:59 +0800

    #877 update RoleMenu

--- platform-core/src/main/java/com/webank/wecube/platform/core/domain/RoleMenu.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/domain/RoleMenu.java
@@ -3,26 +3,59 @@ package com.webank.wecube.platform.core.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Data
-@Entity @Table(name = "role_menu")
+@Entity
+@Table(name = "role_menu")
 public class RoleMenu {
 
-    @Id @GeneratedValue
-    private Integer id;
+    @Id
+    private String id;
 
     @Column(name = "role_id")
-    private Integer roleId;
+    private Long roleId;
 
-    @ManyToOne @JoinColumn(name = "menu_id")
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
     private MenuItem menuItem;
 
+    public RoleMenu(Long roleId, MenuItem menuItem) {
+        this.roleId = roleId;
+        this.menuItem = menuItem;
+    }
+
     public RoleMenu() {
-        this(null);
     }
 
-    public RoleMenu(Integer id) {
-        this.setId(id);
+    @PrePersist
+    public void initGuid() {
+        if (this.id == null | "".equals(this.id)) {
+            this.id = Objects.requireNonNull(this.roleId, "The [roleId] cannot be NULL while persisting [role_menu]").toString()
+                    + Objects.requireNonNull(this.menuItem, "The [menuItem] cannot be NULL while persisting [role_menu]").getCode();
+        }
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
 }
