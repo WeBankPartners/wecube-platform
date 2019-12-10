d38df91f0c6546679f6966cd83deb0f18e0e7f73
HoweChen <yuhao.howe.chen@gmail.com>
2019-12-10 15:58:40 +0800

    #877 Separate User and Role management controllers.

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/UserManagementController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/UserManagementController.java
@@ -6,9 +6,11 @@ import com.webank.wecube.platform.core.service.user.UserManagementServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
+/**
+ * @author howechen
+ */
 @RestController
 @RequestMapping("v1/")
 public class UserManagementController {
@@ -52,81 +54,12 @@ public class UserManagementController {
         }
     }
 
-    @PostMapping("/roles/create")
-    @ResponseBody
-    public CommonResponseDto createRole(@RequestHeader(value = "Authorization") String token,
-                                        @RequestBody Map<String, Object> requestBody) {
-        try {
-            return userManagementService.createRole(token, requestBody);
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-
-    }
-
-    @GetMapping("/roles/retrieve")
-    @ResponseBody
-    public CommonResponseDto retrieveRole(@RequestHeader(value = "Authorization") String token) {
-        try {
-            return userManagementService.retrieveRole(token);
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-    }
-
-    @DeleteMapping("/roles/{role-id}/delete")
-    @ResponseBody
-    public CommonResponseDto deleteRole(@RequestHeader(value = "Authorization") String token,
-                                        @PathVariable("role-id") Long id) {
-        try {
-            return userManagementService.deleteRole(token, id);
-        } catch (WecubeCoreException e) {
-            return CommonResponseDto.error(e.getMessage());
-        }
-    }
-
     @GetMapping("/users/{user-name}/roles")
     @ResponseBody
     public CommonResponseDto getRolesByUsername(@RequestHeader(value = "Authorization") String token,
                                                 @PathVariable(value = "user-name") String userName) {
         try {
             return userManagementService.getRolesByUserName(token, userName);
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-    }
-
-    @GetMapping("/roles/{role-id}/users")
-    @ResponseBody
-    public CommonResponseDto getUsersByRoleId(@RequestHeader(value = "Authorization") String token,
-                                              @PathVariable(value = "role-id") Long roleId) {
-        try {
-            return userManagementService.getUsersByRoleId(token, roleId);
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-
-    }
-
-    @PostMapping("/roles/{role-id}/users/grant")
-    @ResponseBody
-    public CommonResponseDto grantRoleToUsers(@RequestHeader(value = "Authorization") String token,
-                                              @PathVariable(value = "role-id") Long roleId,
-                                              @RequestBody List<Object> userIdList) {
-        try {
-            return userManagementService.grantRoleToUsers(token, roleId, userIdList);
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-    }
-
-    @DeleteMapping("/roles/{role-id}/users/revoke")
-    @ResponseBody
-    public CommonResponseDto revokeRoleFromUsers(@RequestHeader(value = "Authorization") String token,
-                                                 @PathVariable(value = "role-id") Long roleId,
-                                                 @RequestBody List<Object> requestBody) {
-        try {
-            return userManagementService.revokeRoleFromUsers(token, roleId, requestBody);
         } catch (WecubeCoreException ex) {
             return CommonResponseDto.error(ex.getMessage());
         }
