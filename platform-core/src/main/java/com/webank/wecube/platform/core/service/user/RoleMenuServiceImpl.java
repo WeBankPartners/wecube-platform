c2b64dd5788a375af76938c4ea8e230a6b62761b
HoweChen <yuhao.howe.chen@gmail.com>
2019-12-11 11:11:52 +0800

    #877 update RoleMenuServiceImpl, retrieve code from both system and package menu tables

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/user/RoleMenuServiceImpl.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/user/RoleMenuServiceImpl.java
@@ -1,15 +1,21 @@
 package com.webank.wecube.platform.core.service.user;
 
+import com.webank.wecube.platform.core.domain.MenuItem;
 import com.webank.wecube.platform.core.domain.RoleMenu;
+import com.webank.wecube.platform.core.domain.plugin.PluginPackageMenu;
+import com.webank.wecube.platform.core.dto.MenuItemDto;
 import com.webank.wecube.platform.core.dto.user.RoleMenuDto;
 import com.webank.wecube.platform.core.jpa.MenuItemRepository;
+import com.webank.wecube.platform.core.jpa.PluginPackageMenuRepository;
 import com.webank.wecube.platform.core.jpa.user.RoleMenuRepository;
+import com.webank.wecube.platform.core.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -21,11 +27,18 @@ public class RoleMenuServiceImpl implements RoleMenuService {
 
     private RoleMenuRepository roleMenuRepository;
     private MenuItemRepository menuItemRepository;
+    private PluginPackageMenuRepository pluginPackageMenuRepository;
+    private MenuService menuService;
 
     @Autowired
-    public RoleMenuServiceImpl(RoleMenuRepository roleMenuRepository, MenuItemRepository menuItemRepository) {
+    public RoleMenuServiceImpl(RoleMenuRepository roleMenuRepository,
+                               MenuItemRepository menuItemRepository,
+                               PluginPackageMenuRepository pluginPackageMenuRepository,
+                               MenuService menuService) {
         this.roleMenuRepository = roleMenuRepository;
         this.menuItemRepository = menuItemRepository;
+        this.pluginPackageMenuRepository = pluginPackageMenuRepository;
+        this.menuService = menuService;
     }
 
     /**
@@ -37,8 +50,23 @@ public class RoleMenuServiceImpl implements RoleMenuService {
     @Override
     public RoleMenuDto retrieveMenusByRoleId(Long roleId) {
         List<RoleMenu> roleMenuList = this.roleMenuRepository.findAllByRoleId(roleId);
-        List<String> menuCodeList = new ArrayList<>();
-        roleMenuList.forEach(roleMenu -> menuCodeList.add(roleMenu.getMenuItem().getCode()));
+        List<MenuItemDto> menuCodeList = new ArrayList<>();
+        roleMenuList.forEach(roleMenu -> {
+            String menuCode = roleMenu.getMenuCode();
+            // sys menu
+            MenuItem sysMenu = this.menuItemRepository.findByCode(menuCode);
+            // use {if sysMenu is null} to judge if this is a sys menu or package menu
+            if (null != sysMenu) {
+                menuCodeList.add(MenuItemDto.fromSystemMenuItem(sysMenu));
+            } else {
+                // package menu
+                Optional<List<PluginPackageMenu>> allActivateMenuByCode = this.pluginPackageMenuRepository.findAllActivateMenuByCode(menuCode);
+                allActivateMenuByCode.ifPresent(pluginPackageMenus -> pluginPackageMenus.forEach(pluginPackageMenu -> {
+                    menuCodeList.add(this.menuService.transferPackageMenuToMenuItemDto(pluginPackageMenu));
+                }));
+            }
+
+        });
         return new RoleMenuDto(roleId, menuCodeList);
     }
 
@@ -53,10 +81,9 @@ public class RoleMenuServiceImpl implements RoleMenuService {
     public RoleMenuDto updateRoleToMenusByRoleId(Long roleId, List<String> menuCodeList) {
         List<RoleMenu> roleMenuList = this.roleMenuRepository.findAllByRoleId(roleId);
 
-        //
         // current menuCodeList - new menuCodeList = needToDeleteList
         List<RoleMenu> needToDeleteList = roleMenuList.stream().filter(roleMenu -> {
-            String code = roleMenu.getMenuItem().getCode();
+            String code = roleMenu.getMenuCode();
             return !menuCodeList.contains(code);
         }).collect(Collectors.toList());
         if (!needToDeleteList.isEmpty()) {
@@ -67,12 +94,12 @@ public class RoleMenuServiceImpl implements RoleMenuService {
 
         // new menuCodeList - current menuCodeList = needToCreateList
         List<String> needToCreateList;
-        List<String> currentMenuCodeList = roleMenuList.stream().map(roleMenu -> roleMenu.getMenuItem().getCode()).collect(Collectors.toList());
+        List<String> currentMenuCodeList = roleMenuList.stream().map(RoleMenu::getMenuCode).collect(Collectors.toList());
         needToCreateList = menuCodeList.stream().filter(menuCode -> !currentMenuCodeList.contains(menuCode)).collect(Collectors.toList());
 
         if (!needToCreateList.isEmpty()) {
             List<RoleMenu> batchUpdateList = new ArrayList<>();
-            needToCreateList.forEach(menuCode -> batchUpdateList.add(new RoleMenu(roleId, this.menuItemRepository.findByCode(menuCode))));
+            needToCreateList.forEach(menuCode -> batchUpdateList.add(new RoleMenu(roleId, menuCode)));
             this.roleMenuRepository.saveAll(batchUpdateList);
         }
 
