e9b1e9afafbffc881cb8524c9bb214e13f6fc52b
HoweChen <yuhao.howe.chen@gmail.com>
2019-12-13 11:41:02 +0800

    #352 add findAllDeployedOrDraftProcDefsById for subsequent delete feature usage.

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/ProcDefInfoRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/workflow/ProcDefInfoRepository.java
@@ -6,17 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.webank.wecube.platform.core.entity.workflow.ProcDefInfoEntity;
+import org.springframework.data.repository.query.Param;
 
 public interface ProcDefInfoRepository extends JpaRepository<ProcDefInfoEntity, String> {
 
     @Query("select t from ProcDefInfoEntity t "
             + " where t.active = true and (t.status = 'deployed' or t.status = 'draft')")
     List<ProcDefInfoEntity> findAllDeployedOrDraftProcDefs();
-    
+
+    @Query("select t from ProcDefInfoEntity t "
+            + " where t.active = true and t.id=:procId and(t.status = 'deployed' or t.status = 'draft')")
+    List<ProcDefInfoEntity> findAllDeployedOrDraftProcDefsById(@Param("procId") String procId);
+
     @Query("select t from ProcDefInfoEntity t "
             + " where t.active = true and t.status = 'deployed' ")
     List<ProcDefInfoEntity> findAllDeployedProcDefs();
-    
+
 //    @Transactional
 //    @Modifying
 //    @Query("delete from ProcessDefInfoEntity t where t.id = :id")
