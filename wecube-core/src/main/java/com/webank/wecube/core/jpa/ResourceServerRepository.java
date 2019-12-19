5bcfc546ab0b5307026ec3a399a89ce1d5f5fa54
irvinezhao <irvinezhao@gmail.com>
2019-12-19 18:09:17 +0800

    remove wecube core folder

--- wecube-core/src/main/java/com/webank/wecube/core/jpa/ResourceServerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webank.wecube.core.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.webank.wecube.core.domain.ResourceServer;
-
-public interface ResourceServerRepository extends CrudRepository<ResourceServer, Integer> {
-
-}
