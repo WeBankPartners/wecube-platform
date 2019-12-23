240cc0d6ac12f1fe9591d43cbaf652ea133e053d
homiexie <624128114@qq.com>
2019-12-23 15:55:42 +0800

    #1104 support entity filterable

--- wecube-portal/src/pages/collaboration/workflow-orchestration.vue
+++ wecube-portal/src/pages/collaboration/workflow-orchestration.vue
@@ -53,6 +53,8 @@
         <Select
           @on-change="onEntitySelect"
           v-model="currentSelectedEntity"
+          filterable
+          clearable
           style="width: 70%"
         >
           <OptionGroup
@@ -530,7 +532,7 @@ export default {
       }
     },
     onEntitySelect(v) {
-      this.currentSelectedEntity = v;
+      this.currentSelectedEntity = v || "";
       this.rootPkg = this.currentSelectedEntity.split(":")[0];
       this.rootEntity = this.currentSelectedEntity.split(":")[1];
 
