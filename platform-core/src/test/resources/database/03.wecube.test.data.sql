a12a3e7036494f36036ede3a8517914258a16b2b
Benjamin H Q Li <benjamin.h.q.li@outlook.com>
2019-12-24 11:07:05 +0800

    #1010 menu to support chinese and english

--- platform-core/src/test/resources/database/03.wecube.test.data.sql
+++ platform-core/src/test/resources/database/03.wecube.test.data.sql
@@ -4,8 +4,8 @@ insert into menu_items (id,parent_code,code,source,menu_order,description) value
 
 INSERT INTO `plugin_packages` (`id`, `name`, `version`, `status`, `upload_timestamp`, `ui_package_included`) VALUES ('service-mt__v0.6', 'service-mt', 'v0.6', 'REGISTERED', '2019-11-05 03:25:25', '1');
 
-INSERT INTO `plugin_package_menus` (`id`, `plugin_package_id`, `code`, `category`, source, menu_order, `display_name`, `path`) VALUES ('JOBS__JOBS_SERVICE_CATALOG_MANAGEMENT', '25', 'JOBS_SERVICE_CATALOG_MANAGEMENT', 'JOBS', 'PLUGIN', 1, 'Servive Catalog Management', '/service-catalog');
-INSERT INTO `plugin_package_menus` (`id`, `plugin_package_id`, `code`, `category`, source, menu_order, `display_name`, `path`) VALUES ('JOBS__JOBS_TASK_MANAGEMENT', '25', 'JOBS_TASK_MANAGEMENT', 'JOBS', 'PLUGIN', 2, 'Task Management', '/task-management');
+INSERT INTO `plugin_package_menus` (`id`, `plugin_package_id`, `code`, `category`, source, menu_order, `display_name`, `cn_display_name`, `path`) VALUES ('JOBS__JOBS_SERVICE_CATALOG_MANAGEMENT', '25', 'JOBS_SERVICE_CATALOG_MANAGEMENT', 'JOBS', 'PLUGIN', 1, 'Servive Catalog Management', '服务类型管理', '/service-catalog');
+INSERT INTO `plugin_package_menus` (`id`, `plugin_package_id`, `code`, `category`, source, menu_order, `display_name`, `cn_display_name`, `path`) VALUES ('JOBS__JOBS_TASK_MANAGEMENT', '25', 'JOBS_TASK_MANAGEMENT', 'JOBS', 'PLUGIN', 2, 'Task Management', '任务管理', '/task-management');
 
 INSERT INTO `plugin_packages` (`id`, `name`, `version`, `status`, `upload_timestamp`, `ui_package_included`) VALUES ('service-mgmt__v1.0', 'service-mgmt', 'v1.0', 'UNREGISTERED', '2019-11-25 20:31:48', '1');
 INSERT INTO `system_variables` (`id`, `package_name`, `name`, `value`, `default_value`, `scope`, `source`, `status`) VALUES (1, NULL, 'CORE_ADDR', NULL, 'http://127.0.0.1:19090', 'global', 'system', 'active');
