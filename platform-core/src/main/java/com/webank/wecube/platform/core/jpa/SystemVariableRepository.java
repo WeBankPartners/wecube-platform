fdf6d1367ef6613e8c4f93571566eb46304dc6d0
haixinhuang <huanghaixin@126.com>
2019-12-31 15:27:28 +0800

    #1211 - Fixed #1211, add API for return drop-down data of system variable scope

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/SystemVariableRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/SystemVariableRepository.java
@@ -3,12 +3,14 @@ package com.webank.wecube.platform.core.jpa;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.webank.wecube.platform.core.domain.SystemVariable;
 
 public interface SystemVariableRepository extends CrudRepository<SystemVariable, String> {
-    
+
     List<SystemVariable> findAllByStatus(String status);
 
     List<SystemVariable> findAllByScope(String scope);
@@ -19,4 +21,6 @@ public interface SystemVariableRepository extends CrudRepository<SystemVariable,
 
     List<SystemVariable> findAllByScopeAndStatus(String scope, String status);
 
+    @Query(value = "select distinct scope from system_variables", nativeQuery = true)
+    List<String> findDistinctScope();
 }
