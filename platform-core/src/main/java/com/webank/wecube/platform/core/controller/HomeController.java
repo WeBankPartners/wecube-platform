297871b8443331127018eb14668d188a32979fd4
HoweChen <yuhao.howe.chen@gmail.com>
2020-01-03 11:22:51 +0800

    #1168 fetch current user's all usable menus, which is authorized by administrator

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/HomeController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/HomeController.java
@@ -1,8 +1,9 @@
 package com.webank.wecube.platform.core.controller;
 
-import static com.webank.wecube.platform.core.domain.JsonResponse.*;
-import java.security.Principal;
-
+import com.webank.wecube.platform.core.commons.WecubeCoreException;
+import com.webank.wecube.platform.core.domain.JsonResponse;
+import com.webank.wecube.platform.core.dto.MenuItemDto;
+import com.webank.wecube.platform.core.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.webank.wecube.platform.core.domain.JsonResponse;
-import com.webank.wecube.platform.core.service.MenuService;
+import java.security.Principal;
+import java.util.List;
+
+import static com.webank.wecube.platform.core.domain.JsonResponse.error;
+import static com.webank.wecube.platform.core.domain.JsonResponse.okay;
 
 @Controller
 @RequestMapping("/v1")
@@ -20,12 +24,12 @@ public class HomeController {
     @Autowired
     private MenuService menuService;
 
-    @GetMapping(value = { "/", "index.html" })
+    @GetMapping(value = {"/", "index.html"})
     public String index() {
         return "index.html";
     }
 
-    @GetMapping(value = { "login.html" })
+    @GetMapping(value = {"login.html"})
     public String loginPage() {
         return "login.html";
     }
@@ -39,8 +43,14 @@ public class HomeController {
 
     @GetMapping("/my-menus")
     @ResponseBody
-    public JsonResponse getMyMenuItems(Principal principal) {
-        return okay().withData(menuService.getAllMenus());
+    public JsonResponse getMyMenuItems() {
+        List<MenuItemDto> currentUserAllMenus;
+        try {
+            currentUserAllMenus = menuService.getCurrentUserAllMenus();
+        } catch (WecubeCoreException ex) {
+            return error(ex.getMessage());
+        }
+        return okay().withData(currentUserAllMenus);
     }
 
     @GetMapping("/all-menus")
--- platform-core/src/main/java/com/webank/wecube/platform/core/service/MenuService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/MenuService.java
@@ -1,21 +1,23 @@
 package com.webank.wecube.platform.core.service;
 
-import java.util.*;
-
-import com.webank.wecube.platform.core.jpa.PluginPackageMenuRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.google.common.collect.Lists;
+import com.webank.wecube.platform.core.commons.AuthenticationContextHolder;
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import com.webank.wecube.platform.core.domain.MenuItem;
 import com.webank.wecube.platform.core.domain.plugin.PluginPackageMenu;
 import com.webank.wecube.platform.core.dto.MenuItemDto;
 import com.webank.wecube.platform.core.jpa.MenuItemRepository;
+import com.webank.wecube.platform.core.jpa.PluginPackageMenuRepository;
 import com.webank.wecube.platform.core.service.plugin.PluginPackageService;
-
+import com.webank.wecube.platform.core.service.user.RoleMenuServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -30,6 +32,9 @@ public class MenuService {
 
     @Autowired
     private PluginPackageMenuRepository pluginPackageMenuRepository;
+
+    @Autowired
+    private RoleMenuServiceImpl roleMenuService;
 
     public List<MenuItem> getAllSysMenuItems() {
         return Lists.newArrayList(menuItemRepository.findAll());
@@ -52,23 +57,64 @@ public class MenuService {
         List<MenuItemDto> allSysMenus = getAllSysMenus();
         returnMenuDto = new ArrayList<>(allSysMenus);
 
-        Optional<List<PluginPackageMenu>> optionalPluginPackageMenus = pluginPackageMenuRepository.findAndMergePluginMenus();
-        if (optionalPluginPackageMenus.isPresent()) {
-            optionalPluginPackageMenus.get().forEach(packageMenu -> {
-                MenuItem menuItem = menuItemRepository.findByCode(packageMenu.getCategory());
-                if (null == menuItem) {
-                    String msg = String.format("Cannot find system menu item by package menu's category: [%s]",
-                            packageMenu.getCategory());
-                    log.error(msg);
-                    throw new WecubeCoreException(msg);
-                }
-                MenuItemDto packageMenuDto = MenuItemDto.fromPackageMenuItem(packageMenu, menuItem);
-
-                returnMenuDto.add(packageMenuDto);
-            });
-        }
+        returnMenuDto.addAll(getAllPackageMenus());
         Collections.sort(returnMenuDto);
 
+        return returnMenuDto;
+    }
+
+    public List<MenuItemDto> getCurrentUserAllMenus() throws WecubeCoreException {
+        // find all distinct current user's own menu codes
+        Set<String> currentUserRoles = AuthenticationContextHolder.getCurrentUserRoles();
+        List<String> currentUserMenuCodeList;
+        if (CollectionUtils.isEmpty(currentUserRoles)) {
+            return new ArrayList<>();
+        } else {
+            Set<String> currentUserMenuCodeSet = new HashSet<>();
+            for (String userRole : currentUserRoles) {
+                currentUserMenuCodeSet.addAll(roleMenuService.getMenuCodeListByRoleName(userRole));
+            }
+            currentUserMenuCodeList = new ArrayList<>(currentUserMenuCodeSet);
+        }
+
+        // filter all packageMenu which has menuCode in current user's own menu code
+        List<MenuItemDto> allPackageMenus = this.getAllPackageMenus();
+        List<String> finalCurrentUserMenuCodeList = currentUserMenuCodeList;
+        List<MenuItemDto> currentUserPackageMenuDtoList = allPackageMenus.stream()
+                .filter(menuItemDto -> finalCurrentUserMenuCodeList.contains(menuItemDto.getCode()))
+                .peek(packageMenu -> {
+                    MenuItem menuItem = menuItemRepository.findByCode(packageMenu.getCategory());
+                    if (null == menuItem) {
+                        String msg = String.format("Cannot find system menu item by package menu's category: [%s]",
+                                packageMenu.getCategory());
+                        log.error(msg);
+                        throw new WecubeCoreException(msg);
+                    }
+                })
+                .collect(Collectors.toList());
+
+        // append packageMenu and sysMenu
+        List<MenuItemDto> result = getAllSysMenus();
+        result.addAll(currentUserPackageMenuDtoList);
+        Collections.sort(result);
+        return result;
+    }
+
+    public List<MenuItemDto> getAllPackageMenus() {
+        List<MenuItemDto> returnMenuDto = new ArrayList<>();
+        Optional<List<PluginPackageMenu>> optionalPluginPackageMenus = pluginPackageMenuRepository.findAndMergePluginMenus();
+        optionalPluginPackageMenus.ifPresent(pluginPackageMenus -> pluginPackageMenus.forEach(packageMenu -> {
+            MenuItem menuItem = menuItemRepository.findByCode(packageMenu.getCategory());
+            if (null == menuItem) {
+                String msg = String.format("Cannot find system menu item by package menu's category: [%s]",
+                        packageMenu.getCategory());
+                log.error(msg);
+                throw new WecubeCoreException(msg);
+            }
+            MenuItemDto packageMenuDto = MenuItemDto.fromPackageMenuItem(packageMenu, menuItem);
+
+            returnMenuDto.add(packageMenuDto);
+        }));
         return returnMenuDto;
     }
 
