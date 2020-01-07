e18de139dedabd8473ba26e5763ac59b60e16a86
irvinezhao <irvinezhao@gmail.com>
2020-01-07 10:44:01 +0800

    eslint test 3

--- wecube-portal/.eslintrc.js
+++ wecube-portal/.eslintrc.js
@@ -3,26 +3,21 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   env: {
-    browser: true
+    browser: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/essential",
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    "standard"
-  ],
+  extends: ['plugin:vue/essential', 'standard'],
   // required to lint *.vue files
-  plugins: ["vue"],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     // allow async-await
-    "generator-star-spacing": "off",
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }]
-  }
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    // "semi": ["warning", "always"]
+  },
+}
