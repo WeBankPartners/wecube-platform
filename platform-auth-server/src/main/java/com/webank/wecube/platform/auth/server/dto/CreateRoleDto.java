d10ab9a294647e3cb1e6a9d1c91b95706ee37ffe
HoweChen <yuhao.howe.chen@gmail.com>
2020-01-07 10:42:34 +0800

    #1234 update Auth-server, when create role, add email address, and its service's validation

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/dto/CreateRoleDto.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/dto/CreateRoleDto.java
@@ -1,8 +1,12 @@
 package com.webank.wecube.platform.auth.server.dto;
 
+import javax.validation.constraints.Email;
+
 public class CreateRoleDto {
 	private String name;
 	private String displayName;
+	@Email(message = "Please provide a valid E-mail address.")
+	private String email;
 
 	public String getName() {
 		return name;
@@ -18,5 +22,13 @@ public class CreateRoleDto {
 
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
