95df657b86985e29fb48bf56152741e2a8a8d8d6
irvinezhao <irvinezhao@gmail.com>
2020-01-07 11:43:58 +0800

    test

--- wecube-portal/src/pages/admin/components/enum-group-modal.vue
+++ wecube-portal/src/pages/admin/components/enum-group-modal.vue
@@ -8,7 +8,7 @@
     :title="$t('enum_management')"
   >
     <div style="border-bottom: 1px solid grey;">
-      <Form v-model="form">
+      <Form>
         <Row>
           <Col span="5">
             <FormItem :label-width="50" :label="$t('name')">
@@ -41,7 +41,7 @@
           </Col>
           <Col offset="1" span="5">
             <Button type="primary" @click="saveCategoryHandler">{{
-              $t("save")
+              $t('save')
             }}</Button>
           </Col>
         </Row>
@@ -57,54 +57,54 @@
   </Modal>
 </template>
 <script>
-import baseData from "../enums";
-import { createEnumCategory, updateEnumCategory } from "@/api/server";
+import baseData from '../enums'
+import { createEnumCategory, updateEnumCategory } from '@/api/server'
 export default {
   components: {
     baseData
   },
-  data() {
+  data () {
     return {
       form: {
-        catName: "",
-        catTypeId: "",
-        catGroupId: ""
+        catName: '',
+        catTypeId: '',
+        catGroupId: ''
       },
       categoryId: -1,
       catTypes: [
         {
-          label: this.$t("pub_enum"),
+          label: this.$t('pub_enum'),
           value: 2
         },
         {
-          label: this.$t("pri_enum"),
+          label: this.$t('pri_enum'),
           value: 3
         }
       ],
       isVisible: this.enumGroupVisible
-    };
+    }
   },
   watch: {
-    enumGroupVisible(val) {
-      this.isVisible = val;
+    enumGroupVisible (val) {
+      this.isVisible = val
     },
     category: {
-      handler(val) {
+      handler (val) {
         if (val) {
           this.form = {
             ...this.form,
             ...val,
             catTypeId: val.catTypeId === 2 ? val.catTypeId : 3,
             catGroupId: val.groupTypeId
-          };
-          this.categoryId = val.catId;
+          }
+          this.categoryId = val.catId
         } else {
           this.form = {
-            catName: "",
-            catTypeId: "",
-            catGroupId: ""
-          };
-          this.categoryId = -1;
+            catName: '',
+            catTypeId: '',
+            catGroupId: ''
+          }
+          this.categoryId = -1
         }
       }
     }
@@ -118,66 +118,66 @@ export default {
   },
   computed: {},
   methods: {
-    async saveCategoryHandler() {
+    async saveCategoryHandler () {
       const type = this.allEnumCategoryTypes.find(
         _ => _.ciTypeId === this.currentCiType.ciTypeId
-      );
+      )
       if (this.category) {
-        //update
+        // update
         const payload = {
           catId: this.form.catId,
           catName: this.form.catName,
           catTypeId: this.form.catTypeId,
           groupTypeId: this.form.catGroupId
-        };
-        const { message, data, status } = await updateEnumCategory(payload);
-        if (status === "OK") {
+        }
+        const { message, status } = await updateEnumCategory(payload)
+        if (status === 'OK') {
           this.$Notice.success({
-            title: "update category Success",
+            title: 'update category Success',
             desc: message
-          });
-          this.$refs.enumModal.$refs.table.form.catId = this.categoryId;
-          this.$refs.enumModal.getGroupList(this.categoryId);
+          })
+          this.$refs.enumModal.$refs.table.form.catId = this.categoryId
+          this.$refs.enumModal.getGroupList(this.categoryId)
         }
       } else {
-        //create
+        // create
         const payload = {
           catName: this.form.catName,
           catTypeId:
             this.form.catTypeId === 2 ? this.form.catTypeId : type.catTypeId,
           groupTypeId: this.form.catGroupId
-        };
-        const { message, data, status } = await createEnumCategory(payload);
-        if (status === "OK") {
+        }
+        const { message, data, status } = await createEnumCategory(payload)
+        if (status === 'OK') {
           this.$Notice.success({
-            title: "Add category Success",
+            title: 'Add category Success',
             desc: message
-          });
-          this.categoryId = data[0].catId;
+          })
+          this.categoryId = data[0].catId
           this.$nextTick(() => {
-            this.$refs.enumModal.$refs.table.form.catId = this.categoryId;
-            this.$refs.enumModal.getGroupList(this.categoryId);
-          });
-          this.$emit("getAllEnums");
+            this.$refs.enumModal.$refs.table.form.catId = this.categoryId
+            this.$refs.enumModal.getGroupList(this.categoryId)
+          })
+          this.$emit('getAllEnums')
         }
       }
     },
-    visibleChangeHandler(status) {
+    visibleChangeHandler (status) {
       if (!status) {
-        this.$emit("hideHandler");
+        this.$emit('hideHandler')
         this.form = {
-          catName: "",
-          catTypeId: "",
-          catGroupId: ""
-        };
-        this.categoryId = -1;
+          catName: '',
+          catTypeId: '',
+          catGroupId: ''
+        }
+        this.categoryId = -1
       }
       if (status && this.categoryId > 0) {
         this.$nextTick(() => {
-          this.$refs.enumModal.$refs.table.form.catId = this.categoryId;
-        });
+          this.$refs.enumModal.$refs.table.form.catId = this.categoryId
+        })
       }
     }
   }
-};
+}
 </script>
