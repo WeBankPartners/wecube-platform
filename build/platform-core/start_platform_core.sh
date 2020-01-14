bf80dae13a909222116ffcd7809a62fba74c680c
Jun Chen <junchensz@foxmail.com>
2020-01-14 17:38:00 +0800

    add plugin deploy path placeholder

--- build/platform-core/start_platform_core.sh
+++ build/platform-core/start_platform_core.sh
@@ -24,4 +24,6 @@ java -Djava.security.egd=file:/dev/urandom -jar /application/platform-core.jar  
 --wecube.core.plugin.static-resource-server-path=${STATIC_RESOURCE_SERVER_PATH} \
 --wecube.core.auth.jwt-sso-authentication-uri=${JWT_SSO_AUTH_URI} \
 --wecube.core.auth.jwt-sso-access-token-uri=${JWT_SSO_TOKEN_URI} \
+--wecube.core.plugin.plugin-deploy-path=${WECUBE_PLUGIN_DEPLOY_PATH} \
+${WECUBE_CUSTOM_PARAM} \
 >>/log/wecube-core.log 
