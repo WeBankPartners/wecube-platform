62c10514f953208db66391d7f5ddcaa9707c7f65
gavin2lee <gavin2lee@163.com>
2020-01-14 11:18:33 +0800

    #1238 adding quarlifier

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/WorkflowProcInstEndEventNotifier.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/WorkflowProcInstEndEventNotifier.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.webank.wecube.platform.auth.client.http.JwtSsoRestTemplate;
@@ -26,6 +27,7 @@ public class WorkflowProcInstEndEventNotifier {
     private OperationEventRepository operationEventRepository;
 
     @Autowired
+    @Qualifier("jwtSsoRestTemplate")
     private JwtSsoRestTemplate restTemplate;
 
     @Autowired
