9e608b502b5f72d6456ce70c1878d00c4eb84522
gavin2lee <gavin2lee@163.com>
2020-01-14 16:28:08 +0800

    #1311 refined subsystem authorities

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/CompositeAuthority.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/CompositeAuthority.java
@@ -9,6 +9,9 @@ public class CompositeAuthority implements GrantedAuthority {
      */
     private static final long serialVersionUID = 7622659105716440063L;
     
+    public static final String AUTHORITY_TYPE_ROLE = "ROLE";
+    public static final String AUTHORITY_TYPE_PERMISSION = "AUTHORITY";
+    
     private String authorityType;
     
     private String authority;
@@ -32,6 +35,37 @@ public class CompositeAuthority implements GrantedAuthority {
 
     public void setAuthority(String authority) {
         this.authority = authority;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((authority == null) ? 0 : authority.hashCode());
+        result = prime * result + ((authorityType == null) ? 0 : authorityType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CompositeAuthority other = (CompositeAuthority) obj;
+        if (authority == null) {
+            if (other.authority != null)
+                return false;
+        } else if (!authority.equals(other.authority))
+            return false;
+        if (authorityType == null) {
+            if (other.authorityType != null)
+                return false;
+        } else if (!authorityType.equals(other.authorityType))
+            return false;
+        return true;
     }
     
     
