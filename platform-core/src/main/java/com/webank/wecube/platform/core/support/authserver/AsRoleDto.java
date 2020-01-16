99b38eb7ff2a59692e5a240b16a3f49d2d8849e4
gavin2lee <gavin2lee@163.com>
2020-01-16 16:00:41 +0800

    #1311 refactor auth configuration

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/authserver/AsRoleDto.java
@@ -0,0 +1,48 @@
+package com.webank.wecube.platform.core.support.authserver;
+
+import java.io.Serializable;
+
+public class AsRoleDto implements Serializable{
+    
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5419011196635473302L;
+    private String id;
+	private String name;
+	private String displayName;
+	private String email;
+	
+	public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
