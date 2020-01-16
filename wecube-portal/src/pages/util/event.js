51eadc25907f3facfc617091026fb527eab7201d
irvinezhao <irvinezhao@gmail.com>
2020-01-16 11:18:15 +0800

    remove unused code

--- wecube-portal/src/pages/util/event.js
+++ wecube-portal/src/pages/util/event.js
@@ -1,9 +1,6 @@
 export const addEvent = (selector, type, fn) => {
   let nodesEl = document.querySelectorAll(selector)
   let length = nodesEl.length
-  // const func = e => {
-  //   fn(e);
-  // };
   for (let i = 0; i < length; i++) {
     let node = nodesEl[i]
     node.removeEventListener(type, fn)
@@ -14,9 +11,6 @@ export const addEvent = (selector, type, fn) => {
 export const removeEvent = (selector, type, fn) => {
   let nodesEl = document.querySelectorAll(selector)
   let length = nodesEl.length
-  // const func = e => {
-  //   fn(e);
-  // };
   for (let i = 0; i < length; i++) {
     let node = nodesEl[i]
     node.removeEventListener(type, fn)
