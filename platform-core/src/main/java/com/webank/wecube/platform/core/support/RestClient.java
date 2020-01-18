48e14b00d29922112347ec2d1a7b619fe9bb7303
gavin2lee <gavin2lee@163.com>
2020-01-18 22:26:01 +0800

    #1311 refactor rest client

--- platform-core/src/main/java/com/webank/wecube/platform/core/support/RestClient.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/RestClient.java
@@ -2,4 +2,7 @@ package com.webank.wecube.platform.core.support;
 
 public interface RestClient {
 
+    String URI_PATH_DELIMITER = "/";
+    String URI_COMPONENTS_DELIMITER = ":";
+    String URI_SCHEMA_DELIMITER = "//";
 }
