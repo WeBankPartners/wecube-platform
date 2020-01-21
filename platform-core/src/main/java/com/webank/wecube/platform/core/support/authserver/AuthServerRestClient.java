30a020a23e1d5dc15faafff654150a87688a07ef
gavin2lee <gavin2lee@163.com>
2020-01-21 09:13:09 +0800

    #1311 refactor auth config

--- platform-core/src/main/java/com/webank/wecube/platform/core/support/authserver/AuthServerRestClient.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/authserver/AuthServerRestClient.java
@@ -1,6 +1,5 @@
 package com.webank.wecube.platform.core.support.authserver;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -8,46 +7,23 @@ import javax.annotation.PostConstruct;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import com.webank.wecube.platform.auth.client.http.JwtSsoRestTemplate;
-import com.webank.wecube.platform.core.http.UserJwtSsoTokenRestTemplate;
-import com.webank.wecube.platform.core.support.RestClient;
 
 @Service
 @EnableConfigurationProperties({ AuthServerRestClientProperties.class })
-public class AuthServerRestClient implements RestClient {
+public class AuthServerRestClient extends AbstractAuthServerRestClient {
 
     private static final Logger log = LoggerFactory.getLogger(AuthServerRestClient.class);
 
     private static AuthServerRestClient _INSTANCE;
 
-    @Autowired
-    @Qualifier("userJwtSsoTokenRestTemplate")
-    private UserJwtSsoTokenRestTemplate userJwtSsoTokenRestTemplate;
-
-    @Autowired
-    @Qualifier("jwtSsoRestTemplate")
-    private JwtSsoRestTemplate jwtSsoRestTemplate;
-
-    @Autowired
-    private AuthServerRestClientProperties clientProperties;
-
     @PostConstruct
     public void afterPropertiesSet() {
         _INSTANCE = this;
         if (log.isDebugEnabled()) {
-            log.debug("auth server properties:host={},port={}", clientProperties.getHost(),
-                    clientProperties.getPort());
+            log.debug("auth server properties:host={},port={}", clientProperties.getHost(), clientProperties.getPort());
         }
 
     }
@@ -170,91 +146,10 @@ public class AuthServerRestClient implements RestClient {
         deleteObject(clientProperties.getPathDeleteUserAccountByUserId(), userId);
     }
 
-    protected String buildFullUriString(String path, String httpSchema, String host, int port) {
-        if (StringUtils.isNotBlank(path) && !path.startsWith(URI_PATH_DELIMITER)) {
-            path = URI_PATH_DELIMITER + path;
-        }
-        StringBuilder sb = new StringBuilder().append(httpSchema).append(URI_COMPONENTS_DELIMITER)
-                .append(URI_SCHEMA_DELIMITER).append(host).append(URI_COMPONENTS_DELIMITER).append(String.valueOf(port))
-                .append(path);
-
-        return sb.toString();
+    @Override
+    protected Logger getLogger() {
+        return log;
     }
 
-    protected void deleteObject(String path, Object... uriVariables) throws AuthServerClientException {
-        String requestUri = buildFullUriString(path, clientProperties.getHttpSchema(),
-                clientProperties.getHost(), clientProperties.getPort());
-
-        URI expandedUri = userJwtSsoTokenRestTemplate.getUriTemplateHandler().expand(requestUri, uriVariables);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Object> requestEntity = new HttpEntity<Object>(headers);
-        ResponseEntity<AuthServerRestResponseDto<Object>> responseEntity = userJwtSsoTokenRestTemplate.exchange(
-                expandedUri, HttpMethod.DELETE, requestEntity,
-                new ParameterizedTypeReference<AuthServerRestResponseDto<Object>>() {
-                });
-        AuthServerRestResponseDto<Object> responseDto = responseEntity.getBody();
-        String status = responseDto.getStatus();
-        if (!AuthServerRestResponseDto.STATUS_OK.equalsIgnoreCase(status)) {
-            log.error("rest service invocation failed,status={},message={}", responseDto.getStatus(),
-                    responseDto.getMessage());
-            throw new AuthServerClientException(responseDto.getStatus(), responseDto.getMessage());
-        }
-    }
-
-    protected <T> T getForObject(String path, ParameterizedTypeReference<AuthServerRestResponseDto<T>> responseType,
-            Object... uriVariables) throws AuthServerClientException {
-        String requestUri = buildFullUriString(path, clientProperties.getHttpSchema(),
-                clientProperties.getHost(), clientProperties.getPort());
-
-        URI expandedUri = userJwtSsoTokenRestTemplate.getUriTemplateHandler().expand(requestUri, uriVariables);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Object> requestEntity = new HttpEntity<Object>(headers);
-        ResponseEntity<AuthServerRestResponseDto<T>> responseEntity = userJwtSsoTokenRestTemplate.exchange(expandedUri,
-                HttpMethod.GET, requestEntity, responseType);
-        AuthServerRestResponseDto<T> responseDto = responseEntity.getBody();
-        String status = responseDto.getStatus();
-        if (!AuthServerRestResponseDto.STATUS_OK.equalsIgnoreCase(status)) {
-            log.error("rest service invocation failed,status={},message={}", responseDto.getStatus(),
-                    responseDto.getMessage());
-            throw new AuthServerClientException(responseDto.getStatus(), responseDto.getMessage());
-        }
-        return responseDto.getData();
-    }
-
-    protected <T> T postForObject(String path, Object request,
-            ParameterizedTypeReference<AuthServerRestResponseDto<T>> responseType, Object... uriVariables)
-            throws AuthServerClientException {
-        String requestUri = buildFullUriString(path, clientProperties.getHttpSchema(),
-                clientProperties.getHost(), clientProperties.getPort());
-
-        URI expandedUri = userJwtSsoTokenRestTemplate.getUriTemplateHandler().expand(requestUri, uriVariables);
-        ResponseEntity<AuthServerRestResponseDto<T>> responseEntity = userJwtSsoTokenRestTemplate.exchange(expandedUri,
-                HttpMethod.POST, buildRequestEntity(request), responseType);
-        AuthServerRestResponseDto<T> responseDto = responseEntity.getBody();
-        String status = responseDto.getStatus();
-        if (!AuthServerRestResponseDto.STATUS_OK.equalsIgnoreCase(status)) {
-            log.error("rest service invocation failed,status={},message={}", responseDto.getStatus(),
-                    responseDto.getMessage());
-            throw new AuthServerClientException(responseDto.getStatus(), responseDto.getMessage());
-        }
-        return responseDto.getData();
-    }
-
-    protected HttpEntity<Object> buildRequestEntity(Object request) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Object> requestEntity = null;
-
-        if (request != null) {
-            requestEntity = new HttpEntity<Object>(request, headers);
-        } else {
-            requestEntity = new HttpEntity<Object>(headers);
-        }
-
-        return requestEntity;
-    }
+    
 }
