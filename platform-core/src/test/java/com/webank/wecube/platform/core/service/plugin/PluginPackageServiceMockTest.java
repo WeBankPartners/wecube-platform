24e670df7aa7671ce65100d58a161ead6609a9c2
HoweChen <yuhao.howe.chen@gmail.com>
2020-02-10 19:33:19 +0800

    #1407 update unitTest

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/plugin/PluginPackageServiceMockTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/plugin/PluginPackageServiceMockTest.java
@@ -5,6 +5,10 @@ import com.google.common.collect.Sets;
 import com.webank.wecube.platform.core.domain.plugin.PluginPackage;
 import com.webank.wecube.platform.core.domain.plugin.PluginPackageAuthority;
 import com.webank.wecube.platform.core.dto.user.RoleDto;
+import com.webank.wecube.platform.core.jpa.MenuItemRepository;
+import com.webank.wecube.platform.core.jpa.PluginPackageMenuRepository;
+import com.webank.wecube.platform.core.service.user.RoleMenuService;
+import com.webank.wecube.platform.core.service.user.RoleMenuServiceImpl;
 import com.webank.wecube.platform.core.service.user.UserManagementService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +23,15 @@ import static org.mockito.Mockito.*;
 public class PluginPackageServiceMockTest {
     @Mock
     private UserManagementService userManagementService;
+    @Mock
+    private MenuItemRepository menuItemRepository;
+    @Mock
+    private PluginPackageMenuRepository pluginPackageMenuRepository;
+    @Mock
+    private RoleMenuService roleMenuService = new RoleMenuServiceImpl();
     @InjectMocks
     private PluginPackageService pluginPackageService = new PluginPackageService();
+
 
     @Test
     public void givenRoleInPluginPackageAuthorityNotExistInUserManagementServiceWhenRegisterThenCreateRoleShouldBeCalled() {
@@ -49,6 +60,24 @@ public class PluginPackageServiceMockTest {
 
         pluginPackageService.createRolesIfNotExistInSystem(pluginPackage);
         verify(userManagementService, never()).registerLocalRole(any());
+    }
+
+    @Test
+    public void whenRegisterPackageWithGivenCorrectRoleAndCorrectMenuInPluginPackageAuthority_createRoleMenuBinding_shouldSucceed() {
+        // mock data
+        final String MOCK_ROLE_NAME = "ITSM_OPERATOR";
+        final String MOCK_MENU_CODE = "JOB";
+        PluginPackage pluginPackage = new PluginPackage("ITSM", "v1.0");
+        pluginPackage.initId();
+        PluginPackageAuthority authority = new PluginPackageAuthority(null, pluginPackage, MOCK_ROLE_NAME, MOCK_MENU_CODE);
+        authority.initId();
+        pluginPackage.setPluginPackageAuthorities(Sets.newHashSet(authority));
+
+        // action
+        pluginPackageService.bindRoleToMenu(pluginPackage);
+
+        // assertion
+        verify(roleMenuService, times(1)).createRoleMenuBinding(MOCK_ROLE_NAME, MOCK_MENU_CODE);
     }
 
     private RoleDto mockRoleDto(String displayName, String name) {
