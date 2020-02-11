a09403db9d11c311aea9513f37e842f068482f6f
HoweChen <yuhao.howe.chen@gmail.com>
2020-02-11 13:01:25 +0800

    #1433 update RoleMenuServiceImpl and its unit test

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/user/RoleMenuServiceTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/user/RoleMenuServiceTest.java
@@ -1,29 +1,6 @@
 package com.webank.wecube.platform.core.service.user;
 
 
-import static com.webank.wecube.platform.core.domain.plugin.PluginPackage.Status.RUNNING;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.ExpectedCount;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
-
 import com.google.common.collect.Lists;
 import com.webank.wecube.platform.core.DatabaseBasedTest;
 import com.webank.wecube.platform.core.commons.AuthenticationContextHolder;
@@ -37,6 +14,26 @@ import com.webank.wecube.platform.core.jpa.PluginPackageRepository;
 import com.webank.wecube.platform.core.jpa.user.RoleMenuRepository;
 import com.webank.wecube.platform.core.support.RestClient;
 import com.webank.wecube.platform.core.support.authserver.AuthServerRestClientProperties;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.webank.wecube.platform.core.domain.plugin.PluginPackage.Status.RUNNING;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 
 public class RoleMenuServiceTest extends DatabaseBasedTest {
@@ -79,6 +76,7 @@ public class RoleMenuServiceTest extends DatabaseBasedTest {
 
     @Test
     public void whenGivenRoleId_retrieveMenusByRoleId_shouldSucceed() {
+        mockRetrieveMenuByRoleIdServer();
         RoleMenuDto roleMenuDtoFromRoleOne = this.roleMenuService.retrieveMenusByRoleId(ROLE_ONE);
         assertThat(roleMenuDtoFromRoleOne.getMenuList().size()).isEqualTo(2);
         assertThat(roleMenuDtoFromRoleOne.getMenuList().get(0).getDisplayName()).isEqualTo("CI Integrated Enquiry");
@@ -89,7 +87,7 @@ public class RoleMenuServiceTest extends DatabaseBasedTest {
         assertThat(roleMenuDtoFromRoleTwo.getMenuList().size()).isEqualTo(2);
         assertThat(roleMenuDtoFromRoleTwo.getMenuList().get(0).getDisplayName()).isEqualTo("TEST_MENU_ONE");
         assertThat(roleMenuDtoFromRoleTwo.getMenuList().get(1).getDisplayName()).isEqualTo("TEST_MENU_TWO");
-
+        server.verify();
     }
 
     @Test
@@ -152,10 +150,10 @@ public class RoleMenuServiceTest extends DatabaseBasedTest {
 
     private void mockRoleMenuData() {
         List<RoleMenu> roleMenuList = new ArrayList<>();
-        roleMenuList.add(new RoleMenu(ROLE_ONE, ROLE_ONE_NAME, "DESIGNING_CI_INTEGRATED_QUERY_EXECUTION"));
-        roleMenuList.add(new RoleMenu(ROLE_ONE, ROLE_ONE_NAME, "CMDB_DESIGNING_ENUM_ENQUIRY"));
-        roleMenuList.add(new RoleMenu(ROLE_TWO, ROLE_TWO_NAME, "CMDB_DESIGNING_TEST_MENU_ONE"));
-        roleMenuList.add(new RoleMenu(ROLE_TWO, ROLE_TWO_NAME, "CMDB_DESIGNING_TEST_MENU_TWO"));
+        roleMenuList.add(new RoleMenu(ROLE_ONE_NAME, "DESIGNING_CI_INTEGRATED_QUERY_EXECUTION"));
+        roleMenuList.add(new RoleMenu(ROLE_ONE_NAME, "CMDB_DESIGNING_ENUM_ENQUIRY"));
+        roleMenuList.add(new RoleMenu(ROLE_TWO_NAME, "CMDB_DESIGNING_TEST_MENU_ONE"));
+        roleMenuList.add(new RoleMenu(ROLE_TWO_NAME, "CMDB_DESIGNING_TEST_MENU_TWO"));
         this.roleMenuRepository.saveAll(roleMenuList);
     }
 
@@ -182,6 +180,34 @@ public class RoleMenuServiceTest extends DatabaseBasedTest {
         MenuItem menuItem_ONE = new MenuItem("DESIGNING", null, "designing display");
         MenuItem menuItem_TWO = new MenuItem("OPERATING", null, "operating display");
         menuItemRepository.saveAll(Lists.newArrayList(menuItem_ONE, menuItem_TWO));
+    }
+
+    private void mockRetrieveMenuByRoleIdServer() {
+        server.expect(ExpectedCount.manyTimes(), requestTo(String.format("http://%s/auth/v1/roles/%s", this.gatewayUrl, ROLE_ONE)))
+                .andExpect(method(HttpMethod.GET))
+                .andExpect(header("Authorization", TOKEN))
+                .andRespond(withSuccess("{\n" +
+                        "    \"status\": \"OK\",\n" +
+                        "    \"message\": \"Success\",\n" +
+                        "    \"data\": {\n" +
+                        "        \"id\": 1,\n" +
+                        "        \"name\": \"1\",\n" +
+                        "        \"displayName\": \"1_display_name\"\n" +
+                        "    }\n" +
+                        "}", MediaType.APPLICATION_JSON));
+
+        server.expect(ExpectedCount.manyTimes(), requestTo(String.format("http://%s/auth/v1/roles/%s", this.gatewayUrl, ROLE_TWO)))
+                .andExpect(method(HttpMethod.GET))
+                .andExpect(header("Authorization", TOKEN))
+                .andRespond(withSuccess("{\n" +
+                        "    \"status\": \"OK\",\n" +
+                        "    \"message\": \"Success\",\n" +
+                        "    \"data\": {\n" +
+                        "        \"id\": 2,\n" +
+                        "        \"name\": \"2\",\n" +
+                        "        \"displayName\": \"2_display_name\"\n" +
+                        "    }\n" +
+                        "}", MediaType.APPLICATION_JSON));
     }
 
 
