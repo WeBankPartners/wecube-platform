069927ca981057c93d47d847b6393c40c2a8c910
huanghaixin@126.com <huanghaixin@126.com>
2020-02-12 14:55:33 +0800

    #1443 - Fixed #1443, use hostIp+Type to find record

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.webank.wecube.platform.core.domain.ResourceServer;
 
 public interface ResourceServerRepository extends CrudRepository<ResourceServer, String> {
-    List<ResourceServer> findByHost(String host);
-
     List<ResourceServer> findByHostAndType(String host, String type);
 }
