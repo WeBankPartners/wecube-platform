87d80ac5395f99eda2302d519c2738e9d11c500d
gavin2lee <gavin2lee@163.com>
2020-03-15 22:25:14 +0800

    #1485 refactor datamodel expression module

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityQuerySpecification.java
@@ -0,0 +1,52 @@
+package com.webank.wecube.platform.core.service.dme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityQuerySpecification {
+    
+
+    private EntityQueryCriteria criteria;
+    private List<EntityQueryFilter> additionalFilters;
+
+    public EntityQueryCriteria getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(EntityQueryCriteria criteria) {
+        this.criteria = criteria;
+    }
+
+    public List<EntityQueryFilter> getAdditionalFilters() {
+        return additionalFilters;
+    }
+
+    public void setAdditionalFilters(List<EntityQueryFilter> additionalFilters) {
+        this.additionalFilters = additionalFilters;
+    }
+
+    public EntityQuerySpecification addAdditionalFilters(EntityQueryFilter... additionalFilters) {
+        if (this.additionalFilters == null) {
+            this.additionalFilters = new ArrayList<>();
+        }
+
+        for (EntityQueryFilter f : additionalFilters) {
+            this.additionalFilters.add(f);
+        }
+
+        return this;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[criteria=");
+        builder.append(criteria);
+        builder.append(", additionalFilters=");
+        builder.append(additionalFilters);
+        builder.append("]");
+        return builder.toString();
+    }
+    
+    
+}
