0bc344249e0b5c012b27743710b5dd1cd38a3af1
xuxuzhesi <ipqs45010@163.com>
2020-03-16 10:33:22 +0800

    #1523 Fix the  favorites
    #1523 Fix the  favorites

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/RoleManagementController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/RoleManagementController.java
@@ -1,23 +1,20 @@
 package com.webank.wecube.platform.core.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.webank.wecube.platform.core.commons.AuthenticationContextHolder;
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import com.webank.wecube.platform.core.dto.CommonResponseDto;
+import com.webank.wecube.platform.core.dto.FavoritesDto;
 import com.webank.wecube.platform.core.dto.user.RoleDto;
 import com.webank.wecube.platform.core.dto.user.RoleMenuDto;
 import com.webank.wecube.platform.core.dto.user.UserDto;
+import com.webank.wecube.platform.core.dto.workflow.ProcDefOutlineDto;
+import com.webank.wecube.platform.core.service.user.RoleFavoritesService;
 import com.webank.wecube.platform.core.service.user.RoleMenuService;
 import com.webank.wecube.platform.core.service.user.UserManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author howechen
@@ -30,6 +27,8 @@ public class RoleManagementController {
     private UserManagementService userManagementService;
     @Autowired
     private RoleMenuService roleMenuService;
+    @Autowired
+    private RoleFavoritesService roleFavoritesService;
 
     @PostMapping("/roles/create")
     public CommonResponseDto registerLocalRole(@RequestBody RoleDto roleDto) {
@@ -115,6 +114,45 @@ public class RoleManagementController {
         } catch (WecubeCoreException ex) {
             return CommonResponseDto.error(ex.getMessage());
         }
+    }
 
+    @PostMapping("/roles/favorites/create")
+    public CommonResponseDto createCollectionByRole(@RequestBody FavoritesDto favoritesDto) {
+        try {
+            roleFavoritesService.createCollectionByRole(favoritesDto);
+            return CommonResponseDto.okay();
+        } catch (Exception e) {
+            return CommonResponseDto.error(e.getMessage());
+        }
+    }
+
+    @GetMapping("/roles/favorites/retrieve")
+        public CommonResponseDto retrieveAllCollections() {
+        try {
+            List<FavoritesDto> result = roleFavoritesService.retrieveAllCollections();
+            return CommonResponseDto.okayWithData(result);
+        } catch (WecubeCoreException ex) {
+            return CommonResponseDto.error(ex.getMessage());
+        }
+    }
+
+    @PostMapping("/roles/favorites/update")
+    public CommonResponseDto updateCollectionByRole(@RequestBody FavoritesDto favoritesDto) {
+        try {
+            this.roleFavoritesService.updateCollectionByRole(favoritesDto);
+            return CommonResponseDto.okay();
+        } catch (WecubeCoreException ex) {
+            return CommonResponseDto.error(ex.getMessage());
+        }
+    }
+    @DeleteMapping("/roles/favorites/delete")
+    public CommonResponseDto deleteCollectionByRole(@PathVariable("favorites-id") String favoritesId) {
+        try {
+            this.roleFavoritesService.deleteCollectionById(favoritesId);
+            //this.roleMenuService.updateRoleToMenusByRoleId(roleId, menuCodeList);
+            return CommonResponseDto.okay();
+        } catch (WecubeCoreException ex) {
+            return CommonResponseDto.error(ex.getMessage());
+        }
     }
 }
