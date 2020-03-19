24ae90e50daf07bd1def468f42b2ce7a73a3c3ab
huanghaixin@126.com <huanghaixin@126.com>
2020-03-19 10:52:33 +0800

    #1534 - Add TargetEntityFilterRuleDto

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/TargetEntityFilterRuleDto.java
@@ -0,0 +1,15 @@
+package com.webank.wecube.platform.core.dto;
+
+public class TargetEntityFilterRuleDto {
+
+    private String filterRule;
+
+    public String getFilterRule() {
+        return filterRule;
+    }
+
+    public void setFilterRule(String filterRule) {
+        this.filterRule = filterRule;
+    }
+
+}
