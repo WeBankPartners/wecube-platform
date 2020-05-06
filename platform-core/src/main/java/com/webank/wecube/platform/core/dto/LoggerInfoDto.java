777badc1d5960cbbce5cc45914182115f56dd186
gavin2lee <gavin2lee@163.com>
2020-05-06 09:41:32 +0800

    #1715 adding dynamic loggers

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/LoggerInfoDto.java
@@ -0,0 +1,23 @@
+package com.webank.wecube.platform.core.dto;
+
+public class LoggerInfoDto {
+    private String level;
+    private String path;
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}
