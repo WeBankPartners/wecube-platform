d2b647e82ad9e4f842110cfe8114882e682cfbe6
gavin2lee <gavin2lee@163.com>
2020-05-08 15:00:49 +0800

    #1402 add sql script

---
+++ platform-core/src/main/resources/database/act_ge_property_data.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM wecube_cds.act_ge_property
+LIMIT 0, 1000
+
+-- Date: 2020-05-08 14:57
+*/
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('deployment.lock','0',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('history.cleanup.job.lock','0',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('historyLevel','3',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('next.dbid','1',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('schema.history','create(fox)',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('schema.version','fox',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('startup.lock','0',1);
