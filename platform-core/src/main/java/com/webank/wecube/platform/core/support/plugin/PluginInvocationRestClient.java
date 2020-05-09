9003f2a0910e2d523e60a948d5b8bb68fbb463f2
gavin2lee <gavin2lee@163.com>
2020-05-09 15:23:57 +0800

    #1402 add allowed options

--- platform-core/src/main/java/com/webank/wecube/platform/core/support/plugin/PluginInvocationRestClient.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/plugin/PluginInvocationRestClient.java
@@ -18,34 +18,44 @@ import com.webank.wecube.platform.core.support.plugin.dto.PluginResponse.Default
 
 @Service("pluginInvocationRestClient")
 public class PluginInvocationRestClient implements RestClient {
-    private static final Logger log = LoggerFactory.getLogger(PluginInvocationRestClient.class);
-    
-    @Autowired
-    @Qualifier(value = "jwtSsoRestTemplate")
-    private RestTemplate jwtSsoRestTemplate;
-    
-    
-    public PluginResponse<Object> callPluginService(String instanceAddress, String path,
-            List<Map<String, Object>> parameters, String requestId) {
-        return doCallPluginService(asPluginServerUrl(instanceAddress, path),
-                new DefaultPluginRequest().withInputs(parameters).withRequestId(requestId));
-    }
-    
-    protected PluginResponse<Object> doCallPluginService(String targetUrl, PluginRequest<?> parameters) {
-        log.info("About to call {} with parameters: {} ", targetUrl, parameters);
-        PluginResponse<Object> response = jwtSsoRestTemplate.postForObject(targetUrl, parameters,
-                DefaultPluginResponse.class);
-        log.info("Plugin response: {} ", response);
+	private static final Logger log = LoggerFactory.getLogger(PluginInvocationRestClient.class);
 
-        return response;
-    }
-    
-    protected String asPluginServerUrl(String instanceAddress, String originPath, Object... pathVariables) {
-        String solvedPath = originPath;
-        if (pathVariables != null && pathVariables.length > 0) {
-            solvedPath = String.format(originPath, pathVariables);
-        }
-        return "http://" + instanceAddress + solvedPath;
-    }
+	@Autowired
+	@Qualifier(value = "jwtSsoRestTemplate")
+	private RestTemplate jwtSsoRestTemplate;
+
+	public PluginResponse<Object> callPluginService(String instanceAddress, String path,
+			List<Map<String, Object>> parameters, String requestId, List<String> allowedOptions) {
+		PluginRequest<Map<String, Object>> requestObj = new DefaultPluginRequest().withInputs(parameters)
+				.withRequestId(requestId);
+		if (allowedOptions != null && !allowedOptions.isEmpty()) {
+			requestObj = requestObj.withAllowedOptions(allowedOptions);
+		}
+
+		return doCallPluginService(asPluginServerUrl(instanceAddress, path), requestObj);
+	}
+
+	public PluginResponse<Object> callPluginService(String instanceAddress, String path,
+			List<Map<String, Object>> parameters, String requestId) {
+		return doCallPluginService(asPluginServerUrl(instanceAddress, path),
+				new DefaultPluginRequest().withInputs(parameters).withRequestId(requestId));
+	}
+
+	protected PluginResponse<Object> doCallPluginService(String targetUrl, PluginRequest<?> parameters) {
+		log.info("About to call {} with parameters: {} ", targetUrl, parameters);
+		PluginResponse<Object> response = jwtSsoRestTemplate.postForObject(targetUrl, parameters,
+				DefaultPluginResponse.class);
+		log.info("Plugin response: {} ", response);
+
+		return response;
+	}
+
+	protected String asPluginServerUrl(String instanceAddress, String originPath, Object... pathVariables) {
+		String solvedPath = originPath;
+		if (pathVariables != null && pathVariables.length > 0) {
+			solvedPath = String.format(originPath, pathVariables);
+		}
+		return "http://" + instanceAddress + solvedPath;
+	}
 
 }
