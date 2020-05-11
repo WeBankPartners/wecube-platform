7f91451b581f0ccbe36ced4ad1532132cbbd4772
gavin2lee <gavin2lee@163.com>
2020-05-11 12:38:34 +0800

    #1402 add default data to camunda

--- platform-core/src/main/resources/database/03.wecube.flow_engine.schema.sql
+++ platform-core/src/main/resources/database/03.wecube.flow_engine.schema.sql
@@ -1542,4 +1542,14 @@ CREATE TABLE `core_operation_event` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('deployment.lock','0',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('history.cleanup.job.lock','0',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('historyLevel','3',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('next.dbid','1',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('schema.history','create(fox)',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('schema.version','fox',1);
+INSERT INTO `act_ge_property` (`NAME_`,`VALUE_`,`REV_`) VALUES ('startup.lock','0',1);
+
+
 SET FOREIGN_KEY_CHECKS = 1;
