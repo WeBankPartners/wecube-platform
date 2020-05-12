5e6dedf868d6c86c95ca9aa2eb39d63ce6a0183c
huanghaixin@126.com <huanghaixin@126.com>
2020-05-12 15:08:03 +0800

    Adjust regular expression that match package vesion

--- platform-core/src/main/java/com/webank/wecube/platform/core/utils/Constants.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/utils/Constants.java
@@ -2,7 +2,7 @@ package com.webank.wecube.platform.core.utils;
 
 public class Constants {
     public static final String PACKAGE_NAMING_PATTERN = "[a-z0-9][a-z0-9-]{1,61}[a-z0-9]";
-    public static final String PACKAGE_VERSION_PATTERN = "^v([0-99])(\\.[0-99]){2,3}";
+    public static final String PACKAGE_VERSION_PATTERN = "^v([0-99])(\\.[0-99]){1,3}";
     public static final String S3_BUCKET_NAMING_PATTERN = "[a-z0-9][a-z0-9-]{1,61}[a-z0-9]";
     public static final String MYSQL_SCHEMA_NAMING_PATTERN = "[a-z0-9][a-z0-9_]{1,62}[a-z0-9]";
     public static final String KEY_COLUMN_DELIMITER = "__";
