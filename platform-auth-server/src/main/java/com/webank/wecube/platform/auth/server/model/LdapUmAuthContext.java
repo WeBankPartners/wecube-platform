cfeca2084590fca9c367468881b5a331e73e497b
gavin2lee <gavin2lee@163.com>
2020-05-20 20:20:37 +0800

    #1520 add ldap authentication

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/LdapUmAuthContext.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/LdapUmAuthContext.java
@@ -1,5 +1,24 @@
 package com.webank.wecube.platform.auth.server.model;
 
-public class LdapUmAuthContext implements AuthContext{
+public class LdapUmAuthContext implements AuthContext {
+
+    private String host;
+    private int port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
 
 }
