58a58655e4ecfbeb6a4279f00a1032d0f349dfa2
gavin2lee <gavin2lee@163.com>
2020-05-22 16:31:01 +0800

    #1520 modify context type

---
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/UmAuthContext.java
@@ -0,0 +1,52 @@
+package com.webank.wecube.platform.auth.server.model;
+
+public class UmAuthContext implements AuthContext {
+
+	private String protocol;
+	private String host;
+	private int port;
+
+	private String appid;
+	private String appname;
+
+	public String getProtocol() {
+		return protocol;
+	}
+
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getAppid() {
+		return appid;
+	}
+
+	public void setAppid(String appid) {
+		this.appid = appid;
+	}
+
+	public String getAppname() {
+		return appname;
+	}
+
+	public void setAppname(String appname) {
+		this.appname = appname;
+	}
+
+}
