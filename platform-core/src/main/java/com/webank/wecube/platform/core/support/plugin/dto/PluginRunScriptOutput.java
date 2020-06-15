bbb154f3b6e67b167f1e686511a138e28e60d8a0
gavin2lee <gavin2lee@163.com>
2020-06-15 15:42:58 +0800

    #1790 remove lombok

--- platform-core/src/main/java/com/webank/wecube/platform/core/support/plugin/dto/PluginRunScriptOutput.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/support/plugin/dto/PluginRunScriptOutput.java
@@ -1,10 +1,32 @@
 package com.webank.wecube.platform.core.support.plugin.dto;
 
-import lombok.Data;
-
-@Data
 public class PluginRunScriptOutput {
     private Integer retCode;
-    private String  detail;
+    private String detail;
     private String target;
+
+    public Integer getRetCode() {
+        return retCode;
+    }
+
+    public void setRetCode(Integer retCode) {
+        this.retCode = retCode;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
 }
