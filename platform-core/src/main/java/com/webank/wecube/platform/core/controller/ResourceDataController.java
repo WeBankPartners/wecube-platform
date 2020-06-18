f1558f75e142290359907889190f1caa0845b3bf
gavin2lee <gavin2lee@163.com>
2020-06-18 20:20:56 +0800

    #1817 refactor dto

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/ResourceDataController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/ResourceDataController.java
@@ -1,39 +1,33 @@
 package com.webank.wecube.platform.core.controller;
 
-import static com.webank.wecube.platform.core.domain.JsonResponse.okayWithData;
+import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webank.wecube.platform.core.domain.JsonResponse;
+import com.webank.wecube.platform.core.dto.CommonResponseDto;
 import com.webank.wecube.platform.core.dto.SqlQueryRequest;
 import com.webank.wecube.platform.core.service.resource.ResourceDataQueryService;
 
 @RestController()
 @RequestMapping("/v1/packages/{package_id}/resources")
 public class ResourceDataController {
-    private Logger logger = LoggerFactory.getLogger(ResourceDataController.class);
     
     @Autowired
     private ResourceDataQueryService resourceDataQueryService;
     
     @PostMapping("/mysql/query")
-    @ResponseBody
-    public JsonResponse queryDB(@PathVariable("package_id") String packageId, @RequestBody SqlQueryRequest sqlQueryRequest){
+    public CommonResponseDto queryDB(@PathVariable("package_id") String packageId, @RequestBody SqlQueryRequest sqlQueryRequest){
         return okayWithData(resourceDataQueryService.queryDB(packageId, sqlQueryRequest));
     }
 
     @GetMapping("/s3/files")
-    @ResponseBody
-    public JsonResponse queryS3Files(@PathVariable("package_id") String packageId){
+    public CommonResponseDto queryS3Files(@PathVariable("package_id") String packageId){
         return okayWithData(resourceDataQueryService.queryS3Files(packageId));
     }
     
