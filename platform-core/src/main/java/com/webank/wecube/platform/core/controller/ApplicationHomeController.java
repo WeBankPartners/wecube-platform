7dbc40c02b4e7366adcdb0d3b1bf2436490ae9a3
gavin2lee <gavin2lee@163.com>
2020-06-18 20:20:56 +0800

    #1817 refactor dto

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/ApplicationHomeController.java
@@ -0,0 +1,41 @@
+package com.webank.wecube.platform.core.controller;
+
+import static com.webank.wecube.platform.core.dto.CommonResponseDto.error;
+import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webank.wecube.platform.core.commons.WecubeCoreException;
+import com.webank.wecube.platform.core.dto.CommonResponseDto;
+import com.webank.wecube.platform.core.dto.MenuItemDto;
+import com.webank.wecube.platform.core.service.MenuService;
+
+@RestController
+@RequestMapping("/v1")
+public class ApplicationHomeController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @GetMapping("/my-menus")
+    public CommonResponseDto getMyMenuItems() {
+        List<MenuItemDto> currentUserAllMenus;
+        try {
+            currentUserAllMenus = menuService.getCurrentUserAllMenus();
+        } catch (WecubeCoreException ex) {
+            return error(ex.getMessage());
+        }
+        return okayWithData(currentUserAllMenus);
+    }
+
+    @GetMapping("/all-menus")
+    public CommonResponseDto getAllMenuItems(Principal principal) {
+        return okayWithData(menuService.getAllMenus());
+    }
+}
