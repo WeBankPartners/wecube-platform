cc4b68e03996a4352f5cca32d63f70aa64329bcc
gavin2lee <gavin2lee@163.com>
2020-06-24 07:28:54 +0800

    #1829 add auth server init impl

---
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/boot/StatementInfoParser.java
@@ -0,0 +1,8 @@
+package com.webank.wecube.platform.auth.server.boot;
+
+public class StatementInfoParser {
+
+    public StatementInfo parseStatement(String statement, Long lineNum){
+        return null;
+    }
+}
