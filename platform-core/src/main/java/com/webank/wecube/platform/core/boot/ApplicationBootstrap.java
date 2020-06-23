db57976131532fdb6bb050d0d7ea7ec5a714bac2
gavin2lee <gavin2lee@163.com>
2020-06-23 22:15:36 +0800

    #1829 add helpers

--- platform-core/src/main/java/com/webank/wecube/platform/core/boot/ApplicationBootstrap.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/boot/ApplicationBootstrap.java
@@ -1,13 +1,29 @@
 package com.webank.wecube.platform.core.boot;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.webank.wecube.platform.core.commons.ApplicationProperties;
+
 @Component("applicationBootstrap")
 public class ApplicationBootstrap {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationBootstrap.class);
 
     @Autowired
     private DataSource dataSource;
+    
+    @Autowired
+    private ApplicationProperties applicationProperties;
+    
+    private DatabaseInitializer databaseInitializer;
+    
+    @PostConstruct
+    public void afterPropertiesSetting() throws Exception{
+        
+    }
 }
