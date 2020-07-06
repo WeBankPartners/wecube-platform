15f6d3237b4448f410e9f0644672dae5a44ebb03
gavin2lee <gavin2lee@163.com>
2020-07-06 16:46:45 +0800

    #1853 decommissioned antrl4

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/datamodel/ExpressionService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/datamodel/ExpressionService.java
@@ -5,12 +5,5 @@ import java.util.List;
 import com.webank.wecube.platform.core.dto.EntityDto;
 
 public interface ExpressionService {
-
-//    List<Object> fetchData(DataModelExpressionToRootData expressionToRootData);
-
-//    void writeBackData(DataModelExpressionToRootData expressionToRootData, Object updateData);
-
-//    List<TreeNode> getPreviewTree(DataModelExpressionToRootData expressionToRootData);
-
     List<EntityDto> getAllEntities(String dataModelExpression);
 }
