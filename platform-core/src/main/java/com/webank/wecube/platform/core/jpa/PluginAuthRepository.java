b6a69bee77a0ca74447f0a280b4244c4917a778c
gavin2lee <gavin2lee@163.com>
2020-07-15 15:08:14 +0800

    #1858 add auth impl

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginAuthRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginAuthRepository.java
@@ -1,9 +1,16 @@
 package com.webank.wecube.platform.core.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.webank.wecube.platform.core.entity.PluginAuthEntity;
 
-public interface PluginAuthRepository extends JpaRepository<PluginAuthEntity, String>{
+public interface PluginAuthRepository extends JpaRepository<PluginAuthEntity, String> {
 
+    @Query("select t from PluginAuthEntity t where t.pluginConfigId = :pluginConfigId and t.permissionType =:permission")
+    List<PluginAuthEntity> findAllByPluginConfigIdAndPermission(@Param("pluginConfigId") String pluginConfigId,
+            @Param("permission")String permission);
 }
