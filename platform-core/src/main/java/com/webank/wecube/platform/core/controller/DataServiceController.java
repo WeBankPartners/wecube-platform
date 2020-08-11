d5a0ce591fea540c18e1702a25301c803d9a5df5
gavin2lee <gavin2lee@163.com>
2020-08-11 16:01:14 +0800

    #1919 refactor controllers

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/DataServiceController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/DataServiceController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import com.webank.wecube.platform.core.dto.CommonResponseDto;
 import com.webank.wecube.platform.core.service.datamodel.NonExpressionServiceImpl;
 
@@ -25,51 +24,28 @@ public class DataServiceController {
 
     @PostMapping("/packages/{package-name}/entities/{entity-name}/create")
     public CommonResponseDto createEntity(@PathVariable("package-name") String packageName,
-                                     @PathVariable("entity-name") String entityName,
-                                     @RequestBody List<Map<String, Object>> request) {
-        try {
-            return CommonResponseDto.okayWithData(nonExpressionService.createEntity(packageName, entityName, request));
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-
+            @PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+        return CommonResponseDto.okayWithData(nonExpressionService.createEntity(packageName, entityName, request));
     }
 
     @GetMapping("/packages/{package-name}/entities/{entity-name}/retrieve")
-    public CommonResponseDto retrieveEntity(
-            @PathVariable(value = "package-name") String packageName,
+    public CommonResponseDto retrieveEntity(@PathVariable(value = "package-name") String packageName,
             @PathVariable(value = "entity-name") String entityName,
-            @RequestParam(required = false) Map<String, String> allFilters
-    ) {
-        try {
-            return CommonResponseDto.okayWithData(nonExpressionService.retrieveEntity(packageName, entityName, allFilters));
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
-
+            @RequestParam(required = false) Map<String, String> allFilters) {
+        return CommonResponseDto.okayWithData(nonExpressionService.retrieveEntity(packageName, entityName, allFilters));
     }
 
     @PostMapping("/packages/{package-name}/entities/{entity-name}/update")
     public CommonResponseDto updateEntity(@PathVariable("package-name") String packageName,
-                                     @PathVariable("entity-name") String entityName,
-                                     @RequestBody List<Map<String, Object>> request) {
-        try {
-            return CommonResponseDto.okayWithData(nonExpressionService.updateEntity(packageName, entityName, request));
-        } catch (WecubeCoreException ex) {
-            return CommonResponseDto.error(ex.getMessage());
-        }
+            @PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+        return CommonResponseDto.okayWithData(nonExpressionService.updateEntity(packageName, entityName, request));
     }
 
     @PostMapping("/packages/{package-name}/entities/{entity-name}/delete")
     public CommonResponseDto deleteEntity(@PathVariable("package-name") String packageName,
-                                     @PathVariable("entity-name") String entityName,
-                                     @RequestBody List<Map<String, Object>> request) {
-        try {
-            nonExpressionService.deleteEntity(packageName, entityName, request);
-            return CommonResponseDto.okay();
-        } catch (WecubeCoreException e) {
-            return CommonResponseDto.error(e.getMessage());
-        }
+            @PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+        nonExpressionService.deleteEntity(packageName, entityName, request);
+        return CommonResponseDto.okay();
 
     }
 
--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/PluginPackageResourceFileController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/PluginPackageResourceFileController.java
@@ -1,35 +1,25 @@
 package com.webank.wecube.platform.core.controller.plugin;
 
-import static com.webank.wecube.platform.core.dto.CommonResponseDto.error;
 import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webank.wecube.platform.core.commons.WecubeCoreException;
 import com.webank.wecube.platform.core.dto.CommonResponseDto;
 import com.webank.wecube.platform.core.service.plugin.PluginPackageResourceFileService;
 
 @RestController
 @RequestMapping("/v1")
 public class PluginPackageResourceFileController {
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private PluginPackageResourceFileService resourceFileService;
 
     @GetMapping("/resource-files")
     public CommonResponseDto getAllPluginPackageResourceFiles() {
-        try {
-            return okayWithData(resourceFileService.getAllPluginPackageResourceFiles());
-        } catch (WecubeCoreException e) {
-            log.error("Failed to get all PluginPackage resource files.");
-            return error(e.getMessage());
-        }
+        return okayWithData(resourceFileService.getAllPluginPackageResourceFiles());
     }
 
 }
