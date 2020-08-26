ba2c9a35058de3bf052d0589ee5514af9e8dc03c
gavin2lee <gavin2lee@163.com>
2020-08-26 09:45:18 +0800

    #1961 modify column data type to mediutext

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.6.1.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.6.1.sql
@@ -1,2 +1,4 @@
 update menu_items set menu_order = 6 where id = 'ADJUSTMENT';
 update menu_items set menu_order = 5 where id = 'INTELLIGENCE_OPS';
+
+alter table core_re_proc_def_info modify proc_def_data  MEDIUMTEXT;
