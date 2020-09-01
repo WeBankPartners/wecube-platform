e6cad7e5392b9e8c216c2fe6135b74111ddac6aa
gavin2lee <gavin2lee@163.com>
2020-09-01 11:33:06 +0800

    #1987 refactor auth client

--- platform-auth-client/src/main/java/com/webank/wecube/platform/auth/client/filter/JwtClientConfig.java
+++ platform-auth-client/src/main/java/com/webank/wecube/platform/auth/client/filter/JwtClientConfig.java
@@ -1,14 +1,14 @@
 package com.webank.wecube.platform.auth.client.filter;
 
 public class JwtClientConfig {
-	private String signingKey;
+    private String signingKey;
 
-	public String getSigningKey() {
-		return signingKey;
-	}
+    public String getSigningKey() {
+        return signingKey;
+    }
 
-	public void setSigningKey(String signingKey) {
-		this.signingKey = signingKey;
-	}
+    public void setSigningKey(String signingKey) {
+        this.signingKey = signingKey;
+    }
 
 }
--- platform-core/src/main/java/com/webank/wecube/platform/core/config/SpringWebConfig.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/config/SpringWebConfig.java
@@ -35,67 +35,67 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan({ "com.webank.wecube.platform.core.controller", "com.webank.wecube.platform.core.support.cache" })
 public class SpringWebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-	@Autowired
-	private AuthenticationRequestContextInterceptor authenticationRequestContextInterceptor;
+    @Autowired
+    private AuthenticationRequestContextInterceptor authenticationRequestContextInterceptor;
 
-	@Autowired
-	private CacheHandlerInterceptor cacheHandlerInterceptor;
+    @Autowired
+    private CacheHandlerInterceptor cacheHandlerInterceptor;
 
-	@Autowired
-	private ApplicationProperties applicationProperties;
+    @Autowired
+    private ApplicationProperties applicationProperties;
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 
-		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-		registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
-		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
-		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-		registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
-	}
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		configureLocalAuthentication(http);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        configureLocalAuthentication(http);
 
-	}
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(authenticationRequestContextInterceptor).addPathPatterns("/**");
-		registry.addInterceptor(cacheHandlerInterceptor).addPathPatterns("/**");
-		WebMvcConfigurer.super.addInterceptors(registry);
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationRequestContextInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(cacheHandlerInterceptor).addPathPatterns("/**");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 
-	protected void configureLocalAuthentication(HttpSecurity http) throws Exception {
-		http.authorizeRequests() //
-				.antMatchers("/swagger-ui.html/**", "/swagger-resources/**").permitAll()//
-				.antMatchers("/webjars/**").permitAll() //
-				.antMatchers("/v2/api-docs").permitAll() //
-				.antMatchers("/csrf").permitAll() //
-				.antMatchers("/v1/route-items").permitAll() //
-				.antMatchers("/v1/route-items/**").permitAll() //
-				.antMatchers("/v1/health-check").permitAll() //
-				.antMatchers("/v1/appinfo/loggers/query").permitAll() //
-				.antMatchers("/v1/appinfo/loggers/update").permitAll() //
-				.antMatchers("/v1/appinfo/version").permitAll() //
-				.anyRequest().authenticated() //
-				.and()//
-				.addFilter(jwtSsoBasedAuthenticationFilter())//
-				.csrf()//
-				.disable() //
-				.exceptionHandling() //
-				.authenticationEntryPoint(new Http401AuthenticationEntryPoint()); //
-	}
+    protected void configureLocalAuthentication(HttpSecurity http) throws Exception {
+        http.authorizeRequests() //
+                .antMatchers("/swagger-ui.html/**", "/swagger-resources/**").permitAll()//
+                .antMatchers("/webjars/**").permitAll() //
+                .antMatchers("/v2/api-docs").permitAll() //
+                .antMatchers("/csrf").permitAll() //
+                .antMatchers("/v1/route-items").permitAll() //
+                .antMatchers("/v1/route-items/**").permitAll() //
+                .antMatchers("/v1/health-check").permitAll() //
+                .antMatchers("/v1/appinfo/loggers/query").permitAll() //
+                .antMatchers("/v1/appinfo/loggers/update").permitAll() //
+                .antMatchers("/v1/appinfo/version").permitAll() //
+                .anyRequest().authenticated() //
+                .and()//
+                .addFilter(jwtSsoBasedAuthenticationFilter())//
+                .csrf()//
+                .disable() //
+                .exceptionHandling() //
+                .authenticationEntryPoint(new Http401AuthenticationEntryPoint()); //
+    }
 
-	protected Filter jwtSsoBasedAuthenticationFilter() throws Exception {
-		JwtClientConfig jwtClientConfig = new JwtClientConfig();
-		jwtClientConfig.setSigningKey(applicationProperties.getJwtSigningKey());
-		JwtSsoBasedAuthenticationFilter f = new JwtSsoBasedAuthenticationFilter(authenticationManager(),
-				jwtClientConfig);
-		return (Filter) f;
-	}
+    protected Filter jwtSsoBasedAuthenticationFilter() throws Exception {
+        JwtClientConfig jwtClientConfig = new JwtClientConfig();
+        jwtClientConfig.setSigningKey(applicationProperties.getJwtSigningKey());
+        JwtSsoBasedAuthenticationFilter f = new JwtSsoBasedAuthenticationFilter(authenticationManager(),
+                jwtClientConfig);
+        return (Filter) f;
+    }
 
 }
