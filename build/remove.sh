202f7f5b64118e3e2878b5cd17e832389e929af8
zgyzgyhero <894526647@qq.com>
2020-09-07 18:02:02 +0800

    Update remove bash

--- build/remove.sh
+++ build/remove.sh
@@ -2,4 +2,7 @@
 
 docker-compose -f docker-compose.yml down
 docker-compose -f wecube_core_mysql.yml down
-docker rmi `docker images|grep -v $1|grep -E 'platform-core|platform-gateway|wecube-portal|platform-auth-server|wecube-db'|awk '{print $1":"$2}'`
+if [[ -n `docker images|grep -v $1|grep -E 'platform-core|platform-gateway|wecube-portal|platform-auth-server|wecube-db'|awk '{print $1":"$2}'` ]]
+then
+  docker rmi `docker images|grep -v $1|grep -E 'platform-core|platform-gateway|wecube-portal|platform-auth-server|wecube-db'|awk '{print $1":"$2}'`
+fi
