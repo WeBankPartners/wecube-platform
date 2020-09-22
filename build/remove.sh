fd500f5d9dee853f3f5be8886ce6c30361d7cfff
zgyzgyhero <894526647@qq.com>
2020-09-22 09:46:04 +0800

    Update remove bash

--- build/remove.sh
+++ build/remove.sh
@@ -14,7 +14,3 @@ if [[ -n `docker images|grep -v $1|grep -E 'platform-core|platform-gateway|wecub
 then
   docker rmi `docker images|grep -v $1|grep -E 'platform-core|platform-gateway|wecube-portal|platform-auth-server|wecube-db'|awk '{print $1":"$2}'`
 fi
-if [[ -n `docker images|grep none|awk '{print $3}'` ]]
-then
-  docker rmi `docker images|grep none|awk '{print $3}'`
-fi
