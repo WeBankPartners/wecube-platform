5b03f557f72f36dabc11c8c214cc3448163630dd
gavin2lee <gavin2lee@163.com>
2020-10-21 16:20:22 +0800

    #162 add exception handling

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/event/OperationEventManagementService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/event/OperationEventManagementService.java
@@ -1,5 +1,6 @@
 package com.webank.wecube.platform.core.service.event;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -16,33 +17,33 @@ import com.webank.wecube.platform.core.jpa.event.OperationEventRepository;
 
 @Service
 public class OperationEventManagementService {
-    
+
     public static final String OPER_MODE_INSTANT = "instant";
     public static final String OPER_MODE_DEFER = "defer";
 
     private static final Logger log = LoggerFactory.getLogger(OperationEventManagementService.class);
-    
+
     public static final String BOOLEAN_TRUE = "Y";
     public static final String BOOLEAN_FALSE = "N";
 
     @Autowired
     private OperationEventRepository operationEventRepository;
-    
+
     @Autowired
     private OperationEventProcStarter operationEventProcStarter;
 
     public OperationEventResultDto reportOperationEvent(OperationEventDto eventDto) {
         validateOperationEventInput(eventDto);
-        
+
         String eventSeqNo = eventDto.getEventSeqNo();
 
         List<OperationEventEntity> operationEventEntities = operationEventRepository.findAllByEventSeqNo(eventSeqNo);
-        
-        if(operationEventEntities != null && !operationEventEntities.isEmpty()){
+
+        if (operationEventEntities != null && !operationEventEntities.isEmpty()) {
             log.error("operation event already exists,eventSeqNo={}", eventSeqNo);
-            throw new WecubeCoreException("3006","Operation event already exists.");
+            throw new WecubeCoreException("3006", "Operation event already exists.");
         }
-        
+
         OperationEventEntity newOperationEventEntity = new OperationEventEntity();
         newOperationEventEntity.setEventSeqNo(eventDto.getEventSeqNo());
         newOperationEventEntity.setEventType(eventDto.getEventType());
@@ -52,28 +53,37 @@ public class OperationEventManagementService {
         newOperationEventEntity.setOperationKey(eventDto.getOperationKey());
         newOperationEventEntity.setOperationUser(eventDto.getOperationUser());
         newOperationEventEntity.setSourceSubSystem(eventDto.getSourceSubSystem());
-        if(OPER_MODE_INSTANT.equalsIgnoreCase(eventDto.getOperationMode())){
+        if (OPER_MODE_INSTANT.equalsIgnoreCase(eventDto.getOperationMode())) {
             newOperationEventEntity.setStatus(OperationEventEntity.STATUS_IN_PROGRESS);
-        }else{
+        } else {
             newOperationEventEntity.setStatus(OperationEventEntity.STATUS_NEW);
         }
 
         OperationEventEntity savedOperEventEntity = operationEventRepository.saveAndFlush(newOperationEventEntity);
-        
-        if(OPER_MODE_INSTANT.equalsIgnoreCase(eventDto.getOperationMode())){
+
+        if (OPER_MODE_INSTANT.equalsIgnoreCase(eventDto.getOperationMode())) {
             return handleInstantOperationEvent(savedOperEventEntity);
         }
-        
+
         return fromOperationEventEntity(savedOperEventEntity);
     }
-    
-    private OperationEventResultDto handleInstantOperationEvent(OperationEventEntity instantOperEventEntity){
-        OperationEventEntity entity = operationEventProcStarter.startInstantOperationEventProcess(instantOperEventEntity);
-        
-        return fromOperationEventEntity(entity);
+
+    private OperationEventResultDto handleInstantOperationEvent(OperationEventEntity instantOperEventEntity) {
+        try {
+            OperationEventEntity entity = operationEventProcStarter
+                    .startInstantOperationEventProcess(instantOperEventEntity);
+            return fromOperationEventEntity(entity);
+        } catch (Exception e) {
+            log.error("failed to process instant operation event", e);
+            instantOperEventEntity.setUpdatedTime(new Date());
+            instantOperEventEntity.setStatus(OperationEventEntity.STATUS_IN_PROGRESS);
+            instantOperEventEntity.setPriority(-100);
+            throw new WecubeCoreException("Failed to process instant operation event");
+        }
+
     }
-    
-    private OperationEventResultDto fromOperationEventEntity(OperationEventEntity entity){
+
+    private OperationEventResultDto fromOperationEventEntity(OperationEventEntity entity) {
         OperationEventResultDto result = new OperationEventResultDto();
         result.setEventSeqNo(entity.getEventSeqNo());
         result.setEventType(entity.getEventType());
@@ -87,29 +97,29 @@ public class OperationEventManagementService {
         result.setProcInstKey(entity.getProcInstKey());
         result.setSourceSubSystem(entity.getSourceSubSystem());
         result.setStatus(entity.getStatus());
-        
+
         return result;
     }
-    
-    private Boolean parseBoolean(String booleanValueAsString){
-        if(StringUtils.isBlank(booleanValueAsString)){
+
+    private Boolean parseBoolean(String booleanValueAsString) {
+        if (StringUtils.isBlank(booleanValueAsString)) {
             return null;
         }
-        
-        if(BOOLEAN_TRUE.equalsIgnoreCase(booleanValueAsString)){
+
+        if (BOOLEAN_TRUE.equalsIgnoreCase(booleanValueAsString)) {
             return true;
         }
-        
-        if(BOOLEAN_FALSE.equalsIgnoreCase(booleanValueAsString)){
+
+        if (BOOLEAN_FALSE.equalsIgnoreCase(booleanValueAsString)) {
             return false;
         }
-        
-        throw new WecubeCoreException("3007","Boolean value must be 'Y' or 'N'");
+
+        throw new WecubeCoreException("3007", "Boolean value must be 'Y' or 'N'");
     }
 
     private void validateOperationEventInput(OperationEventDto eventDto) {
         if (eventDto == null) {
-            throw new WecubeCoreException("3008","Illegal input,cannot be null.");
+            throw new WecubeCoreException("3008", "Illegal input,cannot be null.");
         }
 
         if (StringUtils.isBlank(eventDto.getEventSeqNo()) || StringUtils.isBlank(eventDto.getEventType())
@@ -119,7 +129,7 @@ public class OperationEventManagementService {
             log.error("mandatory fields must provide,eventSeqNo={},eventType={},sourceSubSystem={},operationKey={}",
                     eventDto.getEventSeqNo(), eventDto.getEventType(), eventDto.getSourceSubSystem(),
                     eventDto.getOperationKey());
-            throw new WecubeCoreException("3009","Mandatory fields must provide.");
+            throw new WecubeCoreException("3009", "Mandatory fields must provide.");
         }
     }
 }
