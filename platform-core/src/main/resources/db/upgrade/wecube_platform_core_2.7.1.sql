a6fe6095e3f8fa0095a0815a95056f1793b77c0e
gavin2lee <gavin2lee@163.com>
2020-10-27 14:14:24 +0800

    #2046 add index

---
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.7.1.sql
@@ -0,0 +1,2 @@
+create index idx_core_operation_event_seq_no_1 on core_operation_event(event_seq_no);
+create index idx_core_ru_proc_exec_binding_inst_id_1 on core_ru_proc_exec_binding(proc_inst_id);
