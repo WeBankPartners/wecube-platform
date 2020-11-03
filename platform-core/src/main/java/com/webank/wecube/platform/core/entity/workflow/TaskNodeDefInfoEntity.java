0220481c1d810e6ebf0b9c0ba9f4535f513532a7
gavin2lee <gavin2lee@163.com>
2020-11-03 20:30:41 +0800

    #dev3 add batis

--- platform-core/src/main/java/com/webank/wecube/platform/core/entity/workflow/TaskNodeDefInfoEntity.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/workflow/TaskNodeDefInfoEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "CORE_RE_TASK_NODE_DEF_INFO")
-public class TaskNodeDefInfoEntity extends BaseStatusFeaturedEntity {
+public class TaskNodeDefInfoEntity extends AbstractStatusFeaturedEntity {
 
     public static final String DRAFT_STATUS = "draft";
     public static final String DEPLOYED_STATUS = "deployed";
