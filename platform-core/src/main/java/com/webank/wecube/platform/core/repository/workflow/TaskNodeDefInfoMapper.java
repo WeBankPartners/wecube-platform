f412c5b037a7fc77c1beded86a1e493deffb8462
gavin2lee <gavin2lee@163.com>
2020-11-04 17:51:23 +0800

    #dev3 add task node inst

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/TaskNodeDefInfoMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/TaskNodeDefInfoMapper.java
@@ -2,8 +2,7 @@ package com.webank.wecube.platform.core.repository.workflow;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
@@ -22,15 +21,11 @@ public interface TaskNodeDefInfoMapper {
 
     int updateByPrimaryKey(TaskNodeDefInfoEntity record);
 
-    @Query("select t from TaskNodeDefInfoEntity t "
-            + " where t.procDefId = :procDefId and t.nodeId = :nodeId and t.status = :status")
     TaskNodeDefInfoEntity findOneWithProcessIdAndNodeIdAndStatus(@Param("procDefId") String procDefId,
             @Param("nodeId") String nodeId, @Param("status") String status);
 
-    @Query("select t from TaskNodeDefInfoEntity t " + " where t.procDefId = :procDefId")
     List<TaskNodeDefInfoEntity> findAllByProcDefId(@Param("procDefId") String procDefId);
 
-    @Query("select t from TaskNodeDefInfoEntity t " + " where t.procDefId = :procDefId and t.status = :status ")
     List<TaskNodeDefInfoEntity> findAllByProcDefIdAndStatus(@Param("procDefId") String procDefId,
             @Param("status") String status);
 }
