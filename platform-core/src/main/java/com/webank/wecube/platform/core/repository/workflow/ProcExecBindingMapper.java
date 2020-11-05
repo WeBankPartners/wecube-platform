03b99c4d1e951e8dca57933697d6bd52380d538b
gavin2lee <gavin2lee@163.com>
2020-11-05 22:11:03 +0800

    #dev3 add mapper

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/ProcExecBindingMapper.java
@@ -0,0 +1,47 @@
+package com.webank.wecube.platform.core.repository.workflow;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.webank.wecube.platform.core.entity.workflow.ProcExecBindingEntity;
+
+@Repository
+public interface ProcExecBindingMapper{
+    
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProcExecBindingEntity record);
+
+    int insertSelective(ProcExecBindingEntity record);
+
+    ProcExecBindingEntity selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProcExecBindingEntity record);
+
+    int updateByPrimaryKey(ProcExecBindingEntity record);
+
+    /**
+     * 
+     * @param procInstId
+     * @param nodeInstId
+     * @return
+     */
+    List<ProcExecBindingEntity> findAllTaskNodeBindings(@Param("procInstId") Integer procInstId,
+            @Param("nodeInstId") Integer nodeInstId);
+
+    /**
+     * 
+     * @param procInstId
+     * @return
+     */
+    ProcExecBindingEntity findProcInstBindings(@Param("procInstId") Integer procInstId);
+    
+    /**
+     * 
+     * @param procInstId
+     * @return
+     */
+    List<ProcExecBindingEntity> findAllTaskNodeBindingsByProcInstId(@Param("procInstId") Integer procInstId);
+}
