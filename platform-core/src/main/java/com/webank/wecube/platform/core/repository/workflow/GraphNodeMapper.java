fd93f10406421f64251fc2e0a23e5680da4363ec
gavin2lee <gavin2lee@163.com>
2020-11-06 17:58:48 +0800

    #dev3 role binding

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/GraphNodeMapper.java
@@ -0,0 +1,39 @@
+package com.webank.wecube.platform.core.repository.workflow;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.webank.wecube.platform.core.entity.workflow.GraphNodeEntity;
+
+@Repository
+public interface GraphNodeMapper{
+    
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(GraphNodeEntity record);
+
+    int insertSelective(GraphNodeEntity record);
+
+    GraphNodeEntity selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(GraphNodeEntity record);
+
+
+    int updateByPrimaryKey(GraphNodeEntity record);
+
+    /**
+     * 
+     * @param processSessionId
+     * @return
+     */
+    List<GraphNodeEntity> findAllByProcessSessionId(@Param("processSessionId") String processSessionId);
+
+    /**
+     * 
+     * @param procInstId
+     * @return
+     */
+    List<GraphNodeEntity> findAllByProcInstId(@Param("procInstId") Integer procInstId);
+}
