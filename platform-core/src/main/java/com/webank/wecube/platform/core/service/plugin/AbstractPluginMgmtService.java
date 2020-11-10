876385ed8a86e2fd2f9562d7bcc1cfc9283ecec7
gavin2lee <gavin2lee@163.com>
2020-11-10 22:24:36 +0800

    #dev3 refactor plugin artifacts management

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/AbstractPluginMgmtService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/AbstractPluginMgmtService.java
@@ -1,5 +1,6 @@
 package com.webank.wecube.platform.core.service.plugin;
 
+import java.io.Closeable;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -7,14 +8,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.webank.wecube.platform.core.commons.ApplicationProperties.PluginProperties;
 import com.webank.wecube.platform.core.entity.plugin.SystemVariables;
 import com.webank.wecube.platform.core.repository.plugin.SystemVariablesMapper;
+import com.webank.wecube.platform.core.support.S3Client;
 
 public abstract class AbstractPluginMgmtService {
     private static final Logger log = LoggerFactory.getLogger(AbstractPluginMgmtService.class);
     
     @Autowired
-    private SystemVariablesMapper systemVariablesMapper;
+    protected SystemVariablesMapper systemVariablesMapper;
+    
+    @Autowired
+    protected PluginProperties pluginProperties;
+    
+    @Autowired
+    protected S3Client s3Client;
     
     protected String getGlobalSystemVariableByName(String varName) {
         List<SystemVariables> vars = systemVariablesMapper.findByNameAndScopeAndStatus(varName,
@@ -29,6 +38,29 @@ public abstract class AbstractPluginMgmtService {
             varVal = var.getDefaultValue();
         }
         return varVal;
+    }
+    
+    protected String stripString(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        if (s.length() > 250) {
+            return s.substring(0, 250);
+        }
+
+        return s;
+    }
+    
+    protected void closeSilently(Closeable c) {
+        if (c == null) {
+            return;
+        }
+
+        try {
+            c.close();
+        } catch (Exception e) {
+        }
     }
 
 }
