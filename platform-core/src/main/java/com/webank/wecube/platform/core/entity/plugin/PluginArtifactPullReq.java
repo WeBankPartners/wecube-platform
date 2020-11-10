4741e040d72980c2eee3dd9cc41595fec10598f6
gavin2lee <gavin2lee@163.com>
2020-11-10 18:06:48 +0800

    #dev3 add artifacts management mapper

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/plugin/PluginArtifactPullReq.java
@@ -0,0 +1,129 @@
+package com.webank.wecube.platform.core.entity.plugin;
+
+import java.util.Date;
+
+public class PluginArtifactPullReq {
+    public static final String STATE_IN_PROGRESS = "InProgress";
+    public static final String STATE_COMPLETED = "Completed";
+    public static final String STATE_FAULTED = "Faulted";
+    
+    private String id;
+
+    private String bucketName;
+
+    private String createdBy;
+
+    private Date createdTime;
+
+    private String errMsg;
+
+    private String keyName;
+
+    private String pkgId;
+
+    private Integer rev;
+
+    private String state;
+
+    private Long totalSize;
+
+    private String updatedBy;
+
+    private Date updatedTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName == null ? null : bucketName.trim();
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy == null ? null : createdBy.trim();
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg == null ? null : errMsg.trim();
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+
+    public void setKeyName(String keyName) {
+        this.keyName = keyName == null ? null : keyName.trim();
+    }
+
+    public String getPkgId() {
+        return pkgId;
+    }
+
+    public void setPkgId(String pkgId) {
+        this.pkgId = pkgId == null ? null : pkgId.trim();
+    }
+
+    public Integer getRev() {
+        return rev;
+    }
+
+    public void setRev(Integer rev) {
+        this.rev = rev;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Long getTotalSize() {
+        return totalSize;
+    }
+
+    public void setTotalSize(Long totalSize) {
+        this.totalSize = totalSize;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy == null ? null : updatedBy.trim();
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
