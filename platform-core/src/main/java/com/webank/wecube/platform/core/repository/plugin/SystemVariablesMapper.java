cb7ac3b5dfef09b7a2f5e6196fedc14279f221a8
gavin2lee <gavin2lee@163.com>
2020-11-10 17:44:24 +0800

    #dev3 add mappers

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/SystemVariablesMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/SystemVariablesMapper.java
@@ -1,7 +1,13 @@
 package com.webank.wecube.platform.core.repository.plugin;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
 import com.webank.wecube.platform.core.entity.plugin.SystemVariables;
 
+@Repository
 public interface SystemVariablesMapper {
     int deleteByPrimaryKey(String id);
 
@@ -14,4 +20,24 @@ public interface SystemVariablesMapper {
     int updateByPrimaryKeySelective(SystemVariables record);
 
     int updateByPrimaryKey(SystemVariables record);
+
+    /**
+     * 
+     * @param name
+     * @param scope
+     * @param status
+     * @return
+     */
+    List<SystemVariables> findByNameAndScopeAndStatus(@Param("name") String name, @Param("scope") String scope,
+            @Param("status") String status);
+
+    /**
+     * 
+     * @param name
+     * @param scope
+     * @param source
+     * @return
+     */
+    List<SystemVariables> findByNameAndScopeAndSource(@Param("name") String name, @Param("scope") String scope,
+            @Param("source") String source);
 }
