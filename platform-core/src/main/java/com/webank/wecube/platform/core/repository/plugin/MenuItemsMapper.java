5dc78dcef266e489a21f0e9a1d65d71fafb2384e
gavin2lee <gavin2lee@163.com>
2020-11-11 14:45:59 +0800

    #dev3 add mappers

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/MenuItemsMapper.java
@@ -0,0 +1,20 @@
+package com.webank.wecube.platform.core.repository.plugin;
+
+import org.springframework.stereotype.Repository;
+
+import com.webank.wecube.platform.core.entity.plugin.MenuItems;
+
+@Repository
+public interface MenuItemsMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(MenuItems record);
+
+    int insertSelective(MenuItems record);
+
+    MenuItems selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(MenuItems record);
+
+    int updateByPrimaryKey(MenuItems record);
+}
