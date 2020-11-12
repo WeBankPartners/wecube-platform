9520f3ddc26286feaec787f70cc846eed84ded5b
gavin2lee <gavin2lee@163.com>
2020-11-12 17:24:56 +0800

    #dev3 add mappers

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/PluginPackageMgmtService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/PluginPackageMgmtService.java
@@ -1,43 +1,46 @@
 package com.webank.wecube.platform.core.service.plugin;
 
-import static com.webank.wecube.platform.core.domain.plugin.PluginPackage.Status.REGISTERED;
-
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
-import com.webank.wecube.platform.core.domain.plugin.PluginPackage;
-import com.webank.wecube.platform.core.domain.plugin.PluginPackageAuthority;
-import com.webank.wecube.platform.core.domain.plugin.PluginPackageMenu;
 import com.webank.wecube.platform.core.dto.PluginPackageInfoDto;
 import com.webank.wecube.platform.core.dto.user.RoleDto;
 import com.webank.wecube.platform.core.entity.plugin.PluginPackageAuthorities;
 import com.webank.wecube.platform.core.entity.plugin.PluginPackageDependencies;
+import com.webank.wecube.platform.core.entity.plugin.PluginPackageMenus;
 import com.webank.wecube.platform.core.entity.plugin.PluginPackages;
+import com.webank.wecube.platform.core.entity.plugin.SystemVariables;
 import com.webank.wecube.platform.core.repository.plugin.PluginPackageAuthoritiesMapper;
 import com.webank.wecube.platform.core.repository.plugin.PluginPackageDependenciesMapper;
+import com.webank.wecube.platform.core.repository.plugin.PluginPackageMenusMapper;
 import com.webank.wecube.platform.core.repository.plugin.PluginPackagesMapper;
+import com.webank.wecube.platform.core.service.user.RoleMenuService;
 import com.webank.wecube.platform.core.support.authserver.AsAuthorityDto;
 import com.webank.wecube.platform.core.support.authserver.AsRoleAuthoritiesDto;
 import com.webank.wecube.platform.core.utils.DateUtils;
+import com.webank.wecube.platform.core.utils.StringUtilsEx;
+import com.webank.wecube.platform.core.utils.SystemUtils;
 
 public class PluginPackageMgmtService extends AbstractPluginMgmtService {
     private static final Logger log = LoggerFactory.getLogger(PluginPackageMgmtService.class);
 
     public static final String PLATFORM_NAME = "platform";
-
-    
 
     @Autowired
     private PluginPackagesMapper pluginPackagesMapper;
@@ -47,6 +50,12 @@ public class PluginPackageMgmtService extends AbstractPluginMgmtService {
 
     @Autowired
     private PluginPackageAuthoritiesMapper pluginPackageAuthoritiesMapper;
+
+    @Autowired
+    private PluginPackageMenusMapper pluginPackageMenusMapper;
+
+    @Autowired
+    private RoleMenuService roleMenuService;
 
     private VersionComparator versionComparator = new VersionComparator();
 
@@ -85,6 +94,7 @@ public class PluginPackageMgmtService extends AbstractPluginMgmtService {
 
     }
 
+    @Transactional
     public PluginPackageInfoDto registerPluginPackage(String pluginPackageId) {
 
         PluginPackages pluginPackageEntity = pluginPackagesMapper.selectByPrimaryKey(pluginPackageId);
@@ -101,44 +111,161 @@ public class PluginPackageMgmtService extends AbstractPluginMgmtService {
 
         createRolesIfNotExistInSystem(pluginPackageEntity);
 
-        bindRoleToMenuWithAuthority(pluginPackage);
+        bindRoleToMenuWithAuthority(pluginPackageEntity);
 
-        updateSystemVariableStatus(pluginPackage);
+        updateSystemVariableStatus(pluginPackageEntity);
 
-        deployPluginUiResourcesIfRequired(pluginPackage);
+        deployPluginUiResourcesIfRequired(pluginPackageEntity);
 
-        pluginPackage.setStatus(REGISTERED);
+        pluginPackageEntity.setStatus(PluginPackages.REGISTERED);
+        pluginPackagesMapper.updateByPrimaryKeySelective(pluginPackageEntity);
 
-        return pluginPackageRepository.save(pluginPackage);
+        PluginPackageInfoDto result = buildPluginPackageInfoDto(pluginPackageEntity);
+
+        return result;
     }
     
-    private void bindRoleToMenuWithAuthority(PluginPackage pluginPackage) {
-        final Set<PluginPackageAuthority> pluginPackageAuthorities = pluginPackage.getPluginPackageAuthorities();
-        final List<String> selfPkgMenuCodeList = pluginPackage.getPluginPackageMenus().stream()
-                .map(PluginPackageMenu::getCode).collect(Collectors.toList());
-        if (null != pluginPackageAuthorities && pluginPackageAuthorities.size() > 0) {
-            pluginPackageAuthorities.forEach(pluginPackageAuthority -> {
-                final String roleName = pluginPackageAuthority.getRoleName();
-                final String menuCode = pluginPackageAuthority.getMenuCode();
+    private void deployPluginUiResourcesIfRequired(PluginPackages pluginPackage) {
+        if (!pluginPackage.getUiPackageIncluded()) {
+            return;
+        }
+        // download UI package from MinIO
+        String tmpFolderName = SystemUtils.getTempFolderPath()
+                + new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+        String downloadUiZipPath = tmpFolderName + File.separator + pluginProperties.getUiFile();
+        log.info("Download UI.zip from S3 to " + downloadUiZipPath);
 
-                // create role menu binding
-                if (!selfPkgMenuCodeList.contains(menuCode)) {
-                    String msg = String.format(
-                            "The declared menu code: [%s] in <authorities> field doesn't declared in <menus> field of register.xml",
-                            menuCode);
-                    log.error(msg);
-                    throw new WecubeCoreException("3104", msg, menuCode);
+        String s3UiPackagePath = pluginPackage.getName() + File.separator + pluginPackage.getVersion() + File.separator
+                + pluginProperties.getUiFile();
+        s3Client.downFile(pluginProperties.getPluginPackageBucketName(), s3UiPackagePath, downloadUiZipPath);
+
+        String remotePath = pluginProperties.getStaticResourceServerPath() + File.separator + pluginPackage.getName()
+                + File.separator + pluginPackage.getVersion() + File.separator;
+        log.info("Upload UI.zip from local to static server:" + remotePath);
+
+        // get all static resource hosts
+        List<String> staticResourceIps = StringUtilsEx.splitByComma(pluginProperties.getStaticResourceServerIp());
+
+        for (String remoteIp : staticResourceIps) {
+
+            // mkdir at remote host
+            if (!remotePath.equals("/") && !remotePath.equals(".")) {
+                String mkdirCmd = String.format("rm -rf %s && mkdir -p %s", remotePath, remotePath);
+
+                try {
+                    commandService.runAtRemote(remoteIp, pluginProperties.getStaticResourceServerUser(),
+                            pluginProperties.getStaticResourceServerPassword(),
+                            pluginProperties.getStaticResourceServerPort(), mkdirCmd);
+                } catch (Exception e) {
+                    log.error("Run command [mkdir] meet error: ", e.getMessage());
+                    throw new WecubeCoreException("3110",
+                            String.format("Run remote command meet error: %s", e.getMessage()), e.getMessage());
                 }
-                this.roleMenuService.createRoleMenuBinding(roleName, menuCode);
+            }
 
-                // grant authority to role and send request to auth server
-                AsAuthorityDto grantAuthority = new AsAuthorityDto();
-                grantAuthority.setCode(menuCode);
-                AsRoleAuthoritiesDto grantAuthorityToRole = new AsRoleAuthoritiesDto();
-                grantAuthorityToRole.setRoleName(roleName);
-                grantAuthorityToRole.setAuthorities(Collections.singletonList(grantAuthority));
-                this.authServerRestClient.configureRoleAuthoritiesWithRoleName(grantAuthorityToRole);
-            });
+            // scp UI.zip to Static Resource Server
+            try {
+                scpService.put(remoteIp, pluginProperties.getStaticResourceServerPort(),
+                        pluginProperties.getStaticResourceServerUser(),
+                        pluginProperties.getStaticResourceServerPassword(), downloadUiZipPath, remotePath);
+            } catch (Exception e) {
+                throw new WecubeCoreException("3111",
+                        String.format("Put file to remote host meet error: ", e.getMessage()));
+            }
+            log.info("scp UI.zip to Static Resource Server - Done");
+
+            // unzip file
+            String unzipCmd = String.format("cd %s && unzip %s", remotePath, pluginProperties.getUiFile());
+            try {
+                commandService.runAtRemote(remoteIp, pluginProperties.getStaticResourceServerUser(),
+                        pluginProperties.getStaticResourceServerPassword(),
+                        pluginProperties.getStaticResourceServerPort(), unzipCmd);
+            } catch (Exception e) {
+                log.error("Run command [unzip] meet error: ", e.getMessage());
+                throw new WecubeCoreException("3112",
+                        String.format("Run remote command meet error: %s", e.getMessage()));
+            }
+        }
+
+        log.info("UI package deployment has done...");
+
+    }
+
+    private void updateSystemVariableStatus(PluginPackages pluginPackage) {
+        List<PluginPackages> pluginPackagesEntities = pluginPackagesMapper.selectAllByName(pluginPackage.getName());
+
+        List<String> pluginPackageIds = new ArrayList<String>();
+        
+        for(PluginPackages p : pluginPackagesEntities){
+            pluginPackageIds.add(p.getId());
+        }
+        
+
+        List<SystemVariables> systemVariablesEntities = systemVariablesMapper.selectAllByPluginPackages(pluginPackageIds);
+        for(SystemVariables systemVariableEntity : systemVariablesEntities){
+            if (SystemVariables.ACTIVE.equals(systemVariableEntity.getStatus())
+                    && !pluginPackage.getId().equals(systemVariableEntity.getSource())) {
+                systemVariableEntity.setStatus(SystemVariables.INACTIVE);
+                systemVariablesMapper.updateByPrimaryKeySelective(systemVariableEntity);
+            }
+            
+            if (SystemVariables.INACTIVE.equals(systemVariableEntity.getStatus())
+                    && pluginPackage.getId().equals(systemVariableEntity.getSource())) {
+                systemVariableEntity.setStatus(SystemVariables.ACTIVE);
+                systemVariablesMapper.updateByPrimaryKeySelective(systemVariableEntity);
+            }
+        }
+    }
+
+    private PluginPackageMenus pickoutAnyPluginPackageMenusByMenuCode(String menuCode,
+            List<PluginPackageMenus> pluginPackageMenusEntities) {
+        for (PluginPackageMenus m : pluginPackageMenusEntities) {
+            if (menuCode.equals(m.getCode())) {
+                return m;
+            }
+        }
+
+        return null;
+    }
+
+    private void bindRoleToMenuWithAuthority(PluginPackages pluginPackage) {
+        List<PluginPackageAuthorities> pluginPackageAuthoritiesEntities = pluginPackageAuthoritiesMapper
+                .selectAllByPackage(pluginPackage.getId());
+
+        if (pluginPackageAuthoritiesEntities == null || pluginPackageAuthoritiesEntities.isEmpty()) {
+            return;
+        }
+
+        List<PluginPackageMenus> pluginPackageMenusEntities = pluginPackageMenusMapper
+                .selectAllMenusByPackage(pluginPackage.getId());
+
+        if (pluginPackageMenusEntities == null || pluginPackageMenusEntities.isEmpty()) {
+            throw new WecubeCoreException(
+                    "There is not any plugin package menus configured for package:" + pluginPackage.getName());
+        }
+
+        for (PluginPackageAuthorities pluginPackageAuthoritiesEntity : pluginPackageAuthoritiesEntities) {
+            String roleName = pluginPackageAuthoritiesEntity.getRoleName();
+            String menuCode = pluginPackageAuthoritiesEntity.getMenuCode();
+
+            PluginPackageMenus pickedOutPluginPackageMenuEntity = pickoutAnyPluginPackageMenusByMenuCode(menuCode,
+                    pluginPackageMenusEntities);
+
+            if (pickedOutPluginPackageMenuEntity == null) {
+                String msg = String.format(
+                        "The declared menu code: [%s] in <authorities> field doesn't declared in <menus> field of register.xml",
+                        menuCode);
+                log.error(msg);
+                throw new WecubeCoreException("3104", msg, menuCode);
+            }
+
+            roleMenuService.createRoleMenuBinding(roleName, menuCode);
+            AsAuthorityDto grantAuthority = new AsAuthorityDto();
+            grantAuthority.setCode(menuCode);
+            AsRoleAuthoritiesDto grantAuthorityToRole = new AsRoleAuthoritiesDto();
+            grantAuthorityToRole.setRoleName(roleName);
+            grantAuthorityToRole.setAuthorities(Collections.singletonList(grantAuthority));
+            this.authServerRestClient.configureRoleAuthoritiesWithRoleName(grantAuthorityToRole);
         }
     }
 
@@ -170,7 +297,7 @@ public class PluginPackageMgmtService extends AbstractPluginMgmtService {
 
     private void ensureNoMoreThanTwoActivePackages(PluginPackages pluginPackage) {
         List<PluginPackages> allActivePluginPackageEntities = pluginPackagesMapper
-                .selectAllByNameAndStatuses(pluginPackage.getName(), PLUGIN_PACKAGE_ACTIVE_STATUS);
+                .selectAllByNameAndStatuses(pluginPackage.getName(), PluginPackages.PLUGIN_PACKAGE_ACTIVE_STATUSES);
 
         if (allActivePluginPackageEntities == null || allActivePluginPackageEntities.isEmpty()) {
             return;
