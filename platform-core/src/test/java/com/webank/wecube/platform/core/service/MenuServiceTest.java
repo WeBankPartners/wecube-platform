433b13b2f693c0789248444aa2c0105750fc7de0
gavin2lee <gavin2lee@163.com>
2020-11-14 22:42:08 +0800

    #dev fix unit tests

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/MenuServiceTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/MenuServiceTest.java
@@ -1,23 +1,22 @@
 package com.webank.wecube.platform.core.service;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import com.webank.wecube.platform.core.DatabaseBasedTest;
-import com.webank.wecube.platform.core.domain.MenuItem;
-import com.webank.wecube.platform.core.domain.plugin.PluginPackage;
-import com.webank.wecube.platform.core.domain.plugin.PluginPackageMenu;
-import com.webank.wecube.platform.core.dto.MenuItemDto;
-import com.webank.wecube.platform.core.jpa.MenuItemRepository;
-import com.webank.wecube.platform.core.jpa.PluginPackageMenuRepository;
-import com.webank.wecube.platform.core.jpa.PluginPackageRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.sql.Timestamp;
-import java.util.List;
-
-import static com.webank.wecube.platform.core.domain.plugin.PluginPackage.Status.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import com.webank.wecube.platform.core.DatabaseBasedTest;
+import com.webank.wecube.platform.core.dto.MenuItemDto;
+import com.webank.wecube.platform.core.entity.plugin.MenuItems;
+import com.webank.wecube.platform.core.entity.plugin.PluginPackageMenus;
+import com.webank.wecube.platform.core.entity.plugin.PluginPackages;
+import com.webank.wecube.platform.core.repository.plugin.MenuItemsMapper;
+import com.webank.wecube.platform.core.repository.plugin.PluginPackageMenusMapper;
+import com.webank.wecube.platform.core.repository.plugin.PluginPackagesMapper;
+import com.webank.wecube.platform.workflow.commons.LocalIdGenerator;
 
 public class MenuServiceTest extends DatabaseBasedTest {
 
@@ -25,53 +24,107 @@ public class MenuServiceTest extends DatabaseBasedTest {
     private MenuService menuService;
 
     @Autowired
-    private PluginPackageRepository pluginPackageRepository;
+    private PluginPackagesMapper pluginPackageRepository;
 
     @Autowired
-    private MenuItemRepository menuItemRepository;
+    private MenuItemsMapper menuItemRepository;
+    
+    @Autowired
+    private PluginPackageMenusMapper pluginPackageMenusMapper;
 
     @Test
-    public void givenMultipleVersionPackagesWithMenuWhenFindAllForAllActivePackagesThenReturnOnlyMenusForActivePackages() {
+    public void givenMultiVerPkgsWithMenuWhenFindAllForAllActivePackagesThenReturnOnlyMenusForActivePackages() {
 
         long now = System.currentTimeMillis();
-        PluginPackage unregisteredPluginPackage = new PluginPackage(null, "wecmdb", "v0.1", UNREGISTERED,
-                new Timestamp(now), false);
-        PluginPackage runningPluginPackage = new PluginPackage(null, "wecmdb", "v0.3", RUNNING,
-                new Timestamp(now + 20000), false);
-        PluginPackage stoppedPluginPackage = new PluginPackage(null, "wecmdb", "v0.4", STOPPED,
-                new Timestamp(now + 30000), false);
-        PluginPackage decommissionedPluginPackage = new PluginPackage(null, "wecmdb", "v0.5", DECOMMISSIONED,
-                new Timestamp(now + 40000), false);
+        PluginPackages unregisteredPluginPackage = buildPluginPackages(LocalIdGenerator.generateId(), "wecmdb", "v0.1",
+                PluginPackages.UNREGISTERED, new Date(now), false);
+        PluginPackages runningPluginPackage = buildPluginPackages(LocalIdGenerator.generateId(), "wecmdb", "v0.3", PluginPackages.RUNNING,
+                new Date(now + 20000), false);
+        PluginPackages stoppedPluginPackage = buildPluginPackages(LocalIdGenerator.generateId(), "wecmdb", "v0.4", PluginPackages.STOPPED,
+                new Date(now + 30000), false);
+        PluginPackages decommissionedPluginPackage = buildPluginPackages(LocalIdGenerator.generateId(), "wecmdb", "v0.5",
+                PluginPackages.DECOMMISSIONED, new Date(now + 40000), false);
 
-        MenuItem menuItem = new MenuItem("DESIGNING", null, null);
+        MenuItems menuItem = new MenuItems();
+        menuItem.setId(LocalIdGenerator.generateId());
+        menuItem.setCode("DESIGNING");
 
-        PluginPackageMenu packageMenuForUnregistered = new PluginPackageMenu(unregisteredPluginPackage,
+        PluginPackageMenus packageMenuForUnregistered = buildPluginPackageMenus(unregisteredPluginPackage,
                 "DESIGNING_CI_DATA_ENQUIRY", "DESIGNING", "CI Data Enquiry", "/wecmdb/designing/ci-data-enquiry");
-        PluginPackageMenu packageMenuForRunning1 = new PluginPackageMenu(runningPluginPackage,
+        
+        pluginPackageMenusMapper.insert(packageMenuForUnregistered);
+        PluginPackageMenus packageMenuForRunning1 = buildPluginPackageMenus(runningPluginPackage,
                 "DESIGNING_CI_INTEGRATED_QUERY_EXECUTION", "DESIGNING", "CI Integrated Enquiry",
                 "/wecmdb/designing/ci-integrated-query-execution");
-        PluginPackageMenu packageMenuForRunning2 = new PluginPackageMenu(runningPluginPackage,
+        
+        pluginPackageMenusMapper.insert(packageMenuForRunning1);
+        PluginPackageMenus packageMenuForRunning2 = buildPluginPackageMenus(runningPluginPackage,
                 "CMDB_DESIGNING_ENUM_ENQUIRY", "DESIGNING", "Enum Enquiry", "/wecmdb/designing/enum-enquiry");
-        PluginPackageMenu packageMenuForStopped = new PluginPackageMenu(stoppedPluginPackage,
+        
+        pluginPackageMenusMapper.insert(packageMenuForRunning2);
+        PluginPackageMenus packageMenuForStopped = buildPluginPackageMenus(stoppedPluginPackage,
                 "DESIGNING_CI_INTEGRATED_QUERY_MANAGEMENT", "DESIGNING", "CI Integrated Enquiry Management",
                 "/wecmdb/designing/ci-integrated-query-management");
-        PluginPackageMenu packageMenuForDecommissioned = new PluginPackageMenu(decommissionedPluginPackage,
+        
+        pluginPackageMenusMapper.insert(packageMenuForStopped);
+        PluginPackageMenus packageMenuForDecommissioned = buildPluginPackageMenus(decommissionedPluginPackage,
                 "DESIGNING_CI_DATA_MANAGEMENT", "DESIGNING", "CI Data Management",
                 "/wecmdb/designing/ci-data-management");
+        
+        
+        pluginPackageMenusMapper.insert(packageMenuForDecommissioned);
 
-        unregisteredPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForUnregistered));
-        runningPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForRunning1, packageMenuForRunning2));
-        stoppedPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForStopped));
-        decommissionedPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForDecommissioned));
+        // unregisteredPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForUnregistered));
+        // runningPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForRunning1,
+        // packageMenuForRunning2));
+        // stoppedPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForStopped));
+        // decommissionedPluginPackage.setPluginPackageMenus(Sets.newHashSet(packageMenuForDecommissioned));
 
-        menuItemRepository.save(menuItem);
+        menuItemRepository.insert(menuItem);
 
-        pluginPackageRepository.saveAll(Lists.newArrayList(unregisteredPluginPackage, runningPluginPackage,
-                stoppedPluginPackage, decommissionedPluginPackage));
+        pluginPackageRepository.insert(unregisteredPluginPackage);
+        pluginPackageRepository.insert(runningPluginPackage);
+
+        pluginPackageRepository.insert(stoppedPluginPackage);
+
+        pluginPackageRepository.insert(decommissionedPluginPackage);
 
         List<MenuItemDto> allMenus = menuService.getAllMenus();
         assertThat(allMenus).isNotNull();
         assertThat(allMenus).hasSize(4);
+
+    }
+
+    private PluginPackageMenus buildPluginPackageMenus(PluginPackages pluginPackage, String code, String category,
+            String displayName, String path) {
+
+        PluginPackageMenus e = new PluginPackageMenus();
+        // e.setActive(active);
+        e.setCategory(category);
+        e.setCode(code);
+        e.setDisplayName(displayName);
+        e.setPath(path);
+        e.setCategory(category);
+        e.setPluginPackageId(pluginPackage.getId());
+        
+        e.setId(LocalIdGenerator.generateId());
+
+        return e;
+
+    }
+
+    private PluginPackages buildPluginPackages(String id, String name, String version, String status,
+            Date uploadTimestamp, boolean uiPackageIncluded) {
+        PluginPackages e = new PluginPackages();
+        e.setId(id);
+        e.setName(name);
+        e.setStatus(status);
+        e.setUiPackageIncluded(uiPackageIncluded);
+        e.setUploadTimestamp(uploadTimestamp);
+        e.setVersion(version);
+        
+
+        return e;
 
     }
 
