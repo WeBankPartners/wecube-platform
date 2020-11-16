a072c458bdb1956ffe7992a9bdc98bb12a610728
gavin2lee <gavin2lee@163.com>
2020-11-16 16:19:57 +0800

    #dev3 plugin config service refactor

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/PluginConfigInterfacesMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/PluginConfigInterfacesMapper.java
@@ -6,7 +6,13 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.plugin.PluginConfigInterfaces;
+import com.webank.wecube.platform.core.entity.plugin.RichPluginConfigInterfaces;
 
+/**
+ * 
+ * @author gavin
+ *
+ */
 @Repository
 public interface PluginConfigInterfacesMapper {
     int deleteByPrimaryKey(String id);
@@ -20,11 +26,20 @@ public interface PluginConfigInterfacesMapper {
     int updateByPrimaryKeySelective(PluginConfigInterfaces record);
 
     int updateByPrimaryKey(PluginConfigInterfaces record);
-    
+
     /**
      * 
      * @param pluginConfigId
      * @return
      */
     List<PluginConfigInterfaces> selectAllByPluginConfig(@Param("pluginConfigId") String pluginConfigId);
+
+    /**
+     * 
+     * @param serviceName
+     * @param configStatus
+     * @return
+     */
+    List<RichPluginConfigInterfaces> selectAllByServiceNameAndConfigStatus(@Param("serviceName") String serviceName,
+            @Param("configStatus") String configStatus);
 }
