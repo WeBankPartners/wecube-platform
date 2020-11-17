5fdf0812c36dd4ddc4694169db727fd1c4e3f06b
gavin2lee <gavin2lee@163.com>
2020-11-17 19:23:24 +0800

    #dev3 refactor plugin instance service

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.webank.wecube.platform.core.domain.ResourceServer;
+import com.webank.wecube.platform.core.domain.ResourceServerDomain;
 
-public interface ResourceServerRepository extends CrudRepository<ResourceServer, String> {
-    List<ResourceServer> findByHostAndType(String host, String type);
+public interface ResourceServerRepository extends CrudRepository<ResourceServerDomain, String> {
+    List<ResourceServerDomain> findByHostAndType(String host, String type);
 }
