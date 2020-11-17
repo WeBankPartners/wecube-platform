def6f4aeff41eac8c9ca8ecd36cb97f24201684d
gavin2lee <gavin2lee@163.com>
2020-11-17 19:23:24 +0800

    #dev3 refactor plugin instance service

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceServerMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceServerMapper.java
@@ -1,5 +1,8 @@
 package com.webank.wecube.platform.core.repository.plugin;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.plugin.ResourceServer;
@@ -17,4 +20,12 @@ public interface ResourceServerMapper {
     int updateByPrimaryKeySelective(ResourceServer record);
 
     int updateByPrimaryKey(ResourceServer record);
+    
+    /**
+     * 
+     * @param host
+     * @param type
+     * @return
+     */
+    List<ResourceServer> selectAllByHostAndType(@Param("host")String host, @Param("type")String type);
 }
