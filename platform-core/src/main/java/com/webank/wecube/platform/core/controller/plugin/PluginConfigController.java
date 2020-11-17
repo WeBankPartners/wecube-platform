7b32d21625cf70b7f24fcf4a4c7d53419ca1b3e5
gavin2lee <gavin2lee@163.com>
2020-11-17 10:47:15 +0800

    #dev3 refactor plugin config interfaces query

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/PluginConfigController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/PluginConfigController.java
@@ -3,8 +3,6 @@ package com.webank.wecube.platform.core.controller.plugin;
 import static com.webank.wecube.platform.core.dto.CommonResponseDto.okay;
 import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,11 +22,10 @@ import com.webank.wecube.platform.core.service.plugin.PluginConfigService;
 @RestController
 @RequestMapping("/v1")
 public class PluginConfigController {
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private PluginConfigService pluginConfigService;
-    
+
     @Autowired
     private PluginConfigMgmtService pluginConfigMgmtService;
 
@@ -42,17 +39,27 @@ public class PluginConfigController {
         return okayWithData(pluginConfigMgmtService.createOrUpdatePluginConfig(pluginConfigDto));
     }
 
+    /**
+     * 
+     * @return
+     */
     @GetMapping("/plugins/interfaces/enabled")
     public CommonResponseDto queryAllEnabledPluginConfigInterface() {
         return okayWithData(pluginConfigMgmtService.queryAllLatestEnabledPluginConfigInterface());
     }
 
+    /**
+     * 
+     * @param packageName
+     * @param entityName
+     * @return
+     */
     @GetMapping("/plugins/interfaces/package/{package-name}/entity/{entity-name}/enabled")
     public CommonResponseDto queryAllEnabledPluginConfigInterfaceForEntityName(
             @PathVariable(value = "package-name") String packageName,
             @PathVariable(value = "entity-name") String entityName) {
         return okayWithData(
-                pluginConfigService.queryAllEnabledPluginConfigInterfaceForEntity(packageName, entityName, null));
+                pluginConfigMgmtService.queryAllEnabledPluginConfigInterfaceForEntity(packageName, entityName, null));
     }
 
     @PostMapping("/plugins/interfaces/enabled/query-by-target-entity-filter-rule")
@@ -62,34 +69,55 @@ public class PluginConfigController {
                 pluginConfigService.queryAllEnabledPluginConfigInterfaceForEntityByFilterRule(filterRuleDto));
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @return
+     */
     @PostMapping("/plugins/enable/{plugin-config-id:.+}")
     public CommonResponseDto enablePlugin(@PathVariable(value = "plugin-config-id") String pluginConfigId) {
         return okayWithData(pluginConfigMgmtService.enablePlugin(pluginConfigId));
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @return
+     */
     @PostMapping("/plugins/disable/{plugin-config-id:.+}")
     public CommonResponseDto disablePlugin(@PathVariable(value = "plugin-config-id") String pluginConfigId) {
         return okayWithData(pluginConfigMgmtService.disablePlugin(pluginConfigId));
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @return
+     */
     @GetMapping("/plugins/interfaces/{plugin-config-id:.+}")
     public CommonResponseDto queryPluginConfigInterfaceByConfigId(
             @PathVariable(value = "plugin-config-id") String pluginConfigId) {
-        return okayWithData(pluginConfigService.queryPluginConfigInterfaceByConfigId(pluginConfigId));
+        return okayWithData(pluginConfigMgmtService.queryPluginConfigInterfacesByConfigId(pluginConfigId));
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @return
+     */
     @DeleteMapping("/plugins/configs/{plugin-config-id:.+}")
     public CommonResponseDto deletePluginConfigByConfigId(
             @PathVariable(value = "plugin-config-id") String pluginConfigId) {
-        try {
-            pluginConfigService.deletePluginConfigById(pluginConfigId);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return CommonResponseDto.error(e.getMessage());
-        }
+        pluginConfigMgmtService.deletePluginConfigById(pluginConfigId);
         return okay();
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @param pluginConfigRoleRequestDto
+     * @return
+     */
     @PostMapping("/plugins/roles/configs/{plugin-config-id:.+}")
     public CommonResponseDto updatePluginConfigRoleBinding(@PathVariable("plugin-config-id") String pluginConfigId,
             @RequestBody PluginConfigRoleRequestDto pluginConfigRoleRequestDto) {
@@ -97,6 +125,12 @@ public class PluginConfigController {
         return CommonResponseDto.okay();
     }
 
+    /**
+     * 
+     * @param pluginConfigId
+     * @param pluginConfigRoleRequestDto
+     * @return
+     */
     @DeleteMapping("/plugins/roles/configs/{plugin-config-id:.+}")
     public CommonResponseDto deletePluginConfigRoleBinding(@PathVariable("plugin-config-id") String pluginConfigId,
             @RequestBody PluginConfigRoleRequestDto pluginConfigRoleRequestDto) {
