c2a3b76a64a12b7cbd40d82245b2cac3666fd44a
gavin2lee <gavin2lee@163.com>
2020-11-18 17:07:52 +0800

    #dev3 resource management refactor

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/ResourceServerRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.webank.wecube.platform.core.domain.ResourceServerDomain;
-
-public interface ResourceServerRepository extends CrudRepository<ResourceServerDomain, String> {
-    List<ResourceServerDomain> findByHostAndType(String host, String type);
-}
+//import com.webank.wecube.platform.core.domain.ResourceServerDomain;
+//
+//public interface ResourceServerRepository extends CrudRepository<ResourceServerDomain, String> {
+//    List<ResourceServerDomain> findByHostAndType(String host, String type);
+//}
