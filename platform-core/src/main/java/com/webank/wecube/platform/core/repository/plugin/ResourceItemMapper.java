110c160a0d9706cbf9147f2f43653e3da37b59cc
gavin2lee <gavin2lee@163.com>
2020-11-18 12:08:39 +0800

    #dev3 refactor launching plugin instance

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceItemMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceItemMapper.java
@@ -1,5 +1,8 @@
 package com.webank.wecube.platform.core.repository.plugin;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.plugin.ResourceItem;
@@ -17,4 +20,13 @@ public interface ResourceItemMapper {
     int updateByPrimaryKeySelective(ResourceItem record);
 
     int updateByPrimaryKey(ResourceItem record);
+
+    /**
+     * 
+     * @param resourceServerId
+     * @param type
+     * @return
+     */
+    List<ResourceItem> selectAllByResourceServerAndType(@Param("resourceServerId") String resourceServerId,
+            @Param("type") String type);
 }
