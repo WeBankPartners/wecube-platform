e875e7bde8b46143fb569f80b7f0437e32b1d505
gavin2lee <gavin2lee@163.com>
2020-11-27 16:10:11 +0800

    #dev3 mapper refactor

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/BatchExecutionController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/BatchExecutionController.java
@@ -1,15 +1,16 @@
 package com.webank.wecube.platform.core.controller;
 
+import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.webank.wecube.platform.core.dto.BatchExecutionRequestDto;
 import com.webank.wecube.platform.core.dto.CommonResponseDto;
 import com.webank.wecube.platform.core.service.plugin.BatchExecutionService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import static com.webank.wecube.platform.core.dto.CommonResponseDto.okayWithData;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/v1")
@@ -18,10 +19,13 @@ public class BatchExecutionController {
     @Autowired
     private BatchExecutionService batchExecutionService;
 
+    /**
+     * 
+     * @param batchExecutionRequest
+     * @return
+     */
     @PostMapping("/batch-execution/run")
-    @ResponseBody
-    public CommonResponseDto runBatchExecution(@RequestBody BatchExecutionRequestDto batchExecutionRequest)
-            throws IOException {
+    public CommonResponseDto runBatchExecution(@RequestBody BatchExecutionRequestDto batchExecutionRequest){
         return okayWithData(batchExecutionService.handleBatchExecutionJob(batchExecutionRequest));
     }
 
