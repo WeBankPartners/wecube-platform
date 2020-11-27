24fb41182a027a5373fade690e561c8c1c64e689
gavin2lee <gavin2lee@163.com>
2020-11-27 11:12:10 +0800

    #dev3 batch execution refactor

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/plugin/FavoritesRole.java
@@ -0,0 +1,53 @@
+package com.webank.wecube.platform.core.entity.plugin;
+
+public class FavoritesRole {
+    private String id;
+
+    private String favoritesId;
+
+    private String permission;
+
+    private String roleId;
+
+    private String roleName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getFavoritesId() {
+        return favoritesId;
+    }
+
+    public void setFavoritesId(String favoritesId) {
+        this.favoritesId = favoritesId == null ? null : favoritesId.trim();
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission == null ? null : permission.trim();
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+}
