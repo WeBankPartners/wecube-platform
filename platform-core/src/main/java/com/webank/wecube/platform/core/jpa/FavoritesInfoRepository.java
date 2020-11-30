603e05f0766d0562742639b544115b825382d775
gavin2lee <gavin2lee@163.com>
2020-11-30 22:30:51 +0800

    #dev3 refactor favorites

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/FavoritesInfoRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/FavoritesInfoRepository.java
@@ -1,19 +1,19 @@
-package com.webank.wecube.platform.core.jpa;
-
-import com.webank.wecube.platform.core.entity.workflow.FavoritesEntity;
-import com.webank.wecube.platform.core.entity.workflow.ProcDefInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface FavoritesInfoRepository extends JpaRepository<FavoritesEntity, String> {
-    List<FavoritesEntity> findAllCollectionByCollectionName(String collectionName);
-
-    // @Transactional
-    // @Modifying
-    // @Query("delete from ProcessDefInfoEntity t where t.id = :id")
-    // void deleteByEntityId(@Param("id") String id);
-}
+//package com.webank.wecube.platform.core.jpa;
+//
+//import com.webank.wecube.platform.core.entity.workflow.FavoritesEntity;
+//import com.webank.wecube.platform.core.entity.workflow.ProcDefInfoEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface FavoritesInfoRepository extends JpaRepository<FavoritesEntity, String> {
+//    List<FavoritesEntity> findAllCollectionByCollectionName(String collectionName);
+//
+//    // @Transactional
+//    // @Modifying
+//    // @Query("delete from ProcessDefInfoEntity t where t.id = :id")
+//    // void deleteByEntityId(@Param("id") String id);
+//}
