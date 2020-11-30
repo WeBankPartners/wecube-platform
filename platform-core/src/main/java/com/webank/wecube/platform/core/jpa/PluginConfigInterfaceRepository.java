ed0e20152c8f5a90359155e918b1ad8b2c2aaac6
gavin2lee <gavin2lee@163.com>
2020-11-30 15:10:32 +0800

    #dev3 batch execution refactor

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginConfigInterfaceRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginConfigInterfaceRepository.java
@@ -1,36 +1,36 @@
-package com.webank.wecube.platform.core.jpa;
-
-import com.webank.wecube.platform.core.domain.plugin.PluginConfig.Status;
-import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterface;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface PluginConfigInterfaceRepository extends CrudRepository<PluginConfigInterface, String> {
-
-    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_Status(Status status);
-
-    Optional<List<PluginConfigInterface>> findAllByPluginConfig_Id(String pluginConfigId);
-
-    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_Status(
-            String targetPackage, String targetEntity, Status status);
-
-    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_TargetEntityFilterRuleAndPluginConfig_Status(
-            String targetPackage, String targetEntity, String targetEntityFilterRule, Status status);
-
-    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_StatusAndPluginConfig_TargetEntityFilterRuleIsNull(
-            String targetPackage, String targetEntity, Status status);
-
-    @Query("select configInterface from PluginConfigInterface configInterface where configInterface.pluginConfig.status = :status and (configInterface.pluginConfig.targetEntity is null or configInterface.pluginConfig.targetEntity='')")
-    Optional<List<PluginConfigInterface>> findAllByEntityNameEmptyAndStatus(@Param("status") Status status);
-    
-    @Query("select intf from PluginConfigInterface intf where intf.pluginConfig.status = 'ENABLED' ")
-    List<PluginConfigInterface> findAllEnabledInterfaces();
-    
-    default Optional<List<PluginConfigInterface>> findAllEnabledWithEntityNameNull() {
-        return findAllByEntityNameEmptyAndStatus(Status.ENABLED);
-    }
-}
+//package com.webank.wecube.platform.core.jpa;
+//
+//import com.webank.wecube.platform.core.domain.plugin.PluginConfig.Status;
+//import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterface;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.query.Param;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface PluginConfigInterfaceRepository extends CrudRepository<PluginConfigInterface, String> {
+//
+//    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_Status(Status status);
+//
+//    Optional<List<PluginConfigInterface>> findAllByPluginConfig_Id(String pluginConfigId);
+//
+//    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_Status(
+//            String targetPackage, String targetEntity, Status status);
+//
+//    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_TargetEntityFilterRuleAndPluginConfig_Status(
+//            String targetPackage, String targetEntity, String targetEntityFilterRule, Status status);
+//
+//    Optional<List<PluginConfigInterface>> findPluginConfigInterfaceByPluginConfig_TargetPackageAndPluginConfig_TargetEntityAndPluginConfig_StatusAndPluginConfig_TargetEntityFilterRuleIsNull(
+//            String targetPackage, String targetEntity, Status status);
+//
+//    @Query("select configInterface from PluginConfigInterface configInterface where configInterface.pluginConfig.status = :status and (configInterface.pluginConfig.targetEntity is null or configInterface.pluginConfig.targetEntity='')")
+//    Optional<List<PluginConfigInterface>> findAllByEntityNameEmptyAndStatus(@Param("status") Status status);
+//    
+//    @Query("select intf from PluginConfigInterface intf where intf.pluginConfig.status = 'ENABLED' ")
+//    List<PluginConfigInterface> findAllEnabledInterfaces();
+//    
+//    default Optional<List<PluginConfigInterface>> findAllEnabledWithEntityNameNull() {
+//        return findAllByEntityNameEmptyAndStatus(Status.ENABLED);
+//    }
+//}
