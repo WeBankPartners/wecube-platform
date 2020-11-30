0a2eee7174c3a8a09345402e7c79fe5bf5621a97
gavin2lee <gavin2lee@163.com>
2020-11-30 17:24:47 +0800

    #dev3 refactor batch execution

--- platform-core/src/main/java/com/webank/wecube/platform/core/entity/plugin/ExecutionJobs.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/plugin/ExecutionJobs.java
@@ -1,8 +1,14 @@
 package com.webank.wecube.platform.core.entity.plugin;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.webank.wecube.platform.core.utils.JsonUtils;
 
 public class ExecutionJobs {
+    public static final String ERROR_CODE_SUCCESSFUL = "0";
+    public static final String ERROR_CODE_FAILED = "1";
     private Integer id;
 
     private String batchExecutionJobId;
@@ -26,6 +32,12 @@ public class ExecutionJobs {
     private String errorMessage;
 
     private String returnJson;
+
+    private transient BatchExecutionJobs batchExecutionJob;
+
+    private transient List<ExecutionJobParameters> parameters = new ArrayList<>();
+    
+    private transient Exception prepareException;
 
     public String getErrorMessage() {
         return errorMessage;
@@ -122,4 +134,47 @@ public class ExecutionJobs {
     public void setPluginConfigInterfaceId(String pluginConfigInterfaceId) {
         this.pluginConfigInterfaceId = pluginConfigInterfaceId == null ? null : pluginConfigInterfaceId.trim();
     }
+
+    public BatchExecutionJobs getBatchExecutionJob() {
+        return batchExecutionJob;
+    }
+
+    public void setBatchExecutionJob(BatchExecutionJobs batchExecutionJob) {
+        this.batchExecutionJob = batchExecutionJob;
+    }
+
+    public List<ExecutionJobParameters> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<ExecutionJobParameters> parameters) {
+        this.parameters = parameters;
+    }
+
+    public void addParameter(ExecutionJobParameters parameter) {
+        if (parameter == null) {
+            return;
+        }
+
+        if (this.parameters == null) {
+            this.parameters = new ArrayList<>();
+        }
+
+        this.parameters.add(parameter);
+    }
+
+    public Exception getPrepareException() {
+        return prepareException;
+    }
+
+    public void setPrepareException(Exception prepareException) {
+        this.prepareException = prepareException;
+    }
+    
+    public void setErrorWithMessage(String errorMessage) {
+        this.errorCode = ERROR_CODE_FAILED;
+        this.errorMessage = errorMessage;
+        this.returnJson = JsonUtils.toJsonString(this);
+    }
+    
 }
