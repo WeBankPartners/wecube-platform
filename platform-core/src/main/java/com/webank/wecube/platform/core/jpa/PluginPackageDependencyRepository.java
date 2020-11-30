a7ffca89c6a24c7c50ad3291c33ad8ade9f23eb0
gavin2lee <gavin2lee@163.com>
2020-11-30 15:10:32 +0800

    #dev3 batch execution refactor

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginPackageDependencyRepository.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginPackageDependencyRepository.java
@@ -1,11 +1,11 @@
-package com.webank.wecube.platform.core.jpa;
-
-import com.webank.wecube.platform.core.domain.plugin.PluginPackageDependency;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface PluginPackageDependencyRepository extends CrudRepository<PluginPackageDependency, String> {
-    Optional<List<PluginPackageDependency>> findAllByPluginPackageNameAndPluginPackageVersion(String packageName, String version);
-}
+//package com.webank.wecube.platform.core.jpa;
+//
+//import com.webank.wecube.platform.core.domain.plugin.PluginPackageDependency;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface PluginPackageDependencyRepository extends CrudRepository<PluginPackageDependency, String> {
+//    Optional<List<PluginPackageDependency>> findAllByPluginPackageNameAndPluginPackageVersion(String packageName, String version);
+//}
