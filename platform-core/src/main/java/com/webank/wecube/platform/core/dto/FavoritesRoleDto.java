395cd41f4b9734ae399ab298d7b2f6c7f4d72528
gavin2lee <gavin2lee@163.com>
2020-11-30 22:30:51 +0800

    #dev3 refactor favorites

--- platform-core/src/main/java/com/webank/wecube/platform/core/dto/FavoritesRoleDto.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/FavoritesRoleDto.java
@@ -1,32 +1,20 @@
 package com.webank.wecube.platform.core.dto;
 
-import com.webank.wecube.platform.core.entity.workflow.FavoritesRoleEntity;
-
 public class FavoritesRoleDto {
     private String favoritesId;
     private String permission;
     private String roleId;
     private String roleName;
 
-    public static FavoritesRoleDto fromDomain(FavoritesRoleEntity favoritesRoleEntity) {
-        FavoritesRoleDto result = new FavoritesRoleDto();
-        result.setFavoritesId(favoritesRoleEntity.getFavoritesId());
-        result.setPermission(favoritesRoleEntity.getPermission().toString());
-        result.setRoleId(favoritesRoleEntity.getRoleId());
-        result.setRoleName(favoritesRoleEntity.getRoleName());
-        return result;
-    }
+//    public static FavoritesRoleDto fromDomain(FavoritesRoleEntity favoritesRoleEntity) {
+//        FavoritesRoleDto result = new FavoritesRoleDto();
+//        result.setFavoritesId(favoritesRoleEntity.getFavoritesId());
+//        result.setPermission(favoritesRoleEntity.getPermission().toString());
+//        result.setRoleId(favoritesRoleEntity.getRoleId());
+//        result.setRoleName(favoritesRoleEntity.getRoleName());
+//        return result;
+//    }
 
-    public static FavoritesRoleEntity toDomain(String id, String favoritesId, String roleId, FavoritesRoleEntity.permissionEnum permissionEnum, String roleName) {
-        FavoritesRoleEntity result = new FavoritesRoleEntity();
-        result.setId(id);
-        result.setFavoritesId(favoritesId);
-        result.setRoleId(roleId);
-        result.setPermission(permissionEnum);
-        result.setRoleName(roleName);
-        return result;
-
-    }
     public FavoritesRoleDto() {
     }
 
