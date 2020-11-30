e3279b487f91d72058d7f8cdfb697eb0c60119ce
gavin2lee <gavin2lee@163.com>
2020-11-30 11:29:26 +0800

    #dev3 refactor route item

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/plugin/SimplePluginConfigInterfaceInfo.java
@@ -0,0 +1,50 @@
+package com.webank.wecube.platform.core.entity.plugin;
+
+public class SimplePluginConfigInterfaceInfo {
+    private String id;
+    private String serviceName;
+    private String path;
+    private String httpMethod;
+    private String packageName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+}
