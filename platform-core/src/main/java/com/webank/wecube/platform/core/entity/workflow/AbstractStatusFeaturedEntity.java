038e15177ad98d4584d234fa37f3652f8f1a23a1
gavin2lee <gavin2lee@163.com>
2020-12-01 10:48:59 +0800

    #dev3 remove unused codes

--- platform-core/src/main/java/com/webank/wecube/platform/core/entity/workflow/AbstractStatusFeaturedEntity.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/workflow/AbstractStatusFeaturedEntity.java
@@ -1,40 +1,40 @@
-package com.webank.wecube.platform.core.entity.workflow;
-
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-
-@MappedSuperclass
-public abstract class AbstractStatusFeaturedEntity extends AbstractTraceableEntity {
-    @Column(name = "ACTIVE")
-    private boolean active = true;
-    @Column(name = "REV")
-    private int revision;
-
-    @Column(name = "STATUS")
-    private String status;
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public int getRevision() {
-        return revision;
-    }
-
-    public void setRevision(int revision) {
-        this.revision = revision;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-}
+//package com.webank.wecube.platform.core.entity.workflow;
+//
+//import javax.persistence.Column;
+//import javax.persistence.MappedSuperclass;
+//
+//@MappedSuperclass
+//public abstract class AbstractStatusFeaturedEntity extends AbstractTraceableEntity {
+//    @Column(name = "ACTIVE")
+//    private boolean active = true;
+//    @Column(name = "REV")
+//    private int revision;
+//
+//    @Column(name = "STATUS")
+//    private String status;
+//
+//    public boolean isActive() {
+//        return active;
+//    }
+//
+//    public void setActive(boolean active) {
+//        this.active = active;
+//    }
+//
+//    public int getRevision() {
+//        return revision;
+//    }
+//
+//    public void setRevision(int revision) {
+//        this.revision = revision;
+//    }
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//}
