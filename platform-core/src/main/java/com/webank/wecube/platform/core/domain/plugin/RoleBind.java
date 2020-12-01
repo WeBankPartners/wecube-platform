7e03a58ceffd49bdea0eee8ace5ea7312ccb37cd
gavin2lee <gavin2lee@163.com>
2020-12-01 10:48:59 +0800

    #dev3 remove unused codes

--- platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/RoleBind.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/domain/plugin/RoleBind.java
@@ -1,21 +1,21 @@
-package com.webank.wecube.platform.core.domain.plugin;
-
-public class RoleBind {
-
-    private String permission;
-    private String roleName;
-    public String getPermission() {
-        return permission;
-    }
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-    public String getRoleName() {
-        return roleName;
-    }
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-    
-    
-}
+//package com.webank.wecube.platform.core.domain.plugin;
+//
+//public class RoleBind {
+//
+//    private String permission;
+//    private String roleName;
+//    public String getPermission() {
+//        return permission;
+//    }
+//    public void setPermission(String permission) {
+//        this.permission = permission;
+//    }
+//    public String getRoleName() {
+//        return roleName;
+//    }
+//    public void setRoleName(String roleName) {
+//        this.roleName = roleName;
+//    }
+//    
+//    
+//}
