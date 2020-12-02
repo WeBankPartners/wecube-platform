1ca1b18e5db82690533e0f139f986fce4cae89d0
gavin2lee <gavin2lee@163.com>
2020-12-02 10:43:15 +0800

    #dev decomission jpa

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceServerMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/ResourceServerMapper.java
@@ -6,16 +6,27 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.plugin.ResourceServer;
+import com.webank.wecube.platform.core.entity.plugin.ResourceServerExample;
 
 @Repository
 public interface ResourceServerMapper {
+    int countByExample(ResourceServerExample example);
+
+    int deleteByExample(ResourceServerExample example);
+
     int deleteByPrimaryKey(String id);
 
     int insert(ResourceServer record);
 
     int insertSelective(ResourceServer record);
 
+    List<ResourceServer> selectByExample(ResourceServerExample example);
+
     ResourceServer selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") ResourceServer record, @Param("example") ResourceServerExample example);
+
+    int updateByExample(@Param("record") ResourceServer record, @Param("example") ResourceServerExample example);
 
     int updateByPrimaryKeySelective(ResourceServer record);
 
