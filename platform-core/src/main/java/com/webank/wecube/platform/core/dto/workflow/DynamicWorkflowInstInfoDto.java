3fa6b18fbb46c62da46bbcdfa757211d2f3a4a46
gavin2lee <gavin2lee@163.com>
2020-12-06 15:52:22 +0800

    #dev3 add itsm suppport interfaces

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/DynamicWorkflowInstInfoDto.java
@@ -0,0 +1,42 @@
+package com.webank.wecube.platform.core.dto.workflow;
+
+public class DynamicWorkflowInstInfoDto {
+    private int id;
+    private String procInstKey;
+    private String procDefId;
+    private String procDefKey;
+    private String status;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProcInstKey() {
+        return procInstKey;
+    }
+    public void setProcInstKey(String procInstKey) {
+        this.procInstKey = procInstKey;
+    }
+    public String getProcDefId() {
+        return procDefId;
+    }
+    public void setProcDefId(String procDefId) {
+        this.procDefId = procDefId;
+    }
+    public String getProcDefKey() {
+        return procDefKey;
+    }
+    public void setProcDefKey(String procDefKey) {
+        this.procDefKey = procDefKey;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+
+}
