01b33792a3b0614b1afc8b6844585452c3c2f5aa
gavin2lee <gavin2lee@163.com>
2020-12-06 15:52:22 +0800

    #dev3 add itsm suppport interfaces

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/DynamicEntityValueDto.java
@@ -0,0 +1,91 @@
+package com.webank.wecube.platform.core.dto.workflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class DynamicEntityValueDto {
+    @Nullable
+    private String entityDefId;//Entity definition id from platform.
+    @Nonnull
+    private String packageName;
+    @Nonnull
+    private String entityName;
+    @Nullable
+    private String dataId;//Existing data id,such as guid in cmdb.
+    @Nonnull
+    private String oid;//Equals to dataId once dataId presents,or a temporary assigned.
+
+    private List<String> previousOids = new ArrayList<>();
+    private List<String> succeedingOids = new ArrayList<>();
+    
+    private List<DynamicEntityAttrValueDto> attrValues = new ArrayList<>();
+
+    public String getEntityDefId() {
+        return entityDefId;
+    }
+
+    public void setEntityDefId(String entityDefId) {
+        this.entityDefId = entityDefId;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public String getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(String dataId) {
+        this.dataId = dataId;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public List<String> getPreviousOids() {
+        return previousOids;
+    }
+
+    public void setPreviousOids(List<String> previousOids) {
+        this.previousOids = previousOids;
+    }
+
+    public List<String> getSucceedingOids() {
+        return succeedingOids;
+    }
+
+    public void setSucceedingOids(List<String> succeedingOids) {
+        this.succeedingOids = succeedingOids;
+    }
+
+    public List<DynamicEntityAttrValueDto> getAttrValues() {
+        return attrValues;
+    }
+
+    public void setAttrValues(List<DynamicEntityAttrValueDto> attrValues) {
+        this.attrValues = attrValues;
+    }
+    
+    
+}
