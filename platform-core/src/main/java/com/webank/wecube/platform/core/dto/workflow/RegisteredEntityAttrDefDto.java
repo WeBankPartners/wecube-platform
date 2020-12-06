3d747adcc6d26ea390e3f3af616b6ce2c20003c3
gavin2lee <gavin2lee@163.com>
2020-12-06 15:52:22 +0800

    #dev3 add itsm suppport interfaces

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/RegisteredEntityAttrDefDto.java
@@ -0,0 +1,101 @@
+package com.webank.wecube.platform.core.dto.workflow;
+
+public class RegisteredEntityAttrDefDto {
+    private String id;
+    private String name;
+    private String description;
+    private String dataType;
+    private boolean mandatory = false;
+
+    private String refPackageName;
+    private String refEntityName;
+    private String refAttrName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public boolean isMandatory() {
+        return mandatory;
+    }
+
+    public void setMandatory(boolean mandatory) {
+        this.mandatory = mandatory;
+    }
+
+    public String getRefPackageName() {
+        return refPackageName;
+    }
+
+    public void setRefPackageName(String refPackageName) {
+        this.refPackageName = refPackageName;
+    }
+
+    public String getRefEntityName() {
+        return refEntityName;
+    }
+
+    public void setRefEntityName(String refEntityName) {
+        this.refEntityName = refEntityName;
+    }
+
+    public String getRefAttrName() {
+        return refAttrName;
+    }
+
+    public void setRefAttrName(String refAttrName) {
+        this.refAttrName = refAttrName;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("RegisteredEntityAttrDefDto [id=");
+        builder.append(id);
+        builder.append(", name=");
+        builder.append(name);
+        builder.append(", description=");
+        builder.append(description);
+        builder.append(", dataType=");
+        builder.append(dataType);
+        builder.append(", mandatory=");
+        builder.append(mandatory);
+        builder.append(", refPackageName=");
+        builder.append(refPackageName);
+        builder.append(", refEntityName=");
+        builder.append(refEntityName);
+        builder.append(", refAttrName=");
+        builder.append(refAttrName);
+        builder.append("]");
+        return builder.toString();
+    }
+
+}
