1a5a90b1f4a3bdcb18e5c11408b91beac17c6093
gavin2lee <gavin2lee@163.com>
2020-12-07 11:45:56 +0800

    #dev3 fix jar dependency

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/workflow/WorkflowPublicAccessServiceTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/workflow/WorkflowPublicAccessServiceTest.java
@@ -2,9 +2,13 @@ package com.webank.wecube.platform.core.service.workflow;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webank.wecube.platform.core.commons.AuthenticationContextHolder;
 import com.webank.wecube.platform.core.commons.AuthenticationContextHolder.AuthenticatedUser;
 import com.webank.wecube.platform.core.dto.workflow.WorkflowDefInfoDto;
@@ -26,6 +31,8 @@ public class WorkflowPublicAccessServiceTest {
     @Autowired
     WorkflowPublicAccessService workflowPublicAccessService;
     
+    ObjectMapper objectMapper = new ObjectMapper();
+    
     @Before
     public void setUp(){
         List<String> roles = new ArrayList<String>();
@@ -35,18 +42,27 @@ public class WorkflowPublicAccessServiceTest {
     }
 
     @Test
-    public void testFetchLatestReleasedWorkflowDefs() {
+    public void testFetchLatestReleasedWorkflowDefs() throws IOException {
 
         List<WorkflowDefInfoDto> procDefInfos = workflowPublicAccessService.fetchLatestReleasedWorkflowDefs();
         procDefInfos.forEach(System.out::println);
+        
+        String data = objectMapper.writeValueAsString(procDefInfos);
+        
+        String fileName = "procDefInfos"+System.currentTimeMillis()+".json";
+        FileUtils.writeStringToFile(new File(fileName), data, Charset.forName("UTF-8"));
     }
 
     @Test
-    public void testFetchWorkflowTasknodeInfos() {
+    public void testFetchWorkflowTasknodeInfos() throws IOException {
         
         String procDefId = "rWMKoelC2BR";
         List<WorkflowNodeDefInfoDto> nodeDefInfos = workflowPublicAccessService.fetchWorkflowTasknodeInfos(procDefId);
         nodeDefInfos.forEach(System.out::println);
+        String data = objectMapper.writeValueAsString(nodeDefInfos);
+        
+        String fileName = "tasknodeInfos"+System.currentTimeMillis()+".json";
+        FileUtils.writeStringToFile(new File(fileName), data, Charset.forName("UTF-8"));
     }
 
     @Test
