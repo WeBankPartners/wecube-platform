22c7f5fbe8a37aaa5ee56430f7ec700f13896503
gavin2lee <gavin2lee@163.com>
2020-12-08 11:38:57 +0800

    #dev3 fix register issue

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
@@ -9,3 +9,5 @@ ADD COLUMN `mandatory` TINYINT NULL DEFAULT 0;
 ALTER TABLE `core_re_task_node_def_info` 
 ADD COLUMN `dynamic_bind` VARCHAR(45) NULL DEFAULT 'N';
 
+ALTER TABLE `core_operation_event`
+ADD COLUMN `rev` INT(11) DEFAULT '0';
