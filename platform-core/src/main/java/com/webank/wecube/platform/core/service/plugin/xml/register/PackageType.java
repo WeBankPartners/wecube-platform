a01d87223974fb959c0a1d880649415083d8fc07
gavin2lee <gavin2lee@163.com>
2020-12-08 11:38:57 +0800

    #dev3 fix register issue

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/xml/register/PackageType.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/xml/register/PackageType.java
@@ -12,6 +12,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -42,6 +43,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement(name = "package", namespace = "")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "packageType", propOrder = {
     "packageDependencies",
---
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/plugin/PluginArtifactsMgmtServiceTest.java
@@ -0,0 +1,34 @@
+package com.webank.wecube.platform.core.service.plugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.webank.wecube.platform.core.service.plugin.xml.register.PackageType;
+import com.webank.wecube.platform.core.utils.JaxbUtils;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PluginArtifactsMgmtServiceTest {
+
+    @Test
+    public void testParseRegisterXmlFile() throws IOException {
+        InputStream input = PluginArtifactsMgmtServiceTest.class.getClassLoader().getResourceAsStream("plugin/register-cmdb.xml");
+        String xmlFileDataStr = IOUtils.toString(input, Charset.forName("UTF-8"));
+        PackageType xmlPackage = JaxbUtils.convertToObject(xmlFileDataStr, PackageType.class);
+        
+        Assert.assertNotNull(xmlPackage);
+        System.out.println(xmlPackage.getName());
+        
+        IOUtils.closeQuietly(input);
+    }
+
+}
