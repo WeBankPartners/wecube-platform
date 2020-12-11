471e8c613e35ad6633beede203fb17a688022523
gavin2lee <gavin2lee@163.com>
2020-12-11 11:55:07 +0800

    #dev3 add query cache

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityQueryCriteria.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityQueryCriteria.java
@@ -1,10 +1,16 @@
 package com.webank.wecube.platform.core.service.dme;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class EntityQueryCriteria {
+public class EntityQueryCriteria implements Serializable{
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3062150549558722047L;
     private String attrName;
     private String condition;
 
@@ -44,7 +50,43 @@ public class EntityQueryCriteria {
         builder.append("]");
         return builder.toString();
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((attrName == null) ? 0 : attrName.hashCode());
+        result = prime * result + ((condition == null) ? 0 : condition.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        EntityQueryCriteria other = (EntityQueryCriteria) obj;
+        if (attrName == null) {
+            if (other.attrName != null) {
+                return false;
+            }
+        } else if (!attrName.equals(other.attrName)) {
+            return false;
+        }
+        if (condition == null) {
+            if (other.condition != null) {
+                return false;
+            }
+        } else if (!condition.equals(other.condition)) {
+            return false;
+        }
+        return true;
+    }
 
 }
