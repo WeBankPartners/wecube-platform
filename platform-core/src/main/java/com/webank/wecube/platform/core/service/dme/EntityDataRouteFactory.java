d92fd2128d110510da8bac549ec7773db9a3075c
gavin2lee <gavin2lee@163.com>
2020-12-11 16:58:35 +0800

    #dev3 refactor entity data route factory

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityDataRouteFactory.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityDataRouteFactory.java
@@ -1,26 +1,19 @@
 package com.webank.wecube.platform.core.service.dme;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+/**
+ * 
+ * @author gavin
+ *
+ */
+public interface EntityDataRouteFactory {
+    String HTTP_SCHEME_HTTP = "http";
+    String HTTP_SCHEME_HTTPS = "https";
 
-import com.webank.wecube.platform.core.commons.ApplicationProperties;
-
-@Service
-public class EntityDataRouteFactory {
-    
-    @Autowired
-    private ApplicationProperties applicationProperties;
-
-    public EntityRouteDescription deduceEntityDescription(String packageName, String entityName){
-        String gatewayUrl = applicationProperties.getGatewayUrl();
-        String [] parts = gatewayUrl.split(":");
-        EntityRouteDescription entityDef = new EntityRouteDescription();
-        entityDef.setEntityName(packageName);
-        entityDef.setHttpPort(parts[1]);
-        entityDef.setHttpHost(parts[0]);
-        entityDef.setHttpScheme("http");
-        entityDef.setPackageName(entityName);
-        
-        return entityDef;
-    }
+    /**
+     * Deduce the entity route base on package and entity.
+     * @param packageName
+     * @param entityName
+     * @return
+     */
+    EntityRouteDescription deduceEntityDescription(String packageName, String entityName);
 }
