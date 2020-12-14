c05208edef2b6a7372f963d7ef8c194449395b9e
gavin2lee <gavin2lee@163.com>
2020-12-14 09:23:34 +0800

    #dev3 decommissioned jpa

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/PluginConfigInterfaceParameterRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.webank.wecube.platform.core.jpa;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//import com.webank.wecube.platform.core.domain.plugin.PluginConfigInterfaceParameter;
-//
-//public interface PluginConfigInterfaceParameterRepository extends CrudRepository<PluginConfigInterfaceParameter, String> {
-//
-//}
