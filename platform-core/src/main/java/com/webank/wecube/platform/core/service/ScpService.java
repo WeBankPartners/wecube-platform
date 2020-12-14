8ef4188da380d6cd2b447e88413fc764fac26ecb
gavin2lee <gavin2lee@163.com>
2020-12-14 16:17:41 +0800

    #dev3 refactor plugin package service

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/ScpService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/ScpService.java
@@ -21,23 +21,23 @@ public class ScpService {
         try {
             return connection.authenticateWithPassword(user, password);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("errors while ssh authentication.", e);
         }
         return false;
     }
-
 
     public boolean isAuthedWithPublicKey(String ip, Integer port, String user, File privateKey, String password) {
         Connection connection = new Connection(ip, port);
         try {
             return connection.authenticateWithPublicKey(user, privateKey, password);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("errors while ssh authentication.", e);
         }
         return false;
     }
 
-    public boolean isAuth(String ip, Integer port, String user, String password, String privateKey, boolean usePassword) {
+    public boolean isAuth(String ip, Integer port, String user, String password, String privateKey,
+            boolean usePassword) {
         if (usePassword) {
             return isAuthedWithPassword(ip, port, user, password);
         } else {
@@ -45,26 +45,28 @@ public class ScpService {
         }
     }
 
-    public void getFile(String ip, Integer port, String user, String password, String privateKey, boolean usePassword, String remoteFile, String path) {
+    public void getFile(String ip, Integer port, String user, String password, String privateKey, boolean usePassword,
+            String remoteFile, String path) {
         Connection connection = new Connection(ip, port);
         try {
             connection.connect();
             boolean isAuthed = isAuth(ip, port, user, password, privateKey, usePassword);
             if (isAuthed) {
-                System.out.println("Authentication is successful!");
+                log.info("Authentication is successful!");
                 SCPClient scpClient = connection.createSCPClient();
                 scpClient.get(remoteFile, path);
             } else {
-                System.out.println("Authentication failed!");
+                log.info("Authentication failed!");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("errors while getting file.", e);
         } finally {
             connection.close();
         }
     }
 
-    public void putFile(String ip, Integer port, String user, String password, String privateKey, boolean usePassword, String localFile, String remoteTargetDirectory) {
+    public void putFile(String ip, Integer port, String user, String password, String privateKey, boolean usePassword,
+            String localFile, String remoteTargetDirectory) {
         log.info("Start to connect {}:{}", ip, port);
         Connection connection = new Connection(ip, port);
         try {
@@ -74,16 +76,17 @@ public class ScpService {
                 SCPClient scpClient = connection.createSCPClient();
                 scpClient.put(localFile, remoteTargetDirectory);
             } else {
-                System.out.println("Authentication failed!");
+                log.info("Authentication failed!");
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            log.error("errors while putting file.", ex);
         } finally {
             connection.close();
         }
     }
 
-    public void put(String ip, Integer port, String user, String password, String localFile, String remoteTargetDirectory) {
+    public void put(String ip, Integer port, String user, String password, String localFile,
+            String remoteTargetDirectory) {
         Connection conn = new Connection(ip, port);
         try {
             conn.connect();
@@ -95,14 +98,13 @@ public class ScpService {
                 scpClient.put(localFile, remoteTargetDirectory, "7777");
             } else {
                 log.info("User or password incorrect");
-                throw new WecubeCoreException("3222","User or password incorrect");
+                throw new WecubeCoreException("3222", "User or password incorrect");
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new WecubeCoreException("3223",String.format("Run 'scp' command meet error: %s" , e.getMessage()));
+            log.error("errors while putting file.", e);
+            throw new WecubeCoreException("3223", String.format("Run 'scp' command meet error: %s", e.getMessage()));
         } finally {
             conn.close();
         }
     }
 }
-
