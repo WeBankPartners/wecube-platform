3b67c54e46a70588263b1ff4618290e4b609192d
gavin2lee <gavin2lee@163.com>
2020-12-14 09:23:34 +0800

    #dev3 decommissioned jpa

--- platform-core/src/main/java/com/webank/wecube/platform/core/jpa/BatchExecutionJobRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.webank.wecube.platform.core.jpa;
-//import org.springframework.data.repository.CrudRepository;
-//
-//import com.webank.wecube.platform.core.domain.BatchExecutionJob;
-//
-//public interface BatchExecutionJobRepository extends  CrudRepository<BatchExecutionJob, String> {
-//
-//}
