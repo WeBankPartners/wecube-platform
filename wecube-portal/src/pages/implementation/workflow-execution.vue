3a58f33c1f04c415c34506c301d1f46bfeb23e29
pobu168 <244202969@qq.com>
2020-12-16 17:52:01 +0800

    upgrade createdTime display

--- wecube-portal/src/pages/implementation/workflow-execution.vue
+++ wecube-portal/src/pages/implementation/workflow-execution.vue
@@ -26,7 +26,7 @@
                       ' ' +
                       item.entityDisplayName +
                       ' ' +
-                      (item.createdTime || 'createdTime') +
+                      (item.createdTime || '0000-00-00 00:00:00') +
                       ' ' +
                       (item.operator || 'operator')
                   "
@@ -35,7 +35,7 @@
                     <span style="color:#2b85e4">{{ item.procInstName + ' ' }}</span>
                     <span style="color:#515a6e">{{ item.entityDisplayName + ' ' }}</span>
                     <span style="color:#ccc;padding-left:8px;float:right">{{ item.status }}</span>
-                    <span style="color:#ccc;float:right">{{ (item.createdTime || 'createdTime') + ' ' }}</span>
+                    <span style="color:#ccc;float:right">{{ (item.createdTime || '0000-00-00 00:00:00') + ' ' }}</span>
                     <span style="float:right;color:#515a6e;margin-right:20px">{{ item.operator || 'operator' }}</span>
                   </span>
                 </Option>
