5b9b0017a059a1e86d53cb637fb636276183c9b3
gavin2lee <gavin2lee@163.com>
2020-12-16 15:36:19 +0800

    #dev3 refined mapper of wf

--- platform-workflow/src/main/java/com/webank/wecube/platform/workflow/parse/AbstractServiceNodeStartListener.java
+++ platform-workflow/src/main/java/com/webank/wecube/platform/workflow/parse/AbstractServiceNodeStartListener.java
@@ -23,7 +23,7 @@ public abstract class AbstractServiceNodeStartListener extends AbstractServiceNo
                 .getBean(ServiceNodeStatusMapper.class);
 
         ServiceNodeStatusEntity entity = serviceNodeStatusRepository
-                .findOneByProcInstanceBizKeyAndNodeId(procInstanceBizKey, nodeId);
+                .selectOneByProcInstanceBizKeyAndNodeId(procInstanceBizKey, nodeId);
 
         if (entity == null) {
             getLogger().warn("{} is null for procInstanceBizKey={},nodeId={}", ServiceNodeStatusEntity.class.getSimpleName(),
