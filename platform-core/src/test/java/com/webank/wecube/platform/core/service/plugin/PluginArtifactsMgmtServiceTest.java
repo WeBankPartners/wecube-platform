4965edada0c9bc9e027f61e3636e3a103e88502a
gavin2lee <gavin2lee@163.com>
2020-12-17 21:48:44 +0800

    #dev3 refactor plugin config update

--- platform-core/src/test/java/com/webank/wecube/platform/core/service/plugin/PluginArtifactsMgmtServiceTest.java
+++ platform-core/src/test/java/com/webank/wecube/platform/core/service/plugin/PluginArtifactsMgmtServiceTest.java
@@ -3,32 +3,40 @@ package com.webank.wecube.platform.core.service.plugin;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.webank.wecube.platform.core.service.plugin.xml.register.PackageType;
 import com.webank.wecube.platform.core.utils.JaxbUtils;
+import com.webank.wecube.platform.core.utils.StringUtilsEx;
 
 
-@Ignore
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class PluginArtifactsMgmtServiceTest {
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testParseRegisterXmlFile() throws IOException {
-        InputStream input = PluginArtifactsMgmtServiceTest.class.getClassLoader().getResourceAsStream("plugin/register-cmdb.xml");
+        InputStream input = PluginArtifactsMgmtServiceTest.class.getClassLoader().getResourceAsStream("plugin/register-artifacts.xml");
         String xmlFileDataStr = IOUtils.toString(input, Charset.forName("UTF-8"));
         PackageType xmlPackage = JaxbUtils.convertToObject(xmlFileDataStr, PackageType.class);
         
         Assert.assertNotNull(xmlPackage);
         System.out.println(xmlPackage.getName());
+        
+        String envString = xmlPackage.getResourceDependencies().getDocker().get(0).getEnvVariables();
+        
+        System.out.println(envString);
+        
+        List<String> envs = StringUtilsEx.findSystemVariableString(envString);
+        
+        
+        System.out.println(envs.size());
+        for(String s : envs){
+            System.out.println(s);
+        }
         
         IOUtils.closeQuietly(input);
     }
