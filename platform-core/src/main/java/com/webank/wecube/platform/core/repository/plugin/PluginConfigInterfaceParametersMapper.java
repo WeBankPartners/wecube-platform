17db0fe4e5e6ceeac081d4dc6028371310e666bb
gavin2lee <gavin2lee@163.com>
2020-12-18 09:47:57 +0800

    #dev3 fix plugin config updating

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/PluginConfigInterfaceParametersMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/plugin/PluginConfigInterfaceParametersMapper.java
@@ -29,4 +29,11 @@ public interface PluginConfigInterfaceParametersMapper {
      */
     List<PluginConfigInterfaceParameters> selectAllByConfigInterfaceAndParamType(
             @Param("pluginConfigInterfaceId") String pluginConfigInterfaceId, @Param("paramType")String paramType);
+    
+    /**
+     * 
+     * @param pluginConfigInterfaceId
+     * @return
+     */
+    int deleteAllByConfigInterface(@Param("pluginConfigInterfaceId") String pluginConfigInterfaceId);
 }
