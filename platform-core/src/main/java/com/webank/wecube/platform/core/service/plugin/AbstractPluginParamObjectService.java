18736ba37dab6b3e0e811e19dd209035d5d59e99
gavin2lee <gavin2lee@163.com>
2020-12-30 12:41:52 +0800

    #dev3 add unit test

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/AbstractPluginParamObjectService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/AbstractPluginParamObjectService.java
@@ -1,5 +1,23 @@
 package com.webank.wecube.platform.core.service.plugin;
 
+import com.webank.wecube.platform.core.entity.plugin.CoreObjectPropertyMeta;
+
 public abstract class AbstractPluginParamObjectService {
+    
+    protected boolean isStringDataType(String dataType){
+        return CoreObjectPropertyMeta.DATA_TYPE_STRING.equals(dataType);
+    }
+    
+    protected boolean isNumberDataType(String dataType){
+        return CoreObjectPropertyMeta.DATA_TYPE_NUMBER.equals(dataType);
+    }
+    
+    protected boolean isListDataType(String dataType){
+        return CoreObjectPropertyMeta.DATA_TYPE_LIST.equals(dataType);
+    }
+    
+    protected boolean isObjectDataType(String dataType){
+        return CoreObjectPropertyMeta.DATA_TYPE_OBJECT.equals(dataType);
+    }
 
 }
