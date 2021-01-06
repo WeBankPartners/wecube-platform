e72db0e2ef0e4d388bfa130b67fdc3ecfa43e041
gavin2lee <gavin2lee@163.com>
2021-01-06 17:09:02 +0800

    #dev3 merge from dev for portal

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityQueryExecutor.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/EntityQueryExecutor.java
@@ -1,14 +1,70 @@
 package com.webank.wecube.platform.core.service.dme;
 
 import java.util.List;
+import java.util.Map;
 
+/**
+ * 
+ * @author gavin
+ *
+ */
 public interface EntityQueryExecutor {
+    /**
+     * 
+     * @param ctx
+     * @param valueToUpdate
+     */
     void executeUpdate(EntityOperationContext ctx, Object valueToUpdate);
+
+    /**
+     * 
+     * @param ctx
+     * @param entityDef
+     * @param recordsToCreate
+     * @return
+     */
+    Map<String, Object> executeCreate(EntityOperationContext ctx, EntityRouteDescription entityDef,
+            List<EntityDataRecord> recordsToCreate);
+
+    /**
+     * 
+     * @param ctx
+     * @return
+     */
     List<Object> executeQueryLeafAttributes(EntityOperationContext ctx);
+
+    /**
+     * 
+     * @param ctx
+     * @return
+     */
     List<EntityDataDelegate> executeQueryLeafEntity(EntityOperationContext ctx);
+
+    /**
+     * 
+     * @param ctx
+     * @param linkNode
+     */
     void performQuery(EntityOperationContext ctx, EntityQueryLinkNode linkNode);
+
+    /**
+     * 
+     * @param ctx
+     * @return
+     */
     List<TreeNode> generatePreviewTree(EntityOperationContext ctx);
+
+    /**
+     * 
+     * @param ctx
+     * @return
+     */
     EntityTreeNodesOverview generateEntityLinkOverview(EntityOperationContext ctx);
+
+    /**
+     * 
+     * @param exprNodeInfos
+     * @return
+     */
     EntityQueryLinkNode buildEntityQueryLinkNode(List<EntityQueryExprNodeInfo> exprNodeInfos);
 }
-
