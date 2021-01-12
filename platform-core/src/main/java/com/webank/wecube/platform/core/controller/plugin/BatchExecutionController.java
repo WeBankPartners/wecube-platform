62a386356c439ae8a0a2630ba0f6b50ef4aedfb4
gavin2lee <gavin2lee@163.com>
2021-01-12 11:34:03 +0800

    #2123 batch execution its

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/BatchExecutionController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/BatchExecutionController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webank.wecube.platform.core.dto.plugin.BatchExecutionRequestDto;
+import com.webank.wecube.platform.core.dto.plugin.BatchExecutionResultDto;
 import com.webank.wecube.platform.core.dto.plugin.CommonResponseDto;
+import com.webank.wecube.platform.core.dto.plugin.ItsDangerConfirmResultDto;
 import com.webank.wecube.platform.core.service.plugin.BatchExecutionService;
 
 @RestController
@@ -25,8 +28,25 @@ public class BatchExecutionController {
      * @return
      */
     @PostMapping("/batch-execution/run")
-    public CommonResponseDto runBatchExecution(@RequestBody BatchExecutionRequestDto batchExecutionRequest){
-        return okayWithData(batchExecutionService.handleBatchExecutionJob(batchExecutionRequest));
+    public CommonResponseDto runBatchExecution(@RequestBody BatchExecutionRequestDto batchExecutionRequest,
+            @RequestParam(value = "continue_token", required = false) String continueToken) {
+
+        BatchExecutionResultDto result = batchExecutionService.handleBatchExecutionJob(batchExecutionRequest,
+                continueToken);
+        ItsDangerConfirmResultDto itsResultDto = result.getItsDangerConfirmResultDto();
+        if (itsResultDto != null) {
+            CommonResponseDto retDto = new CommonResponseDto();
+            retDto.setData(itsResultDto.getData());
+            retDto.setMessage(itsResultDto.getMessage());
+            retDto.setStatus(itsResultDto.getStatus());
+            return retDto;
+        } else {
+
+            CommonResponseDto retObject = okayWithData(result.getResult());
+            return retObject;
+        }
+        // return
+        // okayWithData(batchExecutionService.handleBatchExecutionJob(batchExecutionRequest));
     }
 
 }
