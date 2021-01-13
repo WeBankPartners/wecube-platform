febb67569b218b62896d2fa166127a02358e484f
gavin2lee <gavin2lee@163.com>
2021-01-13 09:31:24 +0800

    #dev3 alter table collation

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.9.0.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.9.0.sql
@@ -1,3 +1,5 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
 ALTER TABLE `plugin_package_attributes` 
 ADD COLUMN `ref_package` VARCHAR(45) NULL,
 ADD COLUMN `ref_entity` VARCHAR(45) NULL,
@@ -18,81 +20,83 @@ ADD COLUMN `pre_check` VARCHAR(45) NULL DEFAULT 'N';
 ALTER TABLE `core_operation_event`
 ADD COLUMN `rev` INT(11) DEFAULT '0';
 
-CREATE TABLE `core_object_meta` (
-  `id` varchar(20) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `package_name` varchar(45) NOT NULL,
-  `source` varchar(45) DEFAULT NULL,
-  `latest_source` varchar(45) DEFAULT NULL,
-  `created_by` varchar(45) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `core_object_meta` (
+  `id` varchar(20) COLLATE utf8_bin  NOT NULL,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `package_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `source` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `latest_source` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `created_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `created_time` datetime NOT NULL,
-  `updated_by` varchar(45) DEFAULT NULL,
+  `updated_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `updated_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE `core_object_property_meta` (
-  `id` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `data_type` varchar(45) NOT NULL,
-  `ref_type` varchar(45) DEFAULT NULL,
-  `map_type` varchar(45) NOT NULL,
-  `map_expr` varchar(500) DEFAULT NULL,
-  `object_meta_id` varchar(45) NOT NULL,
-  `object_name` varchar(45) NOT NULL,
-  `package_name` varchar(45) NOT NULL,
-  `source` varchar(45) DEFAULT NULL,
-  `created_by` varchar(45) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `core_object_property_meta` (
+  `id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `data_type` varchar(45) COLLATE utf8_bin NOT NULL,
+  `ref_type` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `map_type` varchar(45) COLLATE utf8_bin NOT NULL,
+  `map_expr` varchar(500) COLLATE utf8_bin DEFAULT NULL,
+  `object_meta_id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `object_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `package_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `source` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `created_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `created_time` datetime NOT NULL,
-  `updated_by` varchar(45) DEFAULT NULL,
-  `updated_time` datetime DEFAULT NULL,
-  `is_sensitive` bit(1) DEFAULT NULL,
-  `ref_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `core_object_var` (
-  `id` varchar(45) NOT NULL,
-  `object_meta_id` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `package_name` varchar(45) NOT NULL,
-  `created_by` varchar(45) DEFAULT NULL,
-  `created_time` datetime NOT NULL,
-  `updated_by` varchar(45) DEFAULT NULL,
-  `updated_time` datetime DEFAULT NULL,
-  `parent_object_var_id` VARCHAR(45) NULL,
-  `parent_object_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `core_object_property_var` (
-  `id` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `data_type` varchar(45) NOT NULL,
-  `object_property_meta_id` varchar(45) DEFAULT NULL,
-  `object_meta_id` varchar(45) DEFAULT NULL,
-  `object_var_id` varchar(45) NOT NULL,
-  `data_value` varchar(500) DEFAULT NULL,
-  `data_type_id` varchar(45) DEFAULT NULL,
-  `data_id` varchar(45) DEFAULT NULL,
-  `data_name` varchar(45) DEFAULT NULL,
-  `created_by` varchar(45) DEFAULT NULL,
-  `created_time` datetime NOT NULL,
-  `updated_by` varchar(45) DEFAULT NULL,
+  `updated_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `updated_time` datetime DEFAULT NULL,
   `is_sensitive` bit(1) DEFAULT NULL,
-  `object_name` VARCHAR(45) NULL,
-  `package_name` VARCHAR(45) NULL,
+  `ref_name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE `core_object_list_var` (
-  `id` varchar(20) NOT NULL,
-  `data_type` varchar(45) NOT NULL,
-  `data_value` varchar(1000) NOT NULL,
-  `created_by` varchar(45) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `core_object_var` (
+  `id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `object_meta_id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `package_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `created_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `created_time` datetime NOT NULL,
+  `updated_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `parent_object_var_id` VARCHAR(45) COLLATE utf8_bin NULL,
+  `parent_object_name` VARCHAR(45) COLLATE utf8_bin NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `core_object_property_var` (
+  `id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `data_type` varchar(45) COLLATE utf8_bin NOT NULL,
+  `object_property_meta_id` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `object_meta_id` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `object_var_id` varchar(45) COLLATE utf8_bin NOT NULL,
+  `data_value` varchar(500) COLLATE utf8_bin DEFAULT NULL,
+  `data_type_id` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `data_id` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `data_name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `created_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `created_time` datetime NOT NULL,
+  `updated_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `is_sensitive` bit(1) DEFAULT NULL,
+  `object_name` VARCHAR(45) COLLATE utf8_bin NULL,
+  `package_name` VARCHAR(45) COLLATE utf8_bin NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `core_object_list_var` (
+  `id` varchar(20) COLLATE utf8_bin NOT NULL,
+  `data_type` varchar(45) COLLATE utf8_bin NOT NULL,
+  `data_value` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `created_by` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `created_time` datetime NOT NULL,
   `is_sensitive` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
+SET FOREIGN_KEY_CHECKS = 1;
