5c9e6c69d893159e3896075fd5ad4b2f598933cf
gavin2lee <gavin2lee@163.com>
2021-01-14 16:30:51 +0800

    #2124 update column collation

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.6.0.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_2.6.0.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS `plugin_config_roles` (
-  `id` varchar(255) NOT NULL,
-  `created_by` varchar(255) DEFAULT NULL,
+  `id` varchar(255) COLLATE utf8_bin NOT NULL,
+  `created_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `created_time` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
+  `updated_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `updated_time` datetime DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
-  `perm_type` varchar(255) DEFAULT NULL,
-  `plugin_cfg_id` varchar(255) DEFAULT NULL,
-  `role_id` varchar(255) DEFAULT NULL,
-  `role_name` varchar(255) DEFAULT NULL,
+  `perm_type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `plugin_cfg_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `role_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `role_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
