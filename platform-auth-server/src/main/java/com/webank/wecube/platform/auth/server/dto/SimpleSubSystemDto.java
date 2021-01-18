931a94fac1a4742314349c9e8b0efd31f0f6f99e
gavin2lee <gavin2lee@163.com>
2021-01-18 15:53:59 +0800

    #2125 add auth

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/dto/SimpleSubSystemDto.java
+++ platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/dto/SimpleSubSystemDto.java
@@ -5,35 +5,35 @@ import java.io.Serializable;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class SimpleSubSystemDto implements Serializable{
-    
+public class SimpleSubSystemDto implements Serializable {
+
     /**
      * 
      */
     private static final long serialVersionUID = -5229760225564596993L;
     private String id;
-	private String name;
-	private String systemCode;
-	private String description;
-	private String apikey;
-	private boolean active;
-	private boolean blocked;
+    private String name;
+    private String systemCode;
+    private String description;
+    private String apikey;
+    private boolean active;
+    private boolean blocked;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getSystemCode() {
-		return systemCode;
-	}
+    public String getSystemCode() {
+        return systemCode;
+    }
 
-	public void setSystemCode(String systemCode) {
-		this.systemCode = systemCode;
-	}
+    public void setSystemCode(String systemCode) {
+        this.systemCode = systemCode;
+    }
 
     public String getId() {
         return id;
@@ -74,6 +74,26 @@ public class SimpleSubSystemDto implements Serializable{
     public void setApikey(String apikey) {
         this.apikey = apikey;
     }
-	
-	
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("SimpleSubSystemDto [id=");
+        builder.append(id);
+        builder.append(", name=");
+        builder.append(name);
+        builder.append(", systemCode=");
+        builder.append(systemCode);
+        builder.append(", description=");
+        builder.append(description);
+        builder.append(", apikey=");
+        builder.append(apikey);
+        builder.append(", active=");
+        builder.append(active);
+        builder.append(", blocked=");
+        builder.append(blocked);
+        builder.append("]");
+        return builder.toString();
+    }
+
 }
