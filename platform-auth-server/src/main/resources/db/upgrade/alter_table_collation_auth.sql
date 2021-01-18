10d52c8b332b83d5e099f247983b672d85145241
gavin2lee <gavin2lee@163.com>
2021-01-18 11:33:27 +0800

    #2125 refactor auth

--- platform-auth-server/src/main/resources/db/upgrade/alter_table_collation_auth.sql
+++
@@ -1,8 +0,0 @@
-alter table auth_sys_api collate = 'utf8_bin';
-alter table auth_sys_authority collate = 'utf8_bin';
-alter table auth_sys_role collate = 'utf8_bin';
-alter table auth_sys_role_authority collate = 'utf8_bin';
-alter table auth_sys_sub_system collate = 'utf8_bin';
-alter table auth_sys_sub_system_authority collate = 'utf8_bin';
-alter table auth_sys_user collate = 'utf8_bin';
-alter table auth_sys_user_role collate = 'utf8_bin';
