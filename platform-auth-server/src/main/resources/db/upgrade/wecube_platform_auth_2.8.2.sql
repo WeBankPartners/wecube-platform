eefc6768b2eaf29bfcb214533aa0d1dc7620a10c
gavin2lee <gavin2lee@163.com>
2021-01-18 16:49:39 +0800

    #2125 fix sql issue

--- platform-auth-server/src/main/resources/db/upgrade/wecube_platform_auth_2.8.2.sql
+++ platform-auth-server/src/main/resources/db/upgrade/wecube_platform_auth_2.8.2.sql
@@ -7,6 +7,6 @@ alter table auth_sys_role_authority convert to character set utf8 collate utf8_b
 alter table auth_sys_sub_system convert to character set utf8 collate utf8_bin;
 alter table auth_sys_sub_system_authority convert to character set utf8 collate utf8_bin;
 alter table auth_sys_user convert to character set utf8 collate utf8_bin;
-alter table auth_sys_user_role collate convert to character set utf8 collate utf8_bin;
+alter table auth_sys_user_role convert to character set utf8 collate utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
