ce67c41753e84a98e20e4b9e032514743d6032d8
gavin2lee <gavin2lee@163.com>
2021-01-19 11:26:00 +0800

    #2136 add auth control to controllers

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/SystemVariableController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/plugin/SystemVariableController.java
@@ -6,6 +6,7 @@ import static com.webank.wecube.platform.core.dto.plugin.CommonResponseDto.okayW
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,19 +31,27 @@ public class SystemVariableController {
         return okayWithData(systemVariableService.retrieveSystemVariables(queryRequest));
     }
 
+    /**
+     * ADMIN_SYSTEM_PARAMS
+     * @param resourceSystemVariables
+     * @return
+     */
     @PostMapping("/system-variables/create")
+    @PreAuthorize("hasAnyAuthority('ADMIN_SYSTEM_PARAMS')")
     public CommonResponseDto createSystemVariables(@RequestBody List<SystemVariableDto> resourceSystemVariables) {
         systemVariableService.validatePermission();
         return okayWithData(systemVariableService.createSystemVariables(resourceSystemVariables));
     }
 
     @PostMapping("/system-variables/update")
+    @PreAuthorize("hasAnyAuthority('ADMIN_SYSTEM_PARAMS')")
     public CommonResponseDto updateSystemVariables(@RequestBody List<SystemVariableDto> resourceSystemVariables) {
         systemVariableService.validatePermission();
         return okayWithData(systemVariableService.updateSystemVariables(resourceSystemVariables));
     }
 
     @PostMapping("/system-variables/delete")
+    @PreAuthorize("hasAnyAuthority('ADMIN_SYSTEM_PARAMS')")
     public CommonResponseDto deleteSystemVariables(@RequestBody List<SystemVariableDto> resourceSystemVariables) {
         systemVariableService.validatePermission();
         systemVariableService.deleteSystemVariables(resourceSystemVariables);
