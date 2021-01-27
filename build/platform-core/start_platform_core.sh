d1c4fcb66922458dc89b71b88063bc2865909fcc
gavin2lee <gavin2lee@163.com>
2021-01-27 09:28:23 +0800

    #2125 fix time zone issue

--- build/platform-core/start_platform_core.sh
+++ build/platform-core/start_platform_core.sh
@@ -4,9 +4,6 @@ java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=${WECUBE
 -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=${WECUBE_CORE_HOST} -Djava.security.egd=file:/dev/urandom \
 -jar /application/platform-core.jar  --server.address=0.0.0.0 --server.port=8080 \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
---spring.datasource.url=jdbc:mysql://${MYSQL_SERVER_ADDR}:${MYSQL_SERVER_PORT}/${MYSQL_SERVER_DATABASE_NAME}?serverTimezone=Asia\/Shanghai\&characterEncoding=utf8\&nullCatalogMeansCurrent=true \
---spring.datasource.username=${MYSQL_USER_NAME} \
---spring.datasource.password=${MYSQL_USER_PASSWORD}  \
 --wecube.core.s3.endpoint=${S3_ENDPOINT} \
 --wecube.core.s3.access-key=${S3_ACCESS_KEY} \
 --wecube.core.s3.secret-key=${S3_SECRET_KEY} \
