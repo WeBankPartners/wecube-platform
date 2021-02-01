d640022c2e0be575c3a2edbb052837b863cd7325
Tong Zhang <tz.tong.zhang@hotmail.com>
2021-02-01 14:10:39 +0800

    more logs

--- build/sync-github-release-to-gitee.sh
+++ build/sync-github-release-to-gitee.sh
@@ -13,6 +13,7 @@ else
 	GITHUB_RELEASE_URL="${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/tags/${GITHUB_REF}"
 fi
 
+echo "Fetching GitHub release from \"${GITHUB_RELEASE_URL}\"..."
 RELEASE_JSON=$(curl -sSfL \
 	--request GET "${GITHUB_RELEASE_URL}" \
 	| jq --exit-status --arg access_token ${GITEE_API_TOKEN} \
@@ -21,6 +22,7 @@ RELEASE_JSON=$(curl -sSfL \
 
 TAG_NAME=$(jq -r '.tag_name' <<<"$RELEASE_JSON")
 
+echo "Fetching Gitee release for tag \"${TAG_NAME}\"..."
 GITEE_RELEASE_ID=$(curl -sSfL \
 	--request GET "https://gitee.com/api/v5/repos/${GITHUB_REPOSITORY}/releases/tags/${TAG_NAME}" 2>/dev/null \
 	| jq --exit-status '.id'
@@ -28,11 +30,13 @@ GITEE_RELEASE_ID=$(curl -sSfL \
 
 
 if [ -z "${GITEE_RELEASE_ID}" ]; then
+	echo "Creating new Gitee release..."
 	curl -sSfL \
 		--request POST "https://gitee.com/api/v5/repos/${GITHUB_REPOSITORY}/releases" \
 		--header 'Content-Type: application/json;charset=UTF-8' \
 		--data @- <<<"$RELEASE_JSON"
 else
+	echo "Patching Gitee release for id \"${GITEE_RELEASE_ID}\""
 	curl -sSfL \
 		--request PATCH "https://gitee.com/api/v5/repos/${GITHUB_REPOSITORY}/releases/${GITEE_RELEASE_ID}" \
 		--header 'Content-Type: application/json;charset=UTF-8' \
