a3fe2efe1e42805633ff4e735b7959daf8882423
gavin2lee <gavin2lee@163.com>
2021-04-09 10:17:39 +0800

    #2169 add context overview

--- platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/RequestObjectDto.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/RequestObjectDto.java
@@ -1,34 +1,114 @@
 package com.webank.wecube.platform.core.dto.workflow;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestObjectDto {
-    private Map<String, String> inputs = new HashMap<>();
-    private Map<String, String> outputs = new HashMap<>();
+    private String callbackParameter;
+    private List<RequestParamObjectDto> inputs = new ArrayList<>();
+    private List<RequestParamObjectDto> outputs = new ArrayList<>();
 
-    public Map<String, String> getInputs() {
+    public String getCallbackParameter() {
+        return callbackParameter;
+    }
+
+    public void setCallbackParameter(String callbackParameter) {
+        this.callbackParameter = callbackParameter;
+    }
+
+    public List<RequestParamObjectDto> getInputs() {
         return inputs;
     }
 
-    public void setInputs(Map<String, String> inputs) {
+    public void setInputs(List<RequestParamObjectDto> inputs) {
         this.inputs = inputs;
     }
 
-    public Map<String, String> getOutputs() {
+    public List<RequestParamObjectDto> getOutputs() {
         return outputs;
     }
 
-    public void setOutputs(Map<String, String> outputs) {
+    public void setOutputs(List<RequestParamObjectDto> outputs) {
         this.outputs = outputs;
     }
-    
-    public void addInput(String paramName, String paramValAsStr){
-        this.inputs.put(paramName, paramValAsStr);
+
+    public void addInput(RequestParamObjectDto dto) {
+        this.inputs.add(dto);
     }
-    
-    public void addOutput(String paramName, String paramValAsStr){
-        this.outputs.put(paramName, paramValAsStr);
+
+    public void addOutput(RequestParamObjectDto dto) {
+        this.outputs.add(dto);
+    }
+
+    public static class RequestParamObjectDto {
+        private String objectId;
+        private String callbackParameter;
+        private List<RequestParamAttrDto> paramAttrs = new ArrayList<>();
+
+        public String getObjectId() {
+            return objectId;
+        }
+
+        public void setObjectId(String objectId) {
+            this.objectId = objectId;
+        }
+
+        public String getCallbackParameter() {
+            return callbackParameter;
+        }
+
+        public void setCallbackParameter(String callbackParameter) {
+            this.callbackParameter = callbackParameter;
+        }
+
+        public List<RequestParamAttrDto> getParamAttrs() {
+            return paramAttrs;
+        }
+
+        public void setParamAttrs(List<RequestParamAttrDto> paramAttrs) {
+            this.paramAttrs = paramAttrs;
+        }
+
+        public void addParamAttr(String paramName, String paramValAsStr) {
+            RequestParamAttrDto dto = new RequestParamAttrDto(paramName, paramValAsStr);
+            this.paramAttrs.add(dto);
+        }
+
+        public void addParamAttr(RequestParamAttrDto dto) {
+            this.paramAttrs.add(dto);
+        }
+
+    }
+
+    public static class RequestParamAttrDto {
+        private String paramName;
+        private String paramValue;
+
+        public RequestParamAttrDto() {
+
+        }
+
+        public RequestParamAttrDto(String paramName, String paramValue) {
+            this.paramName = paramName;
+            this.paramValue = paramValue;
+        }
+
+        public String getParamName() {
+            return paramName;
+        }
+
+        public void setParamName(String paramName) {
+            this.paramName = paramName;
+        }
+
+        public String getParamValue() {
+            return paramValue;
+        }
+
+        public void setParamValue(String paramValue) {
+            this.paramValue = paramValue;
+        }
+
     }
 
 }
