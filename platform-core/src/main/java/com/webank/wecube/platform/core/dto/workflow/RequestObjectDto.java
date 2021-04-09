386ea5a9a91ba8b21c1cba55f060d6ccfb54c224
gavin2lee <gavin2lee@163.com>
2021-04-09 14:53:58 +0800

    #2169 refactor context overview

--- platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/RequestObjectDto.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/RequestObjectDto.java
@@ -1,12 +1,14 @@
 package com.webank.wecube.platform.core.dto.workflow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RequestObjectDto {
     private String callbackParameter;
-    private List<RequestParamObjectDto> inputs = new ArrayList<>();
-    private List<RequestParamObjectDto> outputs = new ArrayList<>();
+    private List<Map<String, String>> inputs = new ArrayList<>();
+    private List<Map<String, String>> outputs = new ArrayList<>();
 
     public String getCallbackParameter() {
         return callbackParameter;
@@ -16,34 +18,34 @@ public class RequestObjectDto {
         this.callbackParameter = callbackParameter;
     }
 
-    public List<RequestParamObjectDto> getInputs() {
+    public List<Map<String, String>> getInputs() {
         return inputs;
     }
 
-    public void setInputs(List<RequestParamObjectDto> inputs) {
+    public void setInputs(List<Map<String, String>> inputs) {
         this.inputs = inputs;
     }
 
-    public List<RequestParamObjectDto> getOutputs() {
+    public List<Map<String, String>> getOutputs() {
         return outputs;
     }
 
-    public void setOutputs(List<RequestParamObjectDto> outputs) {
+    public void setOutputs(List<Map<String, String>> outputs) {
         this.outputs = outputs;
     }
 
-    public void addInput(RequestParamObjectDto dto) {
-        this.inputs.add(dto);
+    public void addInput(Map<String, String> dtoMap) {
+        this.inputs.add(dtoMap);
     }
 
-    public void addOutput(RequestParamObjectDto dto) {
-        this.outputs.add(dto);
+    public void addOutput(Map<String, String> dtoMap) {
+        this.outputs.add(dtoMap);
     }
 
     public static class RequestParamObjectDto {
         private String objectId;
         private String callbackParameter;
-        private List<RequestParamAttrDto> paramAttrs = new ArrayList<>();
+        private Map<String, String> paramAttrs = new HashMap<>();
 
         public String getObjectId() {
             return objectId;
@@ -61,54 +63,49 @@ public class RequestObjectDto {
             this.callbackParameter = callbackParameter;
         }
 
-        public List<RequestParamAttrDto> getParamAttrs() {
+        public Map<String, String> getParamAttrs() {
             return paramAttrs;
         }
 
-        public void setParamAttrs(List<RequestParamAttrDto> paramAttrs) {
+        public void setParamAttrs(Map<String, String> paramAttrs) {
             this.paramAttrs = paramAttrs;
         }
 
         public void addParamAttr(String paramName, String paramValAsStr) {
-            RequestParamAttrDto dto = new RequestParamAttrDto(paramName, paramValAsStr);
-            this.paramAttrs.add(dto);
-        }
-
-        public void addParamAttr(RequestParamAttrDto dto) {
-            this.paramAttrs.add(dto);
+            this.paramAttrs.put(paramName, paramValAsStr);
         }
 
     }
 
-    public static class RequestParamAttrDto {
-        private String paramName;
-        private String paramValue;
-
-        public RequestParamAttrDto() {
-
-        }
-
-        public RequestParamAttrDto(String paramName, String paramValue) {
-            this.paramName = paramName;
-            this.paramValue = paramValue;
-        }
-
-        public String getParamName() {
-            return paramName;
-        }
-
-        public void setParamName(String paramName) {
-            this.paramName = paramName;
-        }
-
-        public String getParamValue() {
-            return paramValue;
-        }
-
-        public void setParamValue(String paramValue) {
-            this.paramValue = paramValue;
-        }
-
-    }
+    // public static class RequestParamAttrDto {
+    // private String paramName;
+    // private String paramValue;
+    //
+    // public RequestParamAttrDto() {
+    //
+    // }
+    //
+    // public RequestParamAttrDto(String paramName, String paramValue) {
+    // this.paramName = paramName;
+    // this.paramValue = paramValue;
+    // }
+    //
+    // public String getParamName() {
+    // return paramName;
+    // }
+    //
+    // public void setParamName(String paramName) {
+    // this.paramName = paramName;
+    // }
+    //
+    // public String getParamValue() {
+    // return paramValue;
+    // }
+    //
+    // public void setParamValue(String paramValue) {
+    // this.paramValue = paramValue;
+    // }
+    //
+    // }
 
 }
