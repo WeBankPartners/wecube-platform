d65ab3ff321f5cd9e06fadc4fc09e9c1000de185
gavin2lee <gavin2lee@163.com>
2021-04-19 17:08:06 +0800

    #2169 add mappers

---
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `core_ru_graph_node` 
+ADD COLUMN `full_data_id` VARCHAR(300) NULL;
+
+ALTER TABLE `core_ru_proc_exec_binding` 
+ADD COLUMN `full_data_id` VARCHAR(300) NULL;
+
+ALTER TABLE `core_ru_proc_exec_binding_tmp` 
+ADD COLUMN `full_data_id` VARCHAR(300) NULL;
