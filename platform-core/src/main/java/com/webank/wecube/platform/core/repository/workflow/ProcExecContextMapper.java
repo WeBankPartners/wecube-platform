94a391482d2b7a9411be7fc956b77673d7f8f933
gavin2lee <gavin2lee@163.com>
2021-05-08 16:07:41 +0800

    #2109 add user task implementation

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/ProcExecContextMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/ProcExecContextMapper.java
@@ -1,18 +1,22 @@
 package com.webank.wecube.platform.core.repository.workflow;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.webank.wecube.platform.core.entity.workflow.ProcExecContextEntity;
 
 @Repository
 public interface ProcExecContextMapper {
-    
+
     int deleteByPrimaryKey(String id);
 
     int insert(ProcExecContextEntity record);
-    
+
     ProcExecContextEntity selectByPrimaryKey(String id);
 
     int updateByPrimaryKeySelective(ProcExecContextEntity record);
+
+    List<ProcExecContextEntity> selectAllContextByCtxType(String procDefId, Integer procInstId, String ctxType);
 
 }
