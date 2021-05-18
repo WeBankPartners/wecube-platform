e8690616284f4fd4eb3239fe32a0ae661b7dca14
gavin2lee <gavin2lee@163.com>
2021-05-18 15:53:24 +0800

    #2109 add object mapping

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/CoreObjectVarCalculationContext.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/CoreObjectVarCalculationContext.java
@@ -1,5 +1,9 @@
 package com.webank.wecube.platform.core.service.plugin;
 
+import java.util.Map;
+
 public class CoreObjectVarCalculationContext {
+    
+    private Map<Object, Object> externalCacheMap;
 
 }
