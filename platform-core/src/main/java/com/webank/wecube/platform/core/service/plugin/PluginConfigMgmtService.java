a24610b8e3bfc2cf64fbb2d74f4e5dad11963e5b
gavin2lee <gavin2lee@163.com>
2021-06-11 15:44:58 +0800

    #2109 add transaction annotation

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/PluginConfigMgmtService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/plugin/PluginConfigMgmtService.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.webank.wecube.platform.core.commons.AuthenticationContextHolder;
 import com.webank.wecube.platform.core.commons.WecubeCoreException;
@@ -607,6 +608,7 @@ public class PluginConfigMgmtService extends AbstractPluginMgmtService {
      * @param pluginConfigDto
      * @return
      */
+    @Transactional
     public PluginConfigDto createOrUpdatePluginConfig(PluginConfigDto pluginConfigDto) {
         validatePermission(pluginConfigDto.getPermissionToRole());
 
