2494e5d12fa233bb4678b255d20d86b4ea3f00c4
gavin2lee <gavin2lee@163.com>
2021-06-25 10:16:01 +0800

    #2226 add column to object meta

---
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `plugin_object_meta` ADD COLUMN `map_expr` VARCHAR(300) NULL;
+ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_type` VARCHAR(45) NULL;
+ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_name` VARCHAR(45) NULL;
