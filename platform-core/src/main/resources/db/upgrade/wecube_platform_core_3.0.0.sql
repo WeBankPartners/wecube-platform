1d65aa0cc6d3ce019cb1c35f246807e64a71ed96
gavin2lee <gavin2lee@163.com>
2021-06-28 10:59:05 +0800

    #2226 remove ref type

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.0.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `plugin_object_meta` ADD COLUMN `map_expr` VARCHAR(300) NULL;
 ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_type` VARCHAR(45) NULL;
-ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_name` VARCHAR(45) NULL;
