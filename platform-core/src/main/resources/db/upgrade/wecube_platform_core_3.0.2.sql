d2cf93cbc82dc5ffff8b57d3572719a1237474a3
gavin2lee <gavin2lee@163.com>
2021-07-13 15:51:08 +0800

    #2233 add mappings to multiple field

---
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.2.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `multiple` VARCHAR(10) NULL;
+ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_object_name` VARCHAR(60) NULL;
+ALTER TABLE `plugin_object_property_meta` DROP COLUMN `ref_type`;
+ALTER TABLE `plugin_object_property_meta` CHANGE COLUMN `ref_name` `ref_object_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ;
+ALTER TABLE `plugin_object_property_meta` ADD COLUMN `multiple` VARCHAR(10) NULL;
