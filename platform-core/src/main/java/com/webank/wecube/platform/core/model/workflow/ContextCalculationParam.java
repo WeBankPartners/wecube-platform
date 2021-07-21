2dde097564e8ee86ae65f97b93a5f10627307e6e
gavin2lee <gavin2lee@163.com>
2021-07-21 17:02:26 +0800

    #2233 rename field of data type

--- platform-core/src/main/java/com/webank/wecube/platform/core/model/workflow/ContextCalculationParam.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/model/workflow/ContextCalculationParam.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.webank.wecube.platform.core.entity.plugin.PluginConfigInterfaceParameters;
 import com.webank.wecube.platform.core.entity.plugin.PluginConfigInterfaces;
+import com.webank.wecube.platform.core.entity.workflow.ProcDefInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.ProcInstInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeDefInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.TaskNodeExecParamEntity;
@@ -14,89 +15,122 @@ import com.webank.wecube.platform.core.entity.workflow.TaskNodeParamEntity;
 
 public class ContextCalculationParam {
     private String paramName;
-    private String paramType;
+    private String paramDataType;
     private PluginConfigInterfaceParameters param;
+    private PluginConfigInterfaces pluginConfigInterface;
+    private ProcDefInfoEntity procDefInfoEntity;
     private ProcInstInfoEntity procInstEntity;
     private TaskNodeInstInfoEntity currTaskNodeInstEntity;
     private TaskNodeDefInfoEntity currTaskNodeDefEntity;
-    private PluginConfigInterfaces pluginConfigInterface;
     private TaskNodeParamEntity nodeParamEntity;
     private TaskNodeInstInfoEntity boundNodeInstEntity;
     private TaskNodeDefInfoEntity boundNodeDefEntity;
     private TaskNodeExecRequestEntity boundNodeRequestEntity;
     private List<TaskNodeExecParamEntity> boundExecParamEntities = new ArrayList<>();
+
     public String getParamName() {
         return paramName;
     }
+
     public void setParamName(String paramName) {
         this.paramName = paramName;
     }
-    public String getParamType() {
-        return paramType;
+
+    public String getParamDataType() {
+        return paramDataType;
     }
-    public void setParamType(String paramType) {
-        this.paramType = paramType;
+
+    public void setParamDataType(String paramDataType) {
+        this.paramDataType = paramDataType;
     }
+
     public PluginConfigInterfaceParameters getParam() {
         return param;
     }
+
     public void setParam(PluginConfigInterfaceParameters param) {
         this.param = param;
     }
+
     public ProcInstInfoEntity getProcInstEntity() {
         return procInstEntity;
     }
+
     public void setProcInstEntity(ProcInstInfoEntity procInstEntity) {
         this.procInstEntity = procInstEntity;
     }
+
     public TaskNodeInstInfoEntity getCurrTaskNodeInstEntity() {
         return currTaskNodeInstEntity;
     }
+
     public void setCurrTaskNodeInstEntity(TaskNodeInstInfoEntity currTaskNodeInstEntity) {
         this.currTaskNodeInstEntity = currTaskNodeInstEntity;
     }
+
     public TaskNodeDefInfoEntity getCurrTaskNodeDefEntity() {
         return currTaskNodeDefEntity;
     }
+
     public void setCurrTaskNodeDefEntity(TaskNodeDefInfoEntity currTaskNodeDefEntity) {
         this.currTaskNodeDefEntity = currTaskNodeDefEntity;
     }
+
     public PluginConfigInterfaces getPluginConfigInterface() {
         return pluginConfigInterface;
     }
+
     public void setPluginConfigInterface(PluginConfigInterfaces pluginConfigInterface) {
         this.pluginConfigInterface = pluginConfigInterface;
     }
+
     public TaskNodeParamEntity getNodeParamEntity() {
         return nodeParamEntity;
     }
+
     public void setNodeParamEntity(TaskNodeParamEntity nodeParamEntity) {
         this.nodeParamEntity = nodeParamEntity;
     }
+
     public TaskNodeInstInfoEntity getBoundNodeInstEntity() {
         return boundNodeInstEntity;
     }
+
     public void setBoundNodeInstEntity(TaskNodeInstInfoEntity boundNodeInstEntity) {
         this.boundNodeInstEntity = boundNodeInstEntity;
     }
+
     public TaskNodeDefInfoEntity getBoundNodeDefEntity() {
         return boundNodeDefEntity;
     }
+
     public void setBoundNodeDefEntity(TaskNodeDefInfoEntity boundNodeDefEntity) {
         this.boundNodeDefEntity = boundNodeDefEntity;
     }
+
     public TaskNodeExecRequestEntity getBoundNodeRequestEntity() {
         return boundNodeRequestEntity;
     }
+
     public void setBoundNodeRequestEntity(TaskNodeExecRequestEntity boundNodeRequestEntity) {
         this.boundNodeRequestEntity = boundNodeRequestEntity;
     }
+
     public List<TaskNodeExecParamEntity> getBoundExecParamEntities() {
         return boundExecParamEntities;
     }
+
     public void setBoundExecParamEntities(List<TaskNodeExecParamEntity> boundExecParamEntities) {
         this.boundExecParamEntities = boundExecParamEntities;
     }
-    
+
+    public ProcDefInfoEntity getProcDefInfoEntity() {
+        return procDefInfoEntity;
+    }
+
+    public void setProcDefInfoEntity(ProcDefInfoEntity procDefInfoEntity) {
+        this.procDefInfoEntity = procDefInfoEntity;
+    }
+
     
 }
