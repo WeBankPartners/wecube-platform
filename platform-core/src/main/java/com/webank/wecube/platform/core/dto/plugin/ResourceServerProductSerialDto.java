d4b02a8268b56f20d12ef386ab96d44ce164b03d
gavin2lee <gavin2lee@163.com>
2021-08-02 16:55:13 +0800

    #2233 add product serial

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/plugin/ResourceServerProductSerialDto.java
@@ -0,0 +1,16 @@
+package com.webank.wecube.platform.core.dto.plugin;
+
+public class ResourceServerProductSerialDto {
+
+    private String productSerial;
+
+    public String getProductSerial() {
+        return productSerial;
+    }
+
+    public void setProductSerial(String productSerial) {
+        this.productSerial = productSerial;
+    }
+    
+    
+}
