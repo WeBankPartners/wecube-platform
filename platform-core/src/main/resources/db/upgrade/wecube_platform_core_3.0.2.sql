64e06d786184604520bcafacf2e741d22f3ecf02
gavin2lee <gavin2lee@163.com>
2021-08-04 20:15:59 +0800

    #2233 remove change column statement

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.2.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.0.2.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `multiple` VARCHAR(10) NULL;
 ALTER TABLE `plugin_config_interface_parameters` ADD COLUMN `ref_object_name` VARCHAR(60) NULL;
 
-ALTER TABLE `plugin_object_property_meta` CHANGE COLUMN `ref_name` `ref_object_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ;
+ALTER TABLE `plugin_object_property_meta` ADD COLUMN `ref_object_name` VARCHAR(10) NULL;
 ALTER TABLE `plugin_object_property_meta` ADD COLUMN `multiple` VARCHAR(10) NULL;
 
 ALTER TABLE `plugin_object_property_var` RENAME TO `core_object_property_var`;
