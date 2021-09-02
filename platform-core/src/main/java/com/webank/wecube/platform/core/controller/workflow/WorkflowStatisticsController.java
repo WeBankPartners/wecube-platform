5e24ced9183b31044b3267787a447593dfbf1137
gavin2lee <gavin2lee@163.com>
2021-09-02 17:30:47 +0800

    #2254 add query dto

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/workflow/WorkflowStatisticsController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/workflow/WorkflowStatisticsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.webank.wecube.platform.core.dto.workflow.ProcDefInfoDto;
 import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefBriefDto;
 import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefObjectBindInfoDto;
 import com.webank.wecube.platform.core.dto.workflow.WorkflowExecutionReportItemDto;
+import com.webank.wecube.platform.core.dto.workflow.WorkflowExecutionReportQueryDto;
 import com.webank.wecube.platform.core.service.workflow.WorkflowStatisticsService;
 
 @RestController
@@ -37,27 +39,27 @@ public class WorkflowStatisticsController {
      * @return
      */
     @PostMapping("/statistics/process/definitions/tasknodes/query")
-    public CommonResponseDto fetchWorkflowTasknodeInfos() {
-        List<TaskNodeDefBriefDto> nodeDefInfos = workflowStatisticsService.fetchWorkflowTasknodeInfos();
+    public CommonResponseDto fetchWorkflowTasknodeInfos(@RequestBody List<String> procDefIds) {
+        List<TaskNodeDefBriefDto> nodeDefInfos = workflowStatisticsService.fetchWorkflowTasknodeInfos(procDefIds);
         return CommonResponseDto.okayWithData(nodeDefInfos);
     }
     
     @PostMapping("/statistics/process/definitions/tasknodes/tasknode-bindings/query")
-    public CommonResponseDto fetchWorkflowTasknodeBindings() {
+    public CommonResponseDto fetchWorkflowTasknodeBindings(@RequestBody List<String> taskNodeIds) {
         
-        List<TaskNodeDefObjectBindInfoDto> bindings = workflowStatisticsService.fetchWorkflowTasknodeBindings();
+        List<TaskNodeDefObjectBindInfoDto> bindings = workflowStatisticsService.fetchWorkflowTasknodeBindings(taskNodeIds);
         return CommonResponseDto.okayWithData(bindings);
     }
     
     @PostMapping("/statistics/process/definitions/executions/tasknodes/reports/query")
-    public CommonResponseDto fetchWorkflowExecutionTasknodeReports() {
+    public CommonResponseDto fetchWorkflowExecutionTasknodeReports(@RequestBody WorkflowExecutionReportQueryDto queryDto) {
         //TODO
         List<WorkflowExecutionReportItemDto> items = workflowStatisticsService.fetchWorkflowExecutionTasknodeReports();
         return CommonResponseDto.okayWithData(items);
     }
     
     @PostMapping("/statistics/process/definitions/executions/plugin/reports/query")
-    public CommonResponseDto fetchWorkflowExecutionPluginReports() {
+    public CommonResponseDto fetchWorkflowExecutionPluginReports(@RequestBody WorkflowExecutionReportQueryDto queryDto) {
         //TODO
         List<WorkflowExecutionReportItemDto> items = workflowStatisticsService.fetchWorkflowExecutionPluginReports();
         return CommonResponseDto.okayWithData(items);
