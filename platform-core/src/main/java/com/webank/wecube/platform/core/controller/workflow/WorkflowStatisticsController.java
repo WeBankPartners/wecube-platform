0a018643484b3d9ec684be20cde46b6528912d7a
gavin2lee <gavin2lee@163.com>
2021-09-02 16:16:43 +0800

    #2254 add report dtos

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/workflow/WorkflowStatisticsController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/workflow/WorkflowStatisticsController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webank.wecube.platform.core.dto.plugin.CommonResponseDto;
+import com.webank.wecube.platform.core.dto.workflow.ProcDefInfoDto;
+import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefBriefDto;
+import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefObjectBindInfoDto;
+import com.webank.wecube.platform.core.dto.workflow.WorkflowExecutionReportItemDto;
 import com.webank.wecube.platform.core.service.workflow.WorkflowStatisticsService;
-import com.webank.wecube.platform.core.support.plugin.dto.WorkflowDefInfoDto;
-import com.webank.wecube.platform.core.support.plugin.dto.WorkflowNodeDefInfoDto;
 
 @RestController
 @RequestMapping("/v1")
@@ -20,35 +22,45 @@ public class WorkflowStatisticsController {
     @Autowired
     private WorkflowStatisticsService workflowStatisticsService;
 
+    /**
+     * 
+     * @return
+     */
     @GetMapping("/statistics/process/definitions")
     public CommonResponseDto fetchReleasedWorkflowDefs() {
-        List<WorkflowDefInfoDto> procDefInfos = workflowStatisticsService.fetchReleasedWorkflowDefs();
+        List<ProcDefInfoDto> procDefInfos = workflowStatisticsService.fetchReleasedWorkflowDefs();
         return CommonResponseDto.okayWithData(procDefInfos);
     }
     
+    /**
+     * 
+     * @return
+     */
     @PostMapping("/statistics/process/definitions/tasknodes/query")
     public CommonResponseDto fetchWorkflowTasknodeInfos() {
-        //TODO
-        List<WorkflowNodeDefInfoDto> nodeDefInfos = null;
+        List<TaskNodeDefBriefDto> nodeDefInfos = workflowStatisticsService.fetchWorkflowTasknodeInfos();
         return CommonResponseDto.okayWithData(nodeDefInfos);
     }
     
     @PostMapping("/statistics/process/definitions/tasknodes/tasknode-bindings/query")
     public CommonResponseDto fetchWorkflowTasknodeBindings() {
-        //TODO
-        return null;
+        
+        List<TaskNodeDefObjectBindInfoDto> bindings = workflowStatisticsService.fetchWorkflowTasknodeBindings();
+        return CommonResponseDto.okayWithData(bindings);
     }
     
     @PostMapping("/statistics/process/definitions/executions/tasknodes/reports/query")
     public CommonResponseDto fetchWorkflowExecutionTasknodeReports() {
         //TODO
-        return null;
+        List<WorkflowExecutionReportItemDto> items = workflowStatisticsService.fetchWorkflowExecutionTasknodeReports();
+        return CommonResponseDto.okayWithData(items);
     }
     
     @PostMapping("/statistics/process/definitions/executions/plugin/reports/query")
     public CommonResponseDto fetchWorkflowExecutionPluginReports() {
         //TODO
-        return null;
+        List<WorkflowExecutionReportItemDto> items = workflowStatisticsService.fetchWorkflowExecutionPluginReports();
+        return CommonResponseDto.okayWithData(items);
     }
     
     @PostMapping("/statistics/process/definitions/executions/report-details/query")
--- platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/WorkflowStatisticsService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/WorkflowStatisticsService.java
@@ -6,16 +6,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.webank.wecube.platform.core.support.plugin.dto.WorkflowDefInfoDto;
+import com.webank.wecube.platform.core.dto.workflow.ProcDefInfoDto;
+import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefBriefDto;
+import com.webank.wecube.platform.core.dto.workflow.TaskNodeDefObjectBindInfoDto;
+import com.webank.wecube.platform.core.dto.workflow.WorkflowExecutionReportItemDto;
 
 @Service
 public class WorkflowStatisticsService {
     private static final Logger log = LoggerFactory.getLogger(WorkflowStatisticsService.class);
     
-    public List<WorkflowDefInfoDto> fetchReleasedWorkflowDefs(){
+    public List<ProcDefInfoDto> fetchReleasedWorkflowDefs(){
         //TODO
         return null;
     }
     
+    public List<TaskNodeDefBriefDto> fetchWorkflowTasknodeInfos(){
+        return null;
+    }
     
+    public List<TaskNodeDefObjectBindInfoDto> fetchWorkflowTasknodeBindings(){
+        return null;
+    }
+    
+    public List<WorkflowExecutionReportItemDto> fetchWorkflowExecutionTasknodeReports(){
+        return null;
+    }
+    
+    public List<WorkflowExecutionReportItemDto> fetchWorkflowExecutionPluginReports(){
+        return null;
+    }
 }
