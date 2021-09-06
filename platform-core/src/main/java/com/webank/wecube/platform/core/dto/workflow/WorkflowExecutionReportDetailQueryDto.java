d47b1b4ddfaf5ae5c73ff894b2bbd7c68e03b888
gavin2lee <gavin2lee@163.com>
2021-09-06 18:05:44 +0800

    #2254 add impl to detail query

--- platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/WorkflowExecutionReportDetailQueryDto.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/dto/workflow/WorkflowExecutionReportDetailQueryDto.java
@@ -9,6 +9,9 @@ public class WorkflowExecutionReportDetailQueryDto {
     private String entityDataId;
     
     private String status;//Completed,Faulted
+    
+    private String startDate;
+    private String endDate;
 
     public String getProcDefId() {
         return procDefId;
@@ -48,6 +51,22 @@ public class WorkflowExecutionReportDetailQueryDto {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
     }
     
     
