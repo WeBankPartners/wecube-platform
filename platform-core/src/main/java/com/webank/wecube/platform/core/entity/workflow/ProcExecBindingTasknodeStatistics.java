8013a19794315936d2aabb825218bd9080bf2dcd
gavin2lee <gavin2lee@163.com>
2021-09-06 17:05:55 +0800

    #2254 add task node statistics implementation

---
+++ platform-core/src/main/java/com/webank/wecube/platform/core/entity/workflow/ProcExecBindingTasknodeStatistics.java
@@ -0,0 +1,35 @@
+package com.webank.wecube.platform.core.entity.workflow;
+
+public class ProcExecBindingTasknodeStatistics {
+
+    private String nodeDefId;
+    private String entityDataId;
+    private String entityDataName;
+
+    public String getNodeDefId() {
+        return nodeDefId;
+    }
+
+    public void setNodeDefId(String nodeDefId) {
+        this.nodeDefId = nodeDefId;
+    }
+
+    public String getEntityDataId() {
+        return entityDataId;
+    }
+
+    public void setEntityDataId(String entityDataId) {
+        this.entityDataId = entityDataId;
+    }
+
+    public String getEntityDataName() {
+        return entityDataName;
+    }
+
+    public void setEntityDataName(String entityDataName) {
+        this.entityDataName = entityDataName;
+    }
+    
+    
+
+}
