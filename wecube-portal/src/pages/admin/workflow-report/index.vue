05e544e1238eb6c6bae1afaf5e3ea578c1b433fc
pobu168 <244202969@qq.com>
2021-09-08 11:23:28 +0800

    workflow report config

---
+++ wecube-portal/src/pages/admin/workflow-report/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="">
+    <Tabs value="tasknodeAspect" type="card">
+      <TabPane :label="$t('workflow_report_aspect')" name="workflowAspect">
+        <WorkflowAspect></WorkflowAspect>
+      </TabPane>
+      <TabPane :label="$t('workflow_task_node_aspect')" name="pluginAspect">
+        <PluginAspect></PluginAspect>
+      </TabPane>
+      <TabPane :label="$t('workflow_plugin_aspect')" name="tasknodeAspect">
+        <TasknodeAspect></TasknodeAspect>
+      </TabPane>
+    </Tabs>
+  </div>
+</template>
+
+<script>
+import WorkflowAspect from './workflow-aspect'
+import PluginAspect from './plugin-aspect'
+import TasknodeAspect from './tasknode-aspect'
+export default {
+  name: '',
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {
+    WorkflowAspect,
+    PluginAspect,
+    TasknodeAspect
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
