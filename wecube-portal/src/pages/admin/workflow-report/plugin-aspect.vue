f04e4e539782ab5a5b956c2634cb7cacdf652f95
pobu168 <244202969@qq.com>
2021-09-08 11:23:28 +0800

    workflow report config

---
+++ wecube-portal/src/pages/admin/workflow-report/plugin-aspect.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=" ">
+    plugin
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
