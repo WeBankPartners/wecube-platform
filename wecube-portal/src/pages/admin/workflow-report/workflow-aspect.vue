23e58783b5f962bda0d8dc52b1e3d44075f6c7fe
pobu168 <244202969@qq.com>
2021-09-08 11:23:28 +0800

    workflow report config

---
+++ wecube-portal/src/pages/admin/workflow-report/workflow-aspect.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class=" ">
+    workflow
+
+    <!-- <DatePicker type="datetime" format="yyyy-MM-dd HH:mm" placeholder="Select date and time(Excluding seconds)" style="width: 200px"></DatePicker> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
