cc9bf3a92de7c85854717c477251887bed04ffab
gavin2lee <gavin2lee@163.com>
2021-09-08 15:42:04 +0800

    #2254 add process overview

--- platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/ProcInstInfoMapper.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/repository/workflow/ProcInstInfoMapper.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.webank.wecube.platform.core.entity.workflow.ProcDefInfoOverviewEntity;
 import com.webank.wecube.platform.core.entity.workflow.ProcInstInfoEntity;
 import com.webank.wecube.platform.core.entity.workflow.ProcInstInfoQueryEntity;
 
 @Repository
-public interface ProcInstInfoMapper{
+public interface ProcInstInfoMapper {
     int deleteByPrimaryKey(Integer id);
+
     int insert(ProcInstInfoEntity record);
+
     int insertSelective(ProcInstInfoEntity record);
+
     ProcInstInfoEntity selectByPrimaryKey(Integer id);
+
     int updateByPrimaryKeySelective(ProcInstInfoEntity record);
+
     int updateByPrimaryKey(ProcInstInfoEntity record);
-    
+
     /**
      * 
      * @param procInstKernelId
@@ -30,26 +36,39 @@ public interface ProcInstInfoMapper{
      * @return
      */
     List<ProcInstInfoEntity> selectByProcDefIdIn(@Param("procDefIds") List<String> procDefIds);
-    
+
     /**
      * 
      * @param procDefId
      * @return
      */
     List<ProcInstInfoEntity> selectAllByProcDefId(@Param("procDefId") String procDefId);
-    
+
     /**
      * 
      * @param roleNames
      * @return
      */
-    List<ProcInstInfoQueryEntity> selectAllByProcInstInfoByRoleNames(@Param("roleNames")List<String> roleNames);
-    
-    
+    List<ProcInstInfoQueryEntity> selectAllByProcInstInfoByRoleNames(@Param("roleNames") List<String> roleNames);
+
     /**
      * 
      * @param id
      * @return
      */
-    ProcInstInfoQueryEntity selectQueryEntityByPrimaryKey(@Param("id")Integer id);
+    ProcInstInfoQueryEntity selectQueryEntityByPrimaryKey(@Param("id") Integer id);
+
+    /**
+     * 
+     * @return
+     */
+    List<ProcDefInfoOverviewEntity> selectAllProcDefInfoOverviewEntities();
+
+    /**
+     * 
+     * @param procDefId
+     * @param status
+     * @return
+     */
+    int countProcDefInfoOverviewEntities(@Param("procDefId") String procDefId, @Param("status") String status);
 }
