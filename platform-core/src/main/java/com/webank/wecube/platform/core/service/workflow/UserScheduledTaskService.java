33d0e975463519774c1f67f2ab874175fd5fab4c
gavin2lee <gavin2lee@163.com>
2021-09-15 11:44:53 +0800

    #2261 add query dto and method definitions

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/UserScheduledTaskService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/workflow/UserScheduledTaskService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.webank.wecube.platform.core.dto.workflow.UserScheduledTaskDto;
+import com.webank.wecube.platform.core.dto.workflow.UserScheduledTaskQueryDto;
 import com.webank.wecube.platform.core.entity.workflow.UserScheduledTaskEntity;
 
 @Service
@@ -12,6 +14,35 @@ public class UserScheduledTaskService {
     public void execute(){
         
     }
+    
+    public List<UserScheduledTaskDto> fetchUserScheduledTasks(UserScheduledTaskQueryDto queryDto){
+        //TODO
+        return null;
+    }
+    
+    public UserScheduledTaskDto createUserScheduledTask(UserScheduledTaskDto taskDto){
+        //TODO
+        return null;
+    }
+    
+    public List<UserScheduledTaskDto> updateUserSchecduledTasks(List<UserScheduledTaskDto> taskDtos){
+        //TODO
+        return null;
+    }
+    
+    public void deleteUserSchecduledTasks(List<UserScheduledTaskDto> taskDtos){
+        //TODO
+    }
+    
+    public void turnOnUserScheduledTasks(List<UserScheduledTaskDto> taskDtos){
+        
+    }
+    
+    public void stopUserScheduledTasks(List<UserScheduledTaskDto> taskDtos){
+        
+    }
+    
+    
     
     protected List<UserScheduledTaskEntity> scanReadyUserTasks(){
         //TODO
