50359b0f4afcc9c4cc18cc3fad2e78b470dd1d74
pobu168 <244202969@qq.com>
2021-09-23 18:44:37 +0800

    add export for table

--- wecube-portal/src/pages/implementation/timed-execution.vue
+++ wecube-portal/src/pages/implementation/timed-execution.vue
@@ -19,9 +19,11 @@
       </div>
       <div class="item">
         <Button type="primary" @click="getUserScheduledTasks"> {{ $t('query') }}</Button>
+        <Button @click="setTimedExecution"> {{ $t('full_word_add') }}</Button>
+        <Button @click="exportData"> {{ $t('export') }}</Button>
       </div>
     </div>
-    <Table size="small" :columns="tableColumns" :max-height="MODALHEIGHT" :data="tableData"></Table>
+    <Table size="small" ref="table" :columns="tableColumns" :max-height="MODALHEIGHT" :data="tableData"></Table>
     <Modal v-model="showModal" :fullscreen="fullscreen" width="1000" footer-hide>
       <p slot="header">
         <span>{{ $t('details') }}</span>
@@ -40,6 +42,59 @@
       </p>
       <Table :columns="detailTableColums" size="small" :max-height="MODALHEIGHT" :data="detailTableData"></Table>
     </Modal>
+    <Modal v-model="timeConfig.isShow" :title="$t('timed_execution')">
+      <Form :label-width="100" label-colon>
+        <FormItem :label="$t('flow_name')">
+          <Select v-model="timeConfig.params.selectedFlowInstance" style="width:95%">
+            <Option v-for="item in timeConfig.allFlowInstances" :key="item.id" :value="item.id">{{
+              item.procInstName
+            }}</Option>
+          </Select>
+        </FormItem>
+        <FormItem :label="$t('timing_type')">
+          <Select
+            v-model="timeConfig.params.scheduleMode"
+            @on-change="timeConfig.params.time = '00:00:00'"
+            style="width:95%"
+          >
+            <Option v-for="item in timeConfig.scheduleModeOptions" :key="item.value" :value="item.value">{{
+              item.label
+            }}</Option>
+          </Select>
+        </FormItem>
+        <FormItem
+          v-if="['Monthly', 'Weekly'].includes(timeConfig.params.scheduleMode)"
+          :label="timeConfig.params.scheduleMode === 'Monthly' ? $t('day') : $t('week')"
+        >
+          <Select v-model="timeConfig.params.cycle" style="width:96%">
+            <Option
+              v-for="item in timeConfig.modeToValue[timeConfig.params.scheduleMode]"
+              :key="item.value"
+              :value="item.value"
+              >{{ item.label }}</Option
+            >
+          </Select>
+        </FormItem>
+        <FormItem :label="$t('execute_date')">
+          <TimePicker
+            :value="timeConfig.params.time"
+            @on-change="changeTimePicker"
+            style="width: 355px"
+            :disabled-hours="
+              timeConfig.params.scheduleMode === 'Hourly'
+                ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+                : []
+            "
+            :clearable="false"
+            format="HH:mm:ss"
+          ></TimePicker>
+        </FormItem>
+      </Form>
+      <div slot="footer">
+        <Button type="text" @click="timeConfig.isShow = false">{{ $t('bc_cancel') }}</Button>
+        <Button type="primary" @click="saveTime">{{ $t('save') }}</Button>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -49,6 +104,8 @@ import {
   deleteUserScheduledTasks,
   resumeUserScheduledTasks,
   getScheduledTasksByStatus,
+  getProcessInstances,
+  setUserScheduledTasks,
   stopUserScheduledTasks
 } from '@/api/server'
 export default {
@@ -225,7 +282,67 @@ export default {
             )
           }
         }
-      ]
+      ],
+      timeConfig: {
+        isShow: false,
+        params: {
+          selectedFlowInstance: '',
+          scheduleMode: 'Monthly',
+          time: '00:00:00',
+          cycle: ''
+        },
+        scheduleModeOptions: [
+          { label: this.$t('Hourly'), value: 'Hourly' },
+          { label: this.$t('Daily'), value: 'Daily' },
+          { label: this.$t('Weekly'), value: 'Weekly' },
+          { label: this.$t('Monthly'), value: 'Monthly' }
+        ],
+        modeToValue: {
+          Monthly: [
+            { label: '1', value: 1 },
+            { label: '2', value: 2 },
+            { label: '3', value: 3 },
+            { label: '4', value: 4 },
+            { label: '5', value: 5 },
+            { label: '6', value: 6 },
+            { label: '7', value: 7 },
+            { label: '8', value: 8 },
+            { label: '9', value: 9 },
+            { label: '10', value: 10 },
+            { label: '11', value: 11 },
+            { label: '12', value: 12 },
+            { label: '13', value: 13 },
+            { label: '14', value: 14 },
+            { label: '15', value: 15 },
+            { label: '16', value: 16 },
+            { label: '17', value: 17 },
+            { label: '18', value: 18 },
+            { label: '19', value: 19 },
+            { label: '20', value: 20 },
+            { label: '21', value: 21 },
+            { label: '22', value: 22 },
+            { label: '23', value: 23 },
+            { label: '24', value: 24 },
+            { label: '25', value: 25 },
+            { label: '26', value: 26 },
+            { label: '27', value: 27 },
+            { label: '28', value: 28 },
+            { label: '29', value: 29 },
+            { label: '30', value: 30 },
+            { label: '31', value: 31 }
+          ],
+          Weekly: [
+            { label: this.$t('Mon'), value: 1 },
+            { label: this.$t('Tue'), value: 2 },
+            { label: this.$t('Wed'), value: 3 },
+            { label: this.$t('Thu'), value: 4 },
+            { label: this.$t('Fri'), value: 5 },
+            { label: this.$t('Sat'), value: 6 },
+            { label: this.$t('Sun'), value: 7 }
+          ]
+        },
+        allFlowInstances: []
+      }
     }
   },
   mounted () {
@@ -233,6 +350,57 @@ export default {
     this.getUserScheduledTasks()
   },
   methods: {
+    async getProcessInstances () {
+      let { status, data } = await getProcessInstances()
+      if (status === 'OK') {
+        console.log(data)
+        this.timeConfig.allFlowInstances = data.filter(item => item.status === 'Completed')
+      }
+    },
+    changeTimePicker (time) {
+      this.timeConfig.params.time = time
+    },
+    async saveTime () {
+      const found = this.timeConfig.allFlowInstances.find(_ => _.id === this.timeConfig.params.selectedFlowInstance)
+      let scheduleExpr = ''
+      if (['Hourly', 'Daily'].includes(this.timeConfig.params.scheduleMode)) {
+        scheduleExpr = this.timeConfig.params.time
+        if (this.timeConfig.params.scheduleMode === 'Hourly') {
+          scheduleExpr = this.timeConfig.params.time.substring(3)
+        }
+      } else {
+        scheduleExpr = this.timeConfig.params.cycle + ' ' + this.timeConfig.params.time
+      }
+      let params = {
+        scheduleMode: this.timeConfig.params.scheduleMode,
+        scheduleExpr: scheduleExpr,
+        procDefName: found.procInstName,
+        procDefId: found.procDefId,
+        entityDataName: found.entityDisplayName,
+        entityDataId: found.entityDataId
+      }
+      const { status } = await setUserScheduledTasks(params)
+      if (status === 'OK') {
+        this.$Notice.success({
+          title: 'Success',
+          desc: 'Success'
+        })
+        this.timeConfig.isShow = false
+        this.getUserScheduledTasks()
+      }
+    },
+    async setTimedExecution () {
+      this.getProcessInstances()
+      this.timeConfig.params.scheduleMode = 'Monthly'
+      this.timeConfig.params.time = '00:00:00'
+      this.timeConfig.params.cycle = ''
+      this.timeConfig.isShow = true
+    },
+    exportData () {
+      this.$refs.table.exportCsv({
+        filename: 'The original data'
+      })
+    },
     jumpToHistory (row) {
       this.$emit('jumpToHistory', row.procInstId)
     },
