b4805387341e83a3f6d657bc3379628c002ef3fc
gavin2lee <gavin2lee@163.com>
2021-10-28 16:56:29 +0800

    #2272 add order no to attributes

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.1.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.1.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `core_re_task_node_def_info` ADD COLUMN `ass_node_id` VARCHAR(45) NULL;
 
 ALTER TABLE `plugin_package_attributes` ADD COLUMN `created_time` DATETIME NULL;
+ALTER TABLE `plugin_package_attributes` ADD COLUMN `order_no` INT NULL DEFAULT 0;
