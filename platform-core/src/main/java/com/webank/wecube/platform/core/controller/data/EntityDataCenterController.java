375920d030c9ab03a977dfc2fd18d878cbb15418
gavin2lee <gavin2lee@163.com>
2021-11-11 10:48:37 +0800

    #2289 add ssh key

--- platform-core/src/main/java/com/webank/wecube/platform/core/controller/data/EntityDataCenterController.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/controller/data/EntityDataCenterController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webank.wecube.platform.core.dto.data.EntityQuerySpecDto;
