c43c5dd8b56ca53c1fcfd6a8911c7a5a4bf0996f
gavin2lee <gavin2lee@163.com>
2021-11-11 14:51:42 +0800

    #2289 add login mode field to resource server

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.2.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.2.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `smoke2_wecube`.`resource_server` 
-ADD COLUMN `login_mode` VARCHAR(45) NOT NULL DEFAULT 'PASSWD' AFTER `updated_date`,
-ADD COLUMN `ssh_key` TEXT NULL AFTER `login_mode`;
+ALTER TABLE `resource_server` ADD COLUMN `login_mode` VARCHAR(45) NOT NULL DEFAULT 'PASSWD';
+ALTER TABLE `resource_server` ADD COLUMN `ssh_key` TEXT NULL;
