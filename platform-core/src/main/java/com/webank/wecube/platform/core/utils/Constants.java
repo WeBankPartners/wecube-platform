0fe013831b6fd0fa381bab28829d2f39f85751d6
gavin2lee <gavin2lee@163.com>
2021-11-12 10:14:43 +0800

    #2289 refactor aes decryption utils

--- platform-core/src/main/java/com/webank/wecube/platform/core/utils/Constants.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/utils/Constants.java
@@ -87,4 +87,6 @@ public class Constants {
     
     public static final String BIND_FLAG_YES = "Y";
     public static final String BIND_FLAG_NO = "N";
+    
+    public static final String PASSWORD_ENCRYPT_AES_PREFIX = "{AES}";
 }
