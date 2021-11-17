a752f12b7314d264621608d6ad6a73b7c02774a7
gavin2lee <gavin2lee@163.com>
2021-11-17 17:28:07 +0800

    #2289 add plugin edition definition

--- platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.2.sql
+++ platform-core/src/main/resources/db/upgrade/wecube_platform_core_3.2.2.sql
@@ -2,3 +2,5 @@ ALTER TABLE `resource_server` ADD COLUMN `login_mode` VARCHAR(45) NOT NULL DEFAU
 ALTER TABLE `resource_server` CHANGE COLUMN `login_password` `login_password` TEXT CHARACTER SET 'utf8';
 
 ALTER TABLE `plugin_package_runtime_resources_s3` ADD COLUMN `additional_properties` TEXT NULL;
+
+ALTER TABLE `plugin_packages` ADD COLUMN `edition` VARCHAR(45) NOT NULL DEFAULT 'community';
