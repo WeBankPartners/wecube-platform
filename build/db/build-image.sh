25e807620c3bb3e756e80312f936afc32139ff4f
zgyzgyhero <894526647@qq.com>
2021-12-21 14:58:03 +0800

    Update db build image

--- build/db/build-image.sh
+++ build/db/build-image.sh
@@ -25,7 +25,7 @@ echo "SET NAMES utf8;" > ./database/000000_create_database.sql
 echo "create database wecube charset = utf8;" >> ./database/000000_create_database.sql
 
 # setup auth
-cp ../../platform-auth-server/src/main/resources/database/*  database
+#cp ../../platform-auth-server/src/main/resources/database/*  database
 
 TEXT='use auth;'
 
