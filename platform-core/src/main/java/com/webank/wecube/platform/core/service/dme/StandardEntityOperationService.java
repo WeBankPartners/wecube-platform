c5a03a8d8cc95574e9ef59b434ffc375847c773e
gavin2lee <gavin2lee@163.com>
2023-06-15 10:17:19 +0800

    #2378 tuned dme parser

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/StandardEntityOperationService.java
+++ platform-core/src/main/java/com/webank/wecube/platform/core/service/dme/StandardEntityOperationService.java
@@ -166,7 +166,8 @@ public class StandardEntityOperationService {
 
     protected EntityOperationContext buildEntityOperationContext(EntityOperationRootCondition condition,
             RestTemplate restTemplate, Map<Object, Object> externalCacheMap) {
-        List<EntityQueryExprNodeInfo> exprNodeInfos = entityQueryExpressionParser.parse(condition.getEntityLinkExpr());
+    	EntityQueryExpr entityQueryExpr = entityQueryExpressionParser.parse(condition.getEntityLinkExpr());
+        List<EntityQueryExprNodeInfo> exprNodeInfos = entityQueryExpr.getExprNodeInfos();
 
         EntityOperationContext ctx = new EntityOperationContext();
         ctx.setEntityQueryExprNodeInfos(exprNodeInfos);
