6cd31b70208733c32e1f4bb5f53668c02229830d
graychen <junchensz@foxmail.com>
2023-11-18 17:38:59 +0800

    init auth-server of go version

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/model/AuthContext.java
+++
@@ -1,8 +0,0 @@
-package com.webank.wecube.platform.auth.server.model;
-
-public interface AuthContext {
-    String LOCAL_AUTH_SOURCE = "LOCAL";
-    String UM_AUTH_SOURCE = "UM";
-    
-    String PROTOCOL_HTTP = "http";
-}
