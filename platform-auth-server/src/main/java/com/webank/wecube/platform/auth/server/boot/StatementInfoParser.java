d71be3ea57880432fdc9d800e274efe88cb7f396
graychen <junchensz@foxmail.com>
2023-11-18 17:38:59 +0800

    init auth-server of go version

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/boot/StatementInfoParser.java
+++
@@ -1,8 +0,0 @@
-package com.webank.wecube.platform.auth.server.boot;
-
-public class StatementInfoParser {
-
-    public StatementInfo parseStatement(String statement, Long lineNum){
-        return null;
-    }
-}
