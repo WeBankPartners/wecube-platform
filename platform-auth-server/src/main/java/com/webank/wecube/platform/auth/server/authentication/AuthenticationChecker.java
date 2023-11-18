63f398ed6b6daf2f00e2642f8c6b7b314bdd4144
graychen <junchensz@foxmail.com>
2023-11-18 17:38:59 +0800

    init auth-server of go version

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/authentication/AuthenticationChecker.java
+++
@@ -1,8 +0,0 @@
-package com.webank.wecube.platform.auth.server.authentication;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface AuthenticationChecker {
-	void checkAuthentication(UserDetails user, Authentication token);
-}
