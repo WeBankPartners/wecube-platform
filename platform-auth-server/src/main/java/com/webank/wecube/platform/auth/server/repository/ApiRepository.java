04a73725c2a690aaf6f40f011c475e1522b04540
graychen <junchensz@foxmail.com>
2023-11-18 17:38:59 +0800

    init auth-server of go version

--- platform-auth-server/src/main/java/com/webank/wecube/platform/auth/server/repository/ApiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webank.wecube.platform.auth.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.webank.wecube.platform.auth.server.entity.SysApiEntity;
-
-public interface ApiRepository extends JpaRepository<SysApiEntity, String> {
-}
