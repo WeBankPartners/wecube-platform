bbc5907fef18d9ade800775a560e44a0f8a91ab6
zgyzgyhero <894526647@qq.com>
2023-11-28 20:12:05 +0800

    change platform core to go

--- platform-core/src/main/java/com/webank/wecube/platform/core/service/cmder/ssh2/PoolableRemoteCommandExecutor.java
+++
@@ -1,6 +0,0 @@
-package com.webank.wecube.platform.core.service.cmder.ssh2;
-
-public interface PoolableRemoteCommandExecutor extends RemoteCommandExecutor {
-	void init(RemoteCommandExecutorConfig config) throws Exception;
-	void destroy();
-}
