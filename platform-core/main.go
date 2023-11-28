4f65f75c75ef32afc71bbe71e2f53e960e9f166f
zgyzgyhero <894526647@qq.com>
2023-11-28 20:12:05 +0800

    change platform core to go

---
+++ platform-core/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/WeBankPartners/wecube-platform/platform-core/api"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/db"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/log"
+	"github.com/WeBankPartners/wecube-platform/platform-core/models"
+)
+
+func main() {
+	configFile := flag.String("c", "conf/default.json", "config file path")
+	flag.Parse()
+	if initConfigMessage := models.InitConfig(*configFile); initConfigMessage != "" {
+		fmt.Printf("Init config file error,%s \n", initConfigMessage)
+		return
+	}
+	log.InitLogger()
+	if initDbError := db.InitDatabase(); initDbError != nil {
+		return
+	}
+	//start http
+	api.InitHttpServer()
+}
