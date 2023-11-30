888ac56a7076232968dac57c2f96e0130089e4b3
zgyzgyhero <894526647@qq.com>
2023-11-30 18:06:21 +0800

    update platform system variable and resource api

--- platform-core/api/v1/system/variable.go
+++ platform-core/api/v1/system/variable.go
@@ -4,6 +4,7 @@ import (
 	"github.com/WeBankPartners/wecube-platform/platform-core/api/middleware"
 	"github.com/WeBankPartners/wecube-platform/platform-core/common/exterror"
 	"github.com/WeBankPartners/wecube-platform/platform-core/models"
+	"github.com/WeBankPartners/wecube-platform/platform-core/services/database"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +14,12 @@ func QuerySystemVariables(c *gin.Context) {
 		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
 		return
 	}
-
+	result, err := database.QuerySystemVariables(c, &param)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, result)
+	}
 }
 
 func GetSystemVariableScope(c *gin.Context) {
@@ -21,13 +27,43 @@ func GetSystemVariableScope(c *gin.Context) {
 }
 
 func CreateSystemVariable(c *gin.Context) {
-
+	var params []*models.SystemVariables
+	if err := c.ShouldBindJSON(&params); err != nil {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
+		return
+	}
+	err := database.CreateSystemVariables(c, params)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, params)
+	}
 }
 
 func UpdateSystemVariable(c *gin.Context) {
-
+	var params []*models.SystemVariables
+	if err := c.ShouldBindJSON(&params); err != nil {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
+		return
+	}
+	err := database.UpdateSystemVariables(c, params)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, params)
+	}
 }
 
 func DeleteSystemVariable(c *gin.Context) {
-
+	var params []*models.SystemVariables
+	if err := c.ShouldBindJSON(&params); err != nil {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
+		return
+	}
+	err := database.DeleteSystemVariables(c, params)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnSuccess(c)
+	}
 }
