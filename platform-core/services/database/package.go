d0a2c18c163268a118b8c7cfc2a0291fbb434b92
zgyzgyhero <894526647@qq.com>
2023-12-11 18:13:59 +0800

    update core plugin packages upload

--- platform-core/services/database/package.go
+++ platform-core/services/database/package.go
@@ -1,12 +1,14 @@
 package database
 
 import (
+	"context"
+	"fmt"
 	"github.com/WeBankPartners/wecube-platform/platform-core/common/db"
 	"github.com/WeBankPartners/wecube-platform/platform-core/common/exterror"
 	"github.com/WeBankPartners/wecube-platform/platform-core/models"
 )
 
-func GetPackages(allFlag bool) (result []*models.PluginPackages, err error) {
+func GetPackages(ctx context.Context, allFlag bool) (result []*models.PluginPackages, err error) {
 	result = []*models.PluginPackages{}
 	if allFlag {
 		err = db.MysqlEngine.SQL("select * from plugin_packages").Find(&result)
@@ -20,6 +22,92 @@ func GetPackages(allFlag bool) (result []*models.PluginPackages, err error) {
 	for _, v := range result {
 		v.StatusString = models.PluginPackagesStatusMap[v.Status]
 		v.EditionString = models.PluginPackagesEditionMap[v.Edition]
+	}
+	return
+}
+
+func GetPluginDependencies(ctx context.Context, pluginPackageId string) (result []*models.PluginPackageDepObj, err error) {
+	var dependRows []*models.PluginPackageDependencies
+	err = db.MysqlEngine.SQL("select dependency_package_name,dependency_package_version from plugin_package_dependencies where plugin_package_id=?", pluginPackageId).Find(&dependRows)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	result = []*models.PluginPackageDepObj{}
+	for _, v := range dependRows {
+		result = append(result, &models.PluginPackageDepObj{PackageName: v.DependencyPackageName, Version: v.DependencyPackageVersion})
+	}
+	return
+}
+
+func GetPluginMenus(ctx context.Context, pluginPackageId string) (result []*models.PluginPackageMenus, err error) {
+	result = []*models.PluginPackageMenus{}
+	err = db.MysqlEngine.SQL("select id,plugin_package_id,code,display_name,local_display_name,menu_order from plugin_package_menus where plugin_package_id=?", pluginPackageId).Find(&result)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	for _, v := range result {
+		v.Source = v.PluginPackageId
+	}
+	return
+}
+
+func GetPluginSystemParameters(ctx context.Context, pluginPackageId string) (result []*models.SystemVariables, err error) {
+	pluginPackageObj, tmpErr := getPluginPackageObj(ctx, pluginPackageId)
+	if tmpErr != nil {
+		err = tmpErr
+		return
+	}
+	packageName := fmt.Sprintf("%s__%s", pluginPackageObj.Name, pluginPackageObj.Version)
+	result = []*models.SystemVariables{}
+	err = db.MysqlEngine.SQL("select * from system_variables where source=?", packageName).Find(&result)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+	}
+	return
+}
+
+func getPluginPackageObj(ctx context.Context, pluginPackageId string) (pluginPackageObj *models.PluginPackages, err error) {
+	var pluginPackageRows []*models.PluginPackages
+	err = db.MysqlEngine.SQL("select id,name,`version`,status,ui_package_included from plugin_packages where id=?", pluginPackageId).Find(&pluginPackageRows)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+	} else {
+		if len(pluginPackageRows) == 0 {
+			err = fmt.Errorf("can not find pluginPackages with id:%s ", pluginPackageId)
+		} else {
+			pluginPackageObj = pluginPackageRows[0]
+		}
+	}
+	return
+}
+
+func GetPluginAuthorities(ctx context.Context, pluginPackageId string) (result []*models.PluginPackageAuthorities, err error) {
+	result = []*models.PluginPackageAuthorities{}
+	err = db.MysqlEngine.SQL("select * from plugin_package_authorities where plugin_package_id=?", pluginPackageId).Find(&pluginPackageId)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+	}
+	return
+}
+
+func GetPluginRuntimeResources(ctx context.Context, pluginPackageId string) (result *models.PluginRuntimeResourceData, err error) {
+	result = &models.PluginRuntimeResourceData{Docker: []*models.PluginPackageRuntimeResourcesDocker{}, Mysql: []*models.PluginPackageRuntimeResourcesMysql{}, S3: []*models.PluginPackageRuntimeResourcesS3{}}
+	err = db.MysqlEngine.SQL("select * from plugin_package_runtime_resources_docker where plugin_package_id=?", pluginPackageId).Find(&result.Docker)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	err = db.MysqlEngine.SQL("select * from plugin_package_runtime_resources_mysql where plugin_package_id=?", pluginPackageId).Find(&result.Mysql)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	err = db.MysqlEngine.SQL("select * from plugin_package_runtime_resources_s3 where plugin_package_id=?", pluginPackageId).Find(&result.S3)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
 	}
 	return
 }
