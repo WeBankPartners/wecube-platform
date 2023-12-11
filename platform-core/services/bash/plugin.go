12931480b509413024303ba30f2c2a2ef294028f
zgyzgyhero <894526647@qq.com>
2023-12-11 18:13:59 +0800

    update core plugin packages upload

--- platform-core/services/bash/plugin.go
+++ platform-core/services/bash/plugin.go
@@ -1,5 +1,28 @@
 package bash
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
 func GetHostSerialNum(hostIp string) {
+
+}
+
+func SaveTmpFile(fileName string, fileContent []byte) (filePath, fileDir string, err error) {
+	tmpDir := fmt.Sprintf("/tmp/%d", time.Now().UnixNano())
+	if err = os.MkdirAll(tmpDir, 0644); err != nil {
+		err = fmt.Errorf("make tmp dir fail,%s ", err.Error())
+		return
+	}
+	filePath = fmt.Sprintf("%s/%s", tmpDir, fileName)
+	if err = os.WriteFile(filePath, fileContent, 0644); err != nil {
+		err = fmt.Errorf("write tmp file fail,%s ", err.Error())
+	}
+	return
+}
+
+func DecompressFile(filePath string) {
 
 }
