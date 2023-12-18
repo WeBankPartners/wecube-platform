693490729e6f0ab336095f65776491091919318f
zgyzgyhero <894526647@qq.com>
2023-12-18 20:45:41 +0800

    update core plugin packages upload

--- platform-core/services/bash/plugin.go
+++ platform-core/services/bash/plugin.go
@@ -3,6 +3,8 @@ package bash
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -11,18 +13,70 @@ func GetHostSerialNum(hostIp string) {
 }
 
 func SaveTmpFile(fileName string, fileContent []byte) (filePath, fileDir string, err error) {
-	tmpDir := fmt.Sprintf("/tmp/%d", time.Now().UnixNano())
-	if err = os.MkdirAll(tmpDir, 0644); err != nil {
+	fileDir = fmt.Sprintf("/tmp/%d", time.Now().UnixNano())
+	if err = os.MkdirAll(fileDir, 0644); err != nil {
 		err = fmt.Errorf("make tmp dir fail,%s ", err.Error())
 		return
 	}
-	filePath = fmt.Sprintf("%s/%s", tmpDir, fileName)
+	filePath = fmt.Sprintf("%s/%s", fileDir, fileName)
 	if err = os.WriteFile(filePath, fileContent, 0644); err != nil {
 		err = fmt.Errorf("write tmp file fail,%s ", err.Error())
 	}
 	return
 }
 
-func DecompressFile(filePath string) {
+func DecompressFile(filePath, targetDir string) error {
+	var fileDir, fileName, fileType, execCommand string
+	if dirIndex := strings.LastIndex(filePath, "/"); dirIndex >= 0 {
+		fileDir = filePath[:dirIndex]
+		fileName = filePath[dirIndex+1:]
+	} else {
+		fileName = filePath
+	}
+	tmpFp := strings.ToLower(fileName)
+	if lastPointIndex := strings.LastIndex(tmpFp, "."); lastPointIndex > 0 {
+		fileType = tmpFp[lastPointIndex+1:]
+	}
+	if fileType == "" {
+		return fmt.Errorf("fileName: %s type illegal", fileName)
+	}
+	if fileDir != "" {
+		execCommand = "cd " + fileDir + " && "
+	}
+	if fileType == "zip" {
+		execCommand += "unzip " + fileName
+		if targetDir != "" {
+			execCommand += " -d " + targetDir
+		}
+	}
+	_, execErr := exec.Command("/bin/sh", "-c", execCommand).Output()
+	if execErr != nil {
+		return fmt.Errorf("exec decompress file fail,command:%s,error:%s ", execCommand, execErr.Error())
+	}
+	return nil
+}
 
+func ListDirFiles(dirPath string) (result []string, err error) {
+	dirEntry, readErr := os.ReadDir(dirPath)
+	if readErr != nil {
+		err = fmt.Errorf("read dir %s fail,%s ", dirPath, readErr.Error())
+		return
+	}
+	for _, v := range dirEntry {
+		if !v.IsDir() {
+			result = append(result, v.Name())
+		}
+	}
+	return
+}
+
+func ListContains(inputList []string, target string) bool {
+	existFlag := false
+	for _, v := range inputList {
+		if v == target {
+			existFlag = true
+			break
+		}
+	}
+	return existFlag
 }
