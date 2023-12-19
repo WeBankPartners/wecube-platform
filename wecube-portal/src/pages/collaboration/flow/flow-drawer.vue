634c2dc34078c39d4f5d865c78d3189b9a4b6bab
pobu168 <244202969@qq.com>
2023-12-19 09:45:12 +0800

    add: drawer for flow

---
+++ wecube-portal/src/pages/collaboration/flow/flow-drawer.vue
@@ -0,0 +1,45 @@
+<template>
+  <Drawer title="配置" :closable="false" v-model="isDrawerOpen" :mask-closable="false" :width="500">
+    <Form :label-width="80">
+      <template>
+        <FormItem label="id">
+          <Input v-model="flowInfo.id"></Input>
+        </FormItem>
+        <FormItem label="label">
+          <Input v-model="flowInfo.name"></Input>
+        </FormItem>
+      </template>
+    </Form>
+  </Drawer>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isDrawerOpen: false,
+      currentType: '', // flow、node、edge
+      flowInfo: {
+        id: '',
+        name: ''
+      },
+      nodeInfo: {}
+    }
+  },
+  methods: {
+    openDrawer (type, data) {
+      if (type === 'flow') {
+        this.flowInfo = data
+      }
+      this.isDrawerOpen = true
+    },
+    closeDrawer () {
+      this.isDrawerOpen = false
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.ivu-form-item {
+  margin-bottom: 0;
+}
+</style>
