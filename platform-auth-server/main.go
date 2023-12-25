f043e9bd212d922b05481fcd2b9888819ef546be
gray chen <junchensz@foxmail.com>
2023-12-25 21:29:54 +0800

    support auth in go

--- platform-auth-server/main.go
+++ platform-auth-server/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	sw "github.com/WeBankPartners/wecube-platform/platform-auth-server/api"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/api/middleware"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/exterror"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/log"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/model"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/service/jwt"
@@ -21,15 +23,18 @@ func main() {
 	/*	middleware.InitAuth()
 		middleware.InitRedirect()
 	*/
-	/*	err := exterror.InitErrorTemplateList(config.Config.ErrorTemplateDir, config.Config.ErrorDetailReturn)
-		if err != nil {
-			log.Logger.Error("Init error template list fail", log.Error(err))
-			return
-		}
-	*/
+	err := exterror.InitErrorTemplateList(model.Config.ErrorTemplateDir, model.Config.ErrorDetailReturn)
+	if err != nil {
+		log.Logger.Error("Init error template list fail", log.Error(err))
+		return
+	}
 	log.Logger.Info("Server started")
 	if err := jwt.InitKey(); err != nil {
 		fmt.Printf("failed to init jwt key")
+		return
+	}
+	if err := middleware.InitAuth(); err != nil {
+		fmt.Printf("failed to init middleware auth")
 		return
 	}
 
