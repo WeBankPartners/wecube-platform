240ad14e6379b48b997df595bfe238f96157eaf4
gray chen <junchensz@foxmail.com>
2023-12-25 21:29:54 +0800

    support auth in go

---
+++ platform-auth-server/api/middleware/token.go
@@ -0,0 +1,93 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/api/support"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/constant"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/log"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/model"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const (
+	JwtTokenPrefix = "Bearer "
+)
+
+func InitAuth() error {
+	var err error
+	if model.Config.Auth.JwtPublicKeyPath != "" {
+		var jwtPublicBytes []byte
+		jwtPublicBytes, err = ioutil.ReadFile(model.Config.Auth.JwtPublicKeyPath)
+		if err != nil {
+			log.Logger.Error("Read jwt public key fail", log.String("path", model.Config.Auth.JwtPublicKeyPath), log.Error(err))
+			return err
+		}
+
+		model.Config.Auth.JwtPublicKeyBytes = jwtPublicBytes
+	}
+	return nil
+}
+
+func AuthApi(c *gin.Context) {
+	if !strings.HasPrefix(c.Request.URL.Path, constant.UrlPrefix) {
+		c.String(http.StatusNotFound, "404 page not found")
+		c.Abort()
+		return
+	}
+	//apiUri := c.Request.URL.Path[len(constant.UrlPrefix):]
+
+	authClaim, err := GetTokenData(c.GetHeader(constant.AuthorizationHeader), model.Config.Auth.JwtPublicKeyBytes)
+	if err != nil {
+		log.Logger.Warn("failed to validate jwt token", log.Error(err))
+		//support.ReturnErrorWithHttpCode(c, exterror.Catch(exterror.New().AuthManagerTokenError, err), http.StatusUnauthorized)
+		support.ReturnErrorWithHttpCode(c, errors.New("failed to validate jwt token"), http.StatusUnauthorized)
+
+		c.Abort()
+		return
+	}
+	//c.Set("needAuth", true)
+	c.Set(constant.Operator, fmt.Sprintf("%s", authClaim.Subject))
+	//c.Set("auth", authClaim.Auth)
+	c.Next()
+}
+
+func GetTokenData(tokenString string, jwtPublicKeyBytes []byte) (authClaim *model.AuthClaims, err error) {
+	if strings.HasPrefix(tokenString, JwtTokenPrefix) {
+		tokenString = tokenString[7:]
+	}
+	// parse rsa public key
+	parsedKey, parsePublicKeyErr := jwt.ParseRSAPublicKeyFromPEM(jwtPublicKeyBytes)
+	if parsePublicKeyErr != nil {
+		err = fmt.Errorf("parse jwt public key fail,%s ", parsePublicKeyErr.Error())
+		return
+	}
+	// parse Claim
+	jwtToken, parseClaimErr := jwt.ParseWithClaims(tokenString, &model.AuthClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return parsedKey, nil
+	})
+	if parseClaimErr != nil {
+		err = fmt.Errorf("parse jwt claim fail,%s ", parseClaimErr.Error())
+		return
+	}
+	claim, ok := jwtToken.Claims.(*model.AuthClaims)
+	if !ok || !jwtToken.Valid {
+		err = fmt.Errorf("jwt token invalid ")
+		return
+	}
+	authClaim = claim
+	return
+}
+
+func GetRequestUser(c *gin.Context) string {
+	return c.GetString(constant.Operator)
+}
+
+//TODO
+func GetAuthenticatedUser(c *gin.Context) *model.AuthenticatedUser {
+	return nil
+}
