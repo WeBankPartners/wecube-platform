c0aa158f5b5b24a5edd51a99d8b725940a56cd2c
gray chen <junchensz@foxmail.com>
2023-12-27 22:30:12 +0800

    support auth server in go

---
+++ platform-auth-server/service/local_user_service.go
@@ -0,0 +1,126 @@
+package service
+
+import (
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/log"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/model"
+	"github.com/WeBankPartners/wecube-platform/platform-auth-server/service/db"
+)
+
+const (
+	AuthorityTypeRole       = "ROLE"
+	AuthorityTypePermission = "AUTHORITY"
+)
+
+type LocalUserService struct {
+}
+
+var LocalUserServiceInstance LocalUserService
+
+func (LocalUserService) loadUserByUsername(username string) (*model.SysUser, error) {
+	userEntity, err := db.UserRepositoryInstance.FindNotDeletedUserByUsername(username)
+	if err != nil {
+		log.Logger.Warn("failed to FindNotDeletedUserByUsername", log.String("username", username), log.Error(err))
+		return nil, err
+	}
+
+	if userEntity == nil {
+		return nil, nil
+	}
+
+	if !userEntity.IsActive || userEntity.IsDeleted {
+		return nil, nil
+	}
+
+	if userEntity.IsBlocked {
+		return nil, nil
+	}
+
+	user := &model.SysUser{
+		Username:    userEntity.Username,
+		Password:    userEntity.Password,
+		AuthSource:  userEntity.AuthSource,
+		AuthContext: userEntity.AuthContext,
+	}
+
+	userRoles, err := db.UserRoleRsRepositoryInstance.FindAllByUserId(userEntity.Id)
+	if err != nil {
+		log.Logger.Warn("failed to FindAllByUserId for userRoleRs", log.String("userId", userEntity.Id), log.Error(err))
+		return nil, err
+	}
+	if userRoles == nil {
+		return user, nil
+	}
+
+	for _, userRole := range userRoles {
+		if !userRole.Active || userRole.Deleted {
+			continue
+		}
+
+		var role *model.SysRoleEntity
+		_, err := db.Engine.ID(userRole.RoleId).Get(&role)
+		if err != nil {
+			log.Logger.Warn("failed to get role", log.String("roleId", userRole.RoleId), log.Error(err))
+			return nil, err
+		}
+
+		if role == nil {
+			continue
+		}
+
+		if !role.Active || role.Deleted {
+			continue
+		}
+
+		roleObject := &model.CompositeAuthority{
+			Authority:     role.Name,
+			AuthorityType: AuthorityTypeRole,
+		}
+		user.CompositeAuthorities = append(user.CompositeAuthorities, roleObject)
+
+		if err := appendAuthorities(user, role); err != nil {
+			return nil, err
+		}
+
+	}
+	return user, nil
+}
+
+func appendAuthorities(user *model.SysUser, role *model.SysRoleEntity) error {
+	roleAuthorities, err := db.RoleAuthorityRsRepositoryInstance.FindAllConfiguredAuthoritiesByRoleId(role.Id)
+	if err != nil {
+		log.Logger.Warn("failed to FindAllConfiguredAuthoritiesByRoleId", log.String("roleId", role.Id), log.Error(err))
+		return err
+	}
+	if len(roleAuthorities) == 0 {
+		return nil
+	}
+
+	for _, roleAuthority := range roleAuthorities {
+		if !roleAuthority.Active || roleAuthority.Deleted {
+			continue
+		}
+
+		var authority *model.SysAuthorityEntity
+		_, err := db.Engine.ID(roleAuthority.AuthorityID).Get(&authority)
+		if err != nil {
+			log.Logger.Warn("failed to get authority", log.String("authorityId", roleAuthority.AuthorityID), log.Error(err))
+			return err
+		}
+
+		if authority == nil {
+			continue
+		}
+
+		if !authority.Active || authority.Deleted {
+			continue
+		}
+
+		authorityObject := &model.CompositeAuthority{
+			Authority:     authority.Code,
+			AuthorityType: AuthorityTypePermission,
+		}
+		user.CompositeAuthorities = append(user.CompositeAuthorities, authorityObject)
+
+	}
+	return nil
+}
