b63ce7ef6602014cd249c26350ddd55a9e6a9dfc
gray chen <junchensz@foxmail.com>
2023-12-31 12:22:44 +0800

    support auth-server with go

---
+++ platform-auth-server/deploy/db/drop/01.auth.mysql.drop.sql
@@ -0,0 +1,12 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `auth_sys_api`;
+DROP TABLE IF EXISTS `auth_sys_authority`;
+DROP TABLE IF EXISTS `auth_sys_role`;
+DROP TABLE IF EXISTS `auth_sys_role_authority`;
+DROP TABLE IF EXISTS `auth_sys_sub_system`;
+DROP TABLE IF EXISTS `auth_sys_sub_system_authority`;
+DROP TABLE IF EXISTS `auth_sys_user`;
+DROP TABLE IF EXISTS `auth_sys_user_role`;
+
+SET FOREIGN_KEY_CHECKS=1;
