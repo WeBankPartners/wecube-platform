beee1888584e7051db9a37b7e9aeabfbd2d6f38c
zgyzgyhero <894526647@qq.com>
2024-01-02 11:38:15 +0800

    update platform plugin launch

---
+++ platform-core/services/remote/auth-server.go
@@ -0,0 +1,9 @@
+package remote
+
+import "github.com/WeBankPartners/wecube-platform/platform-core/models"
+
+// TODO
+func RegisterSubSystem(pluginPackageObj *models.PluginPackages) (subSystemCode, subSystemKey string, err error) {
+
+	return
+}
