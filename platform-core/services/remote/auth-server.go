1b8fc8c689fc67d15d8ccb2bdff3041ec4e3400e
tangjiawei <tangjiawei@nnyy.com>
2024-01-04 21:15:37 +0800

    权限接口开发

--- platform-core/services/remote/auth-server.go
+++ platform-core/services/remote/auth-server.go
@@ -31,6 +31,14 @@ const (
 	pathDeleteUserAccountByUserId = "/auth/v1/users/%s"
 	// pathUpdateLocalRole 更新角色
 	pathUpdateLocalRole = "/auth/v1/roles/update"
+	// pathConfigureRoleForUsers 角色增加用户
+	pathConfigureRoleForUsers = "/auth/v1/roles/%s/users"
+	// pathRevokeRoleFromUsers 角色移除用户
+	pathRevokeRoleFromUsers = "/auth/v1/roles/%s/users/revoke"
+	// pathRevokeAuthoritiesFromRole 取消角色授权
+	pathRevokeAuthoritiesFromRole = "/auth/v1/roles/%s/authorities/revoke"
+	// pathConfigureRoleAuthorities 配置角色权限
+	pathConfigureRoleAuthorities = "/auth/v1/roles/%s/authorities"
 )
 
 // TODO
@@ -166,9 +174,8 @@ func RetrieveUserByUserId(userId, userToken string) (response models.QuerySingle
 }
 
 // UnregisterLocalUser 删除用户
-func UnregisterLocalUser(userId, userToken string) (err error) {
-	err = network.HttpDeleteCommon(fmt.Sprintf(httpAuthServer+pathDeleteUserAccountByUserId, userId), userToken)
-	return
+func UnregisterLocalUser(userId, userToken string) error {
+	return network.HttpDeleteCommon(fmt.Sprintf(httpAuthServer+pathDeleteUserAccountByUserId, userId), userToken)
 }
 
 // UpdateLocalRole 更新角色
@@ -181,4 +188,38 @@ func UpdateLocalRole(userToken string, param models.SimpleLocalRoleDto) (respons
 		return
 	}
 	return
+}
+
+// ConfigureRoleForUsers 角色添加用户列表
+func ConfigureRoleForUsers(userId, userToken string, userIdList []string) (err error) {
+	var postParams []*models.SimpleLocalUserDto
+	for _, userId := range userIdList {
+		postParams = append(postParams, &models.SimpleLocalUserDto{ID: userId})
+	}
+	postBytes, _ := json.Marshal(postParams)
+	err = network.HttpPostCommon(fmt.Sprintf(httpAuthServer+pathConfigureRoleForUsers, userId), userToken, postBytes)
+	return
+}
+
+// RevokeRoleFromUsers 角色移除用户
+func RevokeRoleFromUsers(roleId, userToken string, userIdList []string) (err error) {
+	var postParams []*models.SimpleLocalUserDto
+	for _, userId := range userIdList {
+		postParams = append(postParams, &models.SimpleLocalUserDto{ID: userId})
+	}
+	postBytes, _ := json.Marshal(postParams)
+	err = network.HttpPostCommon(fmt.Sprintf(httpAuthServer+pathRevokeRoleFromUsers, roleId), userToken, postBytes)
+	return
+}
+
+// RevokeRoleAuthoritiesById 取消角色授权
+func RevokeRoleAuthoritiesById(roleId, userToken string, authoritiesToRevoke []*models.SimpleAuthorityDto) error {
+	postBytes, _ := json.Marshal(authoritiesToRevoke)
+	return network.HttpPostCommon(fmt.Sprintf(httpAuthServer+pathRevokeAuthoritiesFromRole, roleId), userToken, postBytes)
+}
+
+// ConfigureRoleWithAuthoritiesById 配置角色权限
+func ConfigureRoleWithAuthoritiesById(roleId, userToken string, authoritiesToGrantList []*models.SimpleAuthorityDto) error {
+	postBytes, _ := json.Marshal(authoritiesToGrantList)
+	return network.HttpPostCommon(fmt.Sprintf(httpAuthServer+pathConfigureRoleAuthorities, roleId), userToken, postBytes)
 }
