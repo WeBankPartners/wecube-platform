8586688e211d5a5a40b43eb6ed55c6700ac230fc
gray chen <junchensz@foxmail.com>
2024-01-06 11:53:30 +0800

    implement in golang

---
+++ build/platform-gateway/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i "s~\[#LOG_LEVEL\]~$LOG_LEVEL~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#WECUBE_GATEWAY_LOG_PATH\]~$WECUBE_GATEWAY_LOG_PATH~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#GATEWAY_ROUTE_CONFIG_SERVER\]~$GATEWAY_ROUTE_CONFIG_SERVER~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#GATEWAY_ROUTE_CONFIG_URI\]~$GATEWAY_ROUTE_CONFIG_URI~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#WECUBE_CORE_HOST\]~$WECUBE_CORE_HOST~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#AUTH_SERVER_HOST\]~$AUTH_SERVER_HOST~g" /app/platform-auth-server/config/default.json
+
+exec ./platform-auth-server
--- platform-gateway/build/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i "s~\[#server_port\]~$server_port~g" /app/platform-auth-server/config/default.json
-sed -i "s~\[#log_level\]~$log_level~g" /app/platform-auth-server/config/default.json
-sed -i "s~\[#route_config_address\]~$log_level~g" /app/platform-auth-server/config/default.json
-
-exec ./platform-auth-server
