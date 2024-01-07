859d2678b9a51d1024345a6a37210ac6ef205aef
gray chen <junchensz@foxmail.com>
2024-01-07 17:38:16 +0800

    build auth-server

--- build/platform-gateway/start.sh
+++ build/platform-gateway/start.sh
@@ -6,5 +6,6 @@ sed -i "s~\[#GATEWAY_ROUTE_CONFIG_SERVER\]~$GATEWAY_ROUTE_CONFIG_SERVER~g" /app/
 sed -i "s~\[#GATEWAY_ROUTE_CONFIG_URI\]~$GATEWAY_ROUTE_CONFIG_URI~g" /app/platform-auth-server/config/default.json
 sed -i "s~\[#WECUBE_CORE_HOST\]~$WECUBE_CORE_HOST~g" /app/platform-auth-server/config/default.json
 sed -i "s~\[#AUTH_SERVER_HOST\]~$AUTH_SERVER_HOST~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#AUTH_SERVER_LOG_PATH\]~$AUTH_SERVER_LOG_PATH~g" /app/platform-auth-server/config/default.json
 
 exec ./platform-auth-server
