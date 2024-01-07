29e3cc593216d7d214eeaa250e73e1b44e4566de
zgyzgyhero <894526647@qq.com>
2024-01-07 22:42:32 +0800

    update platform core rsa and aes

--- platform-core/common/encrypt/aes.go
+++ platform-core/common/encrypt/aes.go
@@ -10,58 +10,67 @@ import (
 )
 
 func generateKeyFromSeedAndSalt(seed, additionalSalt string) []byte {
-	hash := md5.Sum([]byte(seed + additionalSalt))
-	key := fmt.Sprintf("%16s", hex.EncodeToString(hash[:])[0:15])
+	// 创建一个MD5哈希对象
+	hash := md5.New()
+
+	// 计算MD5摘要
+	hash.Write([]byte(seed + additionalSalt))
+	digest := hash.Sum(nil)
+
+	// 将摘要转换为十六进制字符串表示
+	md5Hex := hex.EncodeToString(digest)
+	key := fmt.Sprintf("%16s", md5Hex[0:15])
 	return []byte(key)
 }
 
-func EncryptWithAesECB(password, seed, additionalSalt string) (string, error) {
+func EncryptWithAesECB(password, seed, additionalSalt string) string {
 	key := generateKeyFromSeedAndSalt(seed, additionalSalt)
 	return encrypt([]byte(password), key)
 }
 
-func encrypt(plaintext []byte, key []byte) (string, error) {
+// pkcs5Padding pkcs5Padding填充密码
+func pkcs5Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - (len(src) % blockSize)
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padText...)
+}
+
+func encrypt(plaintext []byte, key []byte) string {
 	block, err := aes.NewCipher(key) // 创建新的cipher对象
 	if err != nil {
-		return "", fmt.Errorf("Error creating cipher block ")
+		panic("Error creating cipher block")
 	}
-	// 补齐明文长度为16字节（AES分组大小）的倍数
-	paddingLen := len(plaintext) % aes.BlockSize
-	if paddingLen > 0 {
-		padText := bytes.Repeat([]byte{byte(len(plaintext))}, aes.BlockSize-paddingLen)
-		plaintext = append(plaintext, padText...)
-	}
-
+	plaintext = pkcs5Padding(plaintext, aes.BlockSize)
 	encryptedData := make([]byte, len(plaintext))
 	for i := 0; i < len(plaintext); i += aes.BlockSize {
 		block.Encrypt(encryptedData[i:], plaintext[i:]) // 进行加密操作
 	}
-
-	return base64.StdEncoding.EncodeToString(encryptedData), nil // 返回Base64编码后的结果
+	return base64.StdEncoding.EncodeToString(encryptedData) // 返回Base64编码后的结果
 }
 
-func DecryptWithAesECB(password, seed, additionalSalt string) (string, error) {
+func DecryptWithAesECB(password, seed, additionalSalt string) string {
 	key := generateKeyFromSeedAndSalt(seed, additionalSalt)
-	return decrypt(password, key)
+	return string(decrypt(password, key))
 }
 
-func decrypt(ciphertext string, key []byte) (string, error) {
+func decrypt(ciphertext string, key []byte) []byte {
 	decodedData, _ := base64.StdEncoding.DecodeString(ciphertext) // Base64解码
 
 	block, err := aes.NewCipher(key) // 创建新的cipher对象
 	if err != nil {
-		return "", fmt.Errorf("Error creating cipher block ")
+		panic("Error creating cipher block")
 	}
 
 	decryptedData := make([]byte, len(decodedData))
 	for i := 0; i < len(decodedData); i += aes.BlockSize {
 		block.Decrypt(decryptedData[i:], decodedData[i:]) // 进行解密操作
 	}
-	unpaddedData := unPad(decryptedData) // 去除填充内容
-	return string(unpaddedData), nil
+	unPaddedData := pkcs5UnPadding(decryptedData) // 去除填充内容
+	return unPaddedData
 }
 
-func unPad(data []byte) []byte {
-	length := int(data[len(data)-1])
-	return data[:len(data)-length]
+func pkcs5UnPadding(src []byte) []byte {
+	length := len(src)
+	unPaddedData := int(src[length-1])
+	return src[:(length - unPaddedData)]
 }
