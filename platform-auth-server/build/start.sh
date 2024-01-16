9d30d5b55f26b6ffda09895aef53db519aea993a
gray chen <junchensz@foxmail.com>
2024-01-16 22:08:34 +0800

    build jwt token with single signing key

--- platform-auth-server/build/start.sh
+++ platform-auth-server/build/start.sh
@@ -9,8 +9,7 @@ sed -i "s~\[#MYSQL_USER_NAME\]~$MYSQL_USER_NAME~g" /app/platform-auth-server/con
 sed -i "s~\[#MYSQL_USER_PASSWORD\]~$MYSQL_USER_PASSWORD~g" /app/platform-auth-server/config/default.json
 sed -i "s~\[#MYSQL_SERVER_DATABASE_NAME\]~$MYSQL_SERVER_DATABASE_NAME~g" /app/platform-auth-server/config/default.json
 
-sed -i "s~\[#JWT_PRI_KEY_PATH\]~$JWT_PRI_KEY_PATH~g" /app/platform-auth-server/config/default.json
-sed -i "s~\[#JWT_PUB_KEY_PATH\]~$JWT_PUB_KEY_PATH~g" /app/platform-auth-server/config/default.json
+sed -i "s~\[#SIGNING_KEY\]~$SIGNING_KEY~g" /app/platform-auth-server/config/default.json
 sed -i "s~\[#USER_ACCESS_TOKEN\]~$USER_ACCESS_TOKEN~g" /app/platform-auth-server/config/default.json
 sed -i "s~\[#USER_REFRESH_TOKEN\]~$USER_REFRESH_TOKEN~g" /app/platform-auth-server/config/default.json
 
