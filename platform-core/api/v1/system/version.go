5de2b0201316c825a64e94f253fc15bf82ac133c
zgyzgyhero <894526647@qq.com>
2024-01-16 10:35:00 +0800

    update platform core api auth

--- platform-core/api/v1/system/version.go
+++ platform-core/api/v1/system/version.go
@@ -2,10 +2,12 @@ package system
 
 import (
 	"github.com/WeBankPartners/wecube-platform/platform-core/api/middleware"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/log"
 	"github.com/WeBankPartners/wecube-platform/platform-core/models"
 	"github.com/gin-gonic/gin"
 )
 
 func AppVersion(c *gin.Context) {
+	log.Logger.Debug("UserRequest ver api", log.String("user", middleware.GetRequestUser(c)), log.StringList("roles", middleware.GetRequestRoles(c)))
 	middleware.ReturnData(c, models.Config.Version)
 }
