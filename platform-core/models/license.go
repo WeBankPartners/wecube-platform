371ac4fbea9153ea1aa1e0d0e2c27aec74fdc9b5
Roy <576477085@qq.com>
2024-01-18 17:12:34 +0800

    add platform core certification api

---
+++ platform-core/models/license.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+)
+
+type WeLicense struct {
+	Plugin      string `json:"plugin"`      // 插件名称
+	PK          string `json:"pk" `         // 公钥
+	Data        string `json:"data"`        // 加密数据
+	Signature   string `json:"signature"`   // 数据签名
+	Description string `json:"description"` // License说明
+}
+
+type PluginCertification struct {
+	Id          string    `json:"id" xorm:"id"`                    // 唯一标识
+	Plugin      string    `json:"plugin" xorm:"plugin"`            // 插件名称
+	Lpk         string    `json:"lpk" xorm:"lpk"`                  // 公钥
+	EncryptData string    `json:"encryptData" xorm:"encrypt_data"` // 加密数据
+	Signature   string    `json:"signature" xorm:"signature"`      // 数据签名
+	Description string    `json:"description" xorm:"description"`  // License说明
+	CreatedTime time.Time `json:"createdTime" xorm:"created_time"` // 创建时间
+	CreatedBy   string    `json:"createdBy" xorm:"created_by"`     // 创建人
+	UpdatedTime time.Time `json:"updatedTime" xorm:"updated_time"` // 更新时间
+	UpdatedBy   string    `json:"updatedBy" xorm:"updated_by"`     // 更新人
+}
