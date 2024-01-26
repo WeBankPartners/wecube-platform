99d9891e3c06b47ded29cb31c5cdab5d36a2201a
pobu168 <244202969@qq.com>
2024-01-26 10:39:40 +0800

    [platform-core]编排前序节点接口只返回人工/自动/数据节点

--- wecube-portal/src/api/server.js
+++ wecube-portal/src/api/server.js
@@ -53,10 +53,10 @@ export const getParamsInfosByFlowIdAndNodeId = (flowId, nodeId) =>
   req.get(`platform/v1/process/definitions/${flowId}/tasknodes/${nodeId}`)
 
 export const getFlowNodes = flowId => req.get(`platform/v1/process/definitions/${flowId}/tasknodes/briefs`)
-export const getContextParametersNodes = (flowId, taskNodeId, prevCtxNodeIds) => {
+export const getContextParametersNodes = (flowId, taskNodeId, contextParamNodes) => {
   let params = {
     taskNodeId: taskNodeId,
-    prevCtxNodeIds: prevCtxNodeIds.join(',')
+    contextParamNodes: contextParamNodes.join(',')
   }
   return req.get(`/platform/v1/process/definitions/${flowId}/root-context-nodes/briefs`, { params })
 }
@@ -241,7 +241,31 @@ export const stopUserScheduledTasks = data => req.post(`platform/v1/user-schedul
 export const getScheduledTasksByStatus = data =>
   req.post(`platform/v1/user-scheduled-tasks/process-instances/query`, data)
 
-export const getAssociatedNodes = (procDefId, params) =>
-  req.post(`platform/v1/process/definitions/${procDefId}/previous-nodes/briefs`, params)
-
 export const getMetaData = params => req.post(`platform/v1/plugins/configs/interfaces/param/metadata/query`, params)
+
+export const flowList = data => req.post(`platform/v1/process/definitions/list`, data)
+export const flowMgmt = data => req.post(`platform/v1/process/definitions`, data)
+export const getFlowById = id => req.get(`platform/v1/process/definitions/${id}`)
+export const flowNodeMgmt = data => req.post(`platform/v1/process/definitions/tasknodes`, data)
+export const flowNodeDelete = (flowId, nodeId) =>
+  req.delete(`platform/v1/process/definitions/${flowId}/tasknodes/${nodeId}`)
+export const flowEdgeMgmt = data => req.post(`platform/v1/process/definitions/link`, data)
+export const flowEdgeDelete = (flowId, edgeId) => req.delete(`platform/v1/process/definitions/${flowId}/link/${edgeId}`)
+export const getPluginList = () => req.get(`platform/v1/packages/name/list`)
+export const getPluginFunByRule = data => req.post(`platform/v1/plugins/query-by-target-entity`, data)
+export const flowStatusChange = data => req.post(`platform/v1/process/definitions/status`, data)
+export const flowRelease = flowId => req.post(`platform/v1/process/definitions/deploy/${flowId}`, {})
+export const getAssociatedNodes = (flowId, nodeId) =>
+  req.get(`platform/v1/process/definitions/${flowId}/tasknodes/${nodeId}/preorder`)
+export const getNodeParams = (flowId, nodeId) =>
+  req.get(`platform/v1/process/definitions/${flowId}/tasknodes/${nodeId}/parameters`)
+
+export const flowBatchAuth = data => req.post(`platform/v1/process/definitions/permission`, data)
+export const flowBatchChangeStatus = data => req.post(`platform/v1/process/definitions/status`, data)
+export const flowCopy = (flowId, association) =>
+  req.post(`platform/v1/process/definitions/${flowId}/copy/${association}`, {})
+export const getSourceNode = flowId => req.get(`platform/v1/process/definitions/${flowId}/tasknodes/briefs`)
+export const flowExport = data => req.post(`platform/v1/process/definitions/export`, data)
+export const flowImport = data => req.post(`platform/v1/process/definitions/import`, data)
+export const getNodeDetailById = (flowId, nodeId) =>
+  req.get(`platform/v1/process/definitions/${flowId}/tasknodes/${nodeId}`)
---
+++ wecube-portal/src/pages/collaboration/flow/item-panel.vue
@@ -0,0 +1,398 @@
+<template>
+  <div id="itemPanel" ref="itemPanel">
+    <div class="tool-component">组件库</div>
+    <div class="icon-tool">
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="start"
+          data-label="开始"
+          task-category=""
+          data-shape="circle-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/start.svg"
+            style="border: 1px solid #303030; border-radius: 50%"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">开始</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="end"
+          data-label="结束"
+          task-category=""
+          data-shape="circle-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/end.svg"
+            style="border: 1px solid #303030; border-radius: 50%"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">结束</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="abnormal"
+          data-label="异常"
+          task-category=""
+          data-shape="circle-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/abnormal.svg"
+            style="border: 1px solid #303030; border-radius: 50%"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">异常</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="decision"
+          data-label="判断"
+          task-category=""
+          data-shape="diamond-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <div class="diamond-border">
+            <img src="./icon/descision-panel.svg" class="item-tool-icon" draggable="false" />
+          </div>
+          <div class="diamond-item-tool-name">判断</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="fork"
+          data-label="分流节点"
+          task-category=""
+          data-shape="diamond-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <div class="diamond-border">
+            <img src="./icon/fork-panel.svg" class="item-tool-icon" draggable="false" />
+          </div>
+          <div class="diamond-item-tool-name">分流节点</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="merge"
+          data-label="汇聚节点"
+          task-category=""
+          data-shape="diamond-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <div class="diamond-border">
+            <img src="./icon/merge-panel.svg" class="item-tool-icon" draggable="false" />
+          </div>
+          <div class="diamond-item-tool-name">汇聚节点</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="human"
+          data-label="人工节点"
+          task-category="SUTN"
+          data-shape="rect-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/human.svg"
+            style="border: 1px solid #303030; width: 32px"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">人工节点</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="automatic"
+          data-label="自动节点"
+          task-category="SSTN"
+          data-shape="rect-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/automatic.svg"
+            style="border: 1px solid #303030; width: 32px"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">自动节点</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="data"
+          data-label="数据节点"
+          task-category="SDTN"
+          data-shape="rect-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/data.svg"
+            style="border: 1px solid #303030; width: 32px"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">数据节点</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="date"
+          data-label="固定时间"
+          task-category=""
+          data-shape="circle-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/date.svg"
+            style="border: 1px solid #303030; border-radius: 50%"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">固定时间</div>
+        </div>
+      </Tooltip>
+      <Tooltip content="Right Center text" placement="right" :delay="1000">
+        <div
+          class="item-tool"
+          draggable="true"
+          node-type="timeInterval"
+          data-label="时间间隔"
+          task-category=""
+          data-shape="circle-node"
+          fill="white"
+          line-width="1"
+          stroke="#303030"
+        >
+          <img
+            src="./icon/timeInterval.svg"
+            style="border: 1px solid #303030; border-radius: 50%"
+            class="item-tool-icon"
+            draggable="false"
+          />
+          <div class="item-tool-name">时间间隔</div>
+        </div>
+      </Tooltip>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemPanel',
+  data () {
+    return {
+      itemVisible: false
+    }
+  },
+  mounted () {
+    const icons = [...this.$refs.itemPanel.querySelectorAll('.item-tool')]
+
+    icons.forEach(icon => {
+      icon.addEventListener('dragstart', event => {
+        const shape = icon.getAttribute('data-shape')
+        const label = icon.getAttribute('data-label')
+        const fill = icon.getAttribute('fill')
+        const lineWidth = Number(icon.getAttribute('line-width'))
+        const nodeType = icon.getAttribute('node-type')
+        const stroke = icon.getAttribute('stroke')
+
+        /* 设置拖拽传输数据 */
+        event.dataTransfer.setData(
+          'dragComponent',
+          JSON.stringify({
+            label,
+            shape,
+            fill,
+            lineWidth,
+            stroke,
+            nodeType
+          })
+        )
+      })
+    })
+
+    // 阻止默认动作
+    document.addEventListener(
+      'drop',
+      e => {
+        e.preventDefault()
+      },
+      false
+    )
+  }
+}
+</script>
+
+<style lang="scss">
+#itemPanel {
+  position: absolute;
+  top: 137px;
+  left: 16px;
+  bottom: 0;
+  z-index: 10;
+  width: 72px;
+  height: 80%;
+  background: #fff;
+  text-align: center;
+  // padding-top: 65px;
+  transition: transform 0.3s ease-in-out;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.1);
+
+  &.hidden {
+    transform: translate(-100%, 0);
+  }
+
+  .icon-tool {
+    padding: 10px;
+    text-align: center;
+    .iconfont {
+      display: block;
+      width: 40px;
+      height: 40px;
+      line-height: 40px;
+      font-size: 30px;
+      cursor: move;
+      border: 1px solid transparent;
+      margin: 0 auto;
+
+      &:hover {
+        border-color: #ccc;
+      }
+    }
+    .node {
+      width: 32px;
+      height: 32px;
+      margin-bottom: 10px;
+      cursor: move;
+    }
+    .circle-start {
+      height: 50px;
+      width: 50px;
+      line-height: 50px;
+      // border-radius: 50%;
+      // border: 1px solid #ccc;
+      // background: white;
+    }
+    .circle-end {
+      @extend .circle-start;
+      // border: 2px solid #ccc;
+      // background: white;
+    }
+
+    .warning {
+      height: 40px;
+      line-height: 40px;
+      border-left: 4px solid #e6a23c;
+      background: #f8ecda;
+    }
+    .end {
+      height: 40px;
+      line-height: 40px;
+      border-radius: 10px;
+      background: #f9e3e2;
+    }
+  }
+}
+
+.item-tool {
+  background-color: #f0f0f0;
+  width: 52px;
+  height: 52px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  cursor: move;
+  .item-tool-icon {
+    width: 24px;
+    height: 24px;
+    margin-top: 8px;
+  }
+  .item-tool-name {
+    font-size: 11px;
+  }
+}
+.tool-component {
+  padding: 6px 0;
+  border-bottom: 1px solid #e8eaec;
+}
+
+.diamond-border {
+  position: relative;
+  width: 44px; /* 调整为你的图片尺寸 */
+  height: 44px; /* 调整为你的图片尺寸 */
+  overflow: hidden;
+  padding: 4px;
+  bottom: 6px;
+}
+
+.diamond-border img {
+  display: block;
+  width: 100%;
+  height: auto;
+  transform: rotate(45deg);
+  transform-origin: 50% 50%;
+  position: absolute;
+  top: 16%;
+  left: 80%;
+  margin-top: -50%;
+  margin-left: -50%;
+  border: 1px solid #303030;
+  padding: 3px;
+}
+.diamond-item-tool-name {
+  position: relative;
+  bottom: 8px;
+  font-size: 11px;
+}
+</style>
