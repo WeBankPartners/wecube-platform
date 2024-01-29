cc5c8fa2749b9c2cc87667b51cddeea2d527fc61
zhanglin9833 <zhanglin9833@gmail.com>
2024-01-29 10:54:49 +0800

    [platform-core] add UpdateTemplatePermission

--- platform-core/api/v1/batch-execution/batch-execution.go
+++ platform-core/api/v1/batch-execution/batch-execution.go
@@ -174,6 +174,39 @@ func GetTemplate(c *gin.Context) {
 	return
 }
 
+func UpdateTemplatePermission(c *gin.Context) {
+	defer try.ExceptionStack(func(e interface{}, err interface{}) {
+		retErr := fmt.Errorf("%v", err)
+		middleware.ReturnError(c, exterror.Catch(exterror.New().ServerHandleError, retErr))
+		log.Logger.Error(e.(string))
+	})
+
+	reqParam := models.BatchExecutionTemplate{}
+	var err error
+	if err = c.ShouldBindJSON(&reqParam); err != nil {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
+		return
+	}
+	if reqParam.Id == "" {
+		err = exterror.Catch(exterror.New().RequestParamValidateError, fmt.Errorf("request param err, id can not be empty"))
+		middleware.ReturnError(c, err)
+		return
+	}
+	if len(reqParam.PermissionToRole.MGMT) == 0 {
+		err = exterror.Catch(exterror.New().RequestParamValidateError, fmt.Errorf("request param err, MGMT permission role can not be empty"))
+		middleware.ReturnError(c, err)
+		return
+	}
+
+	retData, err := database.UpdateTemplatePermission(c, &reqParam)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, retData)
+	}
+	return
+}
+
 func RetrieveBatchExec(c *gin.Context) {
 	defer try.ExceptionStack(func(e interface{}, err interface{}) {
 		retErr := fmt.Errorf("%v", err)
