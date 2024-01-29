cd3f91d331964a9597b0b6acaf1dbfb041d72fd1
zgyzgyhero <894526647@qq.com>
2024-01-29 20:28:06 +0800

    update platform core plugin package query

---
+++ platform-core/wiki/database/v4.0.0.sql
@@ -0,0 +1,2 @@
+alter table plugin_package_data_model add column `updated_time` datetime DEFAULT NULL COMMENT '更新时间-新';
+alter table plugin_package_attributes add column `is_array` bit(1) DEFAULT b'0' COMMENT '是否数组-新';
