c2d5d142d1412ae1faa8cb74be17a08db82e7d44
pobu168 <244202969@qq.com>
2024-01-29 09:54:18 +0800

    update: Makefile

--- wecube-portal/vue.config.js
+++ wecube-portal/vue.config.js
@@ -1,6 +1,6 @@
 const CompressionPlugin = require('compression-webpack-plugin')
 
-const baseURL = 'http://106.52.160.142:19090/'
+const baseURL = 'http://47.107.100.214:18080/'
 
 module.exports = {
   devServer: {
