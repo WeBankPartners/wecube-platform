fdf2b9e864fd8467768614084da572922d379047
Roy <576477085@qq.com>
2024-02-02 17:15:35 +0800

    update workflow execution

--- platform-core/services/execution/workflow_execution.go
+++ platform-core/services/execution/workflow_execution.go
@@ -16,33 +16,22 @@ import (
  * @params ctx 上下文数据，需要是gin ctx
  * @params operator 操作人用户名
  * @params authToken 用户token
- * @params pluginInterfaceId 要调用的插件接口ID
+ * @params pluginInterfaceId 要调用的插件接口
  * @params entityType 输入entity的表达式
  * @params entityInstances 输入entity的数据
  * @params inputParamConstants 输入数据(常量，即用户输入的)
  * @params continueToken 是否进行高危检测(有值则跳过)
+ * @params dueDate 任务超时控制
+ * @params allowedOptions 任务选项
  *
  * @return 调用结果, 高危结果, 错误
  */
 
-func WorkflowExecutionCallPluginService(ctx context.Context, operator, authToken, pluginInterfaceId string, entityType string,
+func WorkflowExecutionCallPluginService(ctx context.Context, operator, authToken string, pluginInterface *models.PluginConfigInterfaces, entityType string,
 	entityInstances []*models.BatchExecutionPluginExecEntityInstances,
 	inputParamConstants []*models.BatchExecutionPluginDefInputParams,
 	inputParamContext map[string]interface{},
-	continueToken string) (result *models.PluginInterfaceApiResultData, dangerousCheckResult *models.ItsdangerousWorkflowCheckResultData, pluginCallParam *models.BatchExecutionPluginExecParam, err error) {
-	pluginInterface, errGet := database.GetPluginConfigInterfaceById(pluginInterfaceId, true)
-	if errGet != nil {
-		err = errGet
-		return
-	}
-	if pluginInterface == nil {
-		err = fmt.Errorf("invalid plugin interface %s", pluginInterfaceId)
-		return
-	}
-	if pluginInterface.Type != models.PluginInterfaceTypeExecution {
-		err = fmt.Errorf("unsupported plugin interface type %s", pluginInterface.Type)
-		return
-	}
+	continueToken string, dueDate string, allowedOptions []string) (result *models.PluginInterfaceApiResultData, dangerousCheckResult *models.ItsdangerousWorkflowCheckResultData, pluginCallParam *models.BatchExecutionPluginExecParam, err error) {
 	inputConstantMap := make(map[string]string)
 	for _, inputConst := range inputParamConstants {
 		inputConstantMap[inputConst.ParamId] = inputConst.ParameValue
@@ -92,6 +81,8 @@ func WorkflowExecutionCallPluginService(ctx context.Context, operator, authToken
 		ServicePath:     pluginInterface.ServiceDisplayName,
 		EntityInstances: entityInstances,
 		Inputs:          inputParamDatas,
+		DueDate:         dueDate,
+		AllowedOptions:  allowedOptions,
 	}
 	pluginCallParam.RequestId = "flowexec_" + guid.CreateGuid()
 	pluginCallResult, errCall := remote.PluginInterfaceApi(ctx, authToken, pluginInterface, pluginCallParam)
