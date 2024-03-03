e1ff2dec48b5d5fe36e0803e312f956fc6a4d471
zgyzgyhero <894526647@qq.com>
2024-03-03 13:11:11 +0800

    update platform core proc schedule

--- platform-core/services/database/schedule.go
+++ platform-core/services/database/schedule.go
@@ -23,11 +23,12 @@ func CreateProcSchedule(ctx context.Context, param *models.CreateProcSchedulePar
 		EntityDataName: param.EntityDataName,
 		ScheduleMode:   param.ScheduleMode,
 		ScheduleExpr:   param.ScheduleExpr,
+		CronExpr:       param.CronExpr,
 		CreatedBy:      param.Operator,
 		CreatedTime:    time.Now(),
 	}
-	_, err = db.MysqlEngine.Context(ctx).Exec("insert into proc_schedule_config(id,proc_def_id,proc_def_key,proc_def_name,status,entity_data_id,entity_type_id,entity_data_name,schedule_mode,schedule_expr,cron_expr,created_by,created_time) values (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-		result.Id, result.ProcDefId, result.ProcDefKey, result.ProcDefName, result.Status, result.EntityDataId, result.EntityTypeId, result.EntityDataName, result.ScheduleMode, result.ScheduleExpr, result.CronExpr, result.CreatedBy, result.CreatedTime)
+	_, err = db.MysqlEngine.Context(ctx).Exec("insert into proc_schedule_config(id,proc_def_id,proc_def_key,proc_def_name,status,entity_data_id,entity_type_id,entity_data_name,schedule_mode,schedule_expr,cron_expr,exec_times,created_by,created_time) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+		result.Id, result.ProcDefId, result.ProcDefKey, result.ProcDefName, result.Status, result.EntityDataId, result.EntityTypeId, result.EntityDataName, result.ScheduleMode, result.ScheduleExpr, result.CronExpr, 0, result.CreatedBy, result.CreatedTime)
 	if err != nil {
 		err = exterror.Catch(exterror.New().DatabaseExecuteError, err)
 	}
@@ -52,12 +53,16 @@ func GetNewProcScheduleList() (result []*models.ProcScheduleConfig, err error) {
 	return
 }
 
-func UpdateProcScheduleStatus(ctx context.Context, id, status string) (err error) {
-	_, err = db.MysqlEngine.Context(ctx).Exec("update proc_schedule_config set status=? where id=?", status, id)
+func UpdateProcScheduleStatus(ctx context.Context, id, status, operator string) (err error) {
+	_, err = db.MysqlEngine.Context(ctx).Exec("update proc_schedule_config set status=?,updated_by=?,updated_time=? where id=?", status, operator, time.Now(), id)
 	if err != nil {
 		err = exterror.Catch(exterror.New().DatabaseExecuteError, err)
 	}
 	return
+}
+
+func AddProcScheduleExecTimes(ctx context.Context, id string) {
+	db.MysqlEngine.Context(ctx).Exec("update proc_schedule_config set exec_times=exec_times+1 where id=?", id)
 }
 
 func GetProcScheduleConfigStatus(ctx context.Context, id string) (status string) {
@@ -187,6 +192,119 @@ func getHourMinSec(input string) (hour, min, sec int, err error) {
 	if sec < 0 || sec > 59 {
 		err = fmt.Errorf("second:%d illegal", sec)
 		return
+	}
+	return
+}
+
+func QueryProcScheduleList(ctx context.Context, param *models.ProcScheduleQueryParam) (result []*models.ProcScheduleConfigObj, err error) {
+	var psConfigRows []*models.ProcScheduleConfig
+	baseSql := "select * from proc_schedule_config"
+	var filterSqlList []string
+	var filterParams []interface{}
+	if param.ScheduleMode != "" {
+		filterSqlList = append(filterSqlList, "schedule_mode=?")
+		filterParams = append(filterParams, param.ScheduleMode)
+	}
+	if param.Owner != "" {
+		filterSqlList = append(filterSqlList, "created_by=?")
+		filterParams = append(filterParams, param.Owner)
+	}
+	if param.StartTime != "" {
+		filterSqlList = append(filterSqlList, "created_time>=?")
+		filterParams = append(filterParams, param.StartTime)
+	}
+	if param.EndTime != "" {
+		filterSqlList = append(filterSqlList, "created_time<=?")
+		filterParams = append(filterParams, param.EndTime)
+	}
+	baseSql += " where status<>'Deleted' "
+	if len(filterSqlList) > 0 {
+		baseSql += strings.Join(filterSqlList, " and ")
+	}
+	baseSql += " order by created_time desc"
+	err = db.MysqlEngine.Context(ctx).SQL(baseSql, filterParams...).Find(&psConfigRows)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	result = []*models.ProcScheduleConfigObj{}
+	if len(psConfigRows) == 0 {
+		return
+	}
+	var statusCountQueryRows []*models.ProcScheduleQueryRow
+	if len(filterSqlList) > 0 {
+		var idList []string
+		for _, row := range psConfigRows {
+			idList = append(idList, row.Id)
+		}
+		err = db.MysqlEngine.Context(ctx).SQL("select t4.id,t4.status,count(1) as num from (select t1.id,t3.status from proc_schedule_config t1 left join proc_schedule_job t2 on t1.id=t2.schedule_config_id left join proc_ins t3 on t2.proc_ins_id=t3.id where t1.id in ('" + strings.Join(idList, "','") + "')) t4 group by t4.id,t4.status").Find(&statusCountQueryRows)
+	} else {
+		err = db.MysqlEngine.Context(ctx).SQL("select t4.id,t4.status,count(1) as num from (select t1.id,t3.status from proc_schedule_config t1 left join proc_schedule_job t2 on t1.id=t2.schedule_config_id left join proc_ins t3 on t2.proc_ins_id=t3.id) t4 group by t4.id,t4.status").Find(&statusCountQueryRows)
+	}
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
+		return
+	}
+	completedMap := make(map[string]int)
+	faultMap := make(map[string]int)
+	inProgressMap := make(map[string]int)
+	for _, v := range statusCountQueryRows {
+		if v.Status == models.JobStatusSuccess {
+			completedMap[v.Id] = v.Num
+		} else if v.Status == models.JobStatusFail {
+			faultMap[v.Id] = v.Num
+		} else if v.Status == models.JobStatusRunning {
+			inProgressMap[v.Id] = v.Num
+		}
+	}
+	for _, row := range psConfigRows {
+		resultObj := models.ProcScheduleConfigObj{
+			Id:                       row.Id,
+			ScheduleMode:             row.ScheduleMode,
+			ScheduleExpr:             row.ScheduleExpr,
+			ProcDefId:                row.ProcDefId,
+			ProcDefName:              row.ProcDefName,
+			EntityDataId:             row.EntityDataId,
+			EntityDataName:           row.EntityDataName,
+			Owner:                    row.CreatedBy,
+			Status:                   row.Status,
+			CreatedTime:              row.CreatedTime.Format(models.DateTimeFormat),
+			TotalCompletedInstances:  completedMap[row.Id],
+			TotalFaultedInstances:    faultMap[row.Id],
+			TotalInProgressInstances: inProgressMap[row.Id],
+		}
+		result = append(result, &resultObj)
+	}
+	return
+}
+
+func QueryProcScheduleInstance(ctx context.Context, psConfigId, status string) (result []*models.ProcScheduleInstQueryObj, err error) {
+	var procInsRows []*models.ProcIns
+	if status == "" {
+		err = db.MysqlEngine.Context(ctx).SQL("select t3.id,t3.status,t3.created_time,t3.proc_def_id,t3.proc_def_name from proc_schedule_config t1 left join proc_schedule_job t2 on t1.id=t2.schedule_config_id left join proc_ins t3 on t2.proc_ins_id=t3.id where t1.id=?", psConfigId).Find(&procInsRows)
+	} else {
+		err = db.MysqlEngine.Context(ctx).SQL("select t3.id,t3.status,t3.created_time,t3.proc_def_id,t3.proc_def_name from proc_schedule_config t1 left join proc_schedule_job t2 on t1.id=t2.schedule_config_id left join proc_ins t3 on t2.proc_ins_id=t3.id where t1.id=? and t3.status=?", psConfigId, status).Find(&procInsRows)
+	}
+	result = []*models.ProcScheduleInstQueryObj{}
+	for _, row := range procInsRows {
+		if row.Id == "" {
+			continue
+		}
+		result = append(result, &models.ProcScheduleInstQueryObj{
+			ProcInstId:  row.Id,
+			ProcDefId:   row.ProcDefId,
+			ProcDefName: row.ProcDefName,
+			Status:      row.Status,
+			ExecTime:    row.CreatedTime.Format(models.DateTimeFormat),
+		})
+	}
+	return
+}
+
+func GetProcScheduleLoadList() (psConfigList []*models.ProcScheduleConfig, err error) {
+	err = db.MysqlEngine.SQL("select * from proc_schedule_config where status<>'Deleted'").Find(&psConfigList)
+	if err != nil {
+		err = exterror.Catch(exterror.New().DatabaseQueryError, err)
 	}
 	return
 }
