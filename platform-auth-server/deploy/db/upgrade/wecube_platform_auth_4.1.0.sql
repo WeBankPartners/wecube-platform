5200cd3e2a3478b69a99ccbba08c1b6ffe2ba44e
tangjiawei <tangjiawei@nnyy.com>
2024-04-15 18:06:46 +0800

    [platform-auth-server]角色申请添加过期时间,添加邮件提醒

--- platform-auth-server/deploy/db/upgrade/wecube_platform_auth_4.1.0.sql
+++ platform-auth-server/deploy/db/upgrade/wecube_platform_auth_4.1.0.sql
@@ -1,7 +1,7 @@
 SET FOREIGN_KEY_CHECKS = 0;
-alter table auth_sys_role_apply add column expire_time datatime default null COMMENT '过期时间';
-alter table auth_sys_user_role add column expire_time datatime default null COMMENT '过期时间';
+alter table auth_sys_role_apply add column expire_time datetime default null COMMENT '过期时间';
+alter table auth_sys_user_role add column expire_time datetime default null COMMENT '过期时间';
 alter table auth_sys_user_role add column notify_count tinyint default 0 COMMENT '快要过期通知';
-alter table auth_sys_role_apply add index role_apply_expire_time(expire_time);
+alter table auth_sys_user_role add index role_apply_expire_time(expire_time);
 alter table auth_sys_role_apply add index role_apply_status(status);
 SET FOREIGN_KEY_CHECKS = 1;
--- platform-auth-server/service/cron.go
+++ platform-auth-server/service/cron.go
@@ -1,13 +1,12 @@
 package service
 
 import (
+	"fmt"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/common/log"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/model"
 	"github.com/WeBankPartners/wecube-platform/platform-auth-server/service/db"
 	"time"
 )
-
-const percent = 75
 
 func StartCornJob() {
 	go startNotifyCronJob()
@@ -25,7 +24,28 @@ func startNotifyCronJob() {
 func notifyAction() {
 	var err error
 	log.Logger.Info("Start notify action")
+	var allUserList []*model.SysUserEntity
+	var allUserEmailMap = make(map[string]string)
 	var userRoleEntityList []*model.UserRoleRsEntity
+	var allRoleList []*model.SysRoleEntity
+	// 角色显示名 map
+	var allRoleDisplayNameMap = make(map[string]string)
+	if err = db.Engine.SQL("select * from auth_sys_user where is_deleted = 0").Find(&allUserList); err != nil {
+		return
+	}
+	if len(allUserList) > 0 {
+		for _, user := range allUserList {
+			allUserEmailMap[user.Username] = user.EmailAddr
+		}
+	}
+	if err = db.Engine.SQL("select * from auth_sys_role where is_deleted = 0").Find(&allRoleList); err != nil {
+		return
+	}
+	if len(allRoleList) > 0 {
+		for _, role := range allRoleList {
+			allRoleDisplayNameMap[role.Name] = role.DisplayName
+		}
+	}
 	if err = db.Engine.SQL("select  * from auth_sys_user_role where  expire_time is not null  and is_deleted = 0").Find(&userRoleEntityList); err != nil {
 		log.Logger.Error("notify action fail,query auth_sys_user_role error", log.Error(err))
 		return
@@ -36,9 +56,9 @@ func notifyAction() {
 	for _, entity := range userRoleEntityList {
 		// 计算过期百分比
 		entityPercent := calcExpireObj(entity)
-		if entityPercent >= percent && entity.NotifyCount == 0 {
+		if entityPercent >= model.Config.NotifyPercent && entity.NotifyCount == 0 {
 			// 发送邮件
-
+			go NotifyRoleExpireMail(allRoleDisplayNameMap[entity.RoleName], allUserEmailMap[entity.Username])
 		} else if entityPercent >= 100 {
 			// 删除授权角色
 			if _, err = db.Engine.Exec("update auth_sys_user_role set is_deleted = 1 where id = ?", entity.Id); err != nil {
@@ -53,4 +73,18 @@ func calcExpireObj(entity *model.UserRoleRsEntity) float64 {
 	max := entity.ExpireTime.Sub(entity.CreatedTime).Seconds()
 	use := time.Now().Sub(entity.CreatedTime).Seconds()
 	return (use / max) * 100
+}
+
+func NotifyRoleExpireMail(role, email string) (err error) {
+	var subject, content string
+	if model.Config.Mail.AuthServer != "" && model.Config.Mail.SenderMail != "" {
+		subject = "[wecube] [Request return reminder]  【请求被退回提醒】"
+		content = fmt.Sprintf("The [request: %s] you initiated was returned to. Please make the necessary modifications and resubmit. Click the link to view details", role)
+		content = content + fmt.Sprintf("\n\n\n您发起的[请求:%s],在%s节点被%s退回到草稿,请修改之后重新提交,点击链接查看详情", role)
+		err = model.MailSender.Send(subject, content, []string{email})
+		if err != nil {
+			log.Logger.Error("send mail err", log.Error(err))
+		}
+	}
+	return
 }
