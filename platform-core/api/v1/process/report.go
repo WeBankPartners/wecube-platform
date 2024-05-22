7d9d909c49ce9d7963c211a17f8c99715966d125
zhanglin9833 <zhanglin9833@gmail.com>
2024-05-22 17:25:56 +0800

    [platform-core] add StatisticsProDefList, StatisticsServiceNames and StatisticsTasknodeBindingsEntity

---
+++ platform-core/api/v1/process/report.go
@@ -0,0 +1,80 @@
+package process
+
+import (
+	"fmt"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/exterror"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/log"
+
+	"github.com/WeBankPartners/wecube-platform/platform-core/api/middleware"
+	"github.com/WeBankPartners/wecube-platform/platform-core/common/try"
+	"github.com/WeBankPartners/wecube-platform/platform-core/services/database"
+	"github.com/gin-gonic/gin"
+)
+
+// StatisticsProDefList 编排定义列表
+func StatisticsProDefList(c *gin.Context) {
+	defer try.ExceptionStack(func(e interface{}, err interface{}) {
+		retErr := fmt.Errorf("%v", err)
+		middleware.ReturnError(c, exterror.Catch(exterror.New().ServerHandleError, retErr))
+		log.Logger.Error(e.(string))
+	})
+
+	includeDraft := c.Query("includeDraft") // 0 | 1
+	permission := c.Query("permission")     // USE | MGMT
+	tag := c.Query("tag")
+	plugin := c.Query("plugin")
+	if includeDraft == "" {
+		includeDraft = "0"
+	}
+	if permission == "" {
+		permission = "USE"
+	}
+	log.Logger.Debug("procDefList", log.String("includeDraft", includeDraft), log.String("permission", permission), log.String("tag", tag), log.StringList("roleList", middleware.GetRequestRoles(c)))
+	result, err := database.ProcDefList(c, includeDraft, permission, tag, plugin, middleware.GetRequestRoles(c))
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, result)
+	}
+	return
+}
+
+// StatisticsServiceIds 插件注册列表
+func StatisticsServiceNames(c *gin.Context) {
+	defer try.ExceptionStack(func(e interface{}, err interface{}) {
+		retErr := fmt.Errorf("%v", err)
+		middleware.ReturnError(c, exterror.Catch(exterror.New().ServerHandleError, retErr))
+		log.Logger.Error(e.(string))
+	})
+
+	result, err := database.StatisticsServiceNames(c)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, result)
+	}
+	return
+}
+
+// StatisticsTasknodeBindingsEntity 数据对象
+func StatisticsTasknodeBindingsEntity(c *gin.Context) {
+	defer try.ExceptionStack(func(e interface{}, err interface{}) {
+		retErr := fmt.Errorf("%v", err)
+		middleware.ReturnError(c, exterror.Catch(exterror.New().ServerHandleError, retErr))
+		log.Logger.Error(e.(string))
+	})
+
+	var serviceNameList []string
+	var err error
+	if err = c.ShouldBind(&serviceNameList); err != nil {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
+		return
+	}
+	result, err := database.StatisticsTasknodeBindingsEntity(c, serviceNameList)
+	if err != nil {
+		middleware.ReturnError(c, err)
+	} else {
+		middleware.ReturnData(c, result)
+	}
+	return
+}
