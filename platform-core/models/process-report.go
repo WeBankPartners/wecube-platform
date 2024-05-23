1f780d93cbb527d58d029dfb1d796ba41e4fa78f
zhanglin9833 <zhanglin9833@gmail.com>
2024-05-23 10:11:18 +0800

    add StatisticsTasknodes

--- platform-core/models/process-report.go
+++ platform-core/models/process-report.go
@@ -5,3 +5,13 @@ type TasknodeBindsEntityData struct {
 	EntityDataName string `json:"entityDisplayName" xorm:"entity_data_name"` // 数据名称
 	OrderedNo      string `json:"orderedNo" xorm:"-"`
 }
+
+type Tasknode struct {
+	NodeDefId   string `json:"nodeDefId" xorm:"node_id"`
+	NodeId      string `json:"nodeId" xorm:"id"`
+	NodeName    string `json:"nodeName" xorm:"name"`
+	NodeType    string `json:"nodeType" xorm:"node_type"`
+	ProcDefId   string `json:"procDefId" xorm:"proc_def_id"`
+	ServiceId   string `json:"serviceId" xorm:"service_name"`
+	ServiceName string `json:"serviceName" xorm:"service_name"`
+}
