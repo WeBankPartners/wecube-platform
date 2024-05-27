18fb694c6100130a02908ce2591f78ebc6808bf5
pobu168 <244202969@qq.com>
2024-05-27 11:35:58 +0800

    add: tip for workfow control

--- wecube-portal/src/pages/implementation/history-execution.vue
+++ wecube-portal/src/pages/implementation/history-execution.vue
@@ -220,7 +220,10 @@ export default {
     // #region 暂停、继续编排
     async flowControlHandler (operateType, row) {
       this.$Modal.confirm({
-        title: this.$t('bc_confirm') + ' ' + (operateType === 'stop' ? this.$t('pause') : this.$t('be_continue')),
+        title: this.$t('be_workflow_non_owner_title'),
+        content: `${this.$t('be_workflow_non_owner_list_tip1')}[${row.operator}]${this.$t(
+          'be_workflow_non_owner_list_tip2'
+        )}`,
         'z-index': 1000000,
         onOk: async () => {
           let payload = {
@@ -242,7 +245,10 @@ export default {
     // 终止任务
     stopTask (row) {
       this.$Modal.confirm({
-        title: this.$t('bc_confirm') + ' ' + this.$t('stop_orch'),
+        title: this.$t('be_workflow_non_owner_title'),
+        content: `${this.$t('be_workflow_non_owner_list_tip1')}[${row.operator}]${this.$t(
+          'be_workflow_non_owner_list_tip2'
+        )}`,
         'z-index': 1000000,
         onOk: async () => {
           const payload = {
