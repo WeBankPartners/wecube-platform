73c7fe480106f763c1b591c711df12c55da48ae3
wanghao7717 <792974788@qq.com>
2024-07-09 19:50:51 +0800

    feat: 编排设计&&编排执行需求开发

--- wecube-portal/src/pages/collaboration/workflow-mgmt.vue
+++ wecube-portal/src/pages/collaboration/workflow-mgmt.vue
@@ -970,7 +970,8 @@ export default {
       if (this.canRemovedId) {
         this.graph.updateItem(this.canRemovedId, {
           style: {
-            fill: 'white' // 更新后的节点背景色
+            // fill: '#000' // 更新后的节点背景色
+            fill: '' // 清除背景色，避免选中连线后，线条区域内会出现白色背景
           }
         })
       }
