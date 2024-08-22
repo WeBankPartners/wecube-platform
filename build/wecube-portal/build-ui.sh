f9aa7b5703f3ded4b8dc232f69259c4cc9074e53
wanghao7717 <792974788@qq.com>
2024-08-22 14:25:01 +0800

    update: wecube-common-ui版本升级

--- build/wecube-portal/build-ui.sh
+++ build/wecube-portal/build-ui.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e -x
 cd /home/node/app/wecube-portal
-# npm install --registry https://registry.npmmirror.com --unsafe-perm --force
-npm install
+npm install --registry https://registry.npmmirror.com --unsafe-perm --force
 npm run build
