5232dbb67c01369320b70087fd03ec7505adabb3
zgyzgyhero <894526647@qq.com>
2024-09-02 15:07:54 +0800

    update process def query with root entity

--- platform-core/api/v1/process/execute.go
+++ platform-core/api/v1/process/execute.go
@@ -24,6 +24,7 @@ func ProcDefList(c *gin.Context) {
 	tag := c.Query("tag")
 	plugin := c.Query("plugin")
 	subProc := c.Query("subProc")
+	rootEntity := c.Query("rootEntity")
 	if includeDraft == "" {
 		includeDraft = "0"
 	}
@@ -34,7 +35,7 @@ func ProcDefList(c *gin.Context) {
 		subProc = "all"
 	}
 	log.Logger.Debug("procDefList", log.String("includeDraft", includeDraft), log.String(permission, "permission"), log.String("tag", tag), log.StringList("roleList", middleware.GetRequestRoles(c)))
-	result, err := database.ProcDefList(c, includeDraft, permission, tag, plugin, subProc, middleware.GetRequestUser(c), middleware.GetRequestRoles(c))
+	result, err := database.ProcDefList(c, includeDraft, permission, tag, plugin, subProc, middleware.GetRequestUser(c), rootEntity, middleware.GetRequestRoles(c))
 	if err != nil {
 		middleware.ReturnError(c, err)
 	} else {
@@ -48,6 +49,7 @@ func PublicProcDefList(c *gin.Context) {
 	plugin := c.Query("plugin")
 	withAll := c.Query("all")
 	subProc := c.Query("subProc")
+	rootEntity := c.Query("rootEntity")
 	if permission == "" {
 		permission = "USE"
 	}
@@ -58,7 +60,7 @@ func PublicProcDefList(c *gin.Context) {
 		subProc = "main"
 	}
 	log.Logger.Debug("public procDefList", log.String(permission, "permission"), log.String("tag", tag))
-	procList, err := database.ProcDefList(c, "0", permission, tag, plugin, subProc, middleware.GetRequestUser(c), middleware.GetRequestRoles(c))
+	procList, err := database.ProcDefList(c, "0", permission, tag, plugin, subProc, middleware.GetRequestUser(c), rootEntity, middleware.GetRequestRoles(c))
 	if err != nil {
 		middleware.ReturnError(c, err)
 		return
@@ -847,7 +849,7 @@ func SubProcDefList(c *gin.Context) {
 		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, err))
 		return
 	}
-	result, err := database.ProcDefList(c, "0", "USE", "", "", "sub", middleware.GetRequestUser(c), middleware.GetRequestRoles(c))
+	result, err := database.ProcDefList(c, "0", "USE", "", "", "sub", middleware.GetRequestUser(c), "", middleware.GetRequestRoles(c))
 	if err != nil {
 		middleware.ReturnError(c, err)
 		return
