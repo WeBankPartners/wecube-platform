436cc5a5468fd742796ab163f61f837fd3c39b06
tangjiawei <tangjiawei@nnyy.com>
2024-09-13 21:45:28 +0800

    底座导出功能开发

--- platform-core/services/remote/taskman.go
+++ platform-core/services/remote/taskman.go
@@ -12,6 +12,8 @@ const (
 	pathRequestTemplateRoles = "/taskman/api/v1/request-template/roles"
 	// pathRequestTemplates 查询taskman模版列表
 	pathRequestTemplates = "/taskman/api/v1/request-template/export/batch"
+	// pathComponentLibrary 查询组件库
+	pathComponentLibrary = "/taskman/api/v1/form-template-library/export-data"
 )
 
 // GetRequestTemplateRoles 查询taskman模版角色
@@ -46,6 +48,26 @@ func GetRequestTemplates(dto models.GetRequestTemplatesDto, userToken, language 
 		uri = "http://" + uri
 	}
 	byteArr, err = network.HttpPost(uri, userToken, language, postBytes)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(byteArr, &response)
+	if err != nil {
+		err = fmt.Errorf("try to json unmarshal response body fail,%s ", err.Error())
+	}
+	return
+}
+
+// GetComponentLibrary 查询taskman组件库列表
+func GetComponentLibrary(userToken, language string) (response models.QueryComponentLibraryResponse, err error) {
+	var byteArr []byte
+	uri := models.Config.Gateway.Url + pathComponentLibrary
+	if models.Config.HttpsEnable == "true" {
+		uri = "https://" + uri
+	} else {
+		uri = "http://" + uri
+	}
+	byteArr, err = network.HttpGet(uri, userToken, language)
 	if err != nil {
 		return
 	}
