4cf77f96bab0320ec64d26e8a8cee4deb25760b1
wanghao7717 <792974788@qq.com>
2024-09-13 16:04:13 +0800

    feat: 底座迁移导出功能开发

--- wecube-portal/src/pages/admin/base-migration/export/create.vue
+++ wecube-portal/src/pages/admin/base-migration/export/create.vue
@@ -41,10 +41,7 @@ import stepEnviroment from './components/step-enviroment.vue'
 import stepSelectData from './components/step-select-data.vue'
 import stepResult from './components/step-result.vue'
 import { debounce } from '@/const/util'
-import {
-  saveEnvBusiness,
-  exportBaseMigration,
-} from '@/api/server'
+import { saveEnvBusiness, exportBaseMigration } from '@/api/server'
 export default {
   components: {
     stepEnviroment,
@@ -64,20 +61,24 @@ export default {
     this.status = this.$route.query.status || ''
     if (this.status === 'start') {
       this.activeStep = 1
-    } else if (['doing', 'success', 'fail'].includes(this.status)) {
+    }
+    else if (['doing', 'success', 'fail'].includes(this.status)) {
       this.activeStep = 2
     }
   },
   methods: {
     // 保存环境和产品
-    handleSaveEnvBusiness: debounce(async function() {
+    handleSaveEnvBusiness: debounce(async function () {
       const { env, selectionList } = this.$refs.env
-      const pIds = selectionList && selectionList.map(item => item.id) || []
+      const pIds = (selectionList && selectionList.map(item => item.id)) || []
       if (pIds.length === 0) {
         return this.$Message.warning('至少勾选一条产品数据')
       }
       this.loading = true
-      const { status } = await saveEnvBusiness({pIds, env})
+      const { status } = await saveEnvBusiness({
+        pIds,
+        env
+      })
       this.loading = false
       if (status === 'OK') {
         this.activeStep++
@@ -88,12 +89,14 @@ export default {
       this.activeStep--
     },
     // 执行导出
-    handleSaveExport: debounce(async function() {
-      const { roleSelectionList, itsmSelectionList, flowSelectionList, batchSelectionList } = this.$refs.data
-      const roleArr = roleSelectionList && roleSelectionList.map(item => item.name) || []
-      const itsmArr = itsmSelectionList && itsmSelectionList.map(item => item.id) || []
-      const flowArr = flowSelectionList && flowSelectionList.map(item => item.id) || []
-      const batchArr = batchSelectionList && batchSelectionList.map(item => item.id) || []
+    handleSaveExport: debounce(async function () {
+      const {
+        roleSelectionList, itsmSelectionList, flowSelectionList, batchSelectionList
+      } = this.$refs.data
+      const roleArr = (roleSelectionList && roleSelectionList.map(item => item.name)) || []
+      const itsmArr = (itsmSelectionList && itsmSelectionList.map(item => item.id)) || []
+      const flowArr = (flowSelectionList && flowSelectionList.map(item => item.id)) || []
+      const batchArr = (batchSelectionList && batchSelectionList.map(item => item.id)) || []
       const params = {
         transExportId: this.id,
         roles: roleArr,
