0a95ccf9c79b3c9236e1efb678dac5a98ff8764b
zgyzgyhero <894526647@qq.com>
2024-09-22 21:20:17 +0800

    update data trans import

--- platform-core/services/database/data-trans-import.go
+++ platform-core/services/database/data-trans-import.go
@@ -45,7 +45,20 @@ func ExecTransImport(ctx context.Context, nexusUrl string) (err error) {
 	if _, err = bash.DecompressFile(localExportFilePath, tmpImportDir); err != nil {
 		return
 	}
+	// 读解压后的文件录进数据库为了给用户展示要导入什么东西
 
+	// 开始导入
+	// 1、导入角色
+	// 2、导入cmdb插件服务、导入cmdb数据、同步cmdb数据模型、导入其它插件服务
+	// 3、导入编排
+	// 4、导入批量执行
+	// 5、导入物料包
+	// 6、导入监控基础类型、对象组、基础类型指标、对象组指标、对象组阈值配置、业务配置模版
+	// 7、导入taskman模版和公共组件
+	// 开始执行
+	// 8、开始执行编排(创建资源、初始化资源、应用部署)
+	// 继续导入
+	// 9、导入监控业务配置、层级对象指标、层级对象阈值配置、自定义看板
 	return
 }
 
