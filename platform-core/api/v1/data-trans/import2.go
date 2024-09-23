b5d4a1c6e5919e8d0502367e13b55fcb2af48c81
tangjiawei <tangjiawei@nnyy.com>
2024-09-23 16:53:05 +0800

    底座迁移-导出详情添加详细报错返回

--- platform-core/api/v1/data-trans/import2.go
+++ platform-core/api/v1/data-trans/import2.go
@@ -2,12 +2,13 @@ package data_trans
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/WeBankPartners/wecube-platform/platform-core/api/middleware"
 	"github.com/WeBankPartners/wecube-platform/platform-core/common/exterror"
 	"github.com/WeBankPartners/wecube-platform/platform-core/models"
 	"github.com/WeBankPartners/wecube-platform/platform-core/services/database"
 	"github.com/gin-gonic/gin"
-	"strings"
 )
 
 // GetBusinessList 获取环境业务配置
@@ -30,5 +31,51 @@ func GetBusinessList(c *gin.Context) {
 		middleware.ReturnError(c, err)
 		return
 	}
+	// 获取完数据,删除解压文件
+	if err = database.RemoveTempExportDir(localPath); err != nil {
+		middleware.ReturnError(c, err)
+		return
+	}
 	middleware.ReturnData(c, result)
+}
+
+// ExecImport 执行导入
+func ExecImport(c *gin.Context) {
+
+}
+
+// ImportDetail 导入详情
+func ImportDetail(c *gin.Context) {
+	transExportId := c.Query("transExportId")
+	var detail *models.TransExportDetail
+	var err error
+	if strings.TrimSpace(transExportId) == "" {
+		middleware.ReturnError(c, exterror.Catch(exterror.New().RequestParamValidateError, fmt.Errorf("transExportId is empty")))
+		return
+	}
+	if detail, err = database.GetTransExportDetail(c, transExportId); err != nil {
+		middleware.ReturnError(c, err)
+		return
+	}
+	middleware.ReturnData(c, detail)
+}
+
+func GetImportListOptions(c *gin.Context) {
+	var TransExportHistoryOptions models.TransExportHistoryOptions
+	var err error
+	if TransExportHistoryOptions, err = database.GetAllTransExportOptions(c); err != nil {
+		middleware.ReturnError(c, err)
+		return
+	}
+	middleware.ReturnData(c, TransExportHistoryOptions)
+}
+
+func ImportList(c *gin.Context) {
+	var TransExportHistoryOptions models.TransExportHistoryOptions
+	var err error
+	if TransExportHistoryOptions, err = database.GetAllTransExportOptions(c); err != nil {
+		middleware.ReturnError(c, err)
+		return
+	}
+	middleware.ReturnData(c, TransExportHistoryOptions)
 }
