fa349b6c3572c22e6e731fc533587c91dc75776e
wanghao7717 <792974788@qq.com>
2024-09-23 17:22:32 +0800

    一键导入页面开发

--- wecube-portal/src/pages/admin/base-migration/export/create.vue
+++ wecube-portal/src/pages/admin/base-migration/export/create.vue
@@ -137,7 +137,10 @@ export default {
         this.detailData.business = this.detailData.business || ''
         this.detailData.cmdbCICount = this.detailData.cmdbCIData.reduce((sum, cur) => sum + cur.count, 0)
         this.detailData.monitorCount = this.detailData.monitorRes.data.reduce((sum, cur) => sum + cur.count, 0)
-        this.detailData.artifactsCount = this.detailData.artifactsRes.data.reduce((sum, cur) => sum + cur.artifactLen, 0)
+        this.detailData.artifactsCount = this.detailData.artifactsRes.data.reduce(
+          (sum, cur) => sum + cur.artifactLen,
+          0
+        )
         // 成功或失败，取消轮询查状态
         if (['success', 'fail'].includes(this.detailData.status)) {
           clearInterval(this.interval)
