84422104de278284f1413cb748bfa710132446e2
wanghao7717 <792974788@qq.com>
2024-09-25 16:47:51 +0800

    一键导入需求开发

--- wecube-portal/src/pages/admin/base-migration/import/components/step-two.vue
+++ wecube-portal/src/pages/admin/base-migration/import/components/step-two.vue
@@ -5,13 +5,15 @@
       <Button v-if="['doing', 'fail'].includes(stepData.status)" type="error" @click="handleStop">终止</Button>
       <Button v-if="['fail'].includes(stepData.status)" type="warning" @click="handleRetry">重试</Button>
       <Button type="default" @click="handleLast">上一步</Button>
-      <Button v-if="['success'].includes(stepData.status)" type="primary" @click="handleNext">下一步</Button>
+      <Button v-if="['success'].includes(stepData.status)" type="primary" @click="handleSave">下一步</Button>
     </div>
   </div>
 </template>
 
 <script>
 import ImportData from '../../export/components/step-result.vue'
+import { saveImportData } from '@/api/server'
+import { debounce } from '@/const/util'
 export default {
   components: {
     ImportData
@@ -29,7 +31,7 @@ export default {
       ...this.detailData,
       status: 'doing'
     }
-    // 前端遍历所有导出数据，判断第二步单独导入状态
+    // 前端遍历所有导出数据，判断当前步骤数据导入状态
     const {
       artifactsRes, batchRes, cmdbRes, monitorRes, pluginsRes, itsmRes, roleRes, flowRes
     } = this.detailData
@@ -49,9 +51,20 @@ export default {
     handleLast() {
       this.$emit('lastStep')
     },
-    handleNext() {
-      this.$emit('nextStep')
-    }
+    handleSave: debounce(async function () {
+      if (this.detailData.step > 2) {
+        this.$emit('nextStep')
+      } else {
+        const params = {
+          transImportId: this.detailData.id
+        }
+        const { status } = await saveImportData(params)
+        if (status === 'OK') {
+          // 执行导入，生成ID
+          this.$emit('saveStepTwo')
+        }
+      }
+    }, 500)
   }
 }
 </script>
