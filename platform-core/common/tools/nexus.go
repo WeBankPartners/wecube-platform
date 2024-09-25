b6bbc714febae5a1bdeea57529b2052dff293938
tangjiawei <tangjiawei@nnyy.com>
2024-09-25 17:22:31 +0800

    nexus代码回滚

--- platform-core/common/tools/nexus.go
+++ platform-core/common/tools/nexus.go
@@ -34,6 +34,26 @@ type NexusUploadFileRet struct {
 	StorePath      string `json:"storePath"`      // 文件存储的地址
 }
 
+type AssetChecksum struct {
+	Sha1 string `json:"sha1"`
+	Md5  string `json:"md5"`
+}
+
+type AssetItem struct {
+	DownloadUrl  string         `json:"downloadUrl"`
+	Path         string         `json:"path"`
+	Id           string         `json:"id"`
+	Repository   string         `json:"repository"`
+	Checksum     *AssetChecksum `json:"checksum"`
+	ContentType  string         `json:"contentType"`
+	LastModified string         `json:"lastModified"`
+}
+
+type SearchAssetsResp struct {
+	Items             []*AssetItem `json:"items"`
+	ContinuationToken string       `json:"continuationToken"` // 分页 token
+}
+
 func validateNexusReqParam(reqParam *NexusReqParam, isUploadFile bool) (err error) {
 	if isUploadFile {
 		if reqParam.RepoUrl == "" {
@@ -76,7 +96,7 @@ func validateNexusReqParam(reqParam *NexusReqParam, isUploadFile bool) (err erro
 	return
 }
 
-// nexus 上传
+// UploadFile nexus 上传
 func UploadFile(reqParam *NexusReqParam) (result []*NexusUploadFileRet, err error) {
 	err = validateNexusReqParam(reqParam, true)
 	if err != nil {
@@ -157,10 +177,8 @@ func doUploadFile(reqParam *NexusReqParam, uploadFileParam *NexusFileParam) (sto
 		err = fmt.Errorf("upload file: %s failed, resp.Status: %s", srcFilePath, resp.Status)
 		return
 	}
-
-	storePath = fmt.Sprintf("%s/%s/%s", reqParam.RepoUrl, reqParam.Repository, uploadFileParam.DestFilePath)
-
-	log.Logger.Info(fmt.Sprintf("upload file: %s successfully: %s", srcFilePath, bodyBytes))
+	fmt.Printf("upload file: %s successfully: %s\n", srcFilePath, bodyBytes)
+	storePath = fmt.Sprintf("%s/repository/%s/%s", reqParam.RepoUrl, reqParam.Repository, uploadFileParam.DestFilePath)
 	return
 }
 
@@ -216,55 +234,127 @@ func doDownloadFile(reqParam *NexusReqParam, downloadFileParam *NexusFileParam) 
 	return
 }
 
-// for test
-/*
-func main() {
-	uploadReqParam := &NexusReqParam{
-		UserName:   "xx",
-		Password:   "xx",
-		RepoUrl:    "http://127.0.0.1:8081/repository",
-		Repository: "test",
-		TimeoutSec: 60,
-		FileParams: []*NexusFileParam{
-			{
-				SourceFilePath: "/Users/apple/Downloads/ecies.zip",
-				DestFilePath:   "ecies/ecies.zip",
-			},
-			{
-				SourceFilePath: "/Users/apple/Downloads/pic.png",
-				DestFilePath:   "pic/pic.png",
-			},
-		},
-	}
-
-	uploadFileRet, err := UploadFile(uploadReqParam)
-	if err != nil {
-		fmt.Printf("upload files failed: %s", err.Error())
+func validateListFilesInRepoParams(reqParam *NexusReqParam) (err error) {
+	if reqParam.UserName == "" {
+		err = fmt.Errorf("userName should not be empty")
 		return
 	}
-	fmt.Printf("upload files successfully: %v", uploadFileRet)
-
-	downloadReqParam := &NexusReqParam{
-		UserName:   "xx",
-		Password:   "xx",
-		RepoUrl:    "http://127.0.0.1:8081/repository",
-		Repository: "test",
-		TimeoutSec: 60,
-		FileParams: []*NexusFileParam{
-			{
-				SourceFilePath: "http://127.0.0.1:8081/repository/test/ecies/ecies.zip",
-				DestFilePath:   "/tmp/ecies.zip",
-			},
-			{
-				SourceFilePath: "http://127.0.0.1:8081/repository/test/pic/pic.png",
-				DestFilePath:   "/tmp/pic.png",
-			},
-		},
+	if reqParam.Password == "" {
+		err = fmt.Errorf("password should not be empty")
+		return
+	}
+	if reqParam.TimeoutSec <= 0 {
+		err = fmt.Errorf("timeoutSec should > 0")
+		return
 	}
 
-	err = DownloadFile(downloadReqParam)
+	if reqParam.RepoUrl == "" {
+		err = fmt.Errorf("repoUrl should not be empty")
+		return
+	}
+
+	if reqParam.Repository == "" {
+		err = fmt.Errorf("repository should not be empty")
+		return
+	}
+
+	if reqParam.DirPath == "" {
+		err = fmt.Errorf("dirPath should not be empty")
+		return
+	}
+
+	if !strings.HasPrefix(reqParam.DirPath, "/") {
+		err = fmt.Errorf("dirPath should start with /")
+		return
+	}
+
+	if strings.HasSuffix(reqParam.DirPath, "/") && reqParam.DirPath != "/" {
+		reqParam.DirPath = strings.TrimRight(reqParam.DirPath, "/")
+	}
+	return
+}
+
+// ListFilesInRepo 列出 nexus 仓库中特定目录下的所有文件
+func ListFilesInRepo(reqParam *NexusReqParam) (fileNameList []string, err error) {
+	err = validateListFilesInRepoParams(reqParam)
 	if err != nil {
-		fmt.Printf("download files failed: %s", err.Error())
+		err = fmt.Errorf("validate ListFilesInRepo params failed: %s", err.Error())
+		return
+	}
+
+	continuationToken := ""
+	for {
+		queryParams := make(map[string]string)
+		queryParams["repository"] = reqParam.Repository
+		queryParams["group"] = reqParam.DirPath
+		if continuationToken != "" {
+			queryParams["continuationToken"] = continuationToken
+		}
+
+		assetResponse, tmpErr := searchAssets(reqParam, queryParams)
+		if tmpErr != nil {
+			err = fmt.Errorf("list files in repo: %s, dirPath: %s failed: %s", reqParam.Repository, reqParam.DirPath, tmpErr.Error())
+			return
+		}
+
+		for _, item := range assetResponse.Items {
+			if item.Path != "" {
+				tmpList := strings.Split(item.Path, "/")
+				if len(tmpList) > 0 {
+					fileName := tmpList[len(tmpList)-1]
+					fileNameList = append(fileNameList, fileName)
+				}
+			}
+		}
+
+		if assetResponse.ContinuationToken != "" {
+			continuationToken = assetResponse.ContinuationToken
+		} else {
+			break
+		}
+	}
+	return
+}
+
+// searchAssets 查找 assets
+func searchAssets(reqParam *NexusReqParam, queryParams map[string]string) (respData *SearchAssetsResp, err error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Duration(reqParam.TimeoutSec)*time.Second)
+	defer cancelFunc()
+
+	reqUrl := fmt.Sprintf("%s/service/rest/v1/search/assets", reqParam.RepoUrl)
+	req, tmpErr := http.NewRequestWithContext(ctx, http.MethodGet, reqUrl, nil)
+	if tmpErr != nil {
+		err = fmt.Errorf("create request for reqUrl: %s failed: %s", reqUrl, tmpErr.Error())
+		return
+	}
+
+	for k, v := range queryParams {
+		q := req.URL.Query()
+		q.Add(k, v)
+		req.URL.RawQuery = q.Encode()
+	}
+
+	req.SetBasicAuth(reqParam.UserName, reqParam.Password)
+
+	// 发送请求
+	resp, tmpErr := http.DefaultClient.Do(req)
+	if tmpErr != nil {
+		err = fmt.Errorf("do request: %s failed: %s", reqUrl, tmpErr.Error())
+		return
+	}
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	// 检查响应状态
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("search assets failed, resp.Status: %s", resp.Status)
+		return
+	}
+
+	respData = &SearchAssetsResp{}
+	err = json.Unmarshal(bodyBytes, respData)
+	if err != nil {
+		err = fmt.Errorf("unmarshal response body data failed: %s", err.Error())
 		return
 	}
 	return
