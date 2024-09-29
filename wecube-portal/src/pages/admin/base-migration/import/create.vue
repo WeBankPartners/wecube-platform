59cd8e02304813051bb531957fa138bd8e1a7cd5
wanghao7717 <792974788@qq.com>
2024-09-29 19:55:32 +0800

    一键导入页面开发

--- wecube-portal/src/pages/admin/base-migration/import/create.vue
+++ wecube-portal/src/pages/admin/base-migration/import/create.vue
@@ -167,11 +167,11 @@ export default {
         this.detailData.businessNameList = (this.detailData.businessName && this.detailData.businessName.split(',')) || []
         this.detailData.business = this.detailData.business || ''
         this.detailData.cmdbCICount = this.detailData.cmdbCIData.reduce((sum, cur) => sum + cur.count, 0)
-        this.detailData.monitorCount = this.detailData.monitorRes.data.reduce((sum, cur) => sum + cur.count, 0)
-        this.detailData.monitorBusinessCount = this.detailData.monitorBusinessRes.data.reduce(
-          (sum, cur) => sum + cur.count,
-          0
-        )
+        // this.detailData.monitorCount = this.detailData.monitorRes.data.reduce((sum, cur) => sum + cur.count, 0)
+        // this.detailData.monitorBusinessCount = this.detailData.monitorBusinessRes.data.reduce(
+        //   (sum, cur) => sum + cur.count,
+        //   0
+        // )
         this.detailData.artifactsCount = this.detailData.artifactsRes.data.reduce(
           (sum, cur) => sum + cur.artifactLen,
           0
@@ -211,6 +211,33 @@ export default {
               'custom_metric_endpoint_group',
               'custom_metric_monitor_type'
             ].includes(i.name)
+        )
+        // 第二步monitor数据拆分
+        this.detailData.monitorRes.data = this.detailData.monitorRes.data.filter(
+          i =>
+            [
+              'monitor_type',
+              'endpoint',
+              'endpoint_group',
+              'service_group',
+              'log_monitor_template'
+            ].includes(i.name)
+        )
+        this.detailData.monitorCount = this.detailData.monitorRes.data.reduce((sum, cur) => sum + cur.count, 0)
+        // 第四步monitor数据拆分
+        this.detailData.monitorBusinessRes.data = this.detailData.monitorBusinessRes.data.filter(
+          i =>
+            [
+              'metric_list',
+              'log_monitor_service_group',
+              'strategy_list',
+              'logKeyword_service_group',
+              'dashboard'
+            ].includes(i.name)
+        )
+        this.detailData.monitorBusinessCount = this.detailData.monitorBusinessRes.data.reduce(
+          (sum, cur) => sum + cur.count,
+          0
         )
         // 第二步导入状态判断
         const {
