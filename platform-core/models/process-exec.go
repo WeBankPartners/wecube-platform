037fd46a1bf7d59a237dad38bbd117bcb2e53df6
zgyzgyhero <894526647@qq.com>
2024-10-09 23:56:30 +0800

    update process object input handle

--- platform-core/models/process-exec.go
+++ platform-core/models/process-exec.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"time"
@@ -583,4 +584,11 @@ type ParentProcInsObj struct {
 	ProcInsId   string `json:"procInsId" xorm:"proc_ins_id"`
 	ProcDefName string `json:"procDefName" xorm:"proc_def_name"`
 	Version     string `json:"version" xorm:"version"`
+}
+
+type HandleProcessInputDataParam struct {
+	Ctx              context.Context        `json:"ctx"`
+	AuthToken        string                 `json:"authToken"`
+	InputConstantMap map[string]string      `json:"inputConstantMap"`
+	InputContextMap  map[string]interface{} `json:"inputContextMap"`
 }
