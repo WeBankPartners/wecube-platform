584cf3d156d88f80a6a6b352c6886a55c26d3e56
tangjiawei <tangjiawei@nnyy.com>
2024-10-28 17:05:28 +0800

    1、平台实现个函数传入数据id和数据entity类型，算出能执行的编排 2、编排设计那上下文参数中，给开始节点增加多两个附带参数：编排开始执行时间、编排执行人

--- platform-core/wiki/database/v4.3.0.sql
+++ platform-core/wiki/database/v4.3.0.sql
@@ -37,3 +37,6 @@ CREATE TABLE `plugin_object_property_meta` (
 alter table plugin_object_meta modify id varchar(64) not null;
 
 alter table proc_ins add column request_info text default null comment 'taskman请求信息';
+
+alter table proc_def add index index_pd_root_entity(root_entity);
+
