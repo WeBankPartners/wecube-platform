56ffbb65bf2e64ada77f158a28d1ecfe95963048
tangjiawei <tangjiawei@nnyy.com>
2024-11-01 18:06:00 +0800

    底座迁移-导出数据补充支持二级数据弹窗

--- platform-core/services/remote/monitor/monitor.go
+++ platform-core/services/remote/monitor/monitor.go
@@ -185,11 +185,11 @@ func QueryLogMonitorTemplateBatch(ids []string, token string) (list []*LogMonito
 	return
 }
 
-func QueryDashboardNameBatch(ids []string, token string) (list []string, err error) {
+func QueryDashboardBatch(ids []string, token string) (list []*CustomDashboardTable, err error) {
 	if len(ids) == 0 {
 		return
 	}
-	var response CommonBatchNameResp
+	var response QueryDashboardResp
 	var responseBytes []byte
 	param := CommonBatchIdsParam{Ids: ids}
 	responseBytes, err = requestMonitorPluginV2(QueryDashboardNameBatchUrl, http.MethodPost, token, param)
@@ -207,11 +207,11 @@ func QueryDashboardNameBatch(ids []string, token string) (list []string, err err
 	return
 }
 
-func QueryServiceGroupNameBatch(ids []string, token string) (list []string, err error) {
+func QueryServiceGroupBatch(ids []string, token string) (list []*ServiceGroupTable, err error) {
 	if len(ids) == 0 {
 		return
 	}
-	var response CommonBatchNameResp
+	var response QueryServiceGroupResp
 	var responseBytes []byte
 	param := CommonBatchIdsParam{Ids: ids}
 	responseBytes, err = requestMonitorPluginV2(QueryServiceGroupNameBatchUrl, http.MethodPost, token, param)
