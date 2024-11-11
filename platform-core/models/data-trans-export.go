b2c81a58eca7a4ac9fd295b6c65357d75f2c9e28
tangjiawei <tangjiawei@nnyy.com>
2024-11-11 16:33:17 +0800

    底座迁移-导出返回dataType

--- platform-core/models/data-trans-export.go
+++ platform-core/models/data-trans-export.go
@@ -335,10 +335,11 @@ type TransExportDetail struct {
 }
 
 type CommonNameCount struct {
-	Name  string      `json:"name"`
-	Count int         `json:"count"`
-	Group string      `json:"group"`
-	Data  interface{} `json:"data"`
+	Name     string      `json:"name"`
+	Count    int         `json:"count"`
+	Group    string      `json:"group"`
+	DataType string      `json:"dataType"`
+	Data     interface{} `json:"data"`
 }
 
 type PluginPackageCount struct {
