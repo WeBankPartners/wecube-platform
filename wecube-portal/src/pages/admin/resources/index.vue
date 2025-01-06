fcb35e476abdae050f6e3d6246a09d1a217110cc
wanghao7717 <792974788@qq.com>
2025-01-06 11:47:31 +0800

    feat: 资源管理问题修复

--- wecube-portal/src/pages/admin/resources/index.vue
+++ wecube-portal/src/pages/admin/resources/index.vue
@@ -32,11 +32,13 @@ export default {
     async queryServers() {
       const { status, data } = await retrieveServers({})
       if (status === 'OK') {
-        this.servers = data.contents && data.contents.map(_ => ({
-          label: _.name,
-          value: _.id,
-          key: _.id
-        })) || []
+        this.servers = (data.contents
+            && data.contents.map(_ => ({
+              label: _.name,
+              value: _.id,
+              key: _.id
+            })))
+          || []
       }
     }
   },
