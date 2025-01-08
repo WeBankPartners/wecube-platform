eedf1f0873fffe7cf0705ddbcaf53fde68dc31a4
tangjiawei <tangjiawei@nnyy.com>
2025-01-08 14:21:34 +0800

    #2483 底座迁移导出添加目标客户

--- platform-core/services/database/data-trans-export-customer.go
+++ platform-core/services/database/data-trans-export-customer.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetTransExportCustomerList(ctx context.Context) (result []*models.DataTransExportCustomerTable, err error) {
-	err = db.MysqlEngine.Context(ctx).SQL("select * from trans_export_customer order by created_time desc").Find(&result)
+	err = db.MysqlEngine.Context(ctx).SQL("select * from trans_export_customer order by updated_time desc").Find(&result)
 	return
 }
 
@@ -18,9 +18,17 @@ func QueryTransExportCustomerByName(ctx context.Context, name string) (result []
 }
 
 func AddTransExportCustomer(ctx context.Context, exportCustomer *models.DataTransExportCustomerTable) (err error) {
+	now := time.Now().Format(models.DateTimeFormat)
 	_, err = db.MysqlEngine.Context(ctx).Exec("insert into trans_export_customer(id,name,nexus_addr,nexus_account,nexus_pwd,nexus_repo,"+
-		"created_user,created_time) values(?,?,?,?,?,?,?)", exportCustomer.Id, exportCustomer.Name, exportCustomer.NexusAddr,
-		exportCustomer.NexusAccount, exportCustomer.NexusPwd, exportCustomer.NexusRepo, exportCustomer.CreatedUser, time.Now().Format(models.DateTimeFormat))
+		"created_user,created_time,updated_time) values(?,?,?,?,?,?,?,?)", exportCustomer.Id, exportCustomer.Name, exportCustomer.NexusAddr,
+		exportCustomer.NexusAccount, exportCustomer.NexusPwd, exportCustomer.NexusRepo, exportCustomer.CreatedUser, now, now)
+	return
+}
+
+func UpdateTransExportCustomer(ctx context.Context, exportCustomer *models.DataTransExportCustomerTable) (err error) {
+	now := time.Now().Format(models.DateTimeFormat)
+	_, err = db.MysqlEngine.Context(ctx).Exec("update trans_export_customer set name=?,nexus_addr=?,nexus_account=?,nexus_pwd=?,nexus_repo=?,"+
+		"updated_time=? where id=?", exportCustomer.Name, exportCustomer.NexusAddr, exportCustomer.NexusAccount, exportCustomer.NexusPwd, exportCustomer.NexusRepo, now, exportCustomer.Id)
 	return
 }
 
