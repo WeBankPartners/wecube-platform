82740f43fea2821714b6fd730828c60fcde584d9
wanghao7717 <792974788@qq.com>
2025-01-22 14:30:49 +0800

    feat: 平台qiankun改造 初始化

--- wecube-portal/src/main.js
+++ wecube-portal/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { registerMicroApps, start, setDefaultMountApp  } from 'qiankun'
+import { registerMicroApps, start, setDefaultMountApp } from 'qiankun'
 
 import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
