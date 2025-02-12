92da97480f76cc05c3d7d9b3fb7319ad67138cfc
wanghao7717 <792974788@qq.com>
2025-02-12 10:05:50 +0800

    feat: npm install修改

--- build/wecube-portal/build-ui.sh
+++ build/wecube-portal/build-ui.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 cd /home/node/app/wecube-portal
+npm cache clean --force
 npm install --registry https://registry.npmmirror.com --unsafe-perm
 # npm install --registry https://mirrors.cloud.tencent.com/npm/
 npm run build
