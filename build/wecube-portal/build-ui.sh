84099e063a2b42c330e9f3b4e73f0ea25e73087d
wanghao7717 <792974788@qq.com>
2025-02-13 15:53:01 +0800

    feat: 设置腾讯云npm源

--- build/wecube-portal/build-ui.sh
+++ build/wecube-portal/build-ui.sh
@@ -1,7 +1,7 @@
 set -e -x
 cd /home/node/app/wecube-portal
 # npm --registry https://registry.npmmirror.com install --unsafe-perm --force
-npm cache clean --force
+rm -rf node_modules
 npm set registry https://mirrors.cloud.tencent.com/npm/
 npm install --force
 npm run build
