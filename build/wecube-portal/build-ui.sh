ca57595c2593ea20cbe750d26019d5e0c8630124
wanghao7717 <792974788@qq.com>
2025-02-13 15:29:36 +0800

    feat: npm源修改

--- build/wecube-portal/build-ui.sh
+++ build/wecube-portal/build-ui.sh
@@ -1,5 +1,6 @@
 set -e -x
 cd /home/node/app/wecube-portal
-npm --registry https://registry.npmmirror.com install --unsafe-perm --force
+# npm --registry https://registry.npmmirror.com install --unsafe-perm --force
+npm config set registry https://registry.npmjs.org/
 npm install --force
 npm run build
