988772924be31a7777ebe959357b2e739fd7834f
wanghao7717 <792974788@qq.com>
2025-02-18 14:54:26 +0800

    feat: 编排执行样式调整

--- build/wecube-portal/build-ui.sh
+++ build/wecube-portal/build-ui.sh
@@ -1,7 +1,7 @@
 set -e -x
 cd /home/node/app/wecube-portal
 # npm --registry https://registry.npmmirror.com install --unsafe-perm --force
-rm -rf node_modules
+# rm -rf node_modules
 npm set registry https://mirrors.cloud.tencent.com/npm/
-npm install --force
+npm install
 npm run build
