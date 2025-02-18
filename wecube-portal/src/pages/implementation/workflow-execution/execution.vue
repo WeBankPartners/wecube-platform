e62c9805b14b274ebb2360ce08a8427ae1278f4c
wanghao7717 <792974788@qq.com>
2025-02-18 14:22:28 +0800

    feat: 编排添加调试

--- wecube-portal/src/pages/implementation/workflow-execution/execution.vue
+++ wecube-portal/src/pages/implementation/workflow-execution/execution.vue
@@ -2177,11 +2177,13 @@ export default {
             removeEvent('.time-node', 'click', this.timeNodeHandler)
             removeEvent('.decision-node', 'click', this.executeBranchHandler)
           }
+          // 新建执行-节点绑定事件
+           this.bindFlowEvent()
         })
       // 新建执行-节点绑定事件
-      setTimeout(() => {
-        this.bindFlowEvent()
-      }, 1000)
+      // setTimeout(() => {
+      //   this.bindFlowEvent()
+      // }, 1000)
     },
     async excutionFlow() {
       // 区分已存在的flowInstance执行 和 新建的执行
