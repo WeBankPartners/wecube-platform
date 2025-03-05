e5ad592b61619a420975f0d13f223d373b310239
Alexaa123 <1239327906@qq.com>
2025-03-05 14:57:14 +0800

    解决cmdb中无法刷新的问题

--- wecube-portal/src/main.js
+++ wecube-portal/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 import ViewUI from 'view-design'
 import './styles/index.less'
