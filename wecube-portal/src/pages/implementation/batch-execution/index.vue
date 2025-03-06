16e6d05904c9ea26148cf0c54ec702ccdd3109cb
wanghao7717 <792974788@qq.com>
2025-03-06 19:53:55 +0800

    feat: 微前端调整

--- wecube-portal/src/pages/implementation/batch-execution/index.vue
+++ wecube-portal/src/pages/implementation/batch-execution/index.vue
@@ -59,7 +59,7 @@ export default {
       }
     }
   },
-  mounted() {
+  created() {
     this.$eventBusP.$on('expand-menu', val => {
       this.expand = val
     })
--- wecube-portal/src/store/index.js
+++ wecube-portal/src/store/index.js
@@ -1,10 +1,19 @@
+/*
+ * @Author: wanghao7717 792974788@qq.com
+ * @Date: 2025-01-20 09:58:50
+ * @LastEditors: wanghao7717 792974788@qq.com
+ * @LastEditTime: 2025-03-06 12:06:31
+ */
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
   searchMap: new Map(),
-  sideExpand: true
+  sideExpand: false, // 侧边栏展开状态
+  implicitRoute: {}, // 需要额外配置面包屑的路由
+  childRouters: [], // 不需要鉴权的子路由
+  routes: []
 }
 
 const getters = {
@@ -13,6 +22,15 @@ const getters = {
   },
   sideExpand(state) {
     return state.sideExpand
+  },
+  implicitRoute(state) {
+    return state.implicitRoute
+  },
+  childRouters(state) {
+    return state.childRouters
+  },
+  routes(state) {
+    return state.routes
   }
 }
 
@@ -22,6 +40,21 @@ const mutations = {
   },
   setSideExpand(state, val) {
     state.sideExpand = val
+  },
+  setImplicitRoute(state, val) {
+    state.implicitRoute = Object.assign(state.implicitRoute, val)
+  },
+  setChildRouters(state, val) {
+    state.childRouters = state.childRouters.concat(
+      val.map(r => ({
+        ...r,
+        link: r.path,
+        active: true
+      }))
+    )
+  },
+  setRoutes(state, val) {
+    state.routes = state.routes.contact(val)
   }
 }
 
