b29e0f4e26ce35215673a6fe6c479395481cfd38
wanghao7717 <792974788@qq.com>
2025-03-07 19:08:06 +0800

    feat: entry动态获取版本号改造

--- wecube-portal/src/main-app.js
+++ wecube-portal/src/main-app.js
@@ -1,26 +1,37 @@
-const microApps = [
-  {
-    name: 'taskman',
-    entry: process.env.NODE_ENV === 'production' ? '/ui-resources/taskman/v1.4.1.62/plugin/' : 'http://localhost:3010',
-    container: '#micro-app-container',
-    activeRule: '#/taskman',
-    props: {
-      sandbox: {
-        strictStyleIsolation: false // 为该子应用开启严格的样式隔离
+import { getFrontRunningPlugins } from './api/server.js'
+
+const microApps = []
+const initMicroApps = async () => {
+  let microApps = []
+  const { status, data } = await getFrontRunningPlugins()
+  if (status === 'OK') {
+    microApps.push(...[
+      {
+        name: 'taskman',
+        entry: process.env.NODE_ENV === 'production' ? `/ui-resources/taskman/${data['taskman']}/plugin/` : 'http://localhost:3010',
+        container: '#micro-app-container',
+        activeRule: '#/taskman',
+        props: {
+          sandbox: {
+            strictStyleIsolation: false // 为该子应用开启严格的样式隔离
+          }
+        }
+      },
+      {
+        name: 'wecmdb',
+        entry: process.env.NODE_ENV === 'production' ? `/ui-resources/wecmdb/${data['wecmdb']}/plugin/` : 'http://localhost:3020',
+        container: '#micro-app-container',
+        activeRule: '#/wecmdb',
+        props: {
+          sandbox: {
+            strictStyleIsolation: false
+          }
+        }
       }
-    }
-  },
-  {
-    name: 'wecmdb',
-    entry: process.env.NODE_ENV === 'production' ? '/ui-resources/wecmdb/v2.2.0.40/plugin/' : 'http://localhost:3020',
-    container: '#micro-app-container',
-    activeRule: '#/wecmdb',
-    props: {
-      sandbox: {
-        strictStyleIsolation: false
-      }
-    }
+    ])
   }
-]
+}
+
+initMicroApps()
 
 export default microApps
