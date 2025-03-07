d281553b9e075a1f6b49efb1efb924ade64659c2
wanghao7717 <792974788@qq.com>
2025-03-07 11:45:39 +0800

    feat: entry修改

--- wecube-portal/src/main-app.js
+++ wecube-portal/src/main-app.js
@@ -1,7 +1,7 @@
 const microApps = [
   {
     name: 'taskman',
-    entry: process.env.NODE_ENV === 'production' ? '/ui-resources/taskman/v1.4.1.60/plugin/' : '//localhost:3010',
+    entry: process.env.NODE_ENV === 'production' ? '/ui-resources/taskman/v1.4.1.61/plugin/' : '//localhost:3010',
     container: '#micro-app-container',
     activeRule: '#/taskman',
     props: {
