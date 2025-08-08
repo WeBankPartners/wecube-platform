87ef71da72b1ae7d8cdcad32a1762e0f372c1cd8
tangjiawei <1135176001@qq.com>
2025-08-08 22:20:01 +0800

    新增db

--- platform-core/wiki/database/v4.5.6.sql
+++ platform-core/wiki/database/v4.5.6.sql
@@ -1,4 +1,5 @@
 ALTER TABLE trans_export ADD COLUMN selected_tree_json TEXT COMMENT '前端选中的tree结构json';
+ALTER TABLE trans_import ADD COLUMN selected_tree_json TEXT COMMENT '前端选中的tree结构json';
 ALTER TABLE trans_export modify column business text DEFAULT NULL COMMENT '业务';
 ALTER TABLE trans_export modify column business_name text DEFAULT NULL COMMENT '业务名称';
 ALTER TABLE trans_import modify column business text DEFAULT NULL COMMENT '业务';
