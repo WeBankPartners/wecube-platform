369fe21147ec9a7d32b72cdf550fe0e79ad5ec6c
zgyzgyhero <894526647@qq.com>
2026-03-16 11:36:35 +0800

    update proc preview sql

--- platform-core/wiki/database/v4.6.0.sql
+++ platform-core/wiki/database/v4.6.0.sql
@@ -9,3 +9,7 @@ CREATE TABLE `plugin_package_runtime_resources_volume` (
   `mount_path` varchar(255) NOT NULL COMMENT '卷路径',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+alter table proc_data_preview modify column entity_data_name varchar(1024) default null;
+alter table proc_ins_graph_node modify column display_name varchar(1024) default null;
+
