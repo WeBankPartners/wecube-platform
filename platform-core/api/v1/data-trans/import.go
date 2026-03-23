bd7ae4b34b2dccf8467970e490843d978b556a08
Roy <576477085@qq.com>
2026-03-23 16:14:39 +0800

    feat(plugin): 修改LaunchPluginFunc函数参数

--- platform-core/api/v1/data-trans/import.go
+++ platform-core/api/v1/data-trans/import.go
@@ -410,7 +410,7 @@ func RestartPluginInstance(ctx context.Context, pluginPackageId string) (err err
 		return
 	}
 
-	err = plugin.LaunchPluginFunc(ctx, pluginPackageId, resServer, "system", pluginInstanceObj.Port)
+	err = plugin.LaunchPluginFunc(ctx, pluginPackageId, resServer, "system", pluginInstanceObj.Port, "", "")
 	if err != nil {
 		err = fmt.Errorf("launch plugin:%s fail,%s ", pluginInstanceObj.InstanceName, err.Error())
 	}
