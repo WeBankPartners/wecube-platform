fe6d3272fde73aa97583697add948d0d6072746c
Roy <576477085@qq.com>
2026-03-26 14:33:16 +0800

    ``` build(db): 优化数据库构建脚本中的SQL文件处理逻辑
    重构了build-image.sh脚本，通过引入变量PLATFORMCORE_SQL和AUTHSERVER_SQL来分别管理
    平台核心和认证服务器的数据库SQL文件。将原来直接写入固定文件的方式改为使用变量，
    同时简化了SQL内容追加的逻辑，移除了不必要的变量赋值操作，提高了脚本的可读性和
    维护性。
    ```

--- build/db/build-image.sh
+++ build/db/build-image.sh
@@ -10,24 +10,24 @@ set -ex
 
 mkdir -p database
 
-echo "SET NAMES utf8;" > ./database/000000_create_database.sql
-echo "create database wecube charset = utf8;" >> ./database/000000_create_database.sql
-echo "use wecube;" >> ./database/000000_create_database.sql
+PLATFORMCORE_SQL=./database/000000_platformcore_create_database.sql
+AUTHSERVER_SQL=./database/000001_authserver_create_database.sql
+
+echo "SET NAMES utf8;" > $PLATFORMCORE_SQL
+echo "create database wecube charset = utf8;" >> $PLATFORMCORE_SQL
+echo "use wecube;" >> $PLATFORMCORE_SQL
 for i in `ls -1 ../../platform-core/wiki/database/*.sql`; do
-     CONTENTS=`cat $i`
-     echo $CONTENTS >> ./database/000000_create_database.sql
+     cat $i >> $PLATFORMCORE_SQL
 done
 
-echo "SET NAMES utf8;" > ./database/000001_create_database.sql
-echo "create database auth_server charset = utf8;" >> ./database/000001_create_database.sql
-echo "use auth_server;" >> ./database/000001_create_database.sql
+echo "SET NAMES utf8;" > $AUTHSERVER_SQL
+echo "create database auth_server charset = utf8;" >> $AUTHSERVER_SQL
+echo "use auth_server;" >> $AUTHSERVER_SQL
 for i in `ls -1 ../../platform-auth-server/deploy/database/*.sql`; do
-     CONTENTS=`cat $i`
-     echo $CONTENTS >> ./database/000001_create_database.sql
+     cat $i >> $AUTHSERVER_SQL
 done
 for i in `ls -1 ../../platform-auth-server/deploy/db/upgrade/*.sql`; do
-     CONTENTS=`cat $i`
-     echo $CONTENTS >> ./database/000001_create_database.sql
+     cat $i >> $AUTHSERVER_SQL
 done
 
 docker build -t wecube-db:$VERSION .
