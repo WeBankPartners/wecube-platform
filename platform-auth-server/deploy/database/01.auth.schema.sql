6e39ccf6241e092c7ff0ffce2d6c5b6f078861de
tangjiawei <1135176001@qq.com>
2026-03-31 14:50:59 +0800

    auth-server sql更新

--- platform-auth-server/deploy/database/01.auth.schema.sql
+++ platform-auth-server/deploy/database/01.auth.schema.sql
@@ -160,4 +160,9 @@ CREATE TABLE `auth_sys_user_role` (
 ENGINE=InnoDB
 ;
 
+ALTER TABLE `auth_sys_user`
+    ADD COLUMN `mfa_secret` VARCHAR(255) NULL DEFAULT NULL COMMENT 'MFA密钥，用于生成TOTP二维码' AFTER `auth_src`,
+    ADD COLUMN `mfa_bound` BIT(1) NULL DEFAULT b'0' COMMENT 'MFA绑定状态，0-未绑定，1-已绑定' AFTER `mfa_secret`;
+
+
 SET FOREIGN_KEY_CHECKS=1;
